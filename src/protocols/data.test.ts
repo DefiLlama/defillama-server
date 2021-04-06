61c0fe432600c82c35a1917c1044bc39a7840493
0xngmi <0xngmi@protonmail.com>
2021-04-06 05:19:15 +0200

    list more protocols

--- src/protocols/data.test.ts
+++ src/protocols/data.test.ts
@@ -5,3 +5,11 @@ test("all the dynamic imports work", async () => {
     await import(`../../DefiLlama-Adapters/projects/${protocol.module}`);
   }
 });
+
+test("no id is repeated", async () => {
+  const ids = []
+  for (const protocol of protocols) {
+    expect(ids).not.toContain(protocol.id)
+    ids.push(protocol.id)
+  }
+});

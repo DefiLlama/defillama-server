c41765d3879c87601ed9c7db295775f5fd40a320
0xngmi <0xngmi@protonmail.com>
2021-05-26 01:16:24 +0200

    fix slugs

--- src/utils/sluggify.ts
+++ src/utils/sluggify.ts
@@ -1,3 +1,3 @@
 import type { Protocol } from "../protocols/data";
 
-export default (prot: Protocol) => prot.name.toLowerCase().replace(" ", "-");
+export default (prot: Protocol) => prot.name.toLowerCase().split(" ").join("-");

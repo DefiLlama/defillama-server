8edec0db3e91f193df7f23885f213acb62a2a56b
0xngmi <0xngmi@protonmail.com>
2021-06-09 05:01:15 +0200

    Fix new day bug

--- src/getChart.ts
+++ src/getChart.ts
@@ -11,13 +11,14 @@ const handler = async (
   const sumDailyTvls = {} as {
     [timestamp: number]: number | undefined;
   };
-  await Promise.all(
+  let lastDailyTimestamp = 0;
+  const historicalProtocolTvls = await Promise.all(
     protocols.map(async (protocol) => {
-      if ((protocol.name === "Stacks" && chain !== 'stacks') || protocol.name === "Uniswap v3" || protocol.category === "Chain") {
-        return;
+      if (protocol.name === "Uniswap v3" || protocol.category === "Chain") {
+        return undefined;
       }
       if (chain !== undefined && !protocol.chains.map(protocolChain => protocolChain.toLowerCase()).includes(chain)) {
-        return;
+        return undefined;
       }
       const historicalTvl = await dynamodb.query({
         ExpressionAttributeValues: {
@@ -25,27 +26,47 @@ const handler = async (
         },
         KeyConditionExpression: "PK = :pk",
       });
-      if (historicalTvl.Items !== undefined) {
-        historicalTvl.Items.forEach((item) => {
-          const timestamp = getClosestDayStartTimestamp(item.SK);
-          let itemTvl:number;
-          if(chain === undefined){
-            itemTvl = item.tvl;
-          } else {
-            itemTvl = item[normalizeChain(chain)]
-            if(itemTvl === undefined){
-              if(chain === protocol.chain.toLowerCase()){
-                itemTvl = item.tvl;
-              } else {
-                return;
-              }
-            }
-          }
-          sumDailyTvls[timestamp] = itemTvl + (sumDailyTvls[timestamp] ?? 0);
-        });
+      if (historicalTvl.Items === undefined) {
+        return undefined
+      }
+      const lastTimestamp = getClosestDayStartTimestamp(historicalTvl.Items[historicalTvl.Items.length - 1].SK)
+      lastDailyTimestamp = Math.max(lastDailyTimestamp, lastTimestamp)
+      return {
+        protocol,
+        historicalTvl: historicalTvl.Items
       }
     })
   );
+  historicalProtocolTvls.forEach((protocolTvl)=>{
+    if(protocolTvl === undefined){
+      return
+    }
+    const {historicalTvl, protocol} = protocolTvl;
+    const lastTvl = historicalTvl[historicalTvl.length-1];
+    if(lastTvl.SK !== lastDailyTimestamp){
+      historicalTvl.push({
+        ...lastTvl,
+        SK: lastDailyTimestamp
+      })
+    }
+    historicalTvl.forEach((item) => {
+      const timestamp = getClosestDayStartTimestamp(item.SK);
+      let itemTvl:number;
+      if(chain === undefined){
+        itemTvl = item.tvl;
+      } else {
+        itemTvl = item[normalizeChain(chain)]
+        if(itemTvl === undefined){
+          if(chain === protocol.chain.toLowerCase()){
+            itemTvl = item.tvl;
+          } else {
+            return;
+          }
+        }
+      }
+      sumDailyTvls[timestamp] = itemTvl + (sumDailyTvls[timestamp] ?? 0);
+    });
+  })
   let minTimestamp = 1603757978;
   if(chain !== undefined && chain !== "ethereum") {
     minTimestamp = 1612837719;

f68660b1d676ac153485500784c20270ef081f00
0xngmi <0xngmi@protonmail.com>
2021-06-16 20:38:00 +0200

    add cli script

---
+++ src/cli/deleteAtDate.ts
@@ -0,0 +1,26 @@
+import dynamodb from "../utils/dynamodb";
+import { dailyTokensTvl, dailyTvl, dailyUsdTokensTvl } from "../utils/getLastRecord";
+import { getProtocol } from "./utils";
+
+async function main() {
+  const protocol = getProtocol('pancakeswap')
+  for(const tvlFunc of [dailyTokensTvl, dailyTvl, dailyUsdTokensTvl]){
+    const data = await dynamodb.query({
+      ExpressionAttributeValues: {
+        ":pk": tvlFunc(protocol.id),
+      },
+      KeyConditionExpression: "PK = :pk",
+    });
+    for (const d of data.Items ?? []) {
+      if (d.SK < 1619388000) {
+        await dynamodb.delete({
+          Key: {
+            PK: d.PK,
+            SK: d.SK,
+          },
+        });
+      }
+    }
+  }
+}
+main();

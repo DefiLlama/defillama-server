623ec88f11dd85c5f77e55d71e306f54b3749eb9
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-07-06 03:46:51 -0500

    deactivated ViperSwap due to data err

--- src/protocols/data.ts
+++ src/protocols/data.ts
@@ -4040,6 +4040,7 @@ export default [
     twitter: "StackerVentures",
     audit_links: ["https://docs.stacker.vc/docs-stacker-ventures/audits/active-yield-fund-audit-report", "https://docs.stacker.vc/docs-stacker-ventures/audits/smart-contract-audit-report"],
   },
+  /*
   {
     id: "313",
     name: "ViperSwap",
@@ -4060,6 +4061,7 @@ export default [
     twitter: null,
     forkedFrom: ["UniSwap", "SushiSwap", "Bao"],
   },
+  */
   {
     id: "314",
     name: "Gondola Finance",

11a668ca11c81967d15b4ac035e3aed4f72555ed
0xngmi <0xngmi@protonmail.com>
2021-07-27 02:24:56 +0100

    checkChart on all tvls

--- src/cli/checkChart.ts
+++ src/cli/checkChart.ts
@@ -7,14 +7,14 @@ import {
   getCoingeckoLock,
   releaseCoingeckoLock,
 } from "../storeTvlUtils/coingeckoLocks";
-import { dailyTvl } from "../utils/getLastRecord";
+import { dailyTvl, dailyTokensTvl, dailyUsdTokensTvl } from "../utils/getLastRecord";
 
 const date = (timestamp: number) =>
   "\t" + new Date(timestamp * 1000).toDateString();
 
 const projectsToRefill: string[] = [];
 const notify = false;
-const deleteRepeated = false;
+const deleteRepeated = true;
 const extrapolate = false;
 
 const secondsInDay = 86400;
@@ -24,82 +24,85 @@ async function main() {
       releaseCoingeckoLock();
     }, 1e3);
   }
-  await Promise.all(
-    protocols.map(async (protocol) => {
-      const historicalTvl = await dynamodb.query({
-        ExpressionAttributeValues: {
-          ":pk": `dailyTvl#${protocol.id}`,
-        },
-        KeyConditionExpression: "PK = :pk",
-      });
-      if (historicalTvl.Items !== undefined && historicalTvl.Items.length > 0) {
-        const items = historicalTvl.Items;
-        let lastItemDate = getClosestDayStartTimestamp(items[0].SK);
-        for (let i = 1; i < items.length; i++) {
-          const item = items[i];
-          const timestamp = getClosestDayStartTimestamp(item.SK);
-          const diff = timestamp - lastItemDate;
-          const under = diff < secondsInDay * 0.5;
-          const over = diff > secondsInDay * 1.5;
-          if ((over || under) && notify) {
-            console.log(
-              protocol.name.padEnd(19, " "),
-              diff / secondsInDay,
-              date(timestamp),
-              date(lastItemDate)
-            );
-          }
-          if (over && projectsToRefill.includes(protocol.name)) {
-            let nextTimestamp = lastItemDate;
-            do {
-              nextTimestamp = getClosestDayStartTimestamp(
-                nextTimestamp + secondsInDay
+  for (const tvlPrefix of [dailyTvl, dailyTokensTvl, dailyUsdTokensTvl]) {
+    console.log(`# ${tvlPrefix('')}`)
+    await Promise.all(
+      protocols.map(async (protocol) => {
+        const historicalTvl = await dynamodb.query({
+          ExpressionAttributeValues: {
+            ":pk": tvlPrefix(protocol.id),
+          },
+          KeyConditionExpression: "PK = :pk",
+        });
+        if (historicalTvl.Items !== undefined && historicalTvl.Items.length > 0) {
+          const items = historicalTvl.Items;
+          let lastItemDate = getClosestDayStartTimestamp(items[0].SK);
+          for (let i = 1; i < items.length; i++) {
+            const item = items[i];
+            const timestamp = getClosestDayStartTimestamp(item.SK);
+            const diff = timestamp - lastItemDate;
+            const under = diff < secondsInDay * 0.5;
+            const over = diff > secondsInDay * 1.5;
+            if ((over || under) && notify) {
+              console.log(
+                protocol.name.padEnd(19, " "),
+                diff / secondsInDay,
+                date(timestamp),
+                date(lastItemDate)
               );
-              let tvl;
-              if (extrapolate) {
-                const totalTimeDiff = item.SK - items[i - 1].SK;
-                tvl =
-                  (item.tvl * (nextTimestamp - items[i - 1].SK) +
-                    items[i - 1].tvl * (item.SK - nextTimestamp)) /
-                  totalTimeDiff;
-                await dynamodb.put({
-                  PK: dailyTvl(protocol.id),
-                  SK: nextTimestamp,
-                  tvl,
-                });
-              } else {
-                const { ethereumBlock, chainBlocks } = await getBlocksRetry(
-                  nextTimestamp
+            }
+            if (over && projectsToRefill.includes(protocol.name)) {
+              let nextTimestamp = lastItemDate;
+              do {
+                nextTimestamp = getClosestDayStartTimestamp(
+                  nextTimestamp + secondsInDay
                 );
-                tvl = await storeTvl(
-                  nextTimestamp,
-                  ethereumBlock,
-                  chainBlocks,
-                  protocol,
-                  {},
-                  4,
-                  getCoingeckoLock,
-                  false,
-                  false,
-                  true
-                );
-              }
-              console.log(protocol.name, date(nextTimestamp), tvl);
-            } while (nextTimestamp < timestamp);
+                let tvl;
+                if (extrapolate) {
+                  const totalTimeDiff = item.SK - items[i - 1].SK;
+                  tvl =
+                    (item.tvl * (nextTimestamp - items[i - 1].SK) +
+                      items[i - 1].tvl * (item.SK - nextTimestamp)) /
+                    totalTimeDiff;
+                  await dynamodb.put({
+                    PK: tvlPrefix(protocol.id),
+                    SK: nextTimestamp,
+                    tvl,
+                  });
+                } else {
+                  const { ethereumBlock, chainBlocks } = await getBlocksRetry(
+                    nextTimestamp
+                  );
+                  tvl = await storeTvl(
+                    nextTimestamp,
+                    ethereumBlock,
+                    chainBlocks,
+                    protocol,
+                    {},
+                    4,
+                    getCoingeckoLock,
+                    false,
+                    false,
+                    true
+                  );
+                }
+                console.log(protocol.name, date(nextTimestamp), tvl);
+              } while (nextTimestamp < timestamp);
+            }
+            if (under && deleteRepeated) {
+              console.log("Delete", protocol.name, timestamp);
+              await dynamodb.delete({
+                Key: {
+                  PK: item.PK,
+                  SK: item.SK,
+                },
+              });
+            }
+            lastItemDate = timestamp;
           }
-          if (under && deleteRepeated) {
-            console.log("Delete", protocol.name, timestamp);
-            await dynamodb.delete({
-              Key: {
-                PK: item.PK,
-                SK: item.SK,
-              },
-            });
-          }
-          lastItemDate = timestamp;
         }
-      }
-    })
-  );
+      })
+    );
+  }
 }
 main();

4590ae073b8284bf2adf24fc793215e092832572
0xngmi <0xngmi@protonmail.com>
2021-08-22 11:48:15 +0100

    fix env exports

--- env.js
+++ env.js
@@ -1,2 +1,11 @@
 require('dotenv').config()
-module.exports = process.env;
+module.exports = {
+    ETHEREUM_RPC: process.env.ETHEREUM_RPC,
+    BSC_RPC: process.env.BSC_RPC,
+    POLYGON_RPC: process.env.POLYGON_RPC,
+    FANTOM_RPC: process.env.FANTOM_RPC,
+    ARBITRUM_RPC: process.env.ARBITRUM_RPC,
+    OPTIMISM_RPC: process.env.OPTIMISM_RPC,
+    XDAI_RPC: process.env.XDAI_RPC,
+    OUTDATED_WEBHOOK: process.env.OUTDATED_WEBHOOK
+}

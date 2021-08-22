e72500a84bcf46a33c8c17c093df356e4588a9c1
0xngmi <0xngmi@protonmail.com>
2021-08-22 11:28:47 +0100

    upgrade to failsafe calls

--- env.js
+++ env.js
@@ -1,7 +1,2 @@
 require('dotenv').config()
-module.exports = {
-    ETHEREUM_RPC: process.env.ETHEREUM_RPC,
-    BSC_RPC: process.env.BSC_RPC,
-    POLYGON_RPC: process.env.POLYGON_RPC,
-    OUTDATED_WEBHOOK: process.env.OUTDATED_WEBHOOK
-}
+module.exports = process.env;

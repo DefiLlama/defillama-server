c3a8b820a540146e0540ed6badb77fb7302920d4
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-09-19 19:27:57 +0000

    update sushi url

--- src/protocols/data.ts
+++ src/protocols/data.ts
@@ -265,7 +265,7 @@ export default [
     name: "SushiSwap",
     address: "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2",
     symbol: "SUSHI",
-    url: "https://sushiswap.fi/",
+    url: "https://sushi.com/",
     description:
       "A fully decentralized protocol for automated liquidity provision on Ethereum.\r\n",
     chain: "Ethereum",

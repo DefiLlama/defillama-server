a0bcc97f2ad70f797c9954a32255a232b280a0dd
Jonathan Easterman <jonathaneasterman@gmail.com>
2021-09-29 17:09:52 +0100

    Add logo to nft collections

--- src/nfts/collections.ts
+++ src/nfts/collections.ts
@@ -3,36 +3,42 @@ export default [
         id: "bayc",
         contract: "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
         item: "5212",
-        name: "BAYC"
+        name: "BAYC",
+        logo: "https://lh3.googleusercontent.com/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB=s120",
     },
     {
         id: "loot",
         contract: "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7",
         item: "2771",
-        name: "Loot"
+        name: "Loot",
+        logo: "https://lh3.googleusercontent.com/g-NFUWjS4IGgym8PHBxyhg5-G_B4x-IHgPKRkxo00JQFE3LOd-95yU2uhrokITVmV7KHEav6OMfAhfJ4roC5hwP-0tI9dMRd9wQLdw=s120",
     },
     {
         id: "mayc",
         contract: "0x60e4d786628fea6478f785a6d7e704777c86a7c6",
         item: "22182",
-        name: "MAYC"
+        name: "MAYC",
+        logo: "https://lh3.googleusercontent.com/lHexKRMpw-aoSyB1WdFBff5yfANLReFxHzt1DOj_sg7mS14yARpuvYcUtsyyx-Nkpk6WTcUPFoG53VnLJezYi8hAs0OxNZwlw6Y-dmI=s120",
     },
     {
         id: "art-blocks-curated",
         contract: "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
         item: "74000274",
-        name: "Art Blocks Curated"
+        name: "Art Blocks Curated",
+        logo: "https://lh3.googleusercontent.com/GwnosrkaneKGEkWySxvTSzZ5bEUjWkRuQzLSNfrpgy2-gxYjoR3m5PohLT9Fzy0p1tohajZ1g-LFfF_ZLnS1GqlPNHPUaKUbDhbf=s120",
     },
     {
         id: "meebits",
         contract: "0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7",
         item: "7708",
-        name: "Meebits"
+        name: "Meebits",
+        logo: "https://lh3.googleusercontent.com/d784iHHbqQFVH1XYD6HoT4u3y_Fsu_9FZUltWjnOzoYv7qqB5dLUqpGyHBd8Gq3h4mykK5Enj8pxqOUorgD2PfIWcVj9ugvu8l0=s120",
     },
     {
         id: "cool-cats",
         contract: "0x1a92f7381b9f03921564a437210bb9396471050c",
         item: "5847",
-        name: "Cool Cats"
+        name: "Cool Cats",
+        logo: "https://lh3.googleusercontent.com/LIov33kogXOK4XZd2ESj29sqm_Hww5JSdO7AFn5wjt8xgnJJ0UpNV9yITqxra3s_LMEW1AnnrgOVB_hDpjJRA1uF4skI5Sdi_9rULi8=s120",
     },
 ]

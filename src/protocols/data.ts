09fcc67ef3222de9410c3b265d45b01f36237d37
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-10-26 14:25:03 +0000

    update module for sunswap

--- src/protocols/data.ts
+++ src/protocols/data.ts
@@ -6434,7 +6434,7 @@ export default [
     cmcId: "10529",
     category: "Dexes",
     chains: ["Tron"],
-    module: "sun/index.js",
+    module: "sunSwap/index.js",
     twitter: "defi_sunio",
     audit_links: ["https://sun.io/docs/slowmist_audit_report_en_sun.pdf"],
   },

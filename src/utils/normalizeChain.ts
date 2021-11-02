1c49111edc6e4d33bf3758f2df7ff9a258683651
0xngmi <0xngmi@protonmail.com>
2021-11-02 22:58:13 +0000

    add smartbch normalization

--- src/utils/normalizeChain.ts
+++ src/utils/normalizeChain.ts
@@ -360,6 +360,8 @@ export function getChainDisplayName(normalizedChain: string) {
       return "Fuse"
     case "sora":
       return "SORA"
+    case "smartbch":
+      return "smartBCH"
     default:
       return normalizedChain.slice(0, 1).toUpperCase() + normalizedChain.slice(1) // Capitalize first letter
   }

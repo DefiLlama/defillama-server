5d87374bf8ae456be8cf0639185046f8566d20f2
0xngmi <0xngmi@protonmail.com>
2021-11-09 00:13:04 +0000

    hourly refill code

--- src/cli/refillWithHourly.ts
+++ src/cli/refillWithHourly.ts
@@ -1,28 +1,39 @@
-const protocolName = ""
+const protocolName = "anchor"
 
 import dynamodb from "../utils/dynamodb";
 import { dailyTokensTvl, dailyTvl, dailyUsdTokensTvl } from "../utils/getLastRecord";
 import { getProtocol } from "./utils";
+import { getClosestDayStartTimestamp } from "../utils/date";
+import getTVLOfRecordClosestToTimestamp from "../utils/getTVLOfRecordClosestToTimestamp";
+
+const secondsInDay = 24 * 3600;
 
 async function main() {
   const protocol = getProtocol(protocolName)
-  for(const tvlFunc of [dailyTokensTvl, dailyTvl, dailyUsdTokensTvl]){
-    const data = await dynamodb.query({
-      ExpressionAttributeValues: {
-        ":pk": tvlFunc(protocol.id),
-      },
-      KeyConditionExpression: "PK = :pk",
-    });
-    for (const d of data.Items ?? []) {
-      if (d.SK < 1619388000) {
-        await dynamodb.delete({
-          Key: {
-            PK: d.PK,
-            SK: d.SK,
-          },
-        });
+  const now = Math.round(Date.now() / 1000);
+  let timestamp = getClosestDayStartTimestamp(now);
+  while (true) {
+    for (const tvlFunc of [dailyTokensTvl, dailyTvl, dailyUsdTokensTvl]) {
+      const dailyPK = tvlFunc(protocol.id)
+      const hourlyPK = dailyPK.replace('daily', 'hourly')
+      await dynamodb.delete({
+        Key: {
+          PK: hourlyPK,
+          SK: timestamp,
+        },
+      });
+      const hourlyData = await getTVLOfRecordClosestToTimestamp(hourlyPK, timestamp, 3*3600)
+      if(hourlyData.SK === undefined){
+        throw new Error(`No hourly data for ${hourlyPK} @ ${timestamp}`)
       }
+      await dynamodb.put({
+        ...hourlyData,
+        PK:dailyPK,
+        SK:timestamp
+      })
     }
+    console.log(timestamp, new Date(timestamp * 1000).toDateString());
+    timestamp = getClosestDayStartTimestamp(timestamp - secondsInDay);
   }
 }
 main();

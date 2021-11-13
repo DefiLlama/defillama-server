5ec169859909c707c4202a67718e0e3a11eecb29
0xngmi <0xngmi@protonmail.com>
2021-11-13 07:44:49 +0000

    add v2 protocols

--- src/storeGetProtocols.ts
+++ src/storeGetProtocols.ts
@@ -3,6 +3,13 @@ import { wrapScheduledLambda } from "./utils/wrap";
 import { store } from "./utils/s3";
 import { constants, brotliCompressSync } from 'zlib'
 
+function compress(data:string){
+  return brotliCompressSync(data, {
+    [constants.BROTLI_PARAM_MODE]: constants.BROTLI_MODE_TEXT,
+    [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
+  })
+}
+
 const handler = async (_event: any) => {
   const response = await craftProtocolsResponse()
   const trimmedResponse = response.map(protocol=>({
@@ -16,12 +23,16 @@ const handler = async (_event: any) => {
     "change_7d": protocol["change_7d"],
     mcaptvl: protocol.mcap? protocol.mcap/protocol.tvl:undefined,
   }))
-  const compressedRespone = brotliCompressSync(JSON.stringify(trimmedResponse), {
-    [constants.BROTLI_PARAM_MODE]: constants.BROTLI_MODE_TEXT,
-    [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
-  })
+  const compressedRespone = compress(JSON.stringify(trimmedResponse))
   
   await store('lite/protocols', compressedRespone, true)
+  const chainSet = new Set()
+  trimmedResponse.forEach(p=>p.chains.forEach((c:string)=>chainSet.add(c)))
+  const compressedV2Respone = compress(JSON.stringify({
+    protocols: trimmedResponse,
+    chains: Array.from(chainSet)
+  }))
+  await store('lite/protocols2', compressedV2Respone, true)
 };
 
 export default wrapScheduledLambda(handler);

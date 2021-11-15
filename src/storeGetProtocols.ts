f96901f2e34ffdd1ac8593bfbda9f2cedb749f8a
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-11-14 21:08:04 -0800

    remove All from chains

--- src/storeGetProtocols.ts
+++ src/storeGetProtocols.ts
@@ -1,46 +1,52 @@
 import { craftProtocolsResponse } from "./getProtocols";
 import { wrapScheduledLambda } from "./utils/wrap";
 import { store } from "./utils/s3";
-import { constants, brotliCompressSync } from 'zlib'
+import { constants, brotliCompressSync } from "zlib";
 
 function compress(data: string) {
   return brotliCompressSync(data, {
     [constants.BROTLI_PARAM_MODE]: constants.BROTLI_MODE_TEXT,
     [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
-  })
+  });
 }
 
 const handler = async (_event: any) => {
-  const response = await craftProtocolsResponse()
-  const trimmedResponse = response.map(protocol => ({
+  const response = await craftProtocolsResponse();
+  const trimmedResponse = response.map((protocol) => ({
     name: protocol.name,
     symbol: protocol.symbol,
-    "category": protocol.category,
-    "chains": protocol.chains,
+    category: protocol.category,
+    chains: protocol.chains,
     tvl: protocol.tvl,
     chainTvls: protocol.chainTvls,
-    "change_1d": protocol["change_1d"],
-    "change_7d": protocol["change_7d"],
+    change_1d: protocol["change_1d"],
+    change_7d: protocol["change_7d"],
     mcaptvl: protocol.mcap ? protocol.mcap / protocol.tvl : undefined,
-  }))
-  const compressedRespone = compress(JSON.stringify(trimmedResponse))
+  }));
+  const compressedRespone = compress(JSON.stringify(trimmedResponse));
 
-  await store('lite/protocols', compressedRespone, true)
-  const chains = {} as { [chain: string]: number }
-  const protocolCategoriesSet = new Set()
-  trimmedResponse.forEach(p => {
-    protocolCategoriesSet.add(p.category)
+  await store("lite/protocols", compressedRespone, true);
+  const chains = {} as { [chain: string]: number };
+  const protocolCategoriesSet = new Set();
+  trimmedResponse.forEach((p) => {
+    protocolCategoriesSet.add(p.category);
     p.chains.forEach((c: string) => {
-      chains[c] = (chains[c] ?? 0) + p.chainTvls[c]
-    })
-  })
+      chains[c] = (chains[c] ?? 0) + p.chainTvls[c];
+    });
+  });
 
-  const compressedV2Response = compress(JSON.stringify({
-    protocols: trimmedResponse,
-    chains: ["All"].concat(Object.entries(chains).sort((a,b)=>b[1]-a[1]).map(c=>c[0])),
-    protocolCategories: [...protocolCategoriesSet].filter((category) => category)
-  }))
-  await store('lite/protocols2', compressedV2Response, true)
+  const compressedV2Response = compress(
+    JSON.stringify({
+      protocols: trimmedResponse,
+      chains: Object.entries(chains)
+        .sort((a, b) => b[1] - a[1])
+        .map((c) => c[0]),
+      protocolCategories: [...protocolCategoriesSet].filter(
+        (category) => category
+      ),
+    })
+  );
+  await store("lite/protocols2", compressedV2Response, true);
 };
 
 export default wrapScheduledLambda(handler);

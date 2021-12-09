a2fba1ea177488fc8a93621afa9891347c5e4ed3
0xngmi <0xngmi@protonmail.com>
2021-12-09 04:09:11 +0000

    move rawprotocols to config endpoint

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,10 +1,14 @@
 import { successResponse, wrap, IResponse } from "./utils";
 import protocols from "./protocols/data";
+import {chainCoingeckoIds} from "./utils/normalizeChain";
 
 const handler = async (
   _event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
-  return successResponse(protocols);
+  return successResponse({
+    protocols,
+    chainCoingeckoIds
+  });
 };
 
 export default wrap(handler);

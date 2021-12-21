f71aa67ef0a0f0fbefaf02696728cfa128fc63de
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-21 16:14:59 +0000

    update tomb description

--- src/protocols/data.ts
+++ src/protocols/data.ts
@@ -5396,7 +5396,7 @@ export default [
     address: "fantom:0x6c021ae822bea943b2e66552bde1d2696a53fbb7",
     symbol: "TOMB",
     url: "https://tomb.finance/",
-    description: "The first algorithmic stablecoin on Fantom Opera, pegged to the price of 1 FTM via seigniorage.",
+    description: "TOMB is a crosschain algorithmic stablecoin, pegged to the price of 1 FTM via seigniorage.",
     chain: "Fantom",
     logo: `${baseIconsUrl}/tomb-finance.jpg`,
     audits: "3",

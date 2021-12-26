68c08383a238739a7103e58a188d19841e194e0a
0xngmi <0xngmi@protonmail.com>
2021-12-26 10:45:04 +0000

    fix coins to work with redirect

--- coins/src/getCoins.ts
+++ coins/src/getCoins.ts
@@ -1,5 +1,5 @@
 import { successResponse, wrap, IResponse } from "./utils/shared";
-import dynamodb, { TableName } from "./utils/shared/dynamodb";
+import ddb, { batchGet } from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
 
 const step = 100; // Max 100 items per batchGet
@@ -7,32 +7,28 @@ const handler = async (
   event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
   const requestedCoins = parseRequestBody(event.body).coins;
-  const requests = [];
-  for (let i = 0; i < requestedCoins.length; i += step) {
-    requests.push(
-      dynamodb
-        .batchGet(
-          requestedCoins.slice(i, i + step).map((coin: string) => ({
-            PK: `asset#${coin}`,
-            SK: 0,
-          }))
-        )
-        .then((items) => items.Responses![TableName])
-    );
-  }
-  const returnedCoins = (await Promise.all(requests)).reduce(
-    (acc, coins) =>
-      acc.concat(
-        coins.map((coin) => ({
-          decimals: coin.decimals,
-          coin: coin.PK.substr("asset#".length),
-          price: coin.price,
-          symbol: coin.symbol,
-          timestamp: coin.timestamp,
-        }))
-      ),
-    []
-  );
+  const coins = await batchGet(requestedCoins.map((coin: string) => ({
+    PK: `asset#${coin}`,
+    SK: 0,
+  })));
+  const returnedCoins = await Promise.all(coins.map(async coin=>{
+    const formattedCoin = {
+      decimals: coin.decimals,
+      coin: coin.PK.substr("asset#".length),
+      price: coin.price,
+      symbol: coin.symbol,
+      timestamp: coin.timestamp,
+    }
+    if(coin.redirect){
+      const redirectedCoin = await ddb.get({
+        PK: coin.redirect,
+        SK: 0
+      })
+      formattedCoin.price = redirectedCoin.Item?.price
+      formattedCoin.timestamp = redirectedCoin.Item?.timestamp
+    }
+    return formattedCoin
+  }))
   return successResponse(returnedCoins);
 };
 

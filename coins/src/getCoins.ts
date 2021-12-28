6ee76cd85c75f038350f95792c4c7cb5aba0be4c
0xngmi <0xngmi@protonmail.com>
2021-12-28 06:15:59 +0000

    queries for historical data

--- coins/src/getCoins.ts
+++ coins/src/getCoins.ts
@@ -1,30 +1,35 @@
 import { successResponse, wrap, IResponse } from "./utils/shared";
 import ddb, { batchGet } from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
+import getRecordClosestToTimestamp from "./utils/shared/getRecordClosestToTimestamp";
 
-function cutStartWord(text:string, startWord:string){
+function cutStartWord(text: string, startWord: string) {
   return text.substr(startWord.length)
 }
+
+const DAY = 3600*24;
 
 const handler = async (
   event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
-  const requestedCoins = parseRequestBody(event.body).coins;
+  const body = parseRequestBody(event.body)
+  const requestedCoins = body.coins;
+  const timestampRequested = body.timestamp
   const coins = await batchGet(requestedCoins.map((coin: string) => ({
-    PK: coin.startsWith("coingecko:")?`coingecko#${cutStartWord(coin, "coingecko:")}`:`asset#${coin}`,
+    PK: coin.startsWith("coingecko:") ? `coingecko#${cutStartWord(coin, "coingecko:")}` : `asset#${coin}`,
     SK: 0,
   })));
   const response = {} as {
-    [coin:string]:{
-      decimals:number,
-      price:number,
+    [coin: string]: {
+      decimals: number,
+      price: number,
       timestamp: number,
       symbol: string,
     }
   }
-  await Promise.all(coins.map(async coin=>{
-    const coinName = coin.PK.startsWith("asset#")?
-      cutStartWord(coin.PK, "asset#"):
+  await Promise.all(coins.map(async coin => {
+    const coinName = coin.PK.startsWith("asset#") ?
+      cutStartWord(coin.PK, "asset#") :
       `coingecko:${cutStartWord(coin.PK, "coingecko#")}`;
     const formattedCoin = {
       decimals: coin.decimals,
@@ -32,13 +37,22 @@ const handler = async (
       symbol: coin.symbol,
       timestamp: coin.timestamp,
     }
-    if(coin.redirect){
-      const redirectedCoin = await ddb.get({
-        PK: coin.redirect,
-        SK: 0
-      })
-      formattedCoin.price = redirectedCoin.Item?.price
-      formattedCoin.timestamp = redirectedCoin.Item?.timestamp
+    if (coin.redirect || timestampRequested !== undefined) {
+      if (timestampRequested === undefined) {
+        const redirectedCoin = await ddb.get({
+          PK: coin.redirect,
+          SK: 0
+        })
+        formattedCoin.price = redirectedCoin.Item?.price
+        formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
+      } else {
+        const finalCoin = await getRecordClosestToTimestamp(coin.redirect ?? coin.PK, timestampRequested, DAY/2);
+        if(finalCoin.SK === undefined){
+          return
+        }
+        formattedCoin.price = finalCoin.price;
+        formattedCoin.timestamp = finalCoin.SK;
+      }
     }
     response[coinName] = formattedCoin;
   }))

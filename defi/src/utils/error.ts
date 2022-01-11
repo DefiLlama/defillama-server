6cd0c5ba4cd89d74282f04eb28fe9d9054c226ca
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-01-11 01:11:13 -0800

    Add catch error fn

--- defi/src/utils/error.ts
+++ defi/src/utils/error.ts
@@ -13,3 +13,25 @@ export function reportErrorObject(error: Error, tag: string, value: string) {
   scope.setTag(tag, value);
   Sentry.AWSLambda.captureException(error, scope);
 }
+
+export const PUT_DAILY_VOLUME_ERROR = "PUT_DAILY_VOLUME";
+
+export function reportDexVolumeError({
+  id,
+  timestamp,
+  category,
+  message,
+}: {
+  id: number;
+  timestamp: number;
+  category: string;
+  message: string;
+}) {
+  const scope = new Sentry.Scope();
+  const errorName = `${id}-${timestamp}-${category}`;
+  scope.setTag("dexvolume", id);
+  const error = new Error(message);
+  error.name = errorName;
+  Sentry.AWSLambda.captureException(error, scope);
+  throw error;
+}

8df1aac5f33467fc545d1bb87edefa0148dd1624
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-01-11 05:29:40 +0000

    update chain

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -21629,7 +21629,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
   symbol: "-",
   url: "https://parallel.fi",
   description: "Parallel Finance is a Decentralized Money Market Protocol that offers lending, staking, and borrowing in the Polkadot ecosystem. Depositors can lend and stake simultaneously to earn double yield on their staked coins, and borrowers can collateralize to borrow.",
-  chain: "Polkadot",
+  chain: "Parallel",
   logo: `${baseIconsUrl}/parallel-finance.jpg`,
   audits: "0",
   audit_note: null,

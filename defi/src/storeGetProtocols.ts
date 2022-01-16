ee83982b5abad0a54cb199a23c48a2ca07fb1823
mintdart <96025197+mintdart@users.noreply.github.com>
2022-01-16 07:58:58 +0530

    fix incorrect tvl change calc

--- defi/src/storeGetProtocols.ts
+++ defi/src/storeGetProtocols.ts
@@ -2,7 +2,7 @@ import { craftProtocolsResponse } from "./getProtocols";
 import { wrapScheduledLambda } from "./utils/shared/wrap";
 import { store } from "./utils/s3";
 import { constants, brotliCompressSync } from "zlib";
-import { getTvlChange } from "./utils/getTvlChange";
+import { getTvlChange, ProtocolTvlsChange } from "./utils/getTvlChange";
 
 function compress(data: string) {
   return brotliCompressSync(data, {
@@ -14,20 +14,20 @@ function compress(data: string) {
 const handler = async (_event: any) => {
   const response = await craftProtocolsResponse(true);
   const trimmedResponse = await Promise.all(response.map(async (protocol) => {
-    const chainTvlsChange = await getTvlChange(protocol.id)
+  const protocolTvlsChange: ProtocolTvlsChange = await getTvlChange(protocol.id)
     return {
       category: protocol.category,
       chains: protocol.chains,
       chainTvls: protocol.chainTvls,
-      change_1d: protocol["change_1d"],
-      change_7d: protocol["change_7d"],
-      change_1m: chainTvlsChange['tvl']?.['change_1m'] ?? null,
+      change_1d: protocolTvlsChange.change_1d,
+      change_7d: protocolTvlsChange.change_7d,
+      change_1m: protocolTvlsChange.change_1m,
       listedAt: protocol.listedAt,
       mcap: protocol.mcap,
       name: protocol.name,
       symbol: protocol.symbol,
       tvl: protocol.tvl,
-      chainTvlsChange
+      chainTvlsChange: protocolTvlsChange.chainTvlsChange
     }
   }));
 
--- defi/src/utils/getTvlChange.ts
+++ defi/src/utils/getTvlChange.ts
@@ -12,36 +12,55 @@ type ChainTvls = {
     }
 }
 
-export async function getTvlChange(protocolId: string): Promise<ChainTvls> {
+export type ProtocolTvlsChange = {
+    change_1d: number | null,
+    change_7d: number | null,
+    change_1m: number | null,
+    chainTvlsChange: ChainTvls
+}
+
+export async function getTvlChange(protocolId: string): Promise<ProtocolTvlsChange> {
     const now = Math.round(Date.now() / 1000)
     const lastRecord = await getLastRecord(hourlyTvl(protocolId))
     const previousDayRecord = await getTVLOfRecordClosestToTimestamp(hourlyTvl(protocolId), now - secondsInDay, secondsInHour)
     const previousWeekRecord = await getTVLOfRecordClosestToTimestamp(hourlyTvl(protocolId), now - secondsInWeek, secondsInDay)
     const previousMonthRecord = await getTVLOfRecordClosestToTimestamp(hourlyTvl(protocolId), now - secondsInWeek * 4, secondsInDay)
+    const chainTvlsChange: ChainTvls = {}
+    let change_1d: number | null = null;
+    let change_7d: number | null = null;
+    let change_1m: number | null = null;
     if (lastRecord && previousDayRecord?.SK && previousWeekRecord?.SK && previousMonthRecord?.SK) {
         const defaultTvl = lastRecord.tvl || 0
         const oneDayDefaultTvl = previousDayRecord.tvl || 0
         const oneWeekDefaultTvl = previousWeekRecord.tvl || 0
         const oneMonthDefaultTvl = previousMonthRecord.tvl || 0
-        const chainTvls: ChainTvls = {}
         Object.entries(lastRecord).forEach(([chain, chainTvl]) => {
             if (chain !== 'tvl' && nonChains.includes(chain)) {
                 return;
             }
-            chainTvls[chain] = {
-                'change_1d': null,
-                'change_7d': null,
-                'change_1m': null,
-            }
+
             const previousDayTvl = chain ? previousDayRecord[chain] : null
             const previousWeekTvl = chain ? previousWeekRecord[chain] : null
             const previousMonthTvl = chain ? previousMonthRecord[chain] : null
-            if (previousDayTvl && previousWeekTvl) {
-                chainTvls[chain]['change_1d'] = getPercentChange(oneDayDefaultTvl + previousDayTvl, defaultTvl + chainTvl)
-                chainTvls[chain]['change_7d'] = getPercentChange(oneWeekDefaultTvl + previousWeekTvl, defaultTvl + chainTvl)
-                chainTvls[chain]['change_1m'] = getPercentChange(oneMonthDefaultTvl + previousMonthTvl, defaultTvl + chainTvl)
+
+            if (previousDayTvl && previousWeekTvl && previousMonthTvl) {
+                if (chain === 'tvl') {
+                    const previousMonthTvl = chain ? previousMonthRecord[chain] : null
+                    change_1d = getPercentChange(previousDayTvl, chainTvl)
+                    change_7d = getPercentChange(previousWeekTvl, chainTvl)
+                    change_1m = getPercentChange(previousMonthTvl, chainTvl)
+                } else {
+                    chainTvlsChange[chain] = {
+                        'change_1d': null,
+                        'change_7d': null,
+                        'change_1m': null,
+                    }
+                    chainTvlsChange[chain]['change_1d'] = getPercentChange(oneDayDefaultTvl + previousDayTvl, defaultTvl + chainTvl)
+                    chainTvlsChange[chain]['change_7d'] = getPercentChange(oneWeekDefaultTvl + previousWeekTvl, defaultTvl + chainTvl)
+                    chainTvlsChange[chain]['change_1m'] = getPercentChange(oneMonthDefaultTvl + previousMonthTvl, defaultTvl + chainTvl)
+                }
             }
         })
-        return chainTvls
-    } else return {}
+    }
+    return {change_1d, change_7d, change_1m, chainTvlsChange}
 }

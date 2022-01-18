050ee3e99e490662e614b7064f2900aec7819c2f
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-01-17 23:28:28 -0800

    Finish getVolumesFromStart test

--- defi/src/cli/backfillDexVolumes/index.ts
+++ defi/src/cli/backfillDexVolumes/index.ts
@@ -35,7 +35,10 @@ const HOUR = 3600;
 const DAY = HOUR * 24;
 const MAX_HOURS = 25;
 
-type Fetch = (timestamp: number, chainBlocks: ChainBlocks) => FetchResult;
+type Fetch = (
+  timestamp: number,
+  chainBlocks: ChainBlocks
+) => Promise<FetchResult>;
 
 export const getBlocksFromStart = async (
   start: number,
@@ -85,19 +88,13 @@ export const getVolumesFromStart = async ({
 }) => {
   const volumes = [];
 
-  // Add initial volume as 0 for today and prev day if starts at 12:00 as buffer for starts with
-  volumes.push({
-    dailyVolume: "0",
-    totalVolume: "0",
-    timestamp: calcIsNewDay(start)
-      ? getTimestampAtStartOfDayUTC(start - DAY)
-      : getTimestampAtStartOfDayUTC(start),
-  });
-
   // Get everyday up to today
-  for (let timestamp = start; timestamp <= end; timestamp += DAY) {
-    const chainBlocks = { [ecosystem]: blocks[timestamp] };
-    volumes.push(fetch(timestamp, chainBlocks));
+  let dailyTimestamp = start;
+  while (dailyTimestamp < end) {
+    const chainBlocks = { [ecosystem]: blocks[dailyTimestamp] };
+    volumes.push(fetch(dailyTimestamp, chainBlocks));
+
+    dailyTimestamp = getTimestampAtStartOfNextDayUTC(dailyTimestamp);
   }
 
   // Get last 25 hours
@@ -112,14 +109,51 @@ export const getVolumesFromStart = async ({
 
   const allVolumeRes = await Promise.all(volumes);
 
-  return allVolumeRes.reduce((acc: TimestampVolumes, curr: FetchResult) => {
-    const { dailyVolume, timestamp, totalVolume } = curr;
-    acc[timestamp] = {
-      totalVolume,
-      dailyVolume,
+  const allVolumes = allVolumeRes.reduce(
+    (acc: TimestampVolumes, curr: FetchResult) => {
+      const { timestamp, totalVolume } = curr;
+      acc[timestamp] = {
+        totalVolume,
+      };
+      return acc;
+    },
+    {}
+  );
+
+  // Add initial volume as 0 for today or prev day if starts at 12:00 as buffer for starts
+  // Volume didn't start at beginning of day so need a buffer to calc next - start for daily
+  const isNewDay = calcIsNewDay(start);
+  const startHasVolume = Number(allVolumes[start]?.totalVolume) > 0;
+  const startTimestamp = isNewDay
+    ? getTimestampAtStartOfDayUTC(start - DAY)
+    : getTimestampAtStartOfDayUTC(start);
+  if (
+    startHasVolume ||
+    (!isNewDay &&
+      Number(allVolumes[getTimestampAtStartOfNextDayUTC(start)]?.totalVolume) >
+        0)
+  ) {
+    allVolumes[startTimestamp] = {
+      totalVolume: "0",
     };
-    return acc;
-  }, {});
+
+    // Add padded hour for same reasoning if less than 25 timestamps
+    if (startHasVolume && end - start < HOUR * MAX_HOURS) {
+      allVolumes[start - HOUR] = {
+        totalVolume: "0",
+      };
+    }
+
+    return {
+      allVolumes,
+      startTimestamp,
+    };
+  }
+
+  return {
+    allVolumes,
+    startTimestamp: start,
+  };
 };
 
 const fetchEcosystemsFromStart = async ({
@@ -135,20 +169,20 @@ const fetchEcosystemsFromStart = async ({
 }) => {
   const startTimestamp = typeof start === "number" ? start : await start();
 
-  // TODO add error report
   const blocks = await getBlocksFromStart(startTimestamp, ecosystem, end);
-  const volumes = await getVolumesFromStart({
-    blocks,
-    ecosystem,
-    fetch,
-    start: startTimestamp,
-    end,
-  });
+  const { allVolumes, startTimestamp: updatedStartTimestamp } =
+    await getVolumesFromStart({
+      blocks,
+      ecosystem,
+      fetch,
+      start: startTimestamp,
+      end,
+    });
 
   return {
     ecosystem,
-    volumes,
-    startTimestamp,
+    volumes: allVolumes,
+    startTimestamp: updatedStartTimestamp,
   };
 };
 

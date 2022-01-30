728d9c0becb90d8e964d9ea75db5ba4a7cee466e
0xngmi <0xngmi@protonmail.com>
2022-01-30 00:46:27 +0000

    don't send discord messages when refilling

--- defi/src/storeTvlInterval/storeNewTvl.ts
+++ defi/src/storeTvlInterval/storeNewTvl.ts
@@ -97,15 +97,17 @@ export default async function (
     }
   }
 
-  const lastHourlyTVLObject = await lastHourlyTVLRecord;
-  await Promise.all(Object.entries(tvl).map(async ([sectionName, sectionTvl])=>{
-    const prevTvl = lastHourlyTVLObject[sectionName]
-    if(sectionTvl===0 && prevTvl !== 0 && prevTvl !== undefined){
-      await sendMessage(
-        `TVL for ${protocol.name} has dropped to 0 on key "${sectionName}" (previous TVL was ${prevTvl})`,
-      process.env.DROPS_WEBHOOK!)
-    }
-  }))
+  if (storePreviousData) {
+    const lastHourlyTVLObject = await lastHourlyTVLRecord;
+    await Promise.all(Object.entries(tvl).map(async ([sectionName, sectionTvl]) => {
+      const prevTvl = lastHourlyTVLObject[sectionName]
+      if (sectionTvl === 0 && prevTvl !== 0 && prevTvl !== undefined) {
+        await sendMessage(
+          `TVL for ${protocol.name} has dropped to 0 on key "${sectionName}" (previous TVL was ${prevTvl})`,
+          process.env.DROPS_WEBHOOK!)
+      }
+    }))
+  }
 
   let tvlPrev1Day =  (await lastDailyTVLRecord).tvl
   let tvlPrev1Week = (await lastWeeklyTVLRecord).tvl

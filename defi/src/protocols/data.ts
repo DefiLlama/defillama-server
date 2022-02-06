6d6bc50e50cd560be26c4b5a2a0fe7d5476798b6
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-02-06 21:58:05 +0000

    update ooki module

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -1840,7 +1840,7 @@ export default [
     cmcId: "5810",
     category: "Lending",
     chains: ["Ethereum"],
-    module: "ooki.js",
+    module: "ooki/index.js",
     twitter: "OokiTrade",
     audit_links: ["https://bzx.network/security"],
     oracles: ["Chainlink"],

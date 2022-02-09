2c3a94036721c59055ed1e6596c23c4dd4ea4060
mintdart <96025197+mintdart@users.noreply.github.com>
2022-02-10 05:07:19 +0800

    update oracles route to include extra tvls

--- defi/src/getOracles.ts
+++ defi/src/getOracles.ts
@@ -1,10 +1,13 @@
 import { processProtocols, TvlItem } from './storeGetCharts';
 import { successResponse, wrap, IResponse } from './utils/shared';
 import type { Protocol } from './protocols/data';
+import { extraSections } from './utils/normalizeChain';
 
 interface SumDailyTvls {
   [timestamp: number]: {
-    [oracle: string]: number | undefined;
+    [oracle: string]: {
+      [key: string]: number;
+    };
   };
 }
 
@@ -12,18 +15,32 @@ interface OracleProtocols {
   [oracle: string]: Set<string>;
 }
 
+interface Item {
+  [key: string]: number;
+}
+
 function sum(
   total: SumDailyTvls,
   oracle: string,
   time: number,
-  tvl: number,
+  item: Item = {},
   oracleProtocols: OracleProtocols,
   protocol: string
 ) {
   if (total[time] === undefined) {
     total[time] = {};
   }
-  total[time][oracle] = (total[time][oracle] ?? 0) + tvl;
+
+  const data = total[time][oracle] || {};
+
+  for (const i in item) {
+    const section: string = i.includes('-') ? i.split('-')[1] : i;
+    if (section === 'tvl' || extraSections.includes(section)) {
+      data[section] = (data[section] || 0) + item[section];
+    }
+  }
+
+  total[time][oracle] = data;
 
   if (oracleProtocols[oracle] == undefined) {
     oracleProtocols[oracle] = new Set();
@@ -39,7 +56,7 @@ const handler = async (_event: AWSLambda.APIGatewayEvent): Promise<IResponse> =>
     let oracles = protocol.oracles;
     if (oracles) {
       oracles.forEach((oracle) => {
-        sum(sumDailyTvls, oracle, timestamp, item.tvl, oracleProtocols, protocol.name);
+        sum(sumDailyTvls, oracle, timestamp, item, oracleProtocols, protocol.name);
       });
 
       return;

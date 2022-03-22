6de282838666a530bf83d652becc67223fc4f54b
0xngmi <0xngmi@protonmail.com>
2022-03-22 02:32:33 +0000

    move DefiLlama-Adapters to local package

---
+++ defi/src/utils/importAdapter.ts
@@ -0,0 +1,7 @@
+import { Protocol } from "../protocols/types";
+
+export function importAdapter(protocol:Protocol){
+    return import(
+        `@defillama/adapters/projects/${protocol.module}`
+    )
+}

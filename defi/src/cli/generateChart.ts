39834670f033ee26326265585aeacb8947e8c78d
0xngmi <0xngmi@protonmail.com>
2022-04-05 00:42:17 +0100

    remove report/chart stuff

--- defi/src/cli/generateChart.ts
+++
@@ -1,6 +0,0 @@
-import { getDailyVisitors, getPeriodDailyVisitors, getMostVisitedPages } from "../reports/buildReport"
-
-async function main(){
-    await getPeriodDailyVisitors(7)
-}
-main()
--- defi/src/growthReport.ts
+++ defi/src/growthReport.ts
@@ -1,6 +1,6 @@
-import { getMostVisitedPages } from "./reports/buildReport";
+import fetch from "node-fetch"
 import { wrapScheduledLambda } from "./utils/shared/wrap";
 
 export const daily = wrapScheduledLambda(async () => {
-    await getMostVisitedPages()
+    await fetch("https://born-to-llama.herokuapp.com/send-daily-report")
 });

1ce5b1feb1e59c792bc9adf00b00b555e9f48932
realShaman <realShaman@protonmail.com>
2022-04-07 09:19:54 +0100

    add new listings

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -17978,7 +17978,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
   audit_note: null,
   gecko_id: null ,
   cmcId: null ,
-  category: "Yield",
+  category: "Farm",
   chains: ["Fantom"],
   module: "xmaspast/index.js",
   twitter: "BooXmasPast",

7aa293829782fea5dd3619f65a8f9d04b6bf6d65
waynebruce0x <waynebruce0x@gmail.com>
2022-04-12 18:14:22 +0100

    knit category

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -15564,7 +15564,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     audit_note: null,
     gecko_id: "knit-finance",
     cmcId: "9856",
-    category: "Cross Chain",
+    category: "Bridge",
     chains: ["Ethereum", "Binance", "Polygon", "Fantom"],
     module: "knitfinance/index.js",
     twitter: "KnitFinance",

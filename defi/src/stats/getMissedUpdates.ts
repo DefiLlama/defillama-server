792ce573fbc99cf342933158af05aef54af2f43c
0xngmi <0xngmi@protonmail.com>
2022-05-29 19:08:42 +0100

    add more resources and handle current outdated

--- defi/src/stats/getMissedUpdates.ts
+++ defi/src/stats/getMissedUpdates.ts
@@ -2,6 +2,7 @@ import { successResponse, wrap, IResponse } from "../utils/shared";
 import protocols from "../protocols/data";
 import { hourlyTvl } from "../utils/getLastRecord";
 import dynamodb from "../utils/shared/dynamodb";
+import { getCurrentUnixTimestamp } from "../utils/date";
 
 const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
   const timestamp = Number(event.pathParameters!.timestamp); // unix timestamp
@@ -23,6 +24,10 @@ const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => 
       let totalSkippedHourlyUpdates = 0, hourlyDrasticChanges = 0;
       let lastTvl = result.Items[0].tvl;
       let lastTimestamp = result.Items[0].SK;
+      result.Items.push({
+        SK: getCurrentUnixTimestamp(),
+        tvl: result.Items[result.Items.length-1].tvl
+      })
       result.Items?.forEach(item=>{
         if((item.SK - lastTimestamp) > (60+20)*3600){ // max drift is one update getting stored at x:00 and next at x+1:15, so max difference will be <1:20
           totalSkippedHourlyUpdates += Math.round((item.SK - lastTimestamp)/3600) - 1

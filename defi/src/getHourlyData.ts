fe837eff365f5ff0d466ad2bc37468da45407116
0xngmi <0xngmi@protonmail.com>
2022-05-31 17:14:37 +0100

    Revert "sluggify prot name on /hourly"
    This reverts commit 1934a6abe1d007c6fb0c6e09fde23e5698f48fc4.

--- defi/src/getHourlyData.ts
+++ defi/src/getHourlyData.ts
@@ -1,13 +1,12 @@
 import { wrap, IResponse } from "./utils/shared";
 import { craftProtocolResponse, wrapResponseOrRedirect } from './getProtocol'
-import {sluggifyString} from "./utils/sluggify";
 
 // undocumented and likely to change whenever I want
 // data returned will be wrong, requires cleaning
 const handler = async (
     event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
-    const response = await craftProtocolResponse(sluggifyString(event.pathParameters!.protocol!), true, true)
+    const response = await craftProtocolResponse(event.pathParameters?.protocol, true, true)
     return wrapResponseOrRedirect(response);
 };
 
--- defi/src/utils/sluggify.ts
+++ defi/src/utils/sluggify.ts
@@ -1,4 +1,3 @@
 import type { Protocol } from "../protocols/data";
 
-export const sluggifyString = (name: string) => name.toLowerCase().split(" ").join("-").split("'").join("");
-export default (prot: Protocol) => sluggifyString(prot.name);
+export default (prot: Protocol) => prot.name.toLowerCase().split(" ").join("-").split("'").join("");

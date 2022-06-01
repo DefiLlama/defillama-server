832acdf6b4bd799dc106f83099db73d46dc0ee3b
Define101 <Lovelovenow@protonmail.com>
2022-06-01 16:56:08 +0100

    last try

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -8439,7 +8439,7 @@ listedAt: 1650804679
   gecko_id: null,
   cmcId: null,
   category: "Bridge",
-  chains: ["Ethereum", "Polygon", "Avalanche", "Fantom", "Binance", "Arbitrum", "Optimism", "Heco", "Zilliqa", "Ontology", "NEO", "Carbon", "Boba", "Oasis", "Bitcoin", "zkSync"],
+  chains: ["Ethereum", "Polygon", "Avalanche", "Fantom", "Binance", "Arbitrum", "Optimism", "Heco", "Zilliqa", "Ontology", "NEO", "Carbon", "Boba", "Oasis", "zkSync"],
   oracles: [], 
   forkedFrom: [],
   module: "polynetwork/index.js",

3b5fe15d395deca101c1df4a9e637ae33872cbe7
0xngmi <0xngmi@protonmail.com>
2022-06-15 23:04:06 +0100

    add more traces

--- defi/src/storeTvlInterval/block.ts
+++ defi/src/storeTvlInterval/block.ts
@@ -13,9 +13,11 @@ async function getChainBlocks(timestamp: number, chains: Chain[] = chainsForBloc
     chains.map(async (chain) => {
       for (let i = 0; i < blockRetries; i++) {
         try {
+          console.log("block", chain)
           chainBlocks[chain] = await lookupBlock(timestamp, {
             chain,
           }).then((block) => block.block);
+          console.log("block completed", chain)
           break;
         } catch (e) {
           if (i === blockRetries - 1) {
@@ -32,6 +34,7 @@ export async function getCurrentBlocks() {
   const provider = getProvider("ethereum");
   const lastBlockNumber = await provider.getBlockNumber();
   const block = await provider.getBlock(lastBlockNumber - 5); // To allow indexers to catch up
+  console.log("block eth")
   const chainBlocks = await getChainBlocks(block.timestamp);
   chainBlocks['ethereum'] = block.number;
   return {

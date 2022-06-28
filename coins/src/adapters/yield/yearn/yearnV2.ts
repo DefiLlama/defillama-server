c7aff276b67adf19c3d7a354537716c0f05b6143
waynebruce0x <waynebruce0x@gmail.com>
2022-06-28 15:07:30 +0100

    changes to input to db

--- coins/src/adapters/yield/yearn/yearnV2.ts
+++ coins/src/adapters/yield/yearn/yearnV2.ts
@@ -1,11 +1,7 @@
-import { multiCall, call } from "@defillama/sdk/build/abi/index";
-import { getLatestBlock } from "@defillama/sdk/build/util/index";
+import { multiCall } from "@defillama/sdk/build/abi/index";
 import abi from "./abi.json";
-import { request, gql } from "graphql-request";
-import { batchGet } from "../../../utils/shared/dynamodb";
-import { PromisePool } from "@supercharge/promise-pool";
+import { batchGet, batchWrite } from "../../../utils/shared/dynamodb";
 import axios from "axios";
-
 const chains: object = {
   ethereum: 1,
   arbitrum: 42161,
@@ -26,9 +22,15 @@ interface multiCallResults {
 }
 interface result {
   success: boolean;
-  input: object;
+  input: {
+    target: string;
+  };
+  output: any;
 }
-
+function resolveDecimals(value: number, i: number) {
+  if (value > 10) i = resolveDecimals(value / 10, i) + 1;
+  return i;
+}
 async function requery(
   resultsRaw: multiCallResults,
   chain: string,
@@ -51,7 +53,65 @@ async function requery(
     });
   }
 }
+async function getPricePerShare(vaults: any[], chain: string) {
+  let pricePerShares = await multiCall({
+    abi: abi.pricePerShare,
+    calls: vaults.map((v: vaultKeys) => ({
+      target: v.address
+    })),
+    chain: chain as any
+  });
+  await requery(pricePerShares, chain, undefined, abi.getPricePerFullShare);
+  await requery(pricePerShares, chain, undefined, abi.constantPricePerShare);
+  pricePerShares.output = pricePerShares.output.filter(
+    (v) => v.success == true
+  );
+  return pricePerShares;
+}
+async function getUsdValues(
+  pricePerShares: multiCallResults,
+  vaults: any[],
+  underlyingPrices: any[],
+  redirectResults: any[],
+  chain: string
+) {
+  let usdValues = pricePerShares.output.map((t) => {
+    const selectedVaults = vaults.filter(
+      (v: vaultKeys) => v.address.toLowerCase() == t.input.target.toLowerCase()
+    );
+    const underlying = selectedVaults[0].token.address;
+    const redirectKeys = underlyingPrices.filter((p) =>
+      p.PK.includes(underlying.toLowerCase())
+    );
+    if (redirectKeys.length == 0) {
+      return {};
+    }
+    const underlyingPrice = redirectResults.filter(
+      (p) => p.PK == redirectKeys[0].redirect
+    )[0].price;
 
+    const decimals = resolveDecimals(t.output, 0);
+
+    return {
+      address: `asset#${chain}:${t.input.target.toLowerCase()}`,
+      price: (t.output * underlyingPrice) / 10 ** decimals,
+      decimals: decimals
+    };
+    // return JSON.stringify({
+    //   PK: {
+    //     S: `asset#${chain}:${t.input.target.toLowerCase()}`
+    //   },
+    //   price: {
+    //     N: (t.output * underlyingPrice) / 10 ** decimals
+    //   },
+    //   SK: {
+    //     N: Date.now()
+    //   }
+    // });
+  });
+
+  return usdValues.filter((v) => Object.keys(v).length !== 0);
+}
 export async function getTokenPrices(chain: string) {
   let vaults = (
     await axios.get(
@@ -60,64 +120,54 @@ export async function getTokenPrices(chain: string) {
       }/vaults/all`
     )
   ).data;
-  // .map((v: vaultKeys) => ({
-  //   vaultSymbol: v.symbol,
-  //   underlyingSymbol: v.token.symbol,
-  //   vaultAddress: v.address,
-  //   underlyingAddress: v.token.address,
-  //   type: v.type
-  // }));
-
-  let addresses = vaults.reduce(
-    (a: string, b: vaultKeys) => `${a},${b.token.address}`,
-    ""
-  );
-  let tokenPrices = (
-    await axios.get(
-      `https://api.coingecko.com/api/v3/simple/token_price/${chain}?contract_addresses=${addresses}&vs_currencies=usd`
-    )
-  ).data;
-
-  let b = await multiCall({
-    abi: abi.pricePerShare,
-    calls: vaults.map((v: vaultKeys) => ({
-      target: v.address
-    })),
-    chain: chain as any
-  });
-  await requery(b, chain, undefined, abi.getPricePerFullShare);
-  await requery(b, chain, undefined, abi.constantPricePerShare);
-  b.output = b.output.filter((v) => v.success == true);
-
-  const usdValues = b.output.map((t) => {
-    const selectedVaults = vaults.filter(
-      (v: vaultKeys) => v.address.toLowerCase() == t.input.target.toLowerCase()
-    );
-    const underlying = selectedVaults[0].token.address;
-    const underlyingPrice = tokenPrices[underlying.toLowerCase()];
-    if (!underlyingPrice) {
-      return;
-    }
-    const decimals = resolveDecimals(t.output, 0);
-
-    return {
-      address: `asset#${chain}:${t.input.target.toLowerCase()}`,
-      price: (t.output * underlyingPrice.usd) / 10 ** decimals,
-      decimals: decimals
-    };
-  });
 
   const underlyingPrices = await batchGet(
     vaults.map((v: vaultKeys) => ({
-      PK: `asset#${chain}:${v.token.address}`,
+      PK: `asset#${chain}:${v.token.address.toLowerCase()}`,
       SK: 0
     }))
   );
-}
 
-function resolveDecimals(value: number, i: number) {
-  if (value > 10) i = resolveDecimals(value / 10, i) + 1;
-  return i;
+  const redirects = [];
+  for (let i = 0; i < underlyingPrices.length; i++) {
+    if (!("redirect" in underlyingPrices[i])) continue;
+    redirects.push({
+      PK: underlyingPrices[i].redirect,
+      SK: 0
+    });
+  }
+
+  const [redirectResults, pricePerShares] = await Promise.all([
+    batchGet(redirects),
+    getPricePerShare(vaults, chain)
+  ]);
+
+  const usdValues = await getUsdValues(
+    pricePerShares,
+    vaults,
+    underlyingPrices,
+    redirectResults,
+    chain
+  );
+
+  //.map((v) => v || {});
+  // const sendThis: PutItemInputAttributeMap[] = usdValues.map((v) => ({
+  //   "prod-coins-table": v
+  // }));
+  // let queryString: AttributeValue = `{"prod-coins-table": [{"PutRequest": {"Item": {`;
+  // for (let i = 0; i < usdValues.length; i++) {
+  //   queryString = queryString.concat(usdValues[i] || "");
+  //   queryString.concat(`}}}]}`);
+  // }
+
+  await batchWrite(
+    usdValues.map((v) => ({
+      SK: Date.now(),
+      PK: v.address,
+      price: v.price
+    })),
+    true
+  );
 }
 
 async function main() {

a88067a9c02f7dc1a17724e0786eea4fcbc40f2b
realShaman <realShaman@protonmail.com>
2022-07-04 04:07:53 +0100

    update protocol name

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -89,7 +89,7 @@ export default ([
   },
   {
     id: "111",
-    name: "AAVE V2",
+    name: "AAVE",
     address: "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9",
     symbol: "AAVE",
     url: "https://aave.com\r\n",

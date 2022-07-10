4e002a2745b4a25df9e21f595d3b14aa7492434a
0xngmi <0xngmi@protonmail.com>
2022-07-10 23:55:54 +0100

    add /config/yields

---
+++ defi/src/getYieldsConfig.ts
@@ -0,0 +1,23 @@
+import { cache20MinResponse, wrap, IResponse } from "./utils/shared";
+import protocols from "./protocols/data";
+import sluggify from "./utils/sluggify";
+
+const handler = async (
+    _event: AWSLambda.APIGatewayEvent
+): Promise<IResponse> => {
+    return cache20MinResponse({
+        protocols: protocols.reduce((all, p) => ({
+            ...all,
+            [sluggify(p)]: {
+                name: p.name,
+                audits: p.audits,
+                audit_links: p.audit_links,
+                url: p.url,
+                twitter: p.twitter,
+                category: p.category
+            }
+        }), {}),
+    });
+};
+
+export default wrap(handler);

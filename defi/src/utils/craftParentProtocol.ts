5c70d738e9b4c7d7d71d608ef1acdec04e714721
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-13 03:23:41 -0700

    add other protocols list to parent protocolapi

--- defi/src/utils/craftParentProtocol.ts
+++ defi/src/utils/craftParentProtocol.ts
@@ -254,5 +254,9 @@ export default async function craftParentProtocol(
     }
   }
 
+  if (childProtocolsTvls.length > 0) {
+    response.otherProtocols = childProtocolsTvls[0].otherProtocols
+  }
+
   return response;
 }

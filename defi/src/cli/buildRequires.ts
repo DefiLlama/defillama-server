1a67a6500453a0861a3c5016a47b58e6fbf9af37
0xtawa <0xtawanofi@gmail.com>
2022-07-14 08:11:46 +0200

    Fix volumeAdapters import
    Fix volume adapters import 2

--- defi/src/cli/buildRequires.ts
+++ defi/src/cli/buildRequires.ts
@@ -1,7 +1,13 @@
 import protocols from "../protocols/data";
 import {writeFileSync} from "fs"
+import volumeAdapters from "../dexVolumes/dexAdapters";
+
+// Added to avoid duplicate keys with tvl adapters
+export const volumeAdapterPrefix = "volume-"
+
+const volumeAdaptersImports = volumeAdapters.map(p=>`"${volumeAdapterPrefix}${p.volumeAdapter}": require("@defillama/adapters/dexVolumes/${p.volumeAdapter}"),`)
 
 writeFileSync("./src/utils/imports/adapters.ts",
 `export default {
-    ${protocols.map(p=>`"${p.module}": require("@defillama/adapters/projects/${p.module}"),`).join('\n')}
+    ${protocols.map(p=>`"${p.module}": require("@defillama/adapters/projects/${p.module}"),`).concat(volumeAdaptersImports).join('\n')}
 }`)
--- defi/src/utils/imports/importAdapter.ts
+++ defi/src/utils/imports/importAdapter.ts
@@ -1,6 +1,12 @@
+import { volumeAdapterPrefix } from "../../cli/buildRequires";
+import { Dex } from "../../dexVolumes/dexAdapters";
 import { Protocol } from "../../protocols/types";
 import adapters from "./adapters"
 
-export function importAdapter(protocol:Protocol){
+export function importAdapter(protocol: Protocol) {
     return (adapters as any)[protocol.module]
+}
+
+export function importVolumeAdapter(dex: Dex) {
+    return (adapters as any)[`${volumeAdapterPrefix}${dex.volumeAdapter}`]
 }

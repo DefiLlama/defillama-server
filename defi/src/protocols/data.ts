43e7e67b6319992b8c5e65efab5693e67f02a52f
realdealshaman <realdealshaman11@gmail.com>
2022-07-16 06:28:34 +0100

    re-list ellipsis

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -2620,7 +2620,6 @@ const data: Protocol[] = [
     audit_links: ["https://www.certik.org/projects/swipe"],
     oracles: ["Chainlink","Band"],
   },
-  /*
   {
     id: "238",
     name: "Ellipsis Finance",
@@ -2643,7 +2642,6 @@ const data: Protocol[] = [
     ],
     forkedFrom: ["Curve"],
   },
-  */
   {
     id: "239",
     name: "PoolTogether",

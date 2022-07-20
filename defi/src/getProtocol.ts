dc8a53139ffdb2f3a9b7620ea4a31fc8da872aee
0xngmi <0xngmi@protonmail.com>
2022-07-20 17:12:14 +0100

    count bytes correctly

--- defi/src/getProtocol.ts
+++ defi/src/getProtocol.ts
@@ -37,7 +37,7 @@ export async function craftProtocolResponse(rawProtocolName: string|undefined, u
 
 export async function wrapResponseOrRedirect(response: any){
   const jsonData = JSON.stringify(response)
-  const dataLength = jsonData.length
+  const dataLength = Buffer.byteLength(jsonData, 'utf8');
   if(dataLength >= 5.8e6){
     const filename = `protocol-${response.name}.json`;
     await storeDataset(filename, jsonData, "application/json")

f7145a2d919caf8cf489c0509ddc6f8e0fa08e31
waynebruce0x <waynebruce0x@gmail.com>
2022-08-02 17:42:47 +0100

    think this works now

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -2,9 +2,9 @@ export default {
   aave: require("./moneyMarkets/aave/index"),
   geist: require("./moneyMarkets/aave/index"),
   compound: require("./moneyMarkets/compound/index"),
-  ironbank: require("./moneyMarkets/compound/index")
-  // yearn: require("./yield/yearn/index")
-  // curve: require("./lps/curve/index")
+  ironbank: require("./moneyMarkets/compound/index"),
+  yearn: require("./yield/yearn/index"),
+  curve: require("./lps/curve/index")
   // uniswap: require("./lps/uniswap/index"),
   // pancakeswap: require("./lps/uniswap/index")
 };

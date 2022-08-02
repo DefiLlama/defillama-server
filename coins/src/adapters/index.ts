719d17c6173ce3f7d7235ebca1cae65fcd9d21b4
waynebruce0x <waynebruce0x@gmail.com>
2022-08-02 17:27:41 +0100

    debug coins adapters

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -3,8 +3,8 @@ export default {
   geist: require("./moneyMarkets/aave/index"),
   compound: require("./moneyMarkets/compound/index"),
   ironbank: require("./moneyMarkets/compound/index"),
-  yearn: require("./yield/yearn/index"),
-  curve: require("./lps/curve/index"),
-  uniswap: require("./lps/uniswap/index"),
-  pancakeswap: require("./lps/uniswap/index")
+  // yearn: require("./yield/yearn/index")
+  curve: require("./lps/curve/index")
+  // uniswap: require("./lps/uniswap/index"),
+  // pancakeswap: require("./lps/uniswap/index")
 };

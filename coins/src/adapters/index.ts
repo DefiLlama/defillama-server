51f4b3420d9ee3a2b305d0caba81bbcd567a9b00
waynebruce0x <waynebruce0x@gmail.com>
2022-08-03 17:30:57 +0100

    added protocols might not all work

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -1,11 +1,30 @@
 export default {
   aave: require("./moneyMarkets/aave/index"),
   geist: require("./moneyMarkets/aave/index"),
+  radiant: require("./moneyMarkets/aave/index"),
+  klap: require("./moneyMarkets/aave/index"),
+
   compound: require("./moneyMarkets/compound/index"),
+  venus: require("./moneyMarkets/compound/index"),
   ironbank: require("./moneyMarkets/compound/index"),
+  benqi: require("./moneyMarkets/compound/index"),
+  traderjoe: require("./moneyMarkets/compound/index"),
+  rari: require("./moneyMarkets/compound/index"),
+  aurigami: require("./moneyMarkets/compound/index"),
+  cream: require("./moneyMarkets/compound/index"),
+  scream: require("./moneyMarkets/compound/index"),
+
   yearn: require("./yield/yearn/index"),
   curve: require("./lps/curve/index"),
+
   uniswap: require("./lps/uniswap/index"),
   traderJoe: require("./lps/uniswap/index"),
+  vvsFinance: require("./lps/uniswap/index"),
+  quickswap: require("./lps/uniswap/index"),
+  biswap: require("./lps/uniswap/index"),
+  mmFinance: require("./lps/uniswap/index"),
+  trisolaris: require("./lps/uniswap/index"),
+  pangolin: require("./lps/uniswap/index"),
+  spiritswap: require("./lps/uniswap/index"),
   pancakeswap: require("./lps/uniswap/index")
 };

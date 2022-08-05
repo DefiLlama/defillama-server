2645ea10c85982536c7081bc3fa2294940f42a2a
0xngmi <0xngmi@protonmail.com>
2022-08-05 23:25:46 +0100

    add stale tvl script

--- defi/src/cli/check6moDrops.ts
+++ defi/src/cli/check6moDrops.ts
@@ -3,18 +3,23 @@ import { toUNIXTimestamp } from "../utils/date";
 import { dailyTvl, getLastRecord, hourlyTvl } from "../utils/getLastRecord";
 import getTVLOfRecordClosestToTimestamp from "../utils/shared/getRecordClosestToTimestamp";
 
-const old6moTimestamp = toUNIXTimestamp(Date.now() - 1e3*3600*24*30*6);
+const months = 6;
+const timestampToCompareWith = toUNIXTimestamp(Date.now() - 1e3*3600*24*30*months);
+
 
 const main = async () => {
     const changes = (await Promise.all(protocols.map(async protocol=>{
         const current = await getLastRecord(hourlyTvl(protocol.id))
-        const old = await getTVLOfRecordClosestToTimestamp(dailyTvl(protocol.id), old6moTimestamp, 3600*24)
+        const old = await getTVLOfRecordClosestToTimestamp(dailyTvl(protocol.id), timestampToCompareWith, 3600*24)
         if(old.SK === undefined || current === undefined || old.tvl === 0){
             return null
         }
         const difference = (current.tvl - old.tvl)/old.tvl * 100
+        // Remove the following line if you just want a full list of differences
+        if(Math.abs(difference)>5 || current.tvl < 1e6) return null
         return [protocol.name, difference] as [string, number]
     }))).filter(p=>p!=null).sort((a,b)=>a![1]-b![1])
     changes.map(c=>console.log(c![1].toFixed(2) + "\t", c![0]))
+    console.log("Total:", changes.length)
 };
 main();

cd6ce2d3415d21195e1ff4d5d86f92bfdde13444
0xngmi <0xngmi@protonmail.com>
2022-08-13 16:37:32 +0100

    add script for refilling coin adapter

---
+++ coins/src/cli/refillAdapter.ts
@@ -0,0 +1,21 @@
+require("dotenv").config()
+import adapters from "../adapters/index";
+import { batchWrite } from "../utils/shared/dynamodb";
+
+const adapterName = "curve";
+const timestamp = 1660313930;
+
+async function main() {
+    const adapter = adapters[adapterName][adapterName];
+    if (adapter === undefined) {
+        throw new Error(`Adapter "${adapterName}" doesn't exist`)
+    }
+    if (process.env.HISTORICAL !== "true") {
+        throw new Error("You must set env variable HISTORICAL to 'true'")
+    }
+
+    const results: any[] = await adapter(timestamp);
+    await batchWrite(results.flat(), true);
+}
+
+main();

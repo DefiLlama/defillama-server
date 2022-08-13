90114b200ac08a495e7d4dd427b0a228416cfae9
0xngmi <0xngmi@protonmail.com>
2022-08-13 16:08:04 +0100

    disable curve adapter

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -15,7 +15,7 @@ export default {
   scream: require("./moneyMarkets/compound/index"),
 
   yearn: require("./yield/yearn/index"),
-  curve: require("./lps/curve/index"),
+  //curve: require("./lps/curve/index"),
 
   // uniswap: require("./lps/uniswap/index"),
   // traderJoe: require("./lps/uniswap/index"),

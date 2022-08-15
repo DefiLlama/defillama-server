adc07350d57e70f40fee835c2686e2713faa68b8
0xngmi <0xngmi@protonmail.com>
2022-08-15 11:08:08 +0100

    delist elipsis

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -2621,6 +2621,7 @@ const data: Protocol[] = [
     audit_links: ["https://www.certik.org/projects/swipe"],
     oracles: ["Chainlink","Band"],
   },
+/*
   {
     id: "238",
     name: "Ellipsis Finance",
@@ -2643,6 +2644,7 @@ const data: Protocol[] = [
     ],
     forkedFrom: ["Curve"],
   },
+*/
   {
     id: "239",
     name: "PoolTogether",

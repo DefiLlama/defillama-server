de16b16d5bef72aaf29fa44063e178dd9e96b66d
waynebruce0x <waynebruce0x@gmail.com>
2022-08-22 19:36:23 +0100

    polygon

--- coins/src/adapters/lps/curve/curve.ts
+++ coins/src/adapters/lps/curve/curve.ts
@@ -27,9 +27,6 @@ const registryIds = {
   crypto: 5,
   cryptoFactory: 6
 };
-let unknownTokensList: string[] = [];
-let unknownPoolList: any[] = [];
-const writes: Write[] = [];
 async function getPools(chain: string, block: number | undefined) {
   const registries: string[] =
     chain == "bsc"
@@ -180,93 +177,105 @@ async function PoolToToken(
       })
     ).output.toLowerCase();
   } catch {
-    const mapping: { [key: string]: any } = {
-      // pool contract : token contract
-      "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7":
-        "0x6c3f90f043a72fa612cbac8115ee7e52bde6e490",
-      "0x79a8c46dea5ada233abaffd40f3a0a2b1e5a4f27":
-        "0x3b3ac5386837dc563660fb6a0937dfaa5924333b",
-      "0xa2b47e3d5c44877cca798226b7b8118f9bfb7a56":
-        "0x845838df265dcd2c412a1dc9e959c7d08537f8a2",
-      "0x4ca9b3063ec5866a4b82e437059d2c43d1be596f":
-        "0xb19059ebb43466c323583928285a49f558e572fd",
-      "0x06364f10b501e868329afbc005b3492902d6c763":
-        "0xd905e2eaebe188fc92179b6350807d8bd91db0d8",
-      "0x93054188d876f558f4a66b2ef1d97d16edf0895b":
-        "0x49849c98ae39fff122806c06791fa73784fb3675",
-      "0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714":
-        "0x075b1bb99792c9e1041ba13afef80c91a1e70fb3",
-      "0xc5424b857f758e906013f3555dad202e4bdb4567":
-        "0xa3d87fffce63b53e0d54faa1cc983b7eb0b74a9c",
-      "0xa5407eae9ba41422680e2e00537571bcc53efbfd":
-        "0xc25a3a3b969415c80451098fa907ec722572917f",
-      "0x52ea46506b9cc5ef470c5bf89f17dc28bb35d85c":
-        "0x9fc689ccada600b6df723d9e47d84d76664a1f23",
-      "0x45f783cce6b7ff23b2ab2d70e416cdb7d6055f51":
-        "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8",
-      "0x8038c01a0390a8c547446a0b2c18fc9aefecc10c":
-        "0x3a664ab939fd8482048609f652f9a0b0677337b9",
-      "0x4f062658eaaf2c1ccf8c8e36d6824cdf41167956":
-        "0xd2967f45c4f384deea880f807be904762a3dea07",
-      "0x3ef6a01a0f81d6046290f3e2a8c5b843e738e604":
-        "0x5b5cfe992adac0c9d48e05854b2d91c73a003858",
-      "0xe7a24ef0c5e95ffb0f6684b813a78f2a3ad7d171":
-        "0x6d65b498cb23deaba52db31c93da9bffb340fb8f",
-      "0x8474ddbe98f5aa3179b3b3f5942d724afcdec9f6":
-        "0x1aef73d49dedc4b1778d0706583995958dc862e6",
-      "0xc18cc39da8b11da8c3541c598ee022258f9744da":
-        "0xc2ee6b0334c261ed60c72f6054450b61b8f18e35",
-      "0x3e01dd8a5e1fb3481f0f589056b428fc308af0fb":
-        "0x97e2768e8e73511ca874545dc5ff8067eb19b787",
-      "0x0f9cb53ebe405d49a0bbdbd291a65ff571bc83e1":
-        "0x4f3e8f405cf5afc05d68142f3783bdfe13811522",
-      "0x890f4e345b1daed0367a877a1612f86a1f86985f":
-        "0x94e131324b6054c0d789b190b2dac504e4361b53",
-      "0x071c661b4deefb59e2a3ddb20db036821eee8f4b":
-        "0x410e3e86ef427e30b9235497143881f717d93c2a",
-      "0xd81da8d904b52208541bade1bd6595d8a251f8dd":
-        "0x2fe94ea3d5d4a175184081439753de15aef9d614",
-      "0x7f55dde206dbad629c080068923b36fe9d6bdbef":
-        "0xde5331ac4b3630f94853ff322b66407e0d6331e8",
-      "0xc25099792e9349c7dd09759744ea681c7de2cb66":
-        "0x64eda51d3ad40d56b9dfc5554e06f94e1dd786fd",
-      "0x80466c64868e1ab14a1ddf27a676c3fcbe638fe5":
-        "0xca3d75ac011bf5ad07a98d02f18225f9bd9a6bdf",
-      "0xd51a44d3fae010294c616388b506acda1bfaae46":
-        "0xc4ad29ba4b3c580e6d59105fff484999997675ff",
-      "0x9838eccc42659fa8aa7daf2ad134b53984c9427b":
-        "0x3b6831c0077a1e44ed0a21841c3bc4dc11bce833",
-      "0x98a7f18d4e56cfe84e3d081b40001b3d5bd3eb8b":
-        "0x3d229e1b4faab62f621ef2f6a610961f7bd7b23b",
-      "0x8301ae4fc9c624d1d396cbdaa1ed877821d7c511":
-        "0xed4064f376cb8d68f770fb1ff088a3d0f3ff5c4d",
-      "0xb576491f1e6e5e62f1d8f26062ee822b40b0e0d4":
-        "0x3a283d9c08e8b55966afb64c515f5143cf907611",
-      "0xadcfcf9894335dc340f6cd182afa45999f45fc44":
-        "0x8484673ca7bff40f82b041916881aea15ee84834",
-      "0x98638facf9a3865cd033f36548713183f6996122":
-        "0x8282bd15dca2ea2bdf24163e8f2781b30c43a2ef",
-      "0x752ebeb79963cf0732e9c0fec72a49fd1defaeac":
-        "0xcb08717451aae9ef950a2524e33b6dcaba60147b",
-      "0xe84f5b1582ba325fdf9ce6b0c1f087ccfc924e54":
-        "0x70fc957eb90e37af82acdbd12675699797745f68",
-      "0x3a1659ddcf2339be3aea159ca010979fb49155ff":
-        "0x58e57ca18b7a47112b877e31929798cd3d703b0f",
-      "0x960ea3e3c7fb317332d990873d354e18d7645590":
-        "0x8e0b8c8bb9db49a46697f3a5bb8a308e744821d2",
-      "0xa827a652ead76c6b0b3d19dba05452e06e25c27e":
-        "0x3dfe1324a0ee9d86337d06aeb829deb4528db9ca",
-      "0xb755b949c126c04e0348dd881a5cf55d424742b2":
-        "0x1dab6560494b04473a0be3e7d83cf3fdf3a51828"
-    };
-    token = mapping[pool] ? mapping[pool] : pool;
+    token = (
+      await call({
+        target: pool,
+        abi: abi.token,
+        chain: chain as any,
+        block
+      })
+    ).output.toLowerCase();
+    try {
+    } catch {
+      const mapping: { [key: string]: any } = {
+        // pool contract : token contract
+        "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7":
+          "0x6c3f90f043a72fa612cbac8115ee7e52bde6e490",
+        "0x79a8c46dea5ada233abaffd40f3a0a2b1e5a4f27":
+          "0x3b3ac5386837dc563660fb6a0937dfaa5924333b",
+        "0xa2b47e3d5c44877cca798226b7b8118f9bfb7a56":
+          "0x845838df265dcd2c412a1dc9e959c7d08537f8a2",
+        "0x4ca9b3063ec5866a4b82e437059d2c43d1be596f":
+          "0xb19059ebb43466c323583928285a49f558e572fd",
+        "0x06364f10b501e868329afbc005b3492902d6c763":
+          "0xd905e2eaebe188fc92179b6350807d8bd91db0d8",
+        "0x93054188d876f558f4a66b2ef1d97d16edf0895b":
+          "0x49849c98ae39fff122806c06791fa73784fb3675",
+        "0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714":
+          "0x075b1bb99792c9e1041ba13afef80c91a1e70fb3",
+        "0xc5424b857f758e906013f3555dad202e4bdb4567":
+          "0xa3d87fffce63b53e0d54faa1cc983b7eb0b74a9c",
+        "0xa5407eae9ba41422680e2e00537571bcc53efbfd":
+          "0xc25a3a3b969415c80451098fa907ec722572917f",
+        "0x52ea46506b9cc5ef470c5bf89f17dc28bb35d85c":
+          "0x9fc689ccada600b6df723d9e47d84d76664a1f23",
+        "0x45f783cce6b7ff23b2ab2d70e416cdb7d6055f51":
+          "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8",
+        "0x8038c01a0390a8c547446a0b2c18fc9aefecc10c":
+          "0x3a664ab939fd8482048609f652f9a0b0677337b9",
+        "0x4f062658eaaf2c1ccf8c8e36d6824cdf41167956":
+          "0xd2967f45c4f384deea880f807be904762a3dea07",
+        "0x3ef6a01a0f81d6046290f3e2a8c5b843e738e604":
+          "0x5b5cfe992adac0c9d48e05854b2d91c73a003858",
+        "0xe7a24ef0c5e95ffb0f6684b813a78f2a3ad7d171":
+          "0x6d65b498cb23deaba52db31c93da9bffb340fb8f",
+        "0x8474ddbe98f5aa3179b3b3f5942d724afcdec9f6":
+          "0x1aef73d49dedc4b1778d0706583995958dc862e6",
+        "0xc18cc39da8b11da8c3541c598ee022258f9744da":
+          "0xc2ee6b0334c261ed60c72f6054450b61b8f18e35",
+        "0x3e01dd8a5e1fb3481f0f589056b428fc308af0fb":
+          "0x97e2768e8e73511ca874545dc5ff8067eb19b787",
+        "0x0f9cb53ebe405d49a0bbdbd291a65ff571bc83e1":
+          "0x4f3e8f405cf5afc05d68142f3783bdfe13811522",
+        "0x890f4e345b1daed0367a877a1612f86a1f86985f":
+          "0x94e131324b6054c0d789b190b2dac504e4361b53",
+        "0x071c661b4deefb59e2a3ddb20db036821eee8f4b":
+          "0x410e3e86ef427e30b9235497143881f717d93c2a",
+        "0xd81da8d904b52208541bade1bd6595d8a251f8dd":
+          "0x2fe94ea3d5d4a175184081439753de15aef9d614",
+        "0x7f55dde206dbad629c080068923b36fe9d6bdbef":
+          "0xde5331ac4b3630f94853ff322b66407e0d6331e8",
+        "0xc25099792e9349c7dd09759744ea681c7de2cb66":
+          "0x64eda51d3ad40d56b9dfc5554e06f94e1dd786fd",
+        "0x80466c64868e1ab14a1ddf27a676c3fcbe638fe5":
+          "0xca3d75ac011bf5ad07a98d02f18225f9bd9a6bdf",
+        "0xd51a44d3fae010294c616388b506acda1bfaae46":
+          "0xc4ad29ba4b3c580e6d59105fff484999997675ff",
+        "0x9838eccc42659fa8aa7daf2ad134b53984c9427b":
+          "0x3b6831c0077a1e44ed0a21841c3bc4dc11bce833",
+        "0x98a7f18d4e56cfe84e3d081b40001b3d5bd3eb8b":
+          "0x3d229e1b4faab62f621ef2f6a610961f7bd7b23b",
+        "0x8301ae4fc9c624d1d396cbdaa1ed877821d7c511":
+          "0xed4064f376cb8d68f770fb1ff088a3d0f3ff5c4d",
+        "0xb576491f1e6e5e62f1d8f26062ee822b40b0e0d4":
+          "0x3a283d9c08e8b55966afb64c515f5143cf907611",
+        "0xadcfcf9894335dc340f6cd182afa45999f45fc44":
+          "0x8484673ca7bff40f82b041916881aea15ee84834",
+        "0x98638facf9a3865cd033f36548713183f6996122":
+          "0x8282bd15dca2ea2bdf24163e8f2781b30c43a2ef",
+        "0x752ebeb79963cf0732e9c0fec72a49fd1defaeac":
+          "0xcb08717451aae9ef950a2524e33b6dcaba60147b",
+        "0xe84f5b1582ba325fdf9ce6b0c1f087ccfc924e54":
+          "0x70fc957eb90e37af82acdbd12675699797745f68",
+        "0x3a1659ddcf2339be3aea159ca010979fb49155ff":
+          "0x58e57ca18b7a47112b877e31929798cd3d703b0f",
+        "0x960ea3e3c7fb317332d990873d354e18d7645590":
+          "0x8e0b8c8bb9db49a46697f3a5bb8a308e744821d2",
+        "0xa827a652ead76c6b0b3d19dba05452e06e25c27e":
+          "0x3dfe1324a0ee9d86337d06aeb829deb4528db9ca",
+        "0xb755b949c126c04e0348dd881a5cf55d424742b2":
+          "0x1dab6560494b04473a0be3e7d83cf3fdf3a51828"
+      };
+      token = mapping[pool] ? mapping[pool] : pool;
+    }
   }
   return token;
 }
 async function getUnderlyingPrices(
   balances: any,
   chain: string,
-  timestamp: number
+  timestamp: number,
+  unknownTokensList: any[]
 ) {
   const coinsData: Read[] = await getTokenAndRedirectData(
     balances.map((r: Result) => r.input.target.toLowerCase()),
@@ -310,16 +319,17 @@ async function unknownPools(
   timestamp: number,
   poolList: any,
   registries: string[],
-  writes: Write[]
+  writes: Write[],
+  unknownPoolList: any[],
+  unknownTokensList: any[]
 ) {
-  //["cryptoFactory"]) {
   for (let registry of registries) {
     //Object.keys(poolList)) {
     for (let pool of Object.values(poolList[registry])) {
       i++;
       console.log(i);
       try {
-        if (i == 17) {
+        if (i == 5) {
           console.log("here");
         }
         const token: string = await PoolToToken(chain, pool, block);
@@ -331,7 +341,8 @@ async function unknownPools(
         const poolTokens: any[] = await getUnderlyingPrices(
           balances,
           chain,
-          timestamp
+          timestamp,
+          unknownTokensList
         );
         if (poolTokens.includes(undefined) || poolTokens.length == 0) {
           unknownPoolList.push({
@@ -388,7 +399,8 @@ async function unknownTokens(
   chain: string,
   block: number | undefined,
   writes: Write[],
-  timestamp: number
+  timestamp: number,
+  unknownPoolList: any[]
 ) {
   const usdSwapSize = 5 * 10 ** 5;
   const knownTokenIndexes: TokenIndexes[] = unknownPoolList.map((p: any) => {
@@ -474,9 +486,20 @@ export default async function getTokenPrices(
   const block: number | undefined = await getBlock(chain, timestamp);
   const poolList = await getPools(chain, block);
   const writes: Write[] = [];
+  let unknownTokensList: string[] = [];
+  let unknownPoolList: any[] = [];
 
-  await unknownPools(chain, block, timestamp, poolList, registries, writes);
+  await unknownPools(
+    chain,
+    block,
+    timestamp,
+    poolList,
+    registries,
+    writes,
+    unknownPoolList,
+    unknownTokensList
+  );
   //await listUnknownTokens(chain, unknownTokensList, block);
-  await unknownTokens(chain, block, writes, timestamp);
+  await unknownTokens(chain, block, writes, timestamp, unknownPoolList);
   return writes;
 }

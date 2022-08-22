718af4173e2f489f6d18c6b17c08fa2af8f8d6f9
Define101 <Lovelovenow@protonmail.com>
2022-08-22 13:18:48 +0100

    fix?

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -12995,7 +12995,7 @@ listedAt: 1650804679
   gecko_id: null,
   cmcId: null,
   category: "Dexes",
-  chains: ["Godwoken_V1"],
+  chains: ["GodwokenV1"],
   module: "jioswap/index.js",
   twitter: "jioswap",
   forkedFrom: ["Saddle Finance"],

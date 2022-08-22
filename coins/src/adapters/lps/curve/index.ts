92d4068056953156a0b17f020cac22c33b17e1cb
waynebruce0x <waynebruce0x@gmail.com>
2022-08-22 10:11:57 +0100

    debug

--- coins/src/adapters/lps/curve/index.ts
+++ coins/src/adapters/lps/curve/index.ts
@@ -22,38 +22,27 @@ export function curve3(timestamp: number = 0) {
     getTokenPrices("fantom", ["stableswap", "crypto"], timestamp)
   ]);
 }
-export function curve3b(timestamp: number = 0) {
-  console.log("starting curve3");
-  return Promise.all([
-    getTokenPrices("avax", ["stableswap", "crypto"], timestamp)
-    //getTokenPrices("fantom", ["stableswap", "crypto"], timestamp)
-  ]);
-}
 export function curve4(timestamp: number = 0) {
   console.log("starting curve4");
   return Promise.all([
     getTokenPrices("moonbeam", ["stableswap", "crypto"], timestamp),
     getTokenPrices("aurora", ["stableswap", "crypto"], timestamp),
-    getTokenPrices("optimism", ["stableswap", "crypto"], timestamp),
     getTokenPrices("moonbeam", ["stableFactory", "cryptoFactory"], timestamp),
-    getTokenPrices("aurora", ["stableFactory", "cryptoFactory"], timestamp),
+    getTokenPrices("aurora", ["stableFactory", "cryptoFactory"], timestamp)
+  ]);
+}
+export function curve4b(timestamp: number = 0) {
+  console.log("starting curve4b");
+  return Promise.all([
+    getTokenPrices("optimism", ["stableswap", "crypto"], timestamp),
     getTokenPrices("optimism", ["stableFactory", "cryptoFactory"], timestamp)
   ]);
 }
 export function curve5(timestamp: number = 0) {
   console.log("starting curve5");
   return Promise.all([
-    getTokenPrices("arbitrum", ["stableswap", "crypto"], timestamp)
-    // getTokenPrices("polygon", timestamp),
-    // getTokenPrices("xdai", ["stableswap", "crypto"], timestamp)
-  ]);
-}
-export function curve5b(timestamp: number = 0) {
-  console.log("starting curve5b");
-  return Promise.all([
-    // getTokenPrices("arbitrum", ["stableswap", "crypto"], timestamp)
-    // getTokenPrices("polygon", timestamp),
-    getTokenPrices("xdai", ["stableswap", "crypto"], timestamp)
+    getTokenPrices("avax", ["stableswap", "crypto"], timestamp)
+    //getTokenPrices("fantom", ["stableswap", "crypto"], timestamp)
   ]);
 }
 export function curve6(timestamp: number = 0) {

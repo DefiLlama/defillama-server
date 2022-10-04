367ab4a2dd66609d6fe44aee3375cb517d8ddbf1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-04 14:15:22 +0200

    update XSwap description

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -16023,7 +16023,7 @@ listedAt: 1650804679
   address: "xdc:0x36726235dadbdb4658d33e62a249dca7c4b2bc68",
   symbol: "XSP",
   url: "https://app.xspswap.finance",
-  description: "XSwap Protocol is the 1st AMM on XDC Network powered by XDC.",
+  description: "XSWAP is the first decentralized exchange (DEX) that utilizes an automated market maker (AMM) system on the Xinfin network and is powered by XDC. The vision of XSWAP is to grow and expand the Xinfin network. It allows the swap and exchange of XRC20 tokens and offers staking & yield farming. XSwap is also home to the first launchpad in the XDC network which offers token creation, presale creation, token & liquidity locker and multisender for all XRC20 tokens.",
   chain: "XDC",
   logo: `${baseIconsUrl}/xswap-protocol.png`,
   audits: "2",

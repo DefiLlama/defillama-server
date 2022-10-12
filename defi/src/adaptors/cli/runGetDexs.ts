788fef9c02af90a1c50cb6f805a271b445cae9d3
0xtawa <0xtawanofi@gmail.com>
2022-10-12 13:06:07 +0200

    Average record data

--- defi/src/adaptors/cli/runGetDexs.ts
+++ defi/src/adaptors/cli/runGetDexs.ts
@@ -1,15 +1,19 @@
 import "./setup.ts"
 import { handler, IGetDexsResponseBody } from "../handlers/getDexs";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { formatTimestampAsDate } from "../../utils/date";
 
 const event = {
-    pathParameters: { chain: "ethereum", type: "volumes" }
+    pathParameters: { chain: undefined, type: "volumes" }
 } as unknown as APIGatewayProxyEvent
 
 (async () => {
     const r = await handler(event)
-    /* const rr = JSON.parse(r.body) as IGetDexsResponseBody
-    console.log(rr) */
+    const rr = JSON.parse(r.body) as IGetDexsResponseBody
+/* 
+    for (const [time, datapoint] of rr.totalDataChartBreakdown) {
+        console.log(formatTimestampAsDate(time), datapoint)
+    } */
     /*     console.log("totalVolume", rr.totalVolume)
         console.log("changeVolume1d", rr.changeVolume1d)
         console.log("changeVolume7d", rr.changeVolume7d)

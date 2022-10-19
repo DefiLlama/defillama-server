c6d21a0521be404702312cb58b348e9f057db1da
waynebruce0x <waynebruce0x@gmail.com>
2022-10-19 11:11:56 +0100

    finish bridge work

--- coins/src/adapters/bridges/solana.ts
+++ coins/src/adapters/bridges/solana.ts
@@ -1,32 +1,38 @@
-import { fetch } from "../utils"
+import { fetch } from "../utils";
 
 interface Token {
-    "chainId": 101,
-    "address": string,
-    "symbol": string,
-    "name": string,
-    "decimals": number,
-    "logoURI": string,
-    "tags": string[],
-    "extensions": {
-        [id: string]: string
-    } | undefined
+  chainId: 101;
+  address: string;
+  symbol: string;
+  name: string;
+  decimals: number;
+  logoURI: string;
+  tags: string[];
+  extensions:
+    | {
+        [id: string]: string;
+      }
+    | undefined;
 }
 
 export default async function bridge() {
-    const tokenlist = (await fetch(
-        "https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json"
-    )).tokens as Token[];
-    return tokenlist.map(token => {
-        const coingeckoId = token.extensions?.coingeckoId;
-        if(coingeckoId === undefined){
-            return null
-        }
-        return {
-            from: `solana:${token.address}`,
-            to: `coingecko#${coingeckoId}`,
-            symbol: token.symbol,
-            decimals: token.decimals
-        }
-    }).filter(t=>t!==null)
+  const tokenlist = (
+    await fetch(
+      "https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json"
+    )
+  ).tokens as Token[];
+
+  const tokens: any[] = [];
+  tokenlist.map((token) => {
+    const coingeckoId = token.extensions?.coingeckoId;
+    if (coingeckoId === undefined) return;
+    tokens.push({
+      from: `solana:${token.address}`,
+      to: `coingecko#${coingeckoId}`,
+      symbol: token.symbol,
+      decimals: token.decimals
+    });
+  });
+
+  return tokens;
 }

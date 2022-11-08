386c9037890a54ba6eb856ec331a66f099141488
Define101 <Lovelovenow@protonmail.com>
2022-11-08 15:17:43 +0000

    change category yapeswap

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -7441,7 +7441,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield Aggregator",
+    category: "Dexes",
     chains: ["Ethereum"],
     module: "yapeswap/index.js",
     twitter: "yapeswap",

1e9384ebe7450dc3e7efed52f64d991aee8b5ce5
0xtawa <0xtawanofi@gmail.com>
2022-11-11 16:12:30 +0100

    End simulation

--- defi/src/adaptors/handlers/storeAdaptorData/index.ts
+++ defi/src/adaptors/handlers/storeAdaptorData/index.ts
@@ -90,8 +90,8 @@ export const handler = async (event: IHandlerEvent) => {
 
       // Store records // TODO: Change to run in parallel
       for (const [recordType, record] of Object.entries(rawRecords)) {
-        console.log("Simulation:", event.adaptorType, recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType)
-        // await storeAdaptorRecord(new AdaptorRecord(recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType), LAMBDA_TIMESTAMP)
+        console.log(event.adaptorType, recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType)
+        await storeAdaptorRecord(new AdaptorRecord(recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType), LAMBDA_TIMESTAMP)
       }
     }
     catch (error) {

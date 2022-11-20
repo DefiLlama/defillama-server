171055d8288064e77535a69454243f71aad83ab5
0xtawa <0xtawanofi@gmail.com>
2022-11-20 22:15:06 +0100

    Test storeAdaptorData

--- defi/src/adaptors/data/protocols/index.ts
+++ defi/src/adaptors/data/protocols/index.ts
@@ -1,18 +1,13 @@
 import generateProtocolAdaptorsList from "../helpers/generateProtocolAdaptorsList";
 import protocols_imports from "../../../utils/imports/protocols_adapters"
 import config from "./config";
-import { AdaptorRecordType } from "../../db-utils/adaptor-record";
+import { AdaptorRecordType, AdaptorRecordTypeMapReverse } from "../../db-utils/adaptor-record";
 
 // TODO: needs to be optimized. Currently loads to memory all adaptors
 export const importModule = (module: string) => protocols_imports[module]
 
 // KEYS USED TO MAP ATTRIBUTE -> KEY IN DYNAMO
-export const KEYS_TO_STORE = {
-    [AdaptorRecordType.totalPremiumVolume]: "totalPremiumVolume",
-    [AdaptorRecordType.totalNotionalVolume]: "totalNotionalVolume",
-    [AdaptorRecordType.dailyPremiumVolume]: "dailyPremiumVolume",
-    [AdaptorRecordType.dailyNotionalVolume]: "dailyNotionalVolume"
-}
+export const KEYS_TO_STORE = AdaptorRecordTypeMapReverse
 
 export { default as config } from "./config";
 
--- defi/src/adaptors/handlers/storeAdaptorData/index.ts
+++ defi/src/adaptors/handlers/storeAdaptorData/index.ts
@@ -6,9 +6,10 @@ import allSettled from 'promise.allsettled'
 import runAdapter, { getFulfilledResults, getRejectedResults } from "@defillama/adaptors/adapters/utils/runAdapter";
 import { getBlock } from "@defillama/adaptors/helpers/getBlock";
 import { Chain } from "@defillama/sdk/build/general";
-import { AdaptorRecord, AdaptorRecordType, RawRecordMap, storeAdaptorRecord } from "../../db-utils/adaptor-record";
+import { AdaptorRecord, AdaptorRecordType, AdaptorRecordTypeMapReverse, RawRecordMap, storeAdaptorRecord } from "../../db-utils/adaptor-record";
 import { processFulfilledPromises, processRejectedPromises, STORE_ERROR } from "./helpers";
 import loadAdaptorsData from "../../data"
+import { IJSON } from "../../data/types";
 
 // Runs a little bit past each hour, but calls function with timestamp on the hour to allow blocks to sync for high throughput chains. Does not work for api based with 24/hours
 
@@ -16,6 +17,9 @@ export interface IHandlerEvent {
   protocolIndexes: number[]
   timestamp?: number
   adaptorType: AdapterType
+  chain?: Chain
+  adaptorRecordTypes?: string[]
+  protocolVersion?: string
 }
 
 const LAMBDA_TIMESTAMP = Math.trunc((Date.now()) / 1000)
@@ -35,10 +39,11 @@ export const handler = async (event: IHandlerEvent) => {
   const { importModule, KEYS_TO_STORE } = dataModule
 
   // Get list of adaptors to run
-  const adaptorsList = event.protocolIndexes.map(index => dataList[index])
+  const adaptorsList = event.protocolIndexes.map(index => dataList[index]).filter(p => p !== undefined)
 
   // Get closest block to clean day. Only for EVM compatible ones.
-  const allChains = adaptorsList.reduce((acc, { chains }) => {
+  console.log("adaptorsList", adaptorsList)
+  const allChains = event.chain ? [event.chain] : adaptorsList.reduce((acc, { chains }) => {
     acc.push(...chains as Chain[])
     return acc
   }, [] as Chain[]).filter(canGetBlock)
@@ -69,29 +74,39 @@ export const handler = async (event: IHandlerEvent) => {
         adaptersToRun.push([module, adaptor.adapter])
       } else if ("breakdown" in adaptor) {
         const dexBreakDownAdapter = adaptor.breakdown
-        const breakdownAdapters = Object.entries(dexBreakDownAdapter)
+        const breakdownAdapters = Object.entries(dexBreakDownAdapter).filter(([version]) => !event.protocolVersion || version === event.protocolVersion)
         for (const [version, adapter] of breakdownAdapters) {
-          adaptersToRun.push([version, adapter])
+          adaptersToRun.push([
+            version,
+            Object.keys(adapter).reduce((acc, chain) => {
+              if (event.chain && event.chain !== chain) delete acc[chain]
+              return acc
+            }, adapter)
+          ])
         }
       } else {
         throw new Error("Invalid adapter")
       }
 
       // Run adapters // TODO: Change to run in parallel
+      const FILTRED_KEYS_TO_STORE = event.adaptorRecordTypes?.reduce((acc, curr) => {
+        acc[AdaptorRecordTypeMapReverse[curr]] = curr
+        return acc
+      }, {} as IJSON<string>) ?? KEYS_TO_STORE
       const rawRecords: RawRecordMap = {}
       for (const [version, adapter] of adaptersToRun) {
         const runAdapterRes = await runAdapter(adapter, cleanCurrentDayTimestamp, chainBlocks)
         const fulfilledResults = getFulfilledResults(runAdapterRes)
-        processFulfilledPromises(fulfilledResults, rawRecords, version, KEYS_TO_STORE)
+        processFulfilledPromises(fulfilledResults, rawRecords, version, FILTRED_KEYS_TO_STORE)
         const rejectedResults = getRejectedResults(runAdapterRes)
         // Make sure rejected ones are also included in rawRecords
-        processRejectedPromises(rejectedResults, rawRecords, module, KEYS_TO_STORE)
+        processRejectedPromises(rejectedResults, rawRecords, module, FILTRED_KEYS_TO_STORE)
       }
 
       // Store records // TODO: Change to run in parallel
       for (const [recordType, record] of Object.entries(rawRecords)) {
         console.log(event.adaptorType, recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType)
-        await storeAdaptorRecord(new AdaptorRecord(recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType), LAMBDA_TIMESTAMP)
+        // await storeAdaptorRecord(new AdaptorRecord(recordType as AdaptorRecordType, id, cleanPreviousDayTimestamp, record, adaptor.protocolType), LAMBDA_TIMESTAMP)
       }
     }
     catch (error) {

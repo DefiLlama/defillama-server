3ce6272858a7f1bf78f1fa18a226fff0c4d57693
0xngmi <0xngmi@protonmail.com>
2022-11-24 15:05:10 +0000

    add new tokenProtocols endpoint

---
+++ defi/src/getTokenInProtocols.ts
@@ -0,0 +1,30 @@
+import { successResponse, wrap, IResponse } from "./utils/shared";
+import protocols from "./protocols/data";
+import { getLastRecord, hourlyUsdTokensTvl } from "./utils/getLastRecord";
+
+const handler = async (
+  event: AWSLambda.APIGatewayEvent
+): Promise<IResponse> => {
+  const symbol = decodeURI(event.pathParameters?.symbol?.toLowerCase() ?? "");
+  const protocolsIncluded = await Promise.all(
+    protocols.map(async (protocol) => {
+      const [lastTvl, module] = await Promise.all([
+        getLastRecord(hourlyUsdTokensTvl(protocol.id)),
+        importAdapter(protocol),
+      ]);
+      const amountUsd = lastTvl?.tvl[symbol]
+      if(amountUsd === undefined){
+        return null
+      } else {
+        return {
+            name: protocol.name,
+            amountUsd,
+            misrepresentedTokens: module.misrepresentedTokens
+        }
+      }
+    })
+  )
+  return successResponse(protocolsIncluded, 10 * 60); // 10 mins cache
+};
+
+export default wrap(handler);

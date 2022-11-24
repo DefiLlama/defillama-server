c10e38bf3edeb6684cb52541626989c4cfc181c9
0xtawa <0xtawanofi@gmail.com>
2022-11-24 09:16:52 +0100

    Remove all references of old dexs adapters repo

--- defi/src/dexVolumes/cli/others/removeDexVolumes.ts
+++
@@ -1,9 +0,0 @@
-import "./../setup.ts"
-import { removeVolume, VolumeType } from "../../data/volume"
-
-(async () => {
-    console.log("Removing...")
-    const ok = await removeVolume("", VolumeType.dailyVolume)
-    const ok2 = await removeVolume("", VolumeType.totalVolume)
-    console.log(ok, ok2)
-})()

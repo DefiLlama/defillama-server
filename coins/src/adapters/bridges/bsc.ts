fad531cfbc5c1fd9fad4bb17de51e2943b6c48f2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-02 08:05:31 +0100

    HOTFIX for hacked aBnB token

--- coins/src/adapters/bridges/bsc.ts
+++ coins/src/adapters/bridges/bsc.ts
@@ -17,5 +17,6 @@ export default async function bridge() {
 
 const extraTokens = formatExtraTokens("bsc", [
     ["0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c", "avax:0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c", "ELK", 18],
-    ["0x2170ed0880ac9a755fd29b2688956bd959f933f8", "ethereum:0x0000000000000000000000000000000000000000", "ETH", 18]
+    ["0x2170ed0880ac9a755fd29b2688956bd959f933f8", "ethereum:0x0000000000000000000000000000000000000000", "ETH", 18],
+    ["0xe85afccdafbe7f2b096f268e31cce3da8da2990a", "bsc:0x123", "aBnBc", 18],   // HOTFIX for hacked aBnB token
 ])

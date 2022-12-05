a2c2659daafeecc4c4dbf628a80b55adca95e018
0xngmi <0xngmi@protonmail.com>
2022-12-05 23:49:44 +0000

    add chain to contract name

--- defi/src/getContractName.ts
+++ defi/src/getContractName.ts
@@ -1,11 +1,17 @@
 import { successResponse, wrap, IResponse } from "./utils/shared";
 import fetch from "node-fetch";
 
+const chain2url = {
+  ethereum: "etherscan.io",
+  polygon: "polygonscan.com",
+}
+
 const handler = async (
   event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
+  const chain = event.pathParameters?.chain?.toLowerCase();
   const contract = event.pathParameters?.contract?.toLowerCase();
-  const name = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contract}&apikey=YourApiKeyToken`).then(r=>r.json())
+  const name = await fetch(`https://api.${chain2url[chain]}/api?module=contract&action=getsourcecode&address=${contract}&apikey=YourApiKeyToken`).then(r=>r.json())
   return successResponse({
     name: name.result[0].ContractName
   }, 3* 60 * 60); // 3h

cecd2f0753511ebb779da46c3d4002e0f0e31586
waynebruce0x <waynebruce0x@gmail.com>
2022-12-14 13:31:33 +0000

    coins Tvl Flag

--- defi/src/storeTvlInterval/storeNewTokensValueLocked.ts
+++ defi/src/storeTvlInterval/storeNewTokensValueLocked.ts
@@ -1,12 +1,13 @@
 import dynamodb from "../utils/shared/dynamodb";
 import { Protocol } from "../protocols/data";
-import {
+import { 
   getDay,
   getTimestampAtStartOfDay,
   secondsInDay,
 } from "../utils/date";
 import { TokensValueLocked, tvlsObject } from "../types";
 import getTVLOfRecordClosestToTimestamp from "../utils/shared/getRecordClosestToTimestamp";
+import { sendMessage } from "../utils/discord";
 
 type PKconverted = (id: string) => string;
 
@@ -33,6 +34,10 @@ export default async (
     unixTimestamp,
     secondsInDay*1.5
   );
+
+  if (hourlyPK.includes("hourlyUsdTokensTvl"))
+    await checkForOutlierCoins(tvl, closestDailyRecord, protocol.name);
+
   if (getDay(closestDailyRecord?.SK) !== getDay(unixTimestamp)) {
     // First write of the day
     await dynamodb.put({
@@ -42,3 +47,33 @@ export default async (
     });
   }
 };
+async function checkForOutlierCoins(
+  currentTvls: tvlsObject<TokensValueLocked>,
+  previousTvls: tvlsObject<TokensValueLocked>,
+  protocol: string
+) {
+  const threshold = 0.05;
+  const promises: Promise<void>[] = [];
+
+  Object.keys(currentTvls).map((tvlKey) => {
+    Object.keys(currentTvls[tvlKey]).map((coinKey) => {
+      const currentCoinValue = currentTvls[tvlKey][coinKey];
+      if (!(tvlKey in previousTvls)) return;
+      if (!(coinKey in previousTvls[tvlKey])) return;
+
+      const previousCoinValue = previousTvls[tvlKey][coinKey];
+      const upperBound = previousCoinValue * (1 + threshold);
+      const lowerBound = previousCoinValue * (1 - threshold);
+
+      if (currentCoinValue > upperBound || currentCoinValue < lowerBound)
+        promises.push(
+          sendMessage(
+            `TEST ${coinKey.toUpperCase()} value is out of accepted range for ${tvlKey} on ${protocol}.`,
+            process.env.STALE_COINS_ADAPTERS_WEBHOOK!,
+            true
+          )
+        );
+    });
+  });
+  await Promise.all(promises);
+}

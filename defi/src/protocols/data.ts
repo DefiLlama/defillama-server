6439b418578d28d1c2ef49f436b9e9b426194916
realdealshaman <realdealshaman11@gmail.com>
2022-12-14 01:56:13 +0000

    update name

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -12870,7 +12870,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
   },
   {
     id: "753",
-    name: "RealT Token",
+    name: "RealT Tokens",
     address: null,
     symbol: "-",
     url: "https://realt.co",

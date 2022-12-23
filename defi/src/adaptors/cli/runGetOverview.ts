c120062437f09ec483b3ca5e829bc27d3d89b3cd
0xtawa <0xtawanofi@gmail.com>
2022-12-23 17:39:14 +0100

    Fix clean data function

--- defi/src/adaptors/cli/runGetOverview.ts
+++ defi/src/adaptors/cli/runGetOverview.ts
@@ -6,10 +6,10 @@ import { performance } from "perf_hooks";
 
 const event = {
     pathParameters: { chain: undefined, type: "fees" },
-    queryStringParameters: {
+    /* queryStringParameters: {
         excludeTotalDataChart: "true",
         excludeTotalDataChartBreakdown: "true"
-    }
+    } */
 } as unknown as APIGatewayProxyEvent
 
 (async () => {
@@ -18,14 +18,17 @@ const event = {
     var endTime = performance.now()
     const rr = JSON.parse(r.body) as IGetOverviewResponseBody
     // console.log(rr.protocols.filter(p=>p.name.toLowerCase().includes('jupiter')))
-    console.log(JSON.stringify(rr.protocols.find(p=>p.module==='gmx'), null, 2))
+    // console.log(JSON.stringify(rr.protocols.find(p=>p.module==='gmx'), null, 2))
     // console.log("rr.totalDataChartBreakdown->", rr.totalDataChartBreakdown.slice(-1))
-    console.log("length", rr.protocols.length)
+    // console.log("length", rr.protocols.length)
     // @ts-ignore
-    delete rr.protocols
-    console.log(rr)
+    // delete rr.protocols
+    // console.log(rr)
+    console.log("rr.totalDataChart", rr.totalDataChart.find(d => +d[0] === Date.UTC(2021, 11, 15) / 1000))
+    console.log("rr.totalDataChartBreakdown", rr.totalDataChartBreakdown.find(d => +d[0] === Date.UTC(2021, 11, 15) / 1000))
     // console.log(rr.protocols.filter(name=>name.name.toLowerCase().includes("uniswap") || name.name.toLowerCase().includes("pancakeswap")))
     // console.log(rr.protocols[0])
+    console.log(rr.totalDataChart.length)
     console.log("Current run:", (endTime - startTime) / 1000)
     /* for (const [time, datapoint] of rr.totalDataChartBreakdown) {
         console.log(formatTimestampAsDate(time), datapoint)

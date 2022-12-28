65a69118e5903a6d3e591d042a30e4542b8b1683
0xtawa <0xtawanofi@gmail.com>
2022-12-28 14:49:23 +0100

    Improve debug missing protocols

--- defi/src/adaptors/handlers/notifyStatus.ts
+++ defi/src/adaptors/handlers/notifyStatus.ts
@@ -2,13 +2,25 @@ import { handler } from "./getOverview";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { sendDiscordAlert } from "../utils/notify";
 import { autoBackfill } from "../cli/backfillUtilities/backfillFunction";
+import loadAdaptorsData from "../data"
+import { AdapterType } from "@defillama/dimension-adapters/adapters/types";
 
 export default async (event: { type: string }) => {
     const response = await handler({
         pathParameters: { chain: undefined, type: event.type }
     } as unknown as APIGatewayProxyEvent, true)
-    const errorsArr: string[] = JSON.parse(response.body)?.errors
+    const parsedBody = JSON.parse(response.body)
+    const errorsArr: string[] = parsedBody?.errors
     console.log("response", response.body)
+    const returnedProtocols = parsedBody.protocols.map((p: any) => p.module)
+    const protocolsList = Object.entries(loadAdaptorsData(event.type as AdapterType).config).filter(([_key, config]) => config.enabled).map(m => m[0])
+    const notIncluded = []
+    for (const prot of protocolsList) {
+        if (!returnedProtocols.includes(prot)) {
+            console.log("not included", prot)
+            notIncluded.push(prot)
+        }
+    }
     if (errorsArr.length > 0) {
         await sendDiscordAlert(`${errorsArr.length} adapters failed to update... Retrying...`, event.type)
         await autoBackfill(['', '', event.type, 'all'])
@@ -16,5 +28,6 @@ export default async (event: { type: string }) => {
     else {
         await sendDiscordAlert(`Looks like all good`, event.type)
     }
+    await sendDiscordAlert(`The following protocols haven't been included in the response ${notIncluded.join(", ")} <@983314132411482143>`, event.type)
     return
 }

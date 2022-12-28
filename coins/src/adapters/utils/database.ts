661a11f96c82e73540cc54976a7c47488de08359
waynebruce0x <waynebruce0x@gmail.com>
2022-12-28 00:09:30 +0000

    DB again

--- coins/src/adapters/utils/database.ts
+++ coins/src/adapters/utils/database.ts
@@ -166,10 +166,15 @@ async function getTokenAndRedirectDataDB(
     );
 
     // aggregate
-    let validResults: Read[] = latestDbEntries.map((ld: DbEntry, i: number) => {
-      if (timedRedirects[i] == undefined) return { dbEntry: ld, redirect: [] };
-      return { dbEntry: ld, redirect: [timedRedirects[i]] };
-    });
+    let validResults: Read[] = latestDbEntries
+      .map((ld: DbEntry, i: number) => {
+        if (timedDbEntries[i].SK == undefined && timedRedirects[i] == undefined)
+          return { dbEntry: ld, redirect: ["FALSE"] };
+        if (timedRedirects[i] == undefined)
+          return { dbEntry: ld, redirect: [] };
+        return { dbEntry: ld, redirect: [timedRedirects[i]] };
+      })
+      .filter((v: any) => v.redirect[0] != "FALSE");
 
     allReads.push(...validResults);
   }

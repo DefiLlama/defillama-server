de6b69976e29cb3a6a7af8d603c7f2c121668502
waynebruce0x <waynebruce0x@gmail.com>
2022-12-29 08:06:29 +0000

    convex arbitrum

--- coins/src/adapters/yield/convex/index.ts
+++ coins/src/adapters/yield/convex/index.ts
@@ -2,5 +2,8 @@ import getTokenPrices from "./convex";
 
 export function convex(timestamp: number = 0) {
   console.log("starting convex");
-  return getTokenPrices(timestamp);
+  return Promise.all([
+    getTokenPrices(timestamp, "ethereum"),
+    getTokenPrices(timestamp, "arbitrum")
+  ]);
 }

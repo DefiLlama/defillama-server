6d4280ee80ef5ed48ece657331c426b525938266
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-31 00:17:06 +0100

    run 22 adapters per lambda run

--- defi/src/triggerStoreTvls.ts
+++ defi/src/triggerStoreTvls.ts
@@ -9,7 +9,7 @@ function shuffleArray(array:number[]) {
   }
 }
 
-const step = 16;
+const step = 31;
 const handler = async () => {
   const protocolIndexes = Array.from(Array(protocols.length).keys());
   shuffleArray(protocolIndexes);

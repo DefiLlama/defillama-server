f7c947f3f206120a2d0660c8417167c46c758a78
0xtawa <0xtawanofi@gmail.com>
2023-01-11 18:16:53 +0100

    Refactor getAvailableMetricsByModule + fix

--- defi/src/adaptors/data/configs.ts
+++ defi/src/adaptors/data/configs.ts
@@ -5,11 +5,9 @@ import options from "./options/config";
 import incentives from "./incentives/config";
 import protocols from "./protocols/config";
 import derivatives from "./derivatives/config";
+import { IJSON } from "./types";
 
-// With dynamic imports loads less stuff into memory but becomes slower
-// w/ dynamic imports 1 dex -> 19sec
-// without 1 dex -> 1.6s (all dexs =200 aprox 4s)
-export default {
+const configs = {
     dexs,
     fees,
     aggregators,
@@ -18,3 +16,13 @@ export default {
     protocols,
     derivatives
 }
+
+export const getAvailableMetricsByModule = (modulePath: string) => Object.entries(configs).reduce((acc, [metric, map]) => {
+    const [module] = modulePath.split("/")[0].split(/[/.]+/)
+    const isMetricEnabled = map?.[module]?.enabled
+    if (isMetricEnabled === true)
+    acc[metric] = isMetricEnabled
+    return acc
+}, {} as IJSON<boolean>)
+
+export default configs

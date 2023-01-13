72056420c8d541750baf3c33c4668d554df696d9
0xngmi <0xngmi@protonmail.com>
2023-01-13 21:11:02 +0000

    Revert "20 limits"
    This reverts commit 536b265c6eff1a18b09c871b7ad6b16a08ebf807.

--- coins/src/getMcaps.ts
+++ coins/src/getMcaps.ts
@@ -1,62 +1,45 @@
-import {
-  successResponse,
-  wrap,
-  IResponse,
-  errorResponse,
-} from "./utils/shared";
+import { successResponse, wrap, IResponse } from "./utils/shared";
 import ddb, { batchGet } from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
 import { coinToPK, PKToCoin } from "./utils/processCoin";
 
-const limit = 20;
-
 type McapsResponse = {
-  [coin: string]: {
-    mcap: number;
-    timestamp: number;
-  };
-};
+    [coin: string]: {
+        mcap: number,
+        timestamp: number,
+    }
+}
 
 const handler = async (
-  event: AWSLambda.APIGatewayEvent,
+    event: AWSLambda.APIGatewayEvent
 ): Promise<IResponse> => {
-  const body = parseRequestBody(event.body);
-  const requestedCoins = body.coins;
-
-  if (requestedCoins.length > limit)
-    return errorResponse({
-      message: `number of requested coins must be ${limit} or less`,
-    });
-
-  const coins = await batchGet(
-    requestedCoins.map((coin: string) => ({
-      PK: coinToPK(coin),
-      SK: 0,
-    })),
-  );
-  const response = {} as McapsResponse;
-  await Promise.all(
-    coins.map(async (coin) => {
-      const coinName = PKToCoin(coin.PK);
-      const formattedCoin = {
-        mcap: coin.mcap,
-        timestamp: coin.timestamp,
-      };
-      if (coin.redirect) {
-        const redirectedCoin = await ddb.get({
-          PK: coin.redirect,
-          SK: 0,
-        });
-        if (redirectedCoin.Item === undefined) {
-          return;
+    const body = parseRequestBody(event.body)
+    const requestedCoins = body.coins;
+    const coins = await batchGet(requestedCoins.map((coin: string) => ({
+        PK: coinToPK(coin),
+        SK: 0,
+    })));
+    const response = {} as McapsResponse
+    await Promise.all(coins.map(async coin => {
+        const coinName = PKToCoin(coin.PK);
+        const formattedCoin = {
+            mcap: coin.mcap,
+            timestamp: coin.timestamp,
         }
-        formattedCoin.mcap = redirectedCoin.Item?.mcap;
-        formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
-      }
-      response[coinName] = formattedCoin;
-    }),
-  );
-  return successResponse(response);
+        if (coin.redirect) {
+            const redirectedCoin = await ddb.get({
+                PK: coin.redirect,
+                SK: 0
+            })
+            if (redirectedCoin.Item === undefined) {
+                return
+            }
+            formattedCoin.mcap = redirectedCoin.Item?.mcap
+            formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
+        }
+        response[coinName] = formattedCoin;
+    }))
+    return successResponse(response);
 };
 
 export default wrap(handler);

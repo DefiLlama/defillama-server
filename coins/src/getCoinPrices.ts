536b265c6eff1a18b09c871b7ad6b16a08ebf807
waynebruce0x <waynebruce0x@gmail.com>
2023-01-13 14:28:25 +0000

    20 limits

--- coins/src/getCoinPrices.ts
+++ coins/src/getCoinPrices.ts
@@ -1,41 +1,61 @@
-import { successResponse, wrap, IResponse, errorResponse } from "./utils/shared";
+import {
+  successResponse,
+  wrap,
+  IResponse,
+  errorResponse,
+} from "./utils/shared";
 import ddb from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
 import getRecordClosestToTimestamp from "./utils/shared/getRecordClosestToTimestamp";
 import { coinToPK, DAY } from "./utils/processCoin";
+const limit = 20;
 
 const handler = async (
-    event: AWSLambda.APIGatewayEvent
+  event: AWSLambda.APIGatewayEvent,
 ): Promise<IResponse> => {
-    const body = parseRequestBody(event.body)
-    const requestedCoin = body.coin;
-    const timestampsRequested = body.timestamps as number[];
-    const coin = (await ddb.get({
-        PK: coinToPK(requestedCoin),
-        SK: 0,
-    })).Item;
-    if (coin === undefined) {
-        return errorResponse({ message: "Coin doesn't exist" })
-    }
-    const response = {
-        decimals: coin.decimals,
-        symbol: coin.symbol,
-        prices: [] as {
-            timestamp: number,
-            price: number,
-        }[]
-    }
-    await Promise.all(timestampsRequested.map(async timestampRequested => {
-        const finalCoin = await getRecordClosestToTimestamp(coin.redirect ?? coin.PK, timestampRequested, DAY / 2);
-        if (finalCoin.SK === undefined) {
-            return
-        }
-        response.prices.push({
-            price: finalCoin.price,
-            timestamp: finalCoin.SK
-        });
-    }))
-    return successResponse(response);
+  const body = parseRequestBody(event.body);
+  const requestedCoin = body.coin;
+  const timestampsRequested = body.timestamps as number[];
+
+  if (timestampsRequested.length > limit)
+    return errorResponse({
+      message: `number of requested coins must be ${limit} or less`,
+    });
+
+  const coin = (
+    await ddb.get({
+      PK: coinToPK(requestedCoin),
+      SK: 0,
+    })
+  ).Item;
+  if (coin === undefined) {
+    return errorResponse({ message: "Coin doesn't exist" });
+  }
+  const response = {
+    decimals: coin.decimals,
+    symbol: coin.symbol,
+    prices: [] as {
+      timestamp: number;
+      price: number;
+    }[],
+  };
+  await Promise.all(
+    timestampsRequested.map(async (timestampRequested) => {
+      const finalCoin = await getRecordClosestToTimestamp(
+        coin.redirect ?? coin.PK,
+        timestampRequested,
+        DAY / 2,
+      );
+      if (finalCoin.SK === undefined) {
+        return;
+      }
+      response.prices.push({
+        price: finalCoin.price,
+        timestamp: finalCoin.SK,
+      });
+    }),
+  );
+  return successResponse(response);
 };
 
 export default wrap(handler);
--- coins/src/getCoins.ts
+++ coins/src/getCoins.ts
@@ -1,53 +1,74 @@
-import { successResponse, wrap, IResponse } from "./utils/shared";
+import {
+  successResponse,
+  wrap,
+  IResponse,
+  errorResponse,
+} from "./utils/shared";
 import ddb, { batchGet } from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
 import getRecordClosestToTimestamp from "./utils/shared/getRecordClosestToTimestamp";
 import { coinToPK, DAY, PKToCoin } from "./utils/processCoin";
 import { CoinsResponse } from "./utils/getCoinsUtils";
 
+const limit = 20;
+
 const handler = async (
-  event: AWSLambda.APIGatewayEvent
+  event: AWSLambda.APIGatewayEvent,
 ): Promise<IResponse> => {
-  const body = parseRequestBody(event.body)
+  const body = parseRequestBody(event.body);
   const requestedCoins = body.coins;
-  const timestampRequested = body.timestamp
-  const coins = await batchGet(requestedCoins.map((coin: string) => ({
-    PK: coinToPK(coin),
-    SK: 0,
-  })));
-  const response = {} as CoinsResponse
-  await Promise.all(coins.map(async coin => {
-    const coinName = PKToCoin(coin.PK);
-    const formattedCoin = {
-      decimals: coin.decimals,
-      price: coin.price,
-      symbol: coin.symbol,
-      timestamp: coin.timestamp,
-    }
-    if (coin.redirect || timestampRequested !== undefined) {
-      if (timestampRequested === undefined) {
-        const redirectedCoin = await ddb.get({
-          PK: coin.redirect,
-          SK: 0
-        })
-        if(redirectedCoin.Item === undefined){
-          return
+
+  if (requestedCoins.length > limit)
+    return errorResponse({
+      message: `number of requested coins must be ${limit} or less`,
+    });
+
+  const timestampRequested = body.timestamp;
+  const coins = await batchGet(
+    requestedCoins.map((coin: string) => ({
+      PK: coinToPK(coin),
+      SK: 0,
+    })),
+  );
+  const response = {} as CoinsResponse;
+  await Promise.all(
+    coins.map(async (coin) => {
+      const coinName = PKToCoin(coin.PK);
+      const formattedCoin = {
+        decimals: coin.decimals,
+        price: coin.price,
+        symbol: coin.symbol,
+        timestamp: coin.timestamp,
+      };
+      if (coin.redirect || timestampRequested !== undefined) {
+        if (timestampRequested === undefined) {
+          const redirectedCoin = await ddb.get({
+            PK: coin.redirect,
+            SK: 0,
+          });
+          if (redirectedCoin.Item === undefined) {
+            return;
+          }
+          formattedCoin.price = redirectedCoin.Item?.price;
+          formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
+        } else {
+          const finalCoin = await getRecordClosestToTimestamp(
+            coin.redirect ?? coin.PK,
+            timestampRequested,
+            DAY / 2,
+          );
+          if (finalCoin.SK === undefined) {
+            return;
+          }
+          formattedCoin.price = finalCoin.price;
+          formattedCoin.timestamp = finalCoin.SK;
         }
-        formattedCoin.price = redirectedCoin.Item?.price
-        formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
-      } else {
-        const finalCoin = await getRecordClosestToTimestamp(coin.redirect ?? coin.PK, timestampRequested, DAY/2);
-        if(finalCoin.SK === undefined){
-          return
-        }
-        formattedCoin.price = finalCoin.price;
-        formattedCoin.timestamp = finalCoin.SK;
       }
-    }
-    response[coinName] = formattedCoin;
-  }))
+      response[coinName] = formattedCoin;
+    }),
+  );
   return successResponse({
-    coins: response
+    coins: response,
   });
 };
 

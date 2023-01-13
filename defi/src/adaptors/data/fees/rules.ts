e2fa0ffd3c0abb9b45101320bd237920d0b142d7
0xtawa <0xtawanofi@gmail.com>
2023-01-13 15:43:36 +0100

    Improve fees rules

--- defi/src/adaptors/data/fees/rules.ts
+++ defi/src/adaptors/data/fees/rules.ts
@@ -5,7 +5,7 @@ import { IJSON } from "../types"
 const rules = (interval: 'daily' | 'total') => ({
     [`${interval}UserFees`]: (extraDimensions: IJSON<number | null>, category: string) => {
         const dimensionKey = `${interval}UserFees`
-        if (extraDimensions[dimensionKey] !== null) return
+        if (typeof extraDimensions[dimensionKey] === 'number') return
         const categoriesFees: string[] = [CATEGORIES.DEX, CATEGORIES.Lending, CATEGORIES.NFT_Lending, CATEGORIES.Chain, CATEGORIES.Rollup, CATEGORIES.NFT_Marketplace, CATEGORIES.CDP, CATEGORIES.Synthetics, CATEGORIES.Derivatives]
         if (categoriesFees.includes(category)) {
             extraDimensions[dimensionKey] = extraDimensions[`${interval}Fees`]
@@ -35,10 +35,16 @@ const rules = (interval: 'daily' | 'total') => ({
             extraDimensions[dimensionKey] = extraDimensions[`${interval}Revenue`]
             return
         }
+        const Revenue = extraDimensions[`${interval}Revenue`]
+        const SupplySideRevenue = extraDimensions[`${interval}SupplySideRevenue`]
+        if (typeof Revenue === 'number' || typeof SupplySideRevenue === 'number') {
+            extraDimensions[dimensionKey] = (Revenue ?? 0) + (SupplySideRevenue ?? 0)
+            return
+        }
     },
     [`${interval}Revenue`]: (extraDimensions: IJSON<number | null>, category: string) => {
         const dimensionKey = `${interval}Revenue`
-        if (extraDimensions[dimensionKey] != null) return
+        if (typeof extraDimensions[dimensionKey] === 'number') return
         const categoriesFees: string[] = [CATEGORIES.Chain, CATEGORIES.Rollup, CATEGORIES.NFT_Marketplace, CATEGORIES.CDP]
         if (categoriesFees.includes(category)) {
             extraDimensions[dimensionKey] = extraDimensions[`${interval}Fees`]
@@ -46,7 +52,7 @@ const rules = (interval: 'daily' | 'total') => ({
         }
         const Fees = extraDimensions[`${interval}Fees`]
         const SupplySideRevenue = extraDimensions[`${interval}SupplySideRevenue`]
-        if (Fees !== null && SupplySideRevenue !== null) {
+        if (typeof Fees === 'number' && typeof SupplySideRevenue === 'number') {
             extraDimensions[dimensionKey] = Fees - SupplySideRevenue
             return
         }
@@ -59,15 +65,25 @@ const rules = (interval: 'daily' | 'total') => ({
             extraDimensions[dimensionKey] = extraDimensions[`${interval}ProtocolRevenue`]
             return
         }
+        const ProtocolRevenue = extraDimensions[`${interval}ProtocolRevenue`]
+        const HoldersRevenue = extraDimensions[`${interval}HoldersRevenue`]
+        if (typeof ProtocolRevenue === 'number' || typeof HoldersRevenue === 'number') {
+            extraDimensions[dimensionKey] = (ProtocolRevenue ?? 0) + (HoldersRevenue ?? 0)
+            return
+        }
+        if (Fees === 0) {
+            extraDimensions[dimensionKey] = Fees
+            return
+        }
     },
     [`${interval}SupplySideRevenue`]: (extraDimensions: IJSON<number | null>, category: string) => {
         const dimensionKey = `${interval}SupplySideRevenue`
-        if (extraDimensions[dimensionKey] !== null) return
+        if (typeof extraDimensions[dimensionKey] === 'number') return
         const categories: string[] = [CATEGORIES.Lending, CATEGORIES.NFT_Lending, CATEGORIES.DEX, CATEGORIES.Derivatives, CATEGORIES.Options, CATEGORIES.Liquid_Staking, CATEGORIES.Yield, CATEGORIES.Synthetics]
         if (categories.includes(category)) {
             const Fees = extraDimensions[`${interval}Fees`]
             const Revenue = extraDimensions[`${interval}Revenue`]
-            if (Fees !== null && Revenue !== null) {
+            if (typeof Fees === 'number' && typeof Revenue === 'number') {
                 extraDimensions[dimensionKey] = Fees - Revenue
                 return
             }
@@ -80,10 +96,10 @@ const rules = (interval: 'daily' | 'total') => ({
     },
     [`${interval}ProtocolRevenue`]: (extraDimensions: IJSON<number | null>, category: string) => {
         const dimensionKey = `${interval}ProtocolRevenue`
-        if (extraDimensions[dimensionKey] !== null) return
+        if (typeof extraDimensions[dimensionKey] === 'number') return
         const Revenue = extraDimensions[`${interval}Revenue`]
         const HoldersRevenue = extraDimensions[`${interval}HoldersRevenue`]
-        if (HoldersRevenue !== null && Revenue !== null) {
+        if (typeof HoldersRevenue === 'number' && typeof Revenue === 'number') {
             extraDimensions[dimensionKey] = Revenue - HoldersRevenue
             return
         }
@@ -99,12 +115,12 @@ const rules = (interval: 'daily' | 'total') => ({
     },
     [`${interval}HoldersRevenue`]: (extraDimensions: IJSON<number | null>, category: string) => {
         const dimensionKey = `${interval}HoldersRevenue`
-        if (extraDimensions[dimensionKey] !== null) return
+        if (typeof extraDimensions[dimensionKey] === 'number') return
         const Revenue = extraDimensions[`${interval}Revenue`]
         const ProtocolRevenue = extraDimensions[`${interval}ProtocolRevenue`]
         const categories: string[] = [CATEGORIES.DEX, CATEGORIES.Derivatives, CATEGORIES.Options]
         if (categories.includes(category)) {
-            if (ProtocolRevenue !== null && Revenue !== null) {
+            if (typeof ProtocolRevenue === 'number' && typeof Revenue === 'number') {
                 extraDimensions[dimensionKey] = Revenue - ProtocolRevenue
                 return
             }

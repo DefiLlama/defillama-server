c207f6001b03c588afa940c5162baff547b151ca
0xtawa <0xtawanofi@gmail.com>
2023-01-22 22:24:01 +0100

    Debug: fix R2 filename extension

--- defi/src/adaptors/utils/storeR2Response/index.ts
+++ defi/src/adaptors/utils/storeR2Response/index.ts
@@ -4,11 +4,13 @@ import { IGetOverviewResponseBody } from "../../handlers/processProtocolsSummary
 const ADAPTORS_FOLDER_KEY = 'dimensions'
 
 export const cacheResponseOnR2 = async (key: string, response: string) => {
-    await storeR2(`${ADAPTORS_FOLDER_KEY}/${key}`, response)
+    console.log("Storing R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
+    await storeR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`, response)
 }
 
 export const getCachedResponseOnR2 = async (key: string): Promise<IGetOverviewResponseBody | undefined> => {
-    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}`).catch(e=>console.error("R2Storage:", e))
+    console.log("Get R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
+    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`).catch(e=>console.error("R2Storage:", e))
     console.log("R2Storage:response", JSON.stringify(objectString))
     if (objectString) {
         try {

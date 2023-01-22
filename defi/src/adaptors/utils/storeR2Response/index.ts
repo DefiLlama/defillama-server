4eb97cf38e7debfe0e993a8630c58a1d8a5b1f10
0xtawa <0xtawanofi@gmail.com>
2023-01-22 17:06:01 +0100

    Cache overview response

---
+++ defi/src/adaptors/utils/storeR2Response/index.ts
@@ -0,0 +1,20 @@
+import { getR2, storeR2 } from "../../../utils/r2";
+import { IGetOverviewResponseBody } from "../../handlers/processProtocolsSummary";
+
+const ADAPTORS_FOLDER_KEY = 'dimensions'
+
+export const cacheResponseOnR2 = async (key: string, response: string) => {
+    await storeR2(`${ADAPTORS_FOLDER_KEY}/${key}`, response)
+}
+
+export const getCachedResponseOnR2 = async (key: string): Promise<IGetOverviewResponseBody | undefined> => {
+    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}`)
+    if (objectString) {
+        try {
+            return JSON.parse(objectString) as IGetOverviewResponseBody
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return undefined
+}

1130aeea1528a33f8d6d169797d75ea89b258851
0xtawa <0xtawanofi@gmail.com>
2023-01-23 17:32:24 +0100

    Return string from data.Body via transformToString + clean logs

--- defi/src/adaptors/utils/storeR2Response/index.ts
+++ defi/src/adaptors/utils/storeR2Response/index.ts
@@ -4,15 +4,13 @@ import { IGetOverviewResponseBody } from "../../handlers/processProtocolsSummary
 const ADAPTORS_FOLDER_KEY = 'dimensions'
 
 export const cacheResponseOnR2 = async (key: string, response: string) => {
-    console.log("Storing R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`, response)
+    console.info("Storing R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`, response)
     await storeR2JSONString(`${ADAPTORS_FOLDER_KEY}/${key}.json`, response)
 }
 
 export const getCachedResponseOnR2 = async (key: string): Promise<IGetOverviewResponseBody | undefined> => {
-    console.log("Get R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
-    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`).catch(e=>console.error("R2Storage:", e))
-    console.log("R2Storage:response", objectString)
-    console.log("R2Storage:response", objectString, JSON.stringify(objectString))
+    console.info("Get R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
+    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`).catch(e=>console.error("R2Storage:error", e))
     if (objectString) {
         try {
             return JSON.parse(objectString) as IGetOverviewResponseBody

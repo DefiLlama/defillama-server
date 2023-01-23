738dddd89294fb4f1dd2129864e51cb389efb1c1
0xtawa <0xtawanofi@gmail.com>
2023-01-23 21:56:57 +0100

    Fix conflicts

--- defi/src/utils/r2.ts
+++ defi/src/utils/r2.ts
@@ -67,14 +67,10 @@ export async function getR2(filename: string) {
     Key: filename,
   });
   const data = await R2.send(command);
-<<<<<<< Updated upstream
-  return data.Body?.transformToString();
-=======
   return {
     body: await data.Body?.transformToString(),
     lastModified: data.LastModified
   }
->>>>>>> Stashed changes
 }
 
 export async function storeDatasetR2(

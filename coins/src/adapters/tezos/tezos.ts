53573f9b5f37df41adece6a50b35f1ea995d736e
waynebruce0x <waynebruce0x@gmail.com>
2023-02-22 08:54:19 +0000

    tezos API

--- coins/src/adapters/tezos/tezos.ts
+++ coins/src/adapters/tezos/tezos.ts
@@ -15,19 +15,17 @@ export default async function getTokenPrices(timestamp: number) {
 }
 
 async function getPlentyData() {
-  const { data } = await axios.get('https://api.analytics.plenty.network/tracker/tokens')
+  const { data } = await axios.get('https://api.analytics.plenty.network/analytics/tokens')
   const response: any  = []
   data.forEach((i: any) => {
-    const { symbol, decimals, } = i
+    const { token, decimals, } = i
     const tvl = +i.tvl.value
-    if (tvl < 1000) {
-      // console.log('Ignoring', symbol, ' tvl is only', tvl)
-      return;
-    }
+    if (tvl < 1000) return;
     const confidence = tvl > 10000 ? 0.99 : 0.6
     let label = i.contract
+    if ([token, label, decimals, +i.price.value].includes(undefined)) return
     if (i.hasOwnProperty('tokenId') && i.tokenId !== 0) label += '-' + i.tokenId
-    response.push({ symbol, token: label, decimals, price: +i.price.value, confidence })
+    response.push({ symbol: token, token: label, decimals, price: +i.price.value, confidence })
   })
 
   return response

2408af2868f2f4d4d674ae6166490371c9a52c4d
0xtawa <0xtawanofi@gmail.com>
2023-02-23 13:07:22 +0100

    Commit tmp switch device

--- defi/src/adaptors/data/helpers/overrides.ts
+++ defi/src/adaptors/data/helpers/overrides.ts
@@ -1,16 +1,6 @@
 import { IJSON, ProtocolAdaptor } from "../types";
 
 const overrides = {
-    'ghostmarket': {
-        category: "NFT Marketplace",
-        allAddresses: [
-            "neo:0x9b049f1283515eef1d3f6ac610e1595ed25ca3e9",
-            "ethereum:0x35609dc59e15d03c5c865507e1348fa5abb319a8",
-            "polygon:0x6a335ac6a3cdf444967fe03e7b6b273c86043990",
-            "avax:0x0b53b5da7d0f275c31a6a182622bdf02474af253",
-            "bsc:0x0b53b5da7d0f275c31a6a182622bdf02474af253"
-        ]
-    },
     'uniswap': {
         id: "1",
         name: "Uniswap"

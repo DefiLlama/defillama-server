a41ad2c95519f799e31a72538b29699bafc1a140
waynebruce0x <waynebruce0x@gmail.com>
2023-02-23 16:57:24 +0000

    saved JSONs instead of dynamic draw

--- defi/src/getEmissions.ts
+++ defi/src/getEmissions.ts
@@ -4,27 +4,23 @@ import {
   errorResponse,
   successResponse,
 } from "./utils/shared";
-import { createChartData } from "./emissions/utils/convertToChartData";
-import { createRawSections } from "./emissions/utils/convertToRawData";
-import adapters from "./emissions/protocols";
-import { ChartSection, Protocol } from "./emissions/types/adapters";
+import fs from "fs";
 
 const handler = async (event: any): Promise<IResponse> => {
   const protocolName: string = event.pathParameters?.protocol?.toLowerCase();
-  const adapter: Protocol = (adapters as any)[protocolName];
-  if (!adapter) {
+  try {
+    const data = JSON.parse(
+      fs.readFileSync(
+        `defi/src/emissions/results/${protocolName}.json`,
+        "utf8",
+      ),
+    );
+    return successResponse({ data });
+  } catch {
     return errorResponse({
-      message: `The passed protocol name is invalid. Make sure '${adapter}' is a key of './emissions/protocols/index.ts`,
+      message: `protocol '${protocolName}' has no chart to fetch`,
     });
   }
-  const { rawSections, startTime, endTime } = await createRawSections(adapter);
-  const data = createChartData(rawSections, startTime, endTime, false).map(
-    (s: ChartSection) => ({
-      label: s.section,
-      data: s.data.apiData,
-    }),
-  );
-  return successResponse({ data });
 };
 
 export default wrap(handler);

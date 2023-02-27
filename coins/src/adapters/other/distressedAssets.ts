21fb9bd8c9f956399e63bb5ba1f1de2468ac7ca6
define <Lovelovenow@protonmail.com>
2023-02-27 15:06:56 +0000

    add MOR token to distress

--- coins/src/adapters/other/distressedAssets.ts
+++ coins/src/adapters/other/distressedAssets.ts
@@ -39,6 +39,7 @@ const contracts: { [chain: string]: { [token: string]: string } } = {
     GVR2: "0xF33893DE6eB6aE9A67442E066aE9aBd228f5290c",
     PANCAKE_LP_ABNB_BNB: "0x272c2CF847A49215A3A1D4bFf8760E503A06f880",
     BTCBR: "0x0cf8e180350253271f4b917ccfb0accc4862f262",
+    MOR: "0x87bade473ea0513d4aa7085484aeaa6cb6ebe7e3" //MOR
   },
   cronos: {
     CRK: '0x065de42e28e42d90c2052a1b49e7f83806af0e1f',

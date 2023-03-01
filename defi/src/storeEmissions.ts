456c3d4fc0da5dc2202627f8d6724cb5589a4268
0xngmi <0xngmi@protonmail.com>
2023-03-01 16:17:06 +0000

    storeEmissions

---
+++ defi/src/storeEmissions.ts
@@ -0,0 +1,22 @@
+import { createChartData } from "./emissions/utils/convertToChartData";
+import { createRawSections } from "./emissions/utils/convertToRawData";
+import adapters from "./emissions/protocols";
+import { ChartSection, Protocol } from "./emissions/types/adapters";
+import { storeR2JSONString } from "./utils/r2";
+import { wrapScheduledLambda } from "./utils/shared/wrap";
+
+async function handler() {
+    await Promise.all(Object.keys(adapters).map(async protocolName => {
+        const adapter: Protocol = (adapters as any)[protocolName];
+        const { rawSections, startTime, endTime } = await createRawSections(adapter);
+        const data = createChartData(rawSections, startTime, endTime, false).map(
+            (s: ChartSection) => ({
+                label: s.section,
+                data: s.data.apiData,
+            }),
+        );
+        await storeR2JSONString(`emissions/${protocolName}`, JSON.stringify(data), 3600)
+    }))
+}
+
+export default wrapScheduledLambda(handler);

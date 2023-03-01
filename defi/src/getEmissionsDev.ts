831624cf2c07f4ed2058a2f425752a717d90e9c0
waynebruce0x <waynebruce0x@gmail.com>
2023-03-01 16:40:21 +0000

    debug auth header issue

--- defi/src/getEmissionsDev.ts
+++ defi/src/getEmissionsDev.ts
@@ -1,16 +1,16 @@
+import { getR2 } from "./utils/r2";
 import {
   wrap,
   IResponse,
   notFoundResponse,
   successResponse,
 } from "./utils/shared";
-import * as charts from "./emissions/charts";
+
 const handler = async (event: any): Promise<IResponse> => {
   const protocolName: string = event.pathParameters?.protocol?.toLowerCase();
 
   try {
-    //const data = await import(`./emissions/charts/${protocolName}.json`);
-    const data = charts.default[protocolName as keyof typeof charts.default];
+    const data = await getR2(`emissions/${protocolName}`);
     return successResponse(data);
   } catch (e) {
     return notFoundResponse({
@@ -21,8 +21,8 @@ const handler = async (event: any): Promise<IResponse> => {
 
 export default wrap(handler);
 
-// async function main() {
-//   let a = await handler({ pathParameters: { protocol: "aave" } });
-//   return;
-// }
-// main(); // ts-node defi/src/getEmissionsDev.ts
+async function main() {
+  let a = await handler({ pathParameters: { protocol: "aave" } });
+  return;
+}
+main(); // ts-node defi/src/getEmissionsDev.ts

1a1dbfab17cfdeaefa42ca7688320309bfefb3e9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-07 22:11:19 +0100

    coins: price art block NFTs

--- coins/src/adapters/nft/chainlink/chainlink.ts
+++ coins/src/adapters/nft/chainlink/chainlink.ts
@@ -2,7 +2,7 @@ import { Write, } from "../../utils/dbInterfaces";
 import { addToDBWritesList, getTokenAndRedirectData } from "../../utils/database";
 import { getApi } from "../../utils/sdk";
 import pricefeeds from "./priceFeeds";
-import jPricefeeds from "./priceFeeds_jpegd";
+import jPricefeeds from "./priceFeeds_jpegd"
 
 export default async function getTokenPrices(chain: string, timestamp: number) {
   const api = await getApi(chain, timestamp)
@@ -11,6 +11,7 @@ export default async function getTokenPrices(chain: string, timestamp: number) {
   const writes: Write[] = [];
   await Promise.all(data.map(_getWrites))
   await Promise.all(dataJPEGD.map(_getWritesJPEGD))
+  await Promise.all(dataJPEGD.map(_getWritesArtBlocks))
 
   return writes
 
@@ -28,6 +29,7 @@ export default async function getTokenPrices(chain: string, timestamp: number) {
       addToDBWritesList(writes, chain, tokens[i], coinData.price * price / (10 ** coinData.decimals), 0, symbols[i], timestamp, 'chainlink-nft', coinData.confidence as number)
     })
   }
+
   async function _getWritesJPEGD(info: any) {
     const { underlying, nfts } = info
     const tokens = nfts.map((i: any) => i.token)
@@ -40,6 +42,20 @@ export default async function getTokenPrices(chain: string, timestamp: number) {
     
     prices.forEach((price, i) => {
       addToDBWritesList(writes, chain, tokens[i], coinData.price * price / (10 ** coinData.decimals), 0, symbols[i], timestamp, 'chainlink-nft-jpegd', coinData.confidence as number)
+    })
+  }
+
+  async function _getWritesArtBlocks(info: any) {
+    const {artBlocks: { underlying, nfts } }= info
+    const tokens = nfts.map((i: any) => i.label)
+    const oracles = nfts.map((i: any) => i.oracle)
+    const prices = await api.multiCall({ abi: "uint256:getFloorETH", calls: oracles })
+    // const names = await api.multiCall({ abi: "string:name", calls: tokens })
+    const [coinData] = await getTokenAndRedirectData([underlying], chain, timestamp)
+    if (!coinData) return;
+    
+    prices.forEach((price, i) => {
+      addToDBWritesList(writes, 'nft', tokens[i], coinData.price * price / (10 ** coinData.decimals), 0, tokens[i], timestamp, 'chainlink-nft-jpegd-artBlocks', coinData.confidence as number)
     })
   }
 }
--- coins/src/adapters/nft/chainlink/priceFeeds_jpegd.ts
+++ coins/src/adapters/nft/chainlink/priceFeeds_jpegd.ts
@@ -7,7 +7,24 @@ export default {
           token: "0xa3f5998047579334607c47a6a2889bf87a17fc02",
           oracle: "0x9921da2908CC59b13ddbcF45e64BFA91c78c4249",
         },
-      ]
+      ],
+      artBlocks: {
+        underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        nfts: [
+          {
+            label: "chromie-squiggle",
+            oracle: "0x753eF2495D09D6650573F5389fDA3A57017f302A",
+          },
+          {
+            label: "fidenza",
+            oracle: "0x1a6fA849D5bF62F9b7B83D125E77e15292c54b4f",
+          },
+          {
+            label: "ringer",
+            oracle: "0x927318B867f0939Ab501dFd7b1B8d60E52ea5B59",
+          },
+        ],
+      }
     }
   ]
 }

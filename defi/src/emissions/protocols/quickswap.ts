afb4e1d2cf600f2df28c529f4f6a913504b4db6a
waynebruce0x <waynebruce0x@gmail.com>
2023-03-10 17:59:51 +0000

    patch protocol name

--- defi/src/emissions/protocols/quickswap.ts
+++ defi/src/emissions/protocols/quickswap.ts
@@ -5,11 +5,11 @@ import { periodToSeconds } from "../utils/time";
 const start: number = 1610126596;
 const qty: number = 1_000_000;
 
-const perpetual: Protocol = {
+const quickswap: Protocol = {
   Community: manualLinear(start, start + periodToSeconds.year * 4, qty * 0.9),
   Airdrop: manualCliff(start + periodToSeconds.year, qty * 0.06),
   "Team and marketing": manualLinear(start, start + 39139200, qty * 0.04),
   sources: ["https://discord.gg/dSMd7AFH36"],
   token: "polygon:0xb5c064f955d8e7f38fe0460c556a72987494ee17",
 };
-export default perpetual;
+export default quickswap;

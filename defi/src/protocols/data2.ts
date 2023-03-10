5032caa66fa90afeed82e886e921f07fd2d02a17
realdealshaman <realdealshaman11@gmail.com>
2023-03-10 02:16:09 +0000

    update category

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -27551,7 +27551,7 @@ const data2: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquid Staking",
+    category: "Staking",
     chains: ["Ethereum"],
     oracles: [],
     forkedFrom: [],

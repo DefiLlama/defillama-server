9bb1876daf70da896411bd52af5473722333a294
mintdart <96025197+mintdart@users.noreply.github.com>
2023-03-10 02:30:34 -0600

    refactor updatedProtocolDraft api

--- defi/src/utils/craftParentProtocolDraft.ts
+++ defi/src/utils/craftParentProtocolDraft.ts
@@ -75,6 +75,8 @@ export async function craftParentProtocolDraft({
       (acc, curr) => {
         const isTvlDataHourly = isHourlyTvl(curr.tvl);
 
+        console.log(curr.name);
+
         // TOTAL TVL OF EACH CHAIN
         for (const name in curr.currentChainTvls) {
           acc.currentChainTvls = {
@@ -95,8 +97,15 @@ export async function craftParentProtocolDraft({
               };
             }
 
-            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tvl[index - 1].date >= 86400) {
+            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tvl[index - 1].date > 86400) {
               const prev = curr.chainTvls[chain].tvl[index - 1];
+              console.log({
+                prev,
+                date: prev.date + 86400,
+                currTvl: (prev.totalLiquidityUSD + totalLiquidityUSD) / 2,
+                total:
+                  (acc.chainTvls[chain].tvl[prev.date + 86400] || 0) + (prev.totalLiquidityUSD + totalLiquidityUSD) / 2,
+              });
               acc.chainTvls[chain].tvl = {
                 ...acc.chainTvls[chain].tvl,
                 [prev.date + 86400]:
@@ -125,7 +134,7 @@ export async function craftParentProtocolDraft({
               };
             }
 
-            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tokensInUsd![index - 1].date >= 86400) {
+            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tokensInUsd![index - 1].date > 86400) {
               const prev = curr.chainTvls[chain].tokensInUsd![index - 1];
 
               for (const token in tokens) {
@@ -160,7 +169,7 @@ export async function craftParentProtocolDraft({
               };
             }
 
-            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tokens![index - 1].date >= 86400) {
+            if (index !== 0 && !isTvlDataHourly && date - curr.chainTvls[chain].tokens![index - 1].date > 86400) {
               const prev = curr.chainTvls[chain].tokens![index - 1];
 
               for (const token in tokens) {
@@ -190,7 +199,7 @@ export async function craftParentProtocolDraft({
         if (!skipAggregatedTvl) {
           if (curr.tokensInUsd) {
             curr.tokensInUsd.forEach(({ date, tokens }, index) => {
-              if (index !== 0 && !isTvlDataHourly && date - curr.tokensInUsd![index - 1].date >= 86400) {
+              if (index !== 0 && !isTvlDataHourly && date - curr.tokensInUsd![index - 1].date > 86400) {
                 const prev = curr.tokensInUsd![index - 1];
 
                 Object.keys(tokens).forEach((token) => {
@@ -221,7 +230,7 @@ export async function craftParentProtocolDraft({
 
           if (curr.tokens) {
             curr.tokens.forEach(({ date, tokens }, index) => {
-              if (index !== 0 && !isTvlDataHourly && date - curr.tokens![index - 1].date >= 86400) {
+              if (index !== 0 && !isTvlDataHourly && date - curr.tokens![index - 1].date > 86400) {
                 const prev = curr.tokens![index - 1];
 
                 Object.keys(tokens).forEach((token) => {
@@ -251,7 +260,7 @@ export async function craftParentProtocolDraft({
           }
 
           curr.tvl.forEach(({ date, totalLiquidityUSD }, index) => {
-            if (index !== 0 && !isTvlDataHourly && date - curr.tvl[index - 1].date >= 86400) {
+            if (index !== 0 && !isTvlDataHourly && date - curr.tvl[index - 1].date > 86400) {
               const prev = curr.tvl[index - 1];
               acc.tvl[prev.date + 86400] =
                 (acc.tvl[prev.date + 86400] || 0) + (prev.totalLiquidityUSD + totalLiquidityUSD) / 2;

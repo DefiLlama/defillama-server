c4c3db871ca45cd9138bb1bff0cd72d7700835cb
waynebruce0x <waynebruce0x@gmail.com>
2023-03-14 17:18:22 +0000

    more blacklist

--- coins/src/adapters/yield/mean-finance/mean-finance.ts
+++ coins/src/adapters/yield/mean-finance/mean-finance.ts
@@ -5,13 +5,16 @@ import { fetch } from "../../utils";
 
 const blacklist: string[] = [
   // Ethereum
-  '0xCd0E5871c97C663D43c62B5049C123Bb45BfE2cC', // Euler 4626 (USDC)
-  '0xd4dE9D2Fc1607d1DF63E1c95ecBfa8d7946f5457', // Euler 4626 (WETH)
-  '0xc4113b7605D691E073c162809060b6C5Ae402F1e', // Euler 4626 (DAI)
-  '0x48E345cb84895EAb4db4C44ff9B619cA0bE671d9', // Euler 4626 (WBTC)
-  '0xb95E6eee428902C234855990E18a632fA34407dc', // Euler 4626 (LUSD)
-  '0x7C6D161b367Ec0605260628c37B8dd778446256b', // Euler 4626 (wstETH)
-].map(token => token.toLowerCase());
+  "0xCd0E5871c97C663D43c62B5049C123Bb45BfE2cC", // Euler 4626 (USDC)
+  "0xd4dE9D2Fc1607d1DF63E1c95ecBfa8d7946f5457", // Euler 4626 (WETH)
+  "0xc4113b7605D691E073c162809060b6C5Ae402F1e", // Euler 4626 (DAI)
+  "0x48E345cb84895EAb4db4C44ff9B619cA0bE671d9", // Euler 4626 (WBTC)
+  "0xb95E6eee428902C234855990E18a632fA34407dc", // Euler 4626 (LUSD)
+  "0x7C6D161b367Ec0605260628c37B8dd778446256b", // Euler 4626 (wstETH)
+  "0x3c66b18f67ca6c1a71f829e2f6a0c987f97462d0",
+  "0x4169df1b7820702f566cc10938da51f6f597d264",
+  "0xcdd454ce8a23f57e7f4f948056836c5a8e2142ec",
+].map((token) => token.toLowerCase());
 
 export default async function getTokenPrices(chain: string, timestamp: number) {
   const tokens: { type: string; address: string }[] = await fetch(

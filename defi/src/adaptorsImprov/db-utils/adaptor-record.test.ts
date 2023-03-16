ace0bcfa95ca56ccb9c445da4e54a2898c9c5e6e
0xtawa <0xtawanofi@gmail.com>
2023-03-16 17:30:49 +0100

    Delete adaptorsImprov folder

--- defi/src/adaptorsImprov/db-utils/adaptor-record.test.ts
+++
@@ -1,7 +0,0 @@
-import { AdaptorRecordType } from "./adaptor-record";
-
-const isArrayUnique = (arr: any[]) => Array.isArray(arr) && new Set(arr).size === arr.length;
-test("No duplicated adapter type", async () => {
-    const values = Object.values(AdaptorRecordType)
-    expect(isArrayUnique(values)).toBeTruthy();
-});

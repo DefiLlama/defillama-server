cc43354a6b569b834f464a65dfe79e0cbc386653
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-25 10:03:13 +0100

    ignore ZEDXION

--- coins/src/adapters/other/distressedAssets.ts
+++ coins/src/adapters/other/distressedAssets.ts
@@ -44,6 +44,7 @@ const contracts: { [chain: string]: { [token: string]: string } } = {
     MOR: "0x87bade473ea0513d4aa7085484aeaa6cb6ebe7e3", //MOR
     $CINO: "0xdfe6891ce8e5a5c7cf54ffde406a6c2c54145f71", //$cino . Problem with dodo adapter on 13/03/2023 (mispriced)
     VBSWAP: "0x4f0ed527e8a95ecaa132af214dfd41f30b361600",
+    ZEDXION: "0xfbc4f3f645c4003a2e4f4e9b51077d2daa9a9341",  // price manipulated?
   },
   cronos: {
     CRK: "0x065de42e28e42d90c2052a1b49e7f83806af0e1f",

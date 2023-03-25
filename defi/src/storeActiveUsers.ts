482f453cf36285520d11ccf4ad4eb07da62fb103
0xngmi <0xngmi@protonmail.com>
2023-03-25 01:53:53 +0000

    try/catch for active users

--- defi/src/storeActiveUsers.ts
+++ defi/src/storeActiveUsers.ts
@@ -4,15 +4,19 @@ import { wrapScheduledLambda } from "./utils/shared/wrap";
 
 async function storeActiveUsers(){
     await Promise.all(chains.map(async ({name, getUsers})=>{
-        const end = Math.floor(Date.now()/1e3)
-        const start = end - 24*3600
-        const users = await getUsers(start, end)
-        await ddb.put({
-            PK: `users#${name}`,
-            SK: end,
-            start,
-            users: Number(users)
-        })
+        try{
+            const end = Math.floor(Date.now()/1e3)
+            const start = end - 24*3600
+            const users = await getUsers(start, end)
+            await ddb.put({
+                PK: `users#${name}`,
+                SK: end,
+                start,
+                users: Number(users)
+            })
+        } catch(e){
+            console.log(`Storing users for ${name} failed with error`, e)
+        }
     }))
 }
 

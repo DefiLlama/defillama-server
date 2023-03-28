acc5c26e4c7b942cadbae8491e857100de4f3d9f
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-03-28 17:52:39 +0200

    Add quarantined store data invocations (#2682)

--- defi/src/adaptors/cli/runStoreDexVolume.ts
+++ defi/src/adaptors/cli/runStoreDexVolume.ts
@@ -4,7 +4,7 @@ import { AdapterType } from "@defillama/dimension-adapters/adapters/types";
 // import dexs from "../data/dexs";
 
 handler({
-    protocolIndexes: [0],
+    protocolModules: [0],
     timestamp: 1670112000,
     adaptorType: AdapterType.FEES
 })

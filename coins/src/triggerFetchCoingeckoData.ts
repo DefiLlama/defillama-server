66dbb36b9c58ce4cdb30aa9b97fdc03bf1393127
0xngmi <0xngmi@protonmail.com>
2023-03-29 20:07:43 +0100

    shuffle coins array and remove bridge adapters from fetch function

--- coins/src/triggerFetchCoingeckoData.ts
+++ coins/src/triggerFetchCoingeckoData.ts
@@ -1,7 +1,7 @@
 import { wrapScheduledLambda } from "./utils/shared/wrap";
 import fetch from "node-fetch";
 import invokeLambda from "./utils/shared/invokeLambda";
-import { storeTokens } from "./adapters/bridges";
+import { shuffleArray } from "./utils/shared/shuffleArray";
 
 const hourlyLambda = `coins-prod-fetchHourlyCoingeckoData`
 const step = 500;
@@ -9,16 +9,13 @@ const handler = (lambdaFunctioName:string)=> async () => {
   const coins = await fetch(
     "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
   ).then((r) => r.json());
+  shuffleArray(coins)
   for (let i = 0; i < coins.length; i += step) {
     const event = {
       coins: coins.slice(i, i + step),
       depth: 0,
     };
     await invokeLambda(lambdaFunctioName, event);
-  }
-  console.log(lambdaFunctioName)
-  if(lambdaFunctioName === hourlyLambda){
-    await storeTokens()
   }
 };
 

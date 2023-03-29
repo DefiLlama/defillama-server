be82eab82c16fa698b0269e5088be8e2f445e9a6
waynebruce0x <waynebruce0x@gmail.com>
2023-03-29 16:28:42 +0100

    cglock

--- defi/src/utils/shared/coingeckoLocks.ts
+++ defi/src/utils/shared/coingeckoLocks.ts
@@ -12,7 +12,9 @@ export function releaseCoingeckoLock() {
 }
 // Rate limit is 100 calls/min for coingecko's API
 // So we'll release one every 0.6 seconds to match it
-export function setTimer(timeBetweenTicks: number = 1500) {
+// 29/03/23 CG docs now say 10-30 calls/min and we're having issues
+// Increasing timeBetweenTicks from 1500 to 3000 to see if it helps (20 calls a min)
+export function setTimer(timeBetweenTicks: number = 3000) {
   const timer = setInterval(() => {
     releaseCoingeckoLock();
   }, timeBetweenTicks);

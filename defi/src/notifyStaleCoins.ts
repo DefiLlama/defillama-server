26d33e794cf5d76a86c720949d80fc20bea2c96b
waynebruce0x <waynebruce0x@gmail.com>
2023-04-05 13:27:02 +0100

    notify

--- defi/src/notifyStaleCoins.ts
+++ defi/src/notifyStaleCoins.ts
@@ -3,6 +3,8 @@ import { sendMessage } from "./utils/discord";
 import { getCurrentUnixTimestamp } from "./utils/date";
 import postgres from "postgres";
 
+const hours = 2;
+
 const handler = async (_event: any) => {
   const webhookUrl = process.env.STALE_COINS_ADAPTERS_WEBHOOK!;
   const now = getCurrentUnixTimestamp();
@@ -16,9 +18,22 @@ const handler = async (_event: any) => {
       ${now} - lastupdate as latency
     FROM public.stalecoins
     WHERE
-      lastupdate < (${now} - 10800) and lastupdate > (${now} - 86400)
+      lastupdate < (${now - 3600 * hours}) and lastupdate > (${now - 3600 * 24})
     GROUP BY address, symbol, latency, chain
     ORDER BY latency asc;`;
+
+  const recentlyStaleCoins = staleCoins.filter(
+    (s: any) => s.latency < 3600 * (hours + 1),
+  );
+
+  const promises = [];
+  if (recentlyStaleCoins.length > 100)
+    promises.push(
+      sendMessage(
+        `At least 100 coins have just gone stale!!`,
+        process.env.TEAM_WEBHOOK,
+      ),
+    );
 
   const message = (staleCoins as any[])
     .map(
@@ -29,7 +44,8 @@ const handler = async (_event: any) => {
     )
     .join("\n");
 
-  await sendMessage(message, webhookUrl, true);
+  promises.push(sendMessage(message, webhookUrl, true));
+  await Promise.all(promises);
 };
 
 export default wrapScheduledLambda(handler);

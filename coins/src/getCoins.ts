51e1d5e8c9c533ce1c83c4004112acee2a3a7559
waynebruce0x <waynebruce0x@gmail.com>
2023-04-06 15:50:03 +0100

    getCoins patch

--- coins/src/getCoins.ts
+++ coins/src/getCoins.ts
@@ -1,9 +1,10 @@
 import { successResponse, wrap, IResponse } from "./utils/shared";
-import ddb, { batchGet } from "./utils/shared/dynamodb";
+import { batchGet } from "./utils/shared/dynamodb";
 import parseRequestBody from "./utils/shared/parseRequestBody";
 import getRecordClosestToTimestamp from "./utils/shared/getRecordClosestToTimestamp";
 import { coinToPK, DAY, PKToCoin } from "./utils/processCoin";
 import { CoinsResponse } from "./utils/getCoinsUtils";
+import { getCurrentUnixTimestamp } from "./utils/date";
 
 const handler = async (
   event: AWSLambda.APIGatewayEvent
@@ -24,26 +25,16 @@ const handler = async (
       symbol: coin.symbol,
       timestamp: coin.timestamp,
     }
-    if (coin.redirect || timestampRequested !== undefined) {
-      if (timestampRequested === undefined) {
-        const redirectedCoin = await ddb.get({
-          PK: coin.redirect,
-          SK: 0
-        })
-        if(redirectedCoin.Item === undefined){
-          return
-        }
-        formattedCoin.price = redirectedCoin.Item?.price
-        formattedCoin.timestamp = redirectedCoin.Item?.timestamp;
-      } else {
-        const finalCoin = await getRecordClosestToTimestamp(coin.redirect ?? coin.PK, timestampRequested, DAY*2);
-        if(finalCoin.SK === undefined){
-          return
-        }
-        formattedCoin.price = finalCoin.price;
-        formattedCoin.timestamp = finalCoin.SK;
-      }
+    const finalCoin = await getRecordClosestToTimestamp(
+      coin.redirect ?? coin.PK, 
+      timestampRequested ?? getCurrentUnixTimestamp(), 
+      DAY*2,
+    );
+    if(finalCoin.SK === undefined){
+      return
     }
+    formattedCoin.price = finalCoin.price;
+    formattedCoin.timestamp = finalCoin.SK;
     response[coinName] = formattedCoin;
   }))
   return successResponse({

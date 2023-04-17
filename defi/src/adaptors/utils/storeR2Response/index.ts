2be27eff531d22301095d37c050fa0012400fed3
0xtawa <0xtawanofi@gmail.com>
2023-04-17 15:23:58 +0200

    Log key on r3 error

--- defi/src/adaptors/utils/storeR2Response/index.ts
+++ defi/src/adaptors/utils/storeR2Response/index.ts
@@ -4,7 +4,7 @@ const ADAPTORS_FOLDER_KEY = 'dimensions'
 
 export const cacheResponseOnR2 = async (key: string, response: string) => {
     console.info("Storing R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
-    await storeR2JSONString(`${ADAPTORS_FOLDER_KEY}/${key}.json`, response).catch(e => console.error("R2Storage:error", e))
+    await storeR2JSONString(`${ADAPTORS_FOLDER_KEY}/${key}.json`, response).catch(e => console.error("R2Storage:error:store", key, e))
 }
 
 interface ICachedResponse<T> {
@@ -13,7 +13,7 @@ interface ICachedResponse<T> {
 
 export const getCachedResponseOnR2 = async <T>(key: string): Promise<ICachedResponse<T> | undefined> => {
     console.info("Get R2 with key", `${ADAPTORS_FOLDER_KEY}/${key}.json`)
-    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`).catch(e => console.error("R2Storage:error", e))
+    const objectString = await getR2(`${ADAPTORS_FOLDER_KEY}/${key}.json`).catch(e => console.error("R2Storage:error:get", key, e))
     if (!objectString || !objectString.body || !objectString.lastModified) return
     try {
         return {

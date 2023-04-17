e93760720b6a0aee2f85139b48058efcd8da0f15
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-04-17 19:48:50 +0200

    Revert "Add attribute in event to force refresh cache (#2985)" (#2992)
    This reverts commit ed140602def96955fcddd94af9774d9ba885dfb3.

--- defi/src/adaptors/handlers/triggerStoreAdaptorData.ts
+++ defi/src/adaptors/handlers/triggerStoreAdaptorData.ts
@@ -33,7 +33,7 @@ const quarantineList = {
 
 export const handler = async (event: IHandlerEvent) => {
   console.log("event", event)
-  const type = event?.type
+  const type = event.type
   // TODO separate those that need to be called on the hour and those using graphs with timestamp
   const quarantinedModules = quarantineList[type] ?? []
   if (event.backfill) {
@@ -43,7 +43,7 @@ export const handler = async (event: IHandlerEvent) => {
       await invokeLambdas(protocolModules, type, bf.timestamp, bf.chain, bf.adaptorRecordTypes, bf.protocolVersion)
       const protocolModulesConfined = bf.dexNames.filter(m => quarantinedModules.includes(m))
       if (protocolModulesConfined.length > 0)
-        await Promise.all(protocolModulesConfined.map((confinedModule) => invokeLambdas([confinedModule], type, bf.timestamp, bf.chain, bf.adaptorRecordTypes, bf.protocolVersion)))
+        Promise.all(protocolModulesConfined.map((confinedModule) => invokeLambdas([confinedModule], type, bf.timestamp, bf.chain, bf.adaptorRecordTypes, bf.protocolVersion)))
     }
   }
   else if (type) {
@@ -51,10 +51,10 @@ export const handler = async (event: IHandlerEvent) => {
     const protocolModules = adaptorsData.default.filter(m => !quarantinedModules.includes(m.module)).map(m => m.module)
     await invokeLambdas(protocolModules, type)
     const protocolModulesConfined = adaptorsData.default.filter(m => quarantinedModules.includes(m.module)).map(m => m.module)
-    await Promise.all(protocolModulesConfined.map((confinedModule) => invokeLambdas([confinedModule], type)))
+    Promise.all(protocolModulesConfined.map((confinedModule) => invokeLambdas([confinedModule], type)))
   }
   else {
-    await Promise.all(
+    Promise.all(
       [
         AdapterType.FEES,
         AdapterType.OPTIONS,

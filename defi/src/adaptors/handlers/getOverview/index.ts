495743c218224fcc8c52fda6a7a1103d729d7884
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-04-17 19:53:36 +0200

    Reduce dimension refresh time (#2993)

--- defi/src/adaptors/handlers/getOverview/index.ts
+++ defi/src/adaptors/handlers/getOverview/index.ts
@@ -38,8 +38,8 @@ export const handler = async (event: AWSLambda.APIGatewayEvent, enableAlerts: bo
     }
 
     const currentTiemstamp = Date.now()
-    console.debug("Current vs lastModified timestamps", currentTiemstamp, response.lastModified.getTime())
-    if ((currentTiemstamp - response.lastModified.getTime()) > 60 * 60 * 1000 || (currentTiemstamp < getTimestampAtStartOfDay(currentTiemstamp) + 60 * 60 * 3)) {
+    console.debug("lastModified", currentTiemstamp, response.lastModified.getTime())
+    if ((currentTiemstamp - response.lastModified.getTime()) > 30 * 60 * 1000 || (currentTiemstamp < getTimestampAtStartOfDay(currentTiemstamp) + 60 * 60 * 3)) {
         console.info("Response expired, invoking lambda to update it.")
         await invokeLambda("defillama-prod-getOverviewProcess", event)
     }

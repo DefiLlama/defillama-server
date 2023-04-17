cad869218320c6b5dfcd01b48cafa2af129d0f93
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-04-17 16:22:22 +0200

    Add options, aggregators and derivatives alerts (#2981)

--- defi/src/adaptors/handlers/triggerNotifyStatus.ts
+++ defi/src/adaptors/handlers/triggerNotifyStatus.ts
@@ -3,7 +3,13 @@ import invokeLambda from "../../utils/shared/invokeLambda";
 import { AdapterType } from "@defillama/dimension-adapters/adapters/types";
 
 export const handler = async () => {
-  await Promise.all([AdapterType.FEES, AdapterType.DEXS].map(type => invokeLambda(`defillama-prod-notifyStatus`, { type })))
+  await Promise.all([
+    AdapterType.FEES,
+    AdapterType.DEXS,
+    AdapterType.DERIVATIVES,
+    AdapterType.AGGREGATORS,
+    AdapterType.OPTIONS
+  ].map(type => invokeLambda(`defillama-prod-notifyStatus`, { type })))
 };
 
 export default wrapScheduledLambda(handler);

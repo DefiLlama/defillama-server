a144f53f63213696d008c79b84c7c1c58409c5c6
0xngmi <0xngmi@protonmail.com>
2023-04-18 03:44:01 +0100

    clean returned data

--- defi/src/getProtocolUsers.ts
+++ defi/src/getProtocolUsers.ts
@@ -4,7 +4,7 @@ import { getProtocolUsers } from "./users/storeUsers";
 const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
     const protocolId = event.pathParameters?.protocolId?.toLowerCase();
     const records = await getProtocolUsers(protocolId ?? "none")
-    return cache20MinResponse(records)
+    return cache20MinResponse(records.map(({start, chain, users})=>({start, chain, users})))
 }
 
 export default wrap(handler);

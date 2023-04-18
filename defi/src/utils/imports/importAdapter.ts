96486c34f22e674d3511da53b85b9ff62f708aa8
0xngmi <0xngmi@protonmail.com>
2023-04-18 02:36:18 +0100

    easier debugging on local

--- defi/src/utils/imports/importAdapter.ts
+++ defi/src/utils/imports/importAdapter.ts
@@ -2,5 +2,7 @@ import { Protocol } from "../../protocols/types";
 import adapters from "./adapters"
 
 export function importAdapter(protocol: Protocol) {
+    // Use dynamic import to debug locally, will speed up compilation by a lot
+    //return import(`@defillama/adapters/projects/${protocol.module}`)
     return (adapters as any)[protocol.module]
 }

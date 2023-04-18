f0e620d583a12656b89d112952ca233c7e1e3a67
0xngmi <0xngmi@protonmail.com>
2023-04-19 00:11:40 +0100

    improve coin api usage

--- defi/src/users/utils.ts
+++ defi/src/users/utils.ts
@@ -1,13 +1,26 @@
 import fetch from "node-fetch";
+import { getCoingeckoLock, setTimer } from "../utils/shared/coingeckoLocks";
 
 const cache = {} as {
     [key: string]: Promise<number> | undefined
 }
 
+setTimer(100);
+
 async function getAvgPrice(start: number, end: number, chain: string) {
-    const [startPrice, endPrice] = await Promise.all([start, end].map(t =>
-        fetch(`https://coins.llama.fi/prices/historical/${t}/${chain}:0x0000000000000000000000000000000000000000?searchWidth=4h`)
-            .then(r => r.json()).then(r => r.coins[`${chain}:0x0000000000000000000000000000000000000000`].price)))
+    const [startPrice, endPrice] = await Promise.all([start, end].map(async t => {
+        const coinKey = `${chain}:0x0000000000000000000000000000000000000000`
+        const url = `https://coins.llama.fi/prices/historical/${t}/${coinKey}?searchWidth=12h`
+        for (let i = 0; i < 5; i++) {
+            try {
+                await getCoingeckoLock()
+                const data = await fetch(url)
+                    .then(r => r.json())
+                return data.coins[coinKey].price
+            } catch (e) { }
+        }
+        throw new Error(`Failed at ${url}`)
+    }))
     return (startPrice + endPrice) / 2
 }
 

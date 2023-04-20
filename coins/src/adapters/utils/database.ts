f977d4f3960fe4060c6bf03d8669d3dfe9a86e65
waynebruce0x <waynebruce0x@gmail.com>
2023-04-20 16:07:12 +0100

    improve

--- coins/src/adapters/utils/database.ts
+++ coins/src/adapters/utils/database.ts
@@ -341,19 +341,22 @@ async function checkMovement(
   const filteredItems: AWS.DynamoDB.DocumentClient.PutItemInputAttributeMap[] = [];
   const obj: { [PK: string]: any } = {};
   let errors: string = "";
-  items.map((i: any) => (obj[i.PK] = i));
+  previousItems.map((i: any) => (obj[i.PK] = i));
 
-  previousItems.map((d: DbEntry, i: number) => {
-    const currentItem = obj[d.PK];
+  items.map((d: any, i: number) => {
+    if (i % 2 != 0) return;
+    const previousItem = obj[d.PK];
     const percentageChange: number =
-      Math.abs(currentItem.price - d.price) / d.price;
+      Math.abs(previousItem.price - d.price) / previousItem.price;
+
     if (percentageChange > margin) {
       errors += `${d.PK.substring(d.PK.indexOf("#") + 1)} \t ${(
         percentageChange * 100
-      ).toFixed(3)}% change from $${d.price} to $${currentItem.price}\n`;
+      ).toFixed(3)}% change from $${previousItem.price} to $${d.price}\n`;
       return;
     }
-    filteredItems.push(...[items[i * 2], items[i * 2 + 1]]);
+
+    filteredItems.push(...[items[i], items[i + 1]]);
   });
 
   if (errors != "")

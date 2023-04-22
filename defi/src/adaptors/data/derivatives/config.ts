f894cfefabd425b7b50671a41964051c960b491b
0xtawa <0xtawanofi@gmail.com>
2023-04-22 12:02:12 +0200

    Disable spikes check covo-v2

--- defi/src/adaptors/data/derivatives/config.ts
+++ defi/src/adaptors/data/derivatives/config.ts
@@ -60,10 +60,7 @@ export default {
                 "id": "2730",
                 "enabled": true,
                 cleanRecordsConfig: {
-                    genuineSpikes: {
-                        ["1681862400"]: true,
-                        ["1681776000"]: true
-                    }
+                    genuineSpikes: true
                 }
             }
         },

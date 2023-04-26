4a9aa324909c3e74e106c9790c7b2c8fdbd3f64e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-26 06:47:08 +0200

    defi: refill treasury projects

--- defi/src/cli/utils.ts
+++ defi/src/cli/utils.ts
@@ -1,9 +1,14 @@
-import protocols from "../protocols/data";
+import protocols, { treasuries } from "../protocols/data";
 
 export function getProtocol(name: string) {
-  const protocol = protocols.find(
+  let protocol = protocols.find(
     (p) => p.name.toLowerCase() === name.toLowerCase()
   );
+  if (!protocol) {
+    protocol = treasuries.find(
+      (p) => p.name.toLowerCase() === name.toLowerCase()
+    );
+  }
   if (protocol === undefined) {
     throw new Error("No protocol with that name");
   }

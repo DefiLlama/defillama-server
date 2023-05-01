16554d497f3d6739f465fde814f4ac02185a8c80
mintdart <96025197+mintdart@users.noreply.github.com>
2023-05-01 00:31:31 -0500

    fix parent protocol treasury (#3149)

--- defi/src/getTreasury.ts
+++ defi/src/getTreasury.ts
@@ -3,16 +3,50 @@ import { wrapResponseOrRedirect } from "./getProtocol";
 import craftProtocol from "./utils/craftProtocol";
 import { treasuries } from "./protocols/data";
 import sluggify from "./utils/sluggify";
+import parentProtocols from "./protocols/parentProtocols";
+import craftParentProtocol from "./utils/craftParentProtocol";
 
 const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
-  const protocolName = event.pathParameters?.protocol?.toLowerCase() + "-(treasury)"
+  const protocolName = event.pathParameters?.protocol?.toLowerCase() + "-(treasury)";
   const protocolData = treasuries.find((prot) => sluggify(prot) === protocolName);
-  if(protocolData === undefined){
+
+  if (!protocolData && event?.pathParameters?.protocol) {
+    const parentProtocolId = treasuries
+      .filter((prot) => prot.parentProtocol)
+      .find(
+        (prot) => prot.parentProtocol === "parent#" + event.pathParameters!.protocol!.toLowerCase()
+      )?.parentProtocol;
+
+    const parentProtocol = parentProtocols.find((protocol) => protocol.id === parentProtocolId);
+
+    if (!parentProtocol) {
+      return errorResponse({
+        message: "Protocol is not in our database",
+      });
+    }
+
+    const response = await craftParentProtocol({
+      parentProtocol,
+      useHourlyData: false,
+      skipAggregatedTvl: true,
+      isTreasuryApi: true,
+    });
+
+    return wrapResponseOrRedirect(response);
+  }
+
+  if (!protocolData) {
     return errorResponse({
       message: "Protocol is not in our database",
     });
   }
-  const response = await craftProtocol({ protocolData, useNewChainNames: true, useHourlyData: false, skipAggregatedTvl: true });
+
+  const response = await craftProtocol({
+    protocolData,
+    useNewChainNames: true,
+    useHourlyData: false,
+    skipAggregatedTvl: true,
+  });
 
   return wrapResponseOrRedirect(response);
 };

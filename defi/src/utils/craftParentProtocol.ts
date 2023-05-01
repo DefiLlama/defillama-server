b7ea42e4f6346531fdd2d5f6a14ff76828c98b67
mintdart <96025197+mintdart@users.noreply.github.com>
2023-05-01 03:36:07 -0500

    fix parent protocol treasury

--- defi/src/utils/craftParentProtocol.ts
+++ defi/src/utils/craftParentProtocol.ts
@@ -52,7 +52,9 @@ export default async function craftParentProtocol({
   isTreasuryApi?: boolean;
 }) {
   const childProtocols = isTreasuryApi
-    ? treasuries.filter((protocol) => protocol.parentProtocol === parentProtocol.id)
+    ? treasuries
+        .filter((protocol) => protocol.parentProtocol === parentProtocol.id)
+        .map((p) => ({ ...p, name: p.name.replace(" (treasury)", "") }))
     : protocols.filter((protocol) => protocol.parentProtocol === parentProtocol.id);
 
   if (childProtocols.length < 1 || childProtocols.map((p) => p.name).includes(parentProtocol.name)) {

216c6f2b7e0f3eac5de80558ff56afb5b4fa8281
define <Lovelovenow@protonmail.com>
2023-05-20 15:51:29 +0100

    undo

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -34892,7 +34892,7 @@ const data2: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield",
+    category: "RWA",
     chains: ["Ethereum"],
     module: "stream/index.js",
     twitter: "StreamDefi",

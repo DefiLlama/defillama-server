ea821680dc2ba5e507e71a43cc9346b49cb04f66
realdealshaman <realdealshaman11@gmail.com>
2023-05-26 17:46:50 +0100

    update category to gaming for illuvium

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -7174,7 +7174,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     audit_note: null,
     gecko_id: "illuvium",
     cmcId: "8719",
-    category: "Yield",
+    category: "Gaming",
     chains: ["Ethereum"],
     module: "illuvium/index.js",
     twitter: "illuviumio",

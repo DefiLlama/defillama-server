b86f496230024bbca49cee99c771dc3f330eb51b
0xngmi <0xngmi@protonmail.com>
2023-06-02 22:22:29 +0100

    get new users working

--- defi/src/storeNewActiveUsers.ts
+++ defi/src/storeNewActiveUsers.ts
@@ -1,4 +1,4 @@
-import { addressList } from "@defillama/dimension-adapters/users/list";
+import adapterList, { addressList } from "@defillama/dimension-adapters/users/list";
 import { wrapScheduledLambda } from "./utils/shared/wrap";
 import { PromisePool } from '@supercharge/promise-pool'
 import { shuffleArray } from "./utils/shared/shuffleArray";
@@ -6,24 +6,30 @@ import { storeNewUsers } from "./users/storeUsers";
 import { countNewUsers } from "@defillama/dimension-adapters/users/utils/countUsers";
 
 async function storeActiveUsers() {
+    const listWithNewUsers = addressList.concat(adapterList.filter((adapter:any)=>adapter.getNewUsers) as any[])
     await PromisePool
         .withConcurrency(16)
-        .for(shuffleArray(addressList))
-        .process(async ({ name, addresses, getAddresses, id }) => {
+        .for(shuffleArray(listWithNewUsers))
+        .process(async ({ name, addresses, getAddresses, id, getNewUsers }) => {
             if (!id) {
                 console.log(`No id for ${name}, skipping...`) // No id to store
                 return;
             }
             try {
-                if(!addresses){
-                    addresses = await getAddresses()
-                }
-                if(addresses.bsc !== undefined){
-                    return; // allium has no support for bsc atm
-                }
                 const end = Math.floor(Date.now() / 1e3)
                 const start = end - 24 * 3600
-                const users = await countNewUsers(addresses, start, end)
+                let users:number
+                if(getNewUsers){
+                    users = await getNewUsers(start, end)
+                } else {
+                    if(!addresses){
+                        addresses = await getAddresses()
+                    }
+                    if(addresses.bsc !== undefined){
+                        return; // allium has no support for bsc atm
+                    }
+                    users = await countNewUsers(addresses, start, end)
+                }
                 await storeNewUsers(start, end, id, "all", Number(users))
             } catch (e) {
                 console.log(`Storing users for ${name} failed with error`, e)

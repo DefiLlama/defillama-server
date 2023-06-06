6c8055c772c9555aa6237787725198e61a1b45b7
realdealshaman <realdealshaman11@gmail.com>
2023-06-06 19:45:34 +0100

    remove duplicate listing

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -36893,30 +36893,6 @@ const data2: Protocol[] = [
   },
   {
     id: "3072",
-    name: "QuipuSwap V3",
-    address: "tezos:KT193D4vozYnhGJQVtw7CoxxqphqUEEwK6Vb",
-    symbol: "QUIPU",
-    url: "https://quipuswap.com",
-    description:
-      "QuipuSwap is an open-source protocol that provides an interface for the seamless decentralized exchange of Tezos-based Tokens, stable swap with dividends, and farming features. Liquidity providers may earn fees from trading, baking rewards, or receive rewards from staking tokens on farms. QuipuSwap smart contracts were developed by the MadFish Team and audited by Least Authority and  Runtime Verification companies.",
-    chain: "Tezos",
-    logo: `${baseIconsUrl}/quipuswap.png`,
-    audits: "2",
-    audit_note: null,
-    gecko_id: null,
-    cmcId: null,
-    category: "Dexes",
-    chains: ["Tezos"],
-    module: "quipuswap-v3/index.js",
-    twitter: "QuipuSwap",
-    audit_links: [
-      "https://leastauthority.com/static/publications/LeastAuthority_Tezos_Foundation_QuipuSwap_Smart_Contracts_Final_Audit_Report.pdf",
-    ],
-    parentProtocol: "parent#quipuswap",
-    listedAt: 1686076121
-  },
-  {
-    id: "3073",
     name: "Dubble Exchange",
     address: "arbitrum:0xfe3e29361eDDaC64411722854B4FdFCe8e84D7Cd",
     symbol: "DUB",
@@ -36937,7 +36913,6 @@ const data2: Protocol[] = [
     forkedFrom: [],
     audit_links: [],
     listedAt: 1686076139
-
   },
 ]
 export default data2;

fb747be4ed6e8105f50af1480b66c53e069a0254
0xngmi <0xngmi@protonmail.com>
2023-06-12 17:16:47 +0100

    remove lavamoat nfts from yuga

--- defi/src/nfts/parentCompanies.ts
+++ defi/src/nfts/parentCompanies.ts
@@ -14,8 +14,8 @@ export const nftParentCompanies: NFTCompany[] = [
             ["0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d", "BAYC"],
             ["0x60e4d786628fea6478f785a6d7e704777c86a7c6", "MAYC"],
             ["0xba30e5f9bb24caa003e9f2f0497ad287fdf95623", "BAKC"],
-            ["0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb", "Cryptopunks"],
-            ["0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7", "Meebits"],
+            //["0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb", "Cryptopunks"],
+            //["0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7", "Meebits"],
             ["0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258", "Otherdeed for Otherside"]
         ]
     },

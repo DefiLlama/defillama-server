b7bd236d607c3e8a113b7acc112b1caae112833e
waynebruce0x <waynebruce0x@gmail.com>
2023-06-19 11:54:02 +0100

    store refactor

--- defi/src/storeEmissions.ts
+++ defi/src/storeEmissions.ts
@@ -2,75 +2,112 @@ import { createChartData } from "../emissions-adapters/utils/convertToChartData"
 import { createRawSections } from "../emissions-adapters/utils/convertToRawData";
 import { createCategoryData } from "../emissions-adapters/utils/categoryData";
 import adapters from "./utils/imports/emissions_adapters";
-import { ChartSection } from "../emissions-adapters/types/adapters";
+import { ApiChartData, ChartSection, Protocol, SectionData } from "../emissions-adapters/types/adapters";
 import { storeR2JSONString, getR2 } from "./utils/r2";
 import { wrapScheduledLambda } from "./utils/shared/wrap";
 import protocols from "./protocols/data";
 import { sluggifyString } from "./utils/sluggify";
 import parentProtocols from "./protocols/parentProtocols";
-import { PromisePool } from '@supercharge/promise-pool'
+import { PromisePool } from "@supercharge/promise-pool";
 import { shuffleArray } from "./utils/shared/shuffleArray";
+import { sendMessage } from "./utils/discord";
 
-function wait(time: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
+type Chart = { label: string; data: ApiChartData[] | undefined };
+
+function findPId(token: string) {
+  const prefix = "coingecko:";
+  const idStart = token.indexOf(prefix);
+  if (idStart == -1) return null;
+  const cgId = token.substring(idStart + prefix.length);
+  return protocols.find((p) => p.gecko_id == cgId);
+}
+
+function aggregateMetadata(chart: Chart[], rawData: SectionData) {
+  const pId = rawData.metadata.protocolIds?.[0] ?? null;
+  const pData = pId && pId !== "" ? protocols.find((p) => p.id == pId) : findPId(rawData.metadata.token);
+  if (!pData) throw new Error(`token ${rawData.metadata.token} has no pData`);
+  const id = pData.parentProtocol || pData.name;
+  let name = id;
+  if (pData?.parentProtocol) {
+    name = parentProtocols.find((p) => p.id === pData.parentProtocol)?.name ?? id;
+  }
+
+  const tokenAllocation = createCategoryData(chart, rawData.categories, false);
+
+  return {
+    data: {
+      data: chart,
+      metadata: rawData.metadata,
+      name: name,
+      gecko_id: pData?.gecko_id,
+      tokenAllocation,
+    },
+    id,
+  };
+}
+
+async function processSingleProtocol(
+  adapter: Protocol,
+  protocolName: string,
+  protocolsArray: string[]
+): Promise<string[]> {
+  const rawData = await createRawSections(adapter);
+
+  const chart: Chart[] = (await createChartData(protocolName, rawData, false)).map((s: ChartSection) => ({
+    label: s.section,
+    data: s.data.apiData,
+  }));
+
+  const { data, id } = aggregateMetadata(chart, rawData);
+  const sluggifiedId = sluggifyString(id).replace("parent#", "");
+
+  await storeR2JSONString(`emissions/${sluggifiedId}`, JSON.stringify(data));
+  protocolsArray.push(sluggifiedId);
+  console.log(protocolName);
+
+  return protocolsArray;
 }
 
 async function handler() {
   let protocolsArray: string[] = [];
-  // https://github.com/apollographql/apollo-client/issues/4843#issuecomment-495717720
-  // https://stackoverflow.com/questions/53162001/typeerror-during-jests-spyon-cannot-set-property-getrequest-of-object-which
-  await PromisePool
-  .withConcurrency(5)
-  .for(shuffleArray(Object.entries(adapters)))
-  .process(async ([protocolName, rawAdapter]) => {
-    try {
-      console.log(protocolName);
-      await wait(2000);
-      const adapter = typeof rawAdapter.default === "function" ? { default: await rawAdapter.default() } : rawAdapter;
-      const rawData = await createRawSections(adapter);
+  let protocolErrors: string[] = [];
 
-      const chart = (await createChartData(protocolName, rawData, false)).map((s: ChartSection) => ({
-        label: s.section,
-        data: s.data.apiData,
-      }));
-
-      const tokenAllocation = createCategoryData(chart, rawData.categories, false);
-
-      const pId = rawData.metadata?.protocolIds?.[0] ?? null;
-      const pData = pId && pId !== "" ? protocols.find((p) => p.id == pId) : null;
-      const id = pData ? pData.parentProtocol || pData.name : protocolName;
-      let name = id;
-      if (pData?.parentProtocol) {
-        name = parentProtocols.find((p) => p.id === pData.parentProtocol)?.name ?? id;
+  await PromisePool.withConcurrency(2)
+    .for(shuffleArray(Object.entries(adapters)))
+    .process(async ([protocolName, rawAdapter]) => {
+      try {
+        let adapters = typeof rawAdapter.default === "function" ? await rawAdapter.default() : rawAdapter.default;
+        if (!adapters.length) adapters = [adapters];
+        await Promise.all(
+          adapters.map((adapter: Protocol) =>
+            processSingleProtocol(adapter, protocolName, protocolsArray)
+              .then((p: string[]) => (protocolsArray = p))
+              .catch((err: Error) => {
+                console.log(err.message, `: \n storing ${protocolName}`);
+                protocolErrors.push(protocolName);
+              })
+          )
+        );
+      } catch (err) {
+        console.log(err, ` storing ${protocolName}`);
+        protocolErrors.push(protocolName);
       }
-      const data = {
-        data: chart,
-        metadata: rawData.metadata,
-        name: name,
-        gecko_id: pData?.gecko_id,
-        tokenAllocation,
-      };
-      const sluggifiedId = sluggifyString(id).replace("parent#", "");
+    });
 
-      await storeR2JSONString(`emissions/${sluggifiedId}`, JSON.stringify(data));
-      protocolsArray.push(sluggifiedId);
-    } catch (err) {
-      console.log(err, ` storing ${protocolName}`);
-    }
-  })
+  await handlerErrors(protocolErrors);
+  const res = await getR2(`emissionsProtocolsList`);
+  if (res.body) protocolsArray = [...new Set([...protocolsArray, ...JSON.parse(res.body)])];
+  await storeR2JSONString(`emissionsProtocolsList`, JSON.stringify(protocolsArray));
+}
 
-  const errorMessage: string = `Tried to write emissionsProtocolsList as an empty array, Unlocks page needs updating manually.`;
-
-  if (protocolsArray.length == 0) {
-    //await sendMessage(errorMessage, process.env.TEAM_WEBHOOK!);
+async function handlerErrors(errors: string[]) {
+  if (errors.length > 0) {
+    let errorMessage: string = `storeEmissions errors: `;
+    errors.map((e: string) => (errorMessage += `${e}, `));
+    await sendMessage(errorMessage, process.env.TEAM_WEBHOOK!);
     throw new Error(errorMessage);
-  } else {
-    const res = await getR2(`emissionsProtocolsList`);
-    if (res.body) protocolsArray = [...new Set([...protocolsArray, ...JSON.parse(res.body)])];
-    await storeR2JSONString(`emissionsProtocolsList`, JSON.stringify(protocolsArray));
   }
 }
 
 export default wrapScheduledLambda(handler);
+// handler(); // ts-node src/storeEmissions.ts

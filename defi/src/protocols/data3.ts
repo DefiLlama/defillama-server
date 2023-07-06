b21b5b405346601f3196441d3d7fc6551b664ad3
Kyli Bing <kylidboy@hashmix.org>
2023-07-06 11:30:52 +0800

    feat: move Hashmix to Liquid Staking

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -7405,7 +7405,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Staking Pool",
+    category: "Liquid Staking",
     chains: ["Filecoin"],
     module: "hashmix/index.js",
     twitter: "HashMixOfficial",

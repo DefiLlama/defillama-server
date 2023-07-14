167960f601a2b209562a305e2aeada11170a9b5f
sleezyggg <139578304+sleezyggg@users.noreply.github.com>
2023-07-14 19:14:29 +0200

    Update data3.ts

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -7924,7 +7924,7 @@ const data3: Protocol[] = [
     chains: ["Ethereum"],
     module: "raft/index.js",
     twitter: "raft_fi",
-    oracles: ["Chainlink", "Tellor"],
+    oracles: ["Chainlink"],
     forkedFrom: ["Liquity"],
     audit_links: [ "https://github.com/trailofbits/publications/blob/master/reviews/2023-04-tempus-raft-securityreview.pdf"],
     github: ["raft-fi"],

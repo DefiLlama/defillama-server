7f0a477294c6d65d71db0ccb0604b9237b78f833
Cody DallaValle <cody.dallavalle@gmail.com>
2023-07-15 12:16:15 -0400

    Added Snapshot and Tally ID's to Stakehouse/Blockswap

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -2818,7 +2818,8 @@ const data3: Protocol[] = [
     audit_links: [
       "https://www.blockswap.network/security"
     ],
-    listedAt: 1681518744
+    listedAt: 1681518744,
+	governanceID: ["snapshot:blockswapdao.eth", "eip155:1:0x8869A94df9200C75116A285e12E85C24179129E1"]
   },
   {
     id: "2839",

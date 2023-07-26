c62e1fd94007d6f1de62163e563c945f84edd743
define <Lovelovenow@protonmail.com>
2023-07-26 15:13:36 +0100

    category v3

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -11038,7 +11038,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquidity manager",
+    category: "Launchpad",
     chains: ["Ethereum", "Arbitrum", "Binance"],
     module: "unicrypt-v3/index.js",
     twitter: "UNCX_token",

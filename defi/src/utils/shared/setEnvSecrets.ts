a3702b69bec94d1fdeed74fa4b25d27f22883564
0xngmi <0xngmi@protonmail.com>
2023-07-26 16:41:36 +0100

    add tracing

--- defi/src/utils/shared/setEnvSecrets.ts
+++ defi/src/utils/shared/setEnvSecrets.ts
@@ -1,14 +1,17 @@
 import dynamodb from './dynamodb'
 
 async function setEnvSecrets() {
+  console.log("env", process.env.OLYMPUS_GRAPH_API_KEY)
   try {
     const { Item } = await dynamodb.getEnvSecrets()
+    console.log(Item)
     Object.entries((Item as any)).forEach(([key, value]: any) => {
       if (key !== 'PK' && key !== 'SK') process.env[key] = value
     })
   } catch (e) {
     console.log('Unable to get env secrets: ', e)
   }
+  console.log("env2", process.env.OLYMPUS_GRAPH_API_KEY)
 }
 
 export default setEnvSecrets;

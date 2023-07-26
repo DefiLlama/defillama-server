8c5af951f38752cf8c2a217a555f0b59fea43b4d
0xngmi <0xngmi@protonmail.com>
2023-07-26 16:43:00 +0100

    remove log

--- defi/src/utils/shared/setEnvSecrets.ts
+++ defi/src/utils/shared/setEnvSecrets.ts
@@ -4,7 +4,6 @@ async function setEnvSecrets() {
   console.log("env", process.env.OLYMPUS_GRAPH_API_KEY)
   try {
     const { Item } = await dynamodb.getEnvSecrets()
-    console.log(Item)
     Object.entries((Item as any)).forEach(([key, value]: any) => {
       if (key !== 'PK' && key !== 'SK') process.env[key] = value
     })

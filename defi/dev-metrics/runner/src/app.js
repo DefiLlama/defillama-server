7f7719ca7493723a0bedfa10a44525a98d697616
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-11 17:14:51 +0200

    [dev-metrics] init runner

--- defi/dev-metrics/runner/src/app.js
+++ defi/dev-metrics/runner/src/app.js
@@ -1,58 +1,109 @@
 const { spawn } = require('child_process');
 const cron = require('node-cron');
 
-// Schedule a cron job to run the task every hour
+// run every hour
 cron.schedule('0 * * * *', pullRepoCode)
 cron.schedule('5 * * * *', updateAppData)
+cron.schedule('6 * * * *', downloadGitTomlFile)
 
-// Pull the latest code from the repo
-function pullRepoCode() {
-  console.log('Pulling Repo code...');
+// run every 12 hours
+cron.schedule('10 */12 * * *', updateGithubData)
 
-  // Replace with your GitHub repository URL
-  const repoUrl = 'https://github.com/DefiLlama/defillama-server'
+// run every 3rd day
+cron.schedule('0 11 */3 * *', updateTwitterData)
+cron.schedule('55 2 */3 * *', createMappingFromElectricRepo)
 
-  const bashCode = `
-  [ ! -d "repo" ] && git clone  --depth 1 ${repoUrl} repo
-  echo $(pwd)
-  cd repo
-  git checkout master
-  git pull
-  cd defi
-  npm uninstall @defillama/emissions-adapters @defillama/dimension-adapters
-  npm i
-  # npm update @defillama/sdk
-  cd dev-metrics
-  npm i
-  git stash
-  `
+function updateTwitterData() {
+  return runNpmCommand({
+    name: 'Updating twitter data',
+    script: `update-twitter`
+  })
+}
 
-  const childProcess = spawn('bash', ['-c', bashCode], { stdio: 'inherit' });
+function updateGithubData() {
+  return runNpmCommand({
+    name: 'Updating github data',
+    script: `update-github`
+  })
+}
 
-  childProcess.on('close', (code) => {
-    console.log(`Repo code pulled, exiting with code: ${code}`);
+function createMappingFromElectricRepo() {
+  return runNpmCommand({
+    name: 'Updating twitter data',
+    script: `get-electric-mapping`
+  })
+}
+
+function downloadGitTomlFile() {
+  return runNpmCommand({
+    name: 'Download git toml file',
+    script: `download-git-toml`
   })
 }
 
 function updateAppData() {
-  console.log('Updating data mapping for twitter & github...');
-
-  const bashCode = `
-  cd repo/defi/dev-metrics
-  npm run update-dev-mapping
-  `
-
-  const childProcess = spawn('bash', ['-c', bashCode], { stdio: 'inherit' });
-
-  childProcess.on('close', (code) => {
-    console.log(`Repo code pulled, exiting with code: ${code}`);
+  return runNpmCommand({
+    name: 'Updating data mapping for twitter & github',
+    script: `update-dev-mapping`
   })
 }
 
-function onStart() {
+function pullRepoCode() {
+  const repoUrl = 'https://github.com/DefiLlama/defillama-server'
+  return spawnPromise({
+    name: 'Pull repo code',
+    bashCode: `
+      [ ! -d "repo" ] && git clone  --depth 1 ${repoUrl} repo
+      echo $(pwd)
+      cd repo
+      git checkout master
+      git pull
+      cd defi
+      npm uninstall @defillama/emissions-adapters @defillama/dimension-adapters
+      npm i
+      # npm update @defillama/sdk
+      cd dev-metrics
+      npm i
+      git stash
+    `
+  })
+}
+
+async function onStart() {
   console.log('Starting the app...');
-  pullRepoCode()
-  updateAppData()
+  await pullRepoCode()
+  await updateAppData()
+  await downloadGitTomlFile()
 }
 
 onStart()
+
+async function spawnPromise({ bashCode, name }) {
+  console.log('[Start]', name)
+  return new Promise((resolve, reject) => {
+    const childProcess = spawn('bash', ['-c', bashCode], { stdio: 'inherit' });
+
+    childProcess.on('close', (code) => {
+      console.log('[Done] ', name)
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(new Error(`Child process exited with code ${code}`));
+      }
+    });
+
+    /* childProcess.on('error', (error) => {
+      reject(error);
+    }); */
+  });
+}
+
+async function runNpmCommand({ script, name }) {
+  return spawnPromise({
+    name,
+    bashCode: `
+      cd repo/defi/dev-metrics
+      npm run ${script}
+    `
+  })
+}

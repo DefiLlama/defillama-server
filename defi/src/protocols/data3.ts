3905adcd9cd9b9bccb583dccd21de3fb8fcf5ab4
Yuval Weiss <yuval.weiss.ace@gmail.com>
2023-08-11 10:17:33 +0100

    update Nostra category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -2514,7 +2514,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "CDP",
+    category: "Lending",
     chains: ["Starknet"],
     oracles: [],
     forkedFrom: [],

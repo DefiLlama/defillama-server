5284e1b60ef391517210b12b32b894d8eba6f063
waynebruce0x <waynebruce0x@gmail.com>
2023-08-14 15:37:15 +0100

    bvm

--- coins/src/adapters/markets/uniswap/index.ts
+++ coins/src/adapters/markets/uniswap/index.ts
@@ -305,3 +305,13 @@ export function fvm(timestamp: number = 0) {
     timestamp,
   );
 }
+export function velocimeter(timestamp: number = 0) {
+  console.log("starting velocimeter");
+  return getTokenPrices(
+    "base",
+    "0xe21Aac7F113Bd5DC2389e4d8a8db854a87fD6951",
+    "0xE11b93B61f6291d35c5a2beA0A9fF169080160cF",
+    undefined,
+    timestamp,
+  );
+}

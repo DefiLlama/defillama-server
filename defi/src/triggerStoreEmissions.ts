db06b81715b83f53e557058ceb4a379edf68919c
waynebruce0x <waynebruce0x@gmail.com>
2023-08-16 16:16:51 +0100

    better trigger store emissions

--- defi/src/triggerStoreEmissions.ts
+++ defi/src/triggerStoreEmissions.ts
@@ -1,20 +1,21 @@
 import invokeLambda from "./utils/shared/invokeLambda";
 import adapters from "./utils/imports/emissions_adapters";
 
-function shuffleArray(array: [string, any][]) {
+function shuffleArray(array: number[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-const step = 40;
+const step = 10;
 export default async function handler() {
   let adaptersArray = Object.entries(adapters);
-  shuffleArray(adaptersArray);
-  for (let i = 0; i < adaptersArray.length; i += step) {
+  const protocolIndexes: number[] = Array.from(Array(adaptersArray.length).keys());
+  shuffleArray(protocolIndexes);
+  for (let i = 0; i < protocolIndexes.length; i += step) {
     const event = {
-      adapters: adaptersArray.slice(i, i + step),
+      protocolIndexes: protocolIndexes.slice(i, i + step),
     };
     await invokeLambda(`defillama-prod-storeEmissions`, event);
   }

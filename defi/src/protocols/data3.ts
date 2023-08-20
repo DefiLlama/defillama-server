8903f5eacc75163928d58fb99b1032571f484e86
realdealshaman <realdealshaman11@gmail.com>
2023-08-20 22:30:34 +0100

    fix listing

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -15678,7 +15678,6 @@ const data3: Protocol[] = [
     forkedFrom: [],
     oracles: [],
     treasury: "meowl.js",
-    audit_links: ["https://github.com/freshcoins/Smart-Contract-Audits/blob/main/UncleSam_0x80a3d4999b2836a5ac90b4742da133f5f5f6c27d.pdf"],
     github: ["meowlxyz"],
     listedAt: 1692560976
   },

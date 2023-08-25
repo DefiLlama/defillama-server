0c5aa9548d9fc113b008670a6e7ae37faa501a13
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-25 15:53:04 +0200

    [TVL] change when mysql connection is created

--- defi/src/storeTvlInterval/errorDb.ts
+++ defi/src/storeTvlInterval/errorDb.ts
@@ -7,21 +7,27 @@ import mysql from 'mysql2/promise';
 // CREATE TABLE timeouts (time INT, protocol VARCHAR(200), PRIMARY KEY(time, protocol), INDEX `idx_time` (`time` ASC) VISIBLE);
 // CREATE TABLE staleCoins (time INT, address VARCHAR(500), lastUpdate INT, chain VARCHAR(200), symbol VARCHAR(200), PRIMARY KEY(time, address, chain), INDEX `idx_time` (`time` ASC) VISIBLE);
 
-const connection = mysql.createPool({
-  host: process.env.ERROR_DB,
-  port: 9007,
-  user: 'clh3m0sco0zwbbso49r00gqyo',
-  database: 'clh3m0sco0zwdbso44e9u269x',
-  password: process.env.INFLUXDB_TOKEN,
-  waitForConnections: true,
-  connectionLimit: 1,
-});
+let connection: mysql.Pool | undefined;
+
+function getConnection() {
+  if (!connection)
+    connection = mysql.createPool({
+      host: process.env.ERROR_DB,
+      port: 9007,
+      user: 'clh3m0sco0zwbbso49r00gqyo',
+      database: 'clh3m0sco0zwdbso44e9u269x',
+      password: process.env.INFLUXDB_TOKEN,
+      waitForConnections: true,
+      connectionLimit: 1,
+    })
+  return connection
+}
 
 export function execute(sql: string, values: any){
-  return connection.execute(sql, values)
+  return getConnection().execute(sql, values)
 }
 
 export function executeAndIgnoreErrors(sql: string, values: any){
-  return connection.execute(sql, values)
+  return getConnection().execute(sql, values)
     .catch(e => console.log("mysql error", e));
 }

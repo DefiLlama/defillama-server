ad2e456757ee0724356a0d030cc2ac02b235d805
realdealshaman <realdealshaman11@gmail.com>
2023-08-29 04:21:15 +0100

    comment wx network

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -10961,6 +10961,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     audit_links: ["https://backend.swop.fi/files/en/deemru.pdf", "https://backend.swop.fi/files/en/fairyproof.pdf"],
     github: ["swopfi"]
   },
+  /*
   {
     id: "614",
     name: "WX Network",
@@ -10981,6 +10982,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     twitter: "WXNetwork",
     github: ["waves-exchange"]
   },
+  */
   {
     id: "615",
     name: "BNBMiner Finance",

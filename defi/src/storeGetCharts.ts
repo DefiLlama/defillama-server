84274f71933728065fc6cb956924b2a30b5f792a
0xngmi <0xngmi@protonmail.com>
2023-09-06 14:36:07 +0100

    undo

--- defi/src/storeGetCharts.ts
+++ defi/src/storeGetCharts.ts
@@ -258,10 +258,8 @@ const handler = async (_event: any) => {
 
   const dataFalseTrue = getHistoricalTvlForAllProtocols(false, true);
   const dataFalseFalse = getHistoricalTvlForAllProtocols(false, false);
-  await Promise.all([
-    storeR2JSONString("cache/getHistoricalTvlForAllProtocols/false-true.json", JSON.stringify(await dataFalseTrue)),
-    storeR2JSONString("cache/getHistoricalTvlForAllProtocols/false-false.json", JSON.stringify(await dataFalseFalse))
-  ])
+  await storeR2JSONString("cache/getHistoricalTvlForAllProtocols/false-true.json", JSON.stringify(await dataFalseTrue))
+  await storeR2JSONString("cache/getHistoricalTvlForAllProtocols/false-false.json", JSON.stringify(await dataFalseFalse))
 };
 
 export default wrapScheduledLambda(handler);

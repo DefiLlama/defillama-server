d944bbdd437c88efb4388d518afd20cdadab89bf
define <Lovelovenow@protonmail.com>
2023-09-06 12:51:46 +0100

    add stablecoin raft

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -8213,6 +8213,7 @@ const data3: Protocol[] = [
     oracles: ["Chainlink"],
     audit_links: [ "https://github.com/trailofbits/publications/blob/master/reviews/2023-04-tempus-raft-securityreview.pdf"],
     github: ["raft-fi"],
+    stablecoins: ["r"],
     listedAt: 1686074293
   },
   {

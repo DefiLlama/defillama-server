e106cf1754925c1d665456b528abb942a8021e89
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-13 12:21:31 +0200

    [coins] curve:  consolidate code

--- coins/src/adapters/markets/curve/index.ts
+++ coins/src/adapters/markets/curve/index.ts
@@ -1,4 +1,3 @@
-import getTokenPrices from "./curve";
 import getTokenPrices2 from "./curve2";
 import getGaugePrices from "./gauges";
 import getSpareTokens from "./spares";
@@ -15,68 +14,13 @@ export function curve1(timestamp: number = 0) {
     getTokenPrices2("optimism", defaultRegistries, timestamp),
     getTokenPrices2("polygon", defaultRegistries, timestamp),
     getTokenPrices2("celo", defaultRegistries, timestamp),
-    getTokenPrices2("xdai", defaultRegistries, timestamp),
+    getTokenPrices2("bsc", defaultRegistries, timestamp),
     getTokenPrices2("avax", defaultRegistries, timestamp),
+    getTokenPrices2("bsc", ['pcs'], timestamp, 'pcs'),
+    getTokenPrices2("xdai", defaultRegistries, timestamp),
   ]);
-
-}
-export function curve2(_timestamp: number = 0) {
-  console.log("starting curve2");
-  return Promise.all([]);
-}
-export function curve3(_timestamp: number = 0) {
-  console.log("starting curve3");
-  return Promise.all([]);
-}
-export function curve4(_timestamp: number = 0) {
-  console.log("starting curve4");
-  return Promise.all([]);
-}
-export function curve5a(_timestamp: number = 0) {
-  console.log("starting curve5a");
-  return Promise.all([]);
-}
-export function curve5b(_timestamp: number = 0) {
-  console.log("starting curve5b");
-  return Promise.all([]);
-}
-export function curve5c(_timestamp: number = 0) {
-  console.log("starting curve5c");
-  return Promise.all([]);
-}
-export function curve5d(_timestamp: number = 0) {
-  console.log("starting curve5d");
-  return Promise.all([]);
-}
-export function curve6(_timestamp: number = 0) {
-  console.log("starting curve6");
-  return Promise.all([]);
-}
-export function curve6b(_timestamp: number = 0) {
-  console.log("starting curve6b");
-  return Promise.all([]);
-}
-export function curve6c(_timestamp: number = 0) {
-  console.log("starting curve6c");
-  return Promise.all([]);
-}
-export function curve7(_timestamp: number = 0) {
-  console.log("starting curve7");
-  return Promise.all([]);
 }
 
-export function ellipsis(timestamp: number = 0) {
-  console.log("starting ellipsis");
-  return getTokenPrices(
-    "bsc",
-    ["crypto", "stableswap", "stableFactory", "cryptoFactory"],
-    timestamp,
-  );
-}
-export function pcsStable(timestamp: number = 0) {
-  console.log("starting PCS stableswap");
-  return getTokenPrices("bsc", ["pcs"], timestamp, "pcs");
-}
 export function gauges(timestamp: number = 0) {
   console.log("starting gauges");
   return Promise.all([

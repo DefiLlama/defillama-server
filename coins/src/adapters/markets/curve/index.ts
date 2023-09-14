c2bd9813b6d94f12cd0c66fbca605c0d70b3c267
waynebruce0x <waynebruce0x@gmail.com>
2023-09-14 14:07:57 +0100

    curve2

--- coins/src/adapters/markets/curve/index.ts
+++ coins/src/adapters/markets/curve/index.ts
@@ -1,28 +1,37 @@
 import getTokenPrices2 from "./curve2";
 import getGaugePrices from "./gauges";
 
-const defaultRegistries = ['stableswap', 'crypto', 'stableFactory', 'cryptoFactory'];
+const defaultRegistries = [
+  "stableswap",
+  "crypto",
+  "stableFactory",
+  "cryptoFactory",
+];
 export function curve(timestamp: number = 0) {
   console.log("starting curve");
   return Promise.all([
-
     getTokenPrices2("ethereum", ["crypto"], timestamp),
     getTokenPrices2("arbitrum", defaultRegistries, timestamp),
+    getTokenPrices2("optimism", defaultRegistries, timestamp),
+    getTokenPrices2("polygon", defaultRegistries, timestamp),
+    getTokenPrices2("avax", defaultRegistries, timestamp),
+    getTokenPrices2("ethereum", [], timestamp, "eth-custom", [
+      "0x7b0eff0c991f0aa880481fdfa5624cb0bc9b10e1",
+    ]),
+  ]);
+}
+export function curve2(timestamp: number = 0) {
+  console.log("starting curve2");
+  return Promise.all([
     getTokenPrices2("fantom", defaultRegistries, timestamp),
     getTokenPrices2("moonbeam", defaultRegistries, timestamp),
     getTokenPrices2("aurora", defaultRegistries, timestamp),
-    getTokenPrices2("optimism", defaultRegistries, timestamp),
-    getTokenPrices2("polygon", defaultRegistries, timestamp),
     getTokenPrices2("celo", defaultRegistries, timestamp),
     getTokenPrices2("bsc", defaultRegistries, timestamp),
-    getTokenPrices2("avax", defaultRegistries, timestamp),
-    getTokenPrices2("bsc", ['pcs'], timestamp, 'pcs'),
+    getTokenPrices2("bsc", ["pcs"], timestamp, "pcs"),
     getTokenPrices2("xdai", defaultRegistries, timestamp),
-    getTokenPrices2("ethereum", [], timestamp, "eth-custom", ['0x7b0eff0c991f0aa880481fdfa5624cb0bc9b10e1']),
-
   ]);
 }
-
 export function gauges(timestamp: number = 0) {
   console.log("starting gauges");
   return Promise.all([

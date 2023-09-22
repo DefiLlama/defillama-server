b97d5555e13fa62c5fdd59a7f2f58ed694639df6
0xngmi <0xngmi@protonmail.com>
2023-09-23 00:17:09 +0100

    change methodology for storing users

---
+++ defi/src/storeActiveUsersRetrieve.ts
@@ -0,0 +1,38 @@
+import { wrapScheduledLambda } from "./utils/shared/wrap";
+import { PromisePool } from '@supercharge/promise-pool'
+import { shuffleArray } from "./utils/shared/shuffleArray";
+import { storeNewUsers } from "./users/storeUsers";
+import { getR2 } from "./utils/r2";
+import { storeUserInfo, userQueriesFilename } from "./storeActiveUsers";
+import { retrieveAlliumResults } from "@defillama/dimension-adapters/helpers/allium";
+import { parseUserResponse } from "@defillama/dimension-adapters/users/utils/countUsers";
+
+async function storeActiveUsers() {
+    const queries = JSON.parse((await getR2(userQueriesFilename)).body!)
+    await PromisePool
+        .withConcurrency(40)
+        .for(shuffleArray(queries))
+        .process(async ({ users, start, end, id, name, type }) => {
+            if (!id) {
+                console.log(`No id for ${name}, skipping...`) // No id to store
+                return;
+            }
+            try {
+                if(type === "newUsers"){
+                    const query = await retrieveAlliumResults(users.queryId)
+                    const result = query[0].user_count
+                    if(result === undefined){
+                        throw new Error("no numbers")
+                    }
+                    await storeNewUsers(start, end, id, "all", Number(result))
+                } else {
+                    const result = await parseUserResponse(users.queryId, users.params)
+                    await storeUserInfo(result, start, end, id)
+                }
+            } catch (e) {
+                console.log(`Storing users for ${name} failed with error`, e)
+            }
+        })
+}
+
+export default wrapScheduledLambda(storeActiveUsers);

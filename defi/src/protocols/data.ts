1eaa6e66ec98b01ad8c1d8b6b1922674baaee545
Larry the Cucumber <71284258+larrythecucumber321@users.noreply.github.com>
2023-10-18 12:11:09 -0700

    Update RSR token address (#4653)

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -11212,7 +11212,7 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
   {
     id: "626",
     name: "Reserve",
-    address: "0x8762db106b2c2a0bccb3a80d1ed41273552616e8",
+    address: "0x320623b8e4ff03373931769a31fc52a4e78b5d70",
     symbol: "RSR",
     url: "https://reserve.org",
     description:

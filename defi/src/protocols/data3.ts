7a95d6a05da85909dc69e9a51234be68f059037d
supper <mflowers03291117@gmail.com>
2023-10-27 10:14:19 +0800

    add Aptin Finance v2

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -22370,6 +22370,29 @@ const data3: Protocol[] = [
     audit_links: ["https://beosin.com/audits/Match-Finance_202309281659.pdf"],
     listedAt: 1698345189
   },
+  {
+    id: "3705",
+    name: "Aptin Finance v2",
+    address: null,
+    symbol: "-",
+    url: "https://aptin.io",
+    description:
+      "Aptin is an algorithmic, decentralized protocol for lending and borrowing on Aptos. Aptin enables users to effortlessly lend, borrow, and earn interest within a global network.",
+    chain: "Aptos",
+    logo: `${baseIconsUrl}/aptin-finance.png`,
+    audits: "2",
+    audit_note: null,
+    gecko_id: null,
+    cmcId: null,
+    category: "Lending",
+    chains: ["Aptos"],
+    module: "aptin-finance-v2/index.js",
+    twitter: "AptinLabs",
+    forkedFrom: [],
+    oracles: ["Switchboard", "Pyth"],
+    audit_links: ["https://github.com/AptinLabs/audit-report/blob/main/aptin_audit_report.pdf"],
+    listedAt: 1698372809
+  },
 ]
 
 export default data3

79afab9015aacd027400b1474e5eec0b2c90941c
mintdart <96025197+mintdart@users.noreply.github.com>
2023-11-02 00:30:17 -0500

    return totalVolume7d instead of change_7d_abs (#4827)

--- defi/src/adaptors/handlers/getOverviewProcess/index.ts
+++ defi/src/adaptors/handlers/getOverviewProcess/index.ts
@@ -26,9 +26,8 @@ export interface IGeneralStats extends ExtraTypes {
     change_1d: number | null;
     change_7d: number | null;
     change_1m: number | null;
-    change_1d_abs: number | null;
-    change_7d_abs: number | null;
-    change_30d_abs: number | null;
+    totalVolume7d: number | null;
+    totalVolume30d: number | null;
     change_7dover7d: number | null;
     change_30dover30d: number | null;
     breakdown24h: IRecordAdaptorRecordData | null
@@ -281,9 +280,8 @@ export const handler = async (event: AWSLambda.APIGatewayEvent, enableAlerts: bo
         change_1d: enableStats ? generalStats.change_1d : null,
         change_7d: enableStats ? generalStats.change_7d : null,
         change_1m: enableStats ? generalStats.change_1m : null,
-        change_1d_abs: enableStats ? generalStats.change_1d_abs : null,
-        change_7d_abs: enableStats ? generalStats.change_7d_abs : null,
-        change_30d_abs: enableStats ? generalStats.change_30d_abs : null,
+        totalVolume7d: enableStats ? generalStats.totalVolume7d : null,
+        totalVolume30d: enableStats ? generalStats.totalVolume30d : null,
         change_7dover7d: enableStats ? generalStats.change_7dover7d : null,
         change_30dover30d: enableStats ? generalStats.change_30dover30d : null,
         breakdown24h: enableStats ? generalStats.breakdown24h : null,
@@ -319,9 +317,8 @@ const substractSubsetVolumes = (adapter: ProtocolAdaptorSummary, _index: number,
                 change_1d: newSum['change_1d'],
                 change_7d: newSum['change_7d'],
                 change_1m: newSum['change_1m'],
-                change_1d_abs: newSum['change_1d_abs'],
-                change_7d_abs: newSum['change_7d_abs'],
-                change_30d_abs: newSum['change_30d_abs'],
+                totalVolume7d: newSum['totalVolume7d'],
+                totalVolume30d: newSum['totalVolume30d'],
             }
         }
         return computedSummary

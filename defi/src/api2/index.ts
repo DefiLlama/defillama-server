3e04117e15261925e0e4dd05b358b8570827cfce
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-03 08:28:00 +0100

    api2: use pg cache

--- defi/src/api2/index.ts
+++ defi/src/api2/index.ts
@@ -61,11 +61,11 @@ async function getProtocolishData(req: HyperExpress.Request, res: HyperExpress.R
 webserver.get("/protocol/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'protocol', skipAggregatedTvl: false, useNewChainNames: false, }));
 webserver.get("/treasury/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'treasury' }));
 webserver.get("/entity/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'entities' }));
-webserver.get("/updatedProtocol/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'protocol', useHourlyData: true, skipAggregatedTvl: req.query_parameters.includeAggregatedTvl !== 'true' }));
+webserver.get("/updatedProtocol/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'protocol', useHourlyData: false, skipAggregatedTvl: req.query_parameters.includeAggregatedTvl !== 'true' }));
 webserver.get("/hourly/:name", async (req, res) => getProtocolishData(req, res, { dataType: 'protocol', useHourlyData: true, skipAggregatedTvl: false }));
 
 async function main() {
-  await initializeTVLCacheDB()
+  await initializeTVLCacheDB({ isApi2Server: true })
   await initCache()
 
   webserver.listen(port)
---
+++ defi/src/api2/scripts/updateAllCache.ts
@@ -0,0 +1,35 @@
+import protocols from "../../protocols/data";
+import entities from "../../protocols/entities";
+import treasuries from "../../protocols/treasury";
+import { deleteFromPGCache, initializeTVLCacheDB } from "../db";
+import craftProtocolV2 from "../utils/craftProtocolV2";
+import PromisePool from "@supercharge/promise-pool";
+
+async function updateAllCache() {
+  await initializeTVLCacheDB({ isApi2Server: true });
+
+  let actions = [protocols, entities, treasuries].flat()
+  shuffleArray(actions) // randomize order of execution
+  await PromisePool
+    .withConcurrency(200)
+    .for(actions)
+    .process(updateProtocolCache);
+}
+
+async function updateProtocolCache(protocolData: any) {
+  // await deleteFromPGCache(protocolData.id) // clear postgres cache before fetching
+  await craftProtocolV2({ protocolData, useNewChainNames: true, useHourlyData: false, skipAggregatedTvl: true, })
+}
+
+updateAllCache().then(() => {
+  console.log('Done!!!')
+  process.exit(0)
+})
+
+function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}

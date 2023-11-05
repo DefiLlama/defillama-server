1c0c3582f135ce62cfbc200670e4099c7628747d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-05 21:54:43 +0100

    api2: update uni test

--- defi/src/api2/tests/utils.ts
+++ defi/src/api2/tests/utils.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const v1Endpoint = 'https://api.llama.fi'
-const v2Endpoint = 'http://127.0.0.1:5001'
+const v2Endpoint = process.env.API_URL || 'http://127.0.0.1:5001'
 const getProtocolAPI = (name: string, isV2?: boolean) => `${isV2 ? v2Endpoint : v1Endpoint}/protocol/${name}`
 const getTreasuryAPI = (name: string, isV2?: boolean) => `${isV2 ? v2Endpoint : v1Endpoint}/treasury/${name}`
 const getEntityAPI = (name: string, isV2?: boolean) => `${isV2 ? v2Endpoint : v1Endpoint}/entity/${name}`
@@ -18,32 +18,27 @@ export function getRandomItems(array: any[], count: number) {
 }
 
 export function getTests(items: any[], fnKey: string, title: string) {
-  const testCount = 20
+  const testCount = 3
   jest.setTimeout(1000000);
   items = getRandomItems(items, testCount)
-  test(title, async () => {
-    await Promise.all(items.map(async (protocol) => {
+  items.forEach((protocol: any) => {
+    describe(title + ' ' + protocol, () =>
+      test(protocol, getProtoTestFunction(protocol, fnKey)))
+  })
+}
+
+export function getProtoTestFunction(protocol: any, fnKey: string) {
+  return async () => {
+
+      const resV1 = await axios.get(fnMap[fnKey](protocol))
       const resV2 = await axios.get(fnMap[fnKey](protocol, true))
+      expect(resV1.status).toBe(200);
       expect(resV2.status).toBe(200);
       const data = resV2.data
       expect(data).toHaveProperty('chains')
       expect(data).toHaveProperty('id')
       expect(data).toHaveProperty('name')
-    }))
-  })
-
-  /* for (const protocol of items)
-    describe(title + ' ' + protocol, () =>
-      test.concurrent('Test', getProtoTestFunction(protocol, fnKey))) */
-}
-
-export function getProtoTestFunction(protocol: any, fnKey: string) {
-  return async () => {
-    // const resV1 = await axios.get(endPointFn(protocol))
-    const resV2 = await axios.get(fnMap[fnKey](protocol, true))
-    // expect(resV1.status).toBe(200);
-    expect(resV2.status).toBe(200);
-    // compareJSONCustom(resV1.data, resV2.data);
+      compareJSONCustom(resV1.data, resV2.data)
   }
 }
 
@@ -54,17 +49,12 @@ export function compareJSONCustom(actual: any, expected: any) {
   actual.chains.sort()
   expected.chains.sort()
 
-  // compareJSONNumberish(actual.currentChainTvls, expected.currentChainTvls)
+  compareJSONNumberish(actual.currentChainTvls, expected.currentChainTvls)
   compareJSONNumberish(actual.mcap, expected.mcap)
   reduceProtoResponse(actual)
   reduceProtoResponse(expected)
 
   expect(actual).toEqual(expected)
-
-  function closishNumbers(a: number, b: number, range: number = 0.2) {
-    if (typeof actual !== 'number' || typeof expected !== 'number') return expect(actual).toEqual(expected)
-    expect(Math.abs(a - b) / Math.max(a, b)).toBeLessThan(range)
-  }
 
   function compareJSONArray(actual: any, expected: any) {
     closishNumbers(actual.length, expected.length, 0.05)
@@ -79,13 +69,6 @@ export function compareJSONCustom(actual: any, expected: any) {
     }
   }
 
-  function compareJSONNumberish(actual: any, expected: any) {
-    Object.keys(actual).forEach((key: string) => {
-      if (typeof actual[key] === 'number')
-        return closishNumbers(actual[key], expected[key])
-      return expect(actual[key]).toEqual(expected[key])
-    })
-  }
 
   function reduceProtoResponse(obj: any) {
     delete obj.chainTvls
@@ -95,4 +78,17 @@ export function compareJSONCustom(actual: any, expected: any) {
     delete obj.currentChainTvls
     delete obj.mcap
   }
+}
+
+function compareJSONNumberish(actual: any, expected: any) {
+  Object.keys(actual ?? {}).forEach((key: string) => {
+    if (typeof actual[key] === 'number')
+      return closishNumbers(actual[key], expected[key])
+    return expect(actual[key]).toEqual(expected[key])
+  })
+}
+
+function closishNumbers(a: number, b: number, range: number = 0.2) {
+  if (typeof a !== 'number' || typeof a !== 'number') return expect(a).toEqual(b)
+  expect(Math.abs(a - b) / (Math.max(a, b) || 1)).toBeLessThan(range)
 }

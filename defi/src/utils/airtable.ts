dd770b5d032dd246b2ce190ee850f83e91641c6d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-27 15:28:50 +0100

    test AIRTABLE_API_KEY env variable

--- defi/src/utils/airtable.ts
+++ defi/src/utils/airtable.ts
@@ -3,6 +3,8 @@ import fetch from "node-fetch";
 export async function getAllAirtableRecords(sheet:string){
   let offset;
   let allRecords = [] as any[];
+  if (!process.env.AIRTABLE_API_KEY) throw new Error("Missing AIRTABLE_API_KEY");
+  console.log(`Fetching process.env.AIRTABLE_API_KEY: ${process.env.AIRTABLE_API_KEY} from Airtable`);
   do {
     const data: any = await fetch(
       `https://api.airtable.com/v0/${sheet}${offset ? `?offset=${offset}` : ""}`,

74a5fcf6b22c3f9f9d5866267e27dc2742fb42bd
vrtnd <vrotend@proton.me>
2023-11-27 21:34:22 +0300

    total swap volume to date

--- defi/src/getSwapTotalVolume.ts
+++ defi/src/getSwapTotalVolume.ts
@@ -1,9 +1,13 @@
 import { getSwapTotalVolume } from "./dexAggregators/db/getSwapTotalVolume";
 import { wrap, IResponse, successResponse, errorResponse } from "./utils/shared";
 
-const handler = async (): Promise<IResponse> => {
+const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
   try {
-    const volume = await getSwapTotalVolume();
+    const timestamp = event.queryStringParameters?.timestamp;
+
+    if (!timestamp) return successResponse({}, 10);
+
+    const volume = await getSwapTotalVolume(timestamp);
     return successResponse({ volume }, 10);
   } catch (e) {
     console.log(e);

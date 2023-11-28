38cb21ed836b8b7cf56fa83f0ed27ec37d2ce999
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-28 16:19:48 +0100

    rate-limit axios api calls

--- defi/src/utils/airtable.ts
+++ defi/src/utils/airtable.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
+import sleep from "./shared/sleep";
 
 export async function getAllAirtableRecords(sheet: string) {
   let offset;
   let allRecords = [] as any[];
   if (!process.env.AIRTABLE_API_KEY) throw new Error("Missing AIRTABLE_API_KEY");
-  console.log(`Fetching process.env.AIRTABLE_API_KEY: ${process.env.AIRTABLE_API_KEY} from Airtable`);
   do {
     const { data }: any = await axios.get(
       `https://api.airtable.com/v0/${sheet}${offset ? `?offset=${offset}` : ""}`,
@@ -14,13 +14,11 @@ export async function getAllAirtableRecords(sheet: string) {
         },
       }
     )
-    if (!data.offset)
-      console.log(data)
-    console.log(`Fetched data records from Airtable`, Object.keys(data), {
-      Authorization: process.env.AIRTABLE_API_KEY!,
-    });
+    if (!data.records)
+      console.log('error fetching data from Airtable', data)
     offset = data.offset;
     allRecords = allRecords.concat(data.records);
+    if (offset) await sleep(1000)
   } while (offset !== undefined);
   return allRecords
 }

32f95449cefc308543c93a6f32ba8bb4e4fc7cb5
0xngmi <0xngmi@protonmail.com>
2023-12-02 00:19:13 +0000

    sns distressed

--- coins/src/adapters/other/distressedAssets.ts
+++ coins/src/adapters/other/distressedAssets.ts
@@ -104,6 +104,7 @@ export const contracts: { [chain: string]: { [token: string]: string } } = {
   solana: {
     YAKU: "NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s",
     BONK: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
+    SNS: "SNSNkV9zfG5ZKWQs6x4hxvBRV6s8SqMfSGCtECDvdMd"
   },
   avax: {
     DUEL: "0xc1a49c0b9c10f35850bd8e15eaef0346be63e002",

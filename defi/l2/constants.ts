3914fa16759c33214fdf9c3f1017a20272ab8460
waynebruce0x <waynebruce0x@gmail.com>
2023-12-06 16:48:58 +0000

    clean debug

--- defi/l2/constants.ts
+++ defi/l2/constants.ts
@@ -6,37 +6,37 @@ export const zero = BigNumber(0);
 export const excludedTvlKeys = ["PK", "SK", "tvl"];
 
 export const canonicalBridgeIds: { [id: string]: Chain } = {
-  // "3782": "mantle",
+  "3782": "mantle",
   "3777": "arbitrum",
-  // "3778": "nova",
+  "3778": "nova",
   "3780": "base",
-  // "3781": "linea",
+  "3781": "linea",
   // "3783": "metis", // commented out until we get a native bridge token mapping
   "3784": "optimism",
   // "3785": "polygon_zkevm",
-  // "3786": "scroll",
+  "3786": "scroll",
   "3787": "starknet",
   "3788": "zksync",
-  // "1501": "everscale",
+  "1501": "everscale",
   "349": "injecitve",
   "801": "celo",
-  // "1272": "iotex",
-  // "2081": "wanchain",
-  // "2214": "kekchain",
+  "1272": "iotex",
+  "2081": "wanchain",
+  "2214": "kekchain",
   "3699": "elysium",
   "3813": "alephium",
   "129": "xdai",
   "240": "polygon",
   "3779": "avax",
-  // "3866": "aurora",
-  // "3861": "rsk",
+  "3866": "aurora",
+  "3861": "rsk",
 };
 export const protocolBridgeIds: { [chain: string]: Chain } = {
-  // "144": "dydx",
-  // "3139": "immutablex",
-  // "126": "loopring",
-  // "1878": "apex",
-  // "344": "zkswap",
+  "144": "dydx",
+  "3139": "immutablex",
+  "126": "loopring",
+  "1878": "apex",
+  "344": "zkswap",
 };
 export const tokenFlowCategories: (keyof ChainData)[] = ["outgoing", "canonical", "incoming", "native"];
 

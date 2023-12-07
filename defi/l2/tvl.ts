10f7e0971c8d8c84ebd35b705b3b1d00b6c846c2
waynebruce0x <waynebruce0x@gmail.com>
2023-12-07 17:44:47 +0000

    combined flows

--- defi/l2/tvl.ts
+++ defi/l2/tvl.ts
@@ -2,12 +2,10 @@ import { fetchTvls } from "./outgoing";
 import { fetchIncoming } from "./incoming";
 import { fetchMinted } from "./native";
 import { fetchMetadata } from "./metadata";
-import { ChainData, DollarValues, TokenTvlData } from "./types";
+import { ChainData, DollarValues, FinalData, TokenTvlData } from "./types";
 import BigNumber from "bignumber.js";
 import { ownTokens, tokenFlowCategories, zero } from "./constants";
 import { Chain } from "@defillama/sdk/build/general";
-
-type TranslatedData = { [chain: string]: any };
 
 export default async function main() {
   const { data: canonical } = await fetchTvls({ isCanonical: true });
@@ -15,7 +13,7 @@ export default async function main() {
   let { data: outgoing, native: adjustedNativeBalances } = await fetchTvls({ mcapData, native });
   if (!adjustedNativeBalances) throw new Error(`Adjusting for mcaps has failed, debug manually`);
   native = adjustedNativeBalances;
-  const incoming = await fetchIncoming({ chains: Object.keys(canonical) });
+  const incoming = await fetchIncoming({ canonical });
   const { data: protocols } = await fetchTvls({ isCanonical: true, isProtocol: true });
   // const metadata = await Promise.all(Object.keys(canonical).map((chain: string) => fetchMetadata(chain)));
 
@@ -31,26 +29,27 @@ export default async function main() {
   return translateProtocols(chains, protocols);
 }
 
-function translateProtocols(chains: TranslatedData, protocols: TokenTvlData): TranslatedData {
+function translateProtocols(chains: FinalData, protocols: TokenTvlData): FinalData {
   Object.keys(protocols).map((pid: string) => {
     const total = Object.values(protocols[pid]).reduce((p: any, c: any) => c.plus(p), zero);
     const canonical = { total, breakdown: protocols[pid] };
     chains[pid] = {
       canonical,
-      incoming: {},
       native: {},
-      outgoing: {},
+      thirdParty: {},
+      ownTokens: {}, // this should be staking TVL
       total: canonical,
     };
   });
 
   return chains;
 }
-function translateToChainData(data: ChainData): TranslatedData {
-  const translatedData: { [chain: Chain]: { [category: string]: { breakdown: any; total: BigNumber } } } = {};
+function translateToChainData(data: ChainData): FinalData {
+  let translatedData: any = {};
   const selectedChains: Chain[] = Object.keys(data.canonical);
   tokenFlowCategories.map((c: keyof ChainData) => processProperty(data, c));
   // processProperty(data, "metadata");
+  combineThirdPartyFlows();
   processNetFlows();
 
   function processProperty(data: ChainData, key: keyof ChainData) {
@@ -67,19 +66,34 @@ function translateToChainData(data: ChainData): TranslatedData {
     Object.keys(translatedData).map((chain: Chain) => {
       let total: { breakdown: DollarValues; total: BigNumber } = { breakdown: {}, total: zero };
       Object.keys(translatedData[chain]).map((category: string) => {
+        if (category == "ownTokens") return;
         Object.keys(translatedData[chain][category].breakdown).map((symbol: string) => {
           if (!(symbol in total.breakdown)) total.breakdown[symbol] = zero;
-          if (category == "outgoing")
-            total.breakdown[symbol] = total.breakdown[symbol].minus(translatedData[chain][category].breakdown[symbol]);
-          else
-            total.breakdown[symbol] = total.breakdown[symbol].plus(translatedData[chain][category].breakdown[symbol]);
+          total.breakdown[symbol] = total.breakdown[symbol].plus(translatedData[chain][category].breakdown[symbol]);
         });
 
-        if (category == "outgoing") total.total = total.total.minus(translatedData[chain][category].total);
-        else total.total = total.total.plus(translatedData[chain][category].total);
+        total.total = total.total.plus(translatedData[chain][category].total);
       });
 
       translatedData[chain].total = total;
+    });
+  }
+
+  function combineThirdPartyFlows() {
+    Object.keys(translatedData).map((chain: Chain) => {
+      const breakdown: { [symbol: string]: BigNumber } = {};
+      Object.keys(translatedData[chain].incoming.breakdown).map((symbol: string) => {
+        breakdown[symbol] = translatedData[chain].incoming.breakdown[symbol];
+      });
+      Object.keys(translatedData[chain].outgoing.breakdown).map((symbol: string) => {
+        if (symbol in breakdown)
+          breakdown[symbol] = breakdown[symbol].plus(translatedData[chain].incoming.breakdown[symbol]);
+        else breakdown[symbol] = translatedData[chain].outgoing.breakdown[symbol];
+      });
+      const total = translatedData[chain].outgoing.total.plus(translatedData[chain].incoming.total);
+      translatedData[chain].thirdParty = { total, breakdown };
+      delete translatedData[chain].incoming;
+      delete translatedData[chain].outgoing;
     });
   }
 

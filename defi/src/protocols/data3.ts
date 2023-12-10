2889bdcbd19be351b5aeeab8f94d1e14b64ef03f
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2023-12-10 03:02:54 +0000

    Update data3.ts
    add link to oracle entry

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -6228,7 +6228,7 @@ const data3: Protocol[] = [
     audit_links: [],
     forkedFrom: ["GMX V1"],
     parentProtocol: "parent#quickswap",
-    oracles: ["Pyth","API3"],
+    oracles: ["Pyth","API3"], // https://github.com/DefiLlama/defillama-server/pull/5164
     listedAt: 1684213007
   },
   {

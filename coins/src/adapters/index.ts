758de5935defef99f7a4a5188d4b476a1b3e4ff1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-12 14:03:21 +0100

    coins: track algorand token prices

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -3,6 +3,7 @@ export default {
   tangleswap: require("./markets/tangleswap"),
   cantoLending: require("./moneyMarkets/compound"),
   xexchange: require("./markets/xexchange"),
+  tinyman: require("./markets/tinyman"),
   silo: require("./moneyMarkets/silo"),
   hlp: require("./yield/hlp"),
   digift: require("./rwa/digift"),

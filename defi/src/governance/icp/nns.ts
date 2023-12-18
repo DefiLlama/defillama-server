eb5e55a497c654427560a3f05b415db52fa3b723
NikolasHai <113891786+NikolasHai@users.noreply.github.com>
2023-12-18 01:06:44 +0100

    Fix proposal count (#5249)
    * merge with master
    
    * removed libraries
    
    * fmt
    
    * fmt
    
    * fixed proposal count
    
    * fmt
    
    * fmt

--- defi/src/governance/icp/nns.ts
+++ defi/src/governance/icp/nns.ts
@@ -82,7 +82,8 @@ export async function get_metadata ()
         // id: "rrkah-fqaaa-aaaaa-aaaaq-cai",
         id: NNS_GOV_ID,
         type: "Network Nervous System",
-        latest_proposal_id,
+        latestProposalId: latest_proposal_id,
+        proposalsCount:latest_proposal_id,
         symbol: "NNS",
         chainName: "Internet Computer",
         name: "Network Nervous System",
@@ -143,14 +144,14 @@ export async function addICPProposals ( overview : any = {} )
         ns_api_url:NNS_API_BASE_URL,
         dashboard_url:DASHBOARD_BASE_URL,
         ledger_url:ICP_LEDGER_API_BASE_URL,
-        latest_proposal_id: cache.metadata.latest_proposal_id,
+        latest_proposal_id: cache.metadata.latestProposalId,
         decimals:DECIMALS,
         convert_proposals:convert_proposal_format,
         proposal_filter:(p:NetworkNervousSystemProposalResponse) => p.topic ? !EXCLUDED_TOPICS.includes( p.topic ) : false,
         excluded_topics:EXCLUDED_TOPICS
     };
     await update_nervous_system_cache( cache as any ,config)
-
+    cache.metadata.proposalsCount = Object.keys(cache.proposals).length
     cache.id = NNS_GOV_ID
     updateStats( cache, overview, cache.id )
     if ( overview[ cache.id ] )

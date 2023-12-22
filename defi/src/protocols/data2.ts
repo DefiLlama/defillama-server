7c8b9489e670c882da610b5f01530b033f14c50a
realdealshaman <realdealshaman11@gmail.com>
2023-12-22 04:15:25 +0000

    update to bridge

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -946,7 +946,7 @@ const data2: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield",
+    category: "Bridge",
     chains: ["RSK"],
     oracles: [],
     forkedFrom: [],

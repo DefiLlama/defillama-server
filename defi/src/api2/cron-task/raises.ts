f325300bf0535bcd0bbca6262eb084810d7ff6a2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-28 01:24:20 +0100

    disable fetchArticles in api2 cron for now

--- defi/src/api2/cron-task/raises.ts
+++ defi/src/api2/cron-task/raises.ts
@@ -1,13 +1,13 @@
 import { storeRouteData, } from "../cache/file-cache";
 import { getRaisesInternal } from "../../getRaises";
 import { getHacksInternal } from "../../getHacks";
-import { fetchArticles } from "../../getNewsArticles";
+// import { fetchArticles } from "../../getNewsArticles";
 
 async function run() {
 
   await writeRaises()
   await writeHacks()
-  await writeArticles()
+  // await writeArticles()
 
   async function writeRaises() {
     console.time('write /raises')
@@ -23,12 +23,12 @@ async function run() {
     console.timeEnd('write /hacks')
   }
 
-  async function writeArticles() {
+  /* async function writeArticles() {
     console.time('write /news/articles')
     const data = await fetchArticles()
     await storeRouteData('news/articles', data)
     console.timeEnd('write /news/articles')
-  }
+  } */
 }
 
 run().catch(console.error).then(() => process.exit(0))

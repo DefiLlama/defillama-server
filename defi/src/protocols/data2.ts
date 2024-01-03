0844c3bea749a3fe809cf3f15103eedf3d51489f
Define101 <Lovelovenow@protonmail.com>
2024-01-03 10:05:14 +0000

    delist Streamflow

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -27760,6 +27760,7 @@ const data2: Protocol[] = [
     listedAt: 1677584770,
     stablecoins: ["frax-price-index"],
   },
+  /*
   {
     id: "2608",
     name: "Streamflow",
@@ -27783,6 +27784,7 @@ const data2: Protocol[] = [
     listedAt: 1677586293,
     github: ["StreamFlow-Finance"]
   },
+  */
   {
     id: "2609",
     name: "Hord",

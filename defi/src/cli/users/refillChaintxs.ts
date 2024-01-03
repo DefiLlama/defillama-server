81149c5dd32144813291a0dbb575700df8e493f4
Rory Graman <rorygraman@gmail.com>
2024-01-03 13:19:55 -0600

    update conveyor information

--- defi/src/cli/users/refillChaintxs.ts
+++ defi/src/cli/users/refillChaintxs.ts
@@ -1,72 +1,15 @@
 require("dotenv").config();
-import { queryFlipside } from "../../../dimension-adapters/helpers/flipsidecrypto";
+import protocolAddresses from "../../../dimension-adapters/users/routers/routerAddresses";
+import { isAcceptedChain } from "../../../dimension-adapters/users/utils/convertChain";
 import { PromisePool } from '@supercharge/promise-pool'
-import { storeTxs } from "../../users/storeUsers";
+import { storeChainTxs } from "./queries/txs";
 
-async function storeTxsInDb(chain:string, usersChart:[string, number][]){
+async function main() {
+    const filtered = protocolAddresses.filter(addresses => {
+        return Object.entries(addresses.addresses).some(([chain, addys]) => isAcceptedChain(chain) && addys.length > 0)
+    })
     await PromisePool
-            .withConcurrency(10)
-            .for(usersChart).process(async ([dateString, users]) => {
-                const date = new Date(`${dateString}`)
-                const start = Math.round(date.getTime() / 1e3)
-                const end = start + 24 * 3600
-                if(end > Date.now()/1e3){
-                    return
-                }
-                try{
-                    await storeTxs(start, end, `chain#${chain}`, "all", users) // if already stored -> don't overwrite
-                } catch(e){
-                    if(!String(e).includes("duplicate key value violates unique constraint")){
-                        console.error(`Couldn't store users on ${chain}`, e)
-                    }
-                }
-            })
+        .withConcurrency(5)
+        .for(filtered).process(storeChainTxs)
 }
-
-async function main(){
-    await Promise.all([
-        "arbitrum", "avalanche", "bsc", "ethereum", "gnosis", "optimism", "polygon",
-    ].map(async (chain) => {
-        const usersChart = await queryFlipside(`SELECT
-            date_trunc(day, block_timestamp) as dt, 
-            count(tx_hash) uniques
-        from
-            ${chain}.core.fact_transactions
-        group by dt`)
-        await storeTxsInDb(chain, usersChart)
-
-    }))
-}
-
-async function solana(){
-    const usersChart = await queryFlipside(`SELECT
-        date_trunc(day, block_timestamp) as dt, 
-        count(TX_ID) uniques
-    from
-        solana.core.fact_transactions
-    group by dt`)
-    await storeTxsInDb("solana", usersChart)
-}
-
-async function near(){
-    const usersChart = await queryFlipside(`SELECT
-        date_trunc(day, block_timestamp) as dt, 
-        count(tx_hash) uniques
-    from
-        near.core.fact_transactions
-    group by dt`)
-    await storeTxsInDb("near", usersChart)
-}
-
-async function osmosis(){
-    const usersChart = await queryFlipside(`SELECT
-        date_trunc(day, block_timestamp) as dt, 
-        count(tx_id) uniques
-    from
-        osmosis.core.fact_transactions
-    group by dt`)
-    await storeTxsInDb("osmosis", usersChart)
-}
-Promise.all([main()
-    , solana(), near(), osmosis()
-]).then(()=>console.log("finished!"))
+main()

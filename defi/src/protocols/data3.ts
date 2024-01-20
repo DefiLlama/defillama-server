7b02fd694d8a9690bd23f6ecd22489a98aa65555
realdealshaman <realdealshaman11@gmail.com>
2024-01-20 01:48:08 +0000

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -29985,7 +29985,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Farm",
+    category: "Dexes",
     chains: ["Manta"],
     oracles: [], 
     forkedFrom: ["Uniswap V2"],

5facb4e5ea0c153cc32cd2accb606fcedb75b2eb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-13 10:59:23 +0100

    minor fix

--- defi/src/api2/ecosystem.config.js
+++ defi/src/api2/ecosystem.config.js
@@ -6,13 +6,10 @@ module.exports = {
       script: './src/api2/index.ts', // Path to your main TypeScript file
       interpreter: 'node',
       args: '-r ts-node/register', // Use ts-node for running TypeScript files
-      listen_timeout: 120_000, // Wait 60 seconds for the app to start
+      listen_timeout: 120_000, // Wait 120 seconds for the app to start
       kill_timeout: 10_000, // Wait 10 seconds for the app to start
       wait_ready: true, // Wait for the 'ready' signal
       instances: 2,
-      max_restarts: 3,
-      min_uptime: "60s", // The process must stay up for at least 10 seconds before it's considered a normal start
-      exp_backoff_restart_delay: 100,
       exec_mode: 'cluster', // Start in cluster mode
       env: {
         TS_NODE_TRANSPILE_ONLY: 'true', // Enable ts-node's transpile-only mode, setting it via args is not working for some reason

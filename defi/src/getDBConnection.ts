b9167c4e7f04fa604a3114b88c80bfc831d14677
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-13 07:41:11 +0100

    Refactor getCoins2Connection to use Promise

--- defi/src/getDBConnection.ts
+++ defi/src/getDBConnection.ts
@@ -3,7 +3,7 @@ import setEnvSecrets from "./utils/shared/setEnvSecrets";
 
 let accountsDBConnection: ReturnType<typeof postgres>
 let errorReportsConnection: ReturnType<typeof postgres>
-let coins2Connection: ReturnType<typeof postgres>
+let coins2Connection: Promise<ReturnType<typeof postgres>>
 
 export function getAccountsDBConnection() {
   if (!accountsDBConnection)
@@ -20,13 +20,16 @@ export function getErrorDBConnection() {
 }
 
 export async function getCoins2Connection() {
-  if (!coins2Connection) (coins2Connection as any) = 'isBeingSet'
-  if ((coins2Connection as any) === 'isBeingSet') {
-    let auth: any = process.env.COINS2_AUTH
-    if (!auth) await setEnvSecrets()
-    auth = process.env.COINS2_AUTH?.split(",") ?? [];
-    if (!auth || auth.length != 3) throw new Error("there arent 3 auth params. Cannot initialize coins2 connection.");
-    coins2Connection = postgres(auth[0]);
+  if (!coins2Connection) {
+    coins2Connection = new Promise(async (resolve) => {
+      // @ts-ignore
+      let auth: any = process.env.COINS2_AUTH
+      if (!auth) await setEnvSecrets()
+      // @ts-ignore
+      auth = process.env.COINS2_AUTH?.split(",") ?? [];
+      if (!auth || auth.length != 3) throw new Error("there arent 3 auth params. Cannot initialize coins2 connection.");
+      resolve(postgres(auth[0]))
+    })
   }
   return coins2Connection
 }
@@ -47,7 +50,7 @@ export async function closeConnection() {
 
   if (coins2Connection && (coins2Connection as any) !== 'isBeingSet') {
     console.log('Closing Coins2 DB connection')
-    await coins2Connection.end({ timeout: 2 })
+    await (await coins2Connection).end({ timeout: 2 })
     console.log('Coins2 DB connection closed')
   }
 }

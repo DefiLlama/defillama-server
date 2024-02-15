9434406a6ce4b0502225fcc60befaca96477344d
mariobern <110480440+mariobern@users.noreply.github.com>
2024-02-15 23:49:50 +0700

    add new Pyth users (#5856)
    * add new Pyth users
    
    * Update data2.ts
    
    add reference link
    
    * Update data3.ts
    
    add reference link
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -5510,7 +5510,7 @@ const data3: Protocol[] = [
     module: "typus-finance/index.js",
     twitter: "TypusFinance",
     forkedFrom: [],
-    oracles: ["Internal"],
+    oracles: ["Internal","Pyth","Supra"], // https://github.com/DefiLlama/defillama-server/pull/5856
     audit_links: ["https://docs.typus.finance/audit"],
     listedAt: 1683552946
   },
@@ -25770,7 +25770,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "Options",
     chains: ["Arbitrum", "Linea", "zkSync Era"],
-    oracles: [],
+    oracles: ["Pyth"], // https://github.com/DefiLlama/defillama-server/pull/5856
     forkedFrom: [],
     module: "deri-v4/index.js",
     twitter: "DeriProtocol",

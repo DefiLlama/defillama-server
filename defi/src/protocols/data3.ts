c38d3134c7f4ccfadbf02bb21f0dc64d61a8ab8b
realdealshaman <realdealshaman11@gmail.com>
2024-02-26 17:32:06 +0000

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -32907,7 +32907,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Farm",
+    category: "Dexes",
     chains: ["Chiliz"],
     module: "imonswap/index.js",
     twitter: "imondotai",

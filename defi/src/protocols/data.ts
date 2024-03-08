bd62ab17e1e9980c2077f64c060beb852038d6fb
Define101 <Lovelovenow@protonmail.com>
2024-03-08 12:20:10 +0000

    fix server

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -2284,7 +2284,7 @@ const data: Protocol[] = [
     forkedFrom: ["Compound V2"],
     oraclesByChain: {
       bsc: ["Chainlink"], // https://docs-v4.venus.io/risk/resilient-price-oracle Venus team confirmed:  Our Core Pool Resilient Oracle system uses Chainlink as principal source and 4 additional Oracles. The main one is Chainlink but, we also use Pyth, RedStone, Binance Oracle and TWAP
-      op_bnb: ["Binance Oracle"]
+   //   op_bnb: ["Binance Oracle"]
     },
     parentProtocol: "parent#venus-finance"
   },

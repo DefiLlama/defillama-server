7c69260dccacd0c684e6e1883cf6e4c2e264dfa9
realdealshaman <realdealshaman11@gmail.com>
2024-03-12 18:17:15 +0000

    fix twitter handle

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -35369,7 +35369,7 @@ const data3: Protocol[] = [
     oracles: [],
     forkedFrom: ["Uniswap V3"],
     module: "Blaster/index.js",
-    twitter: "blasterdex_",
+    twitter: "blasterdex__",
     github: ["Blaster-Labs"],
     listedAt: 1710246595
   },

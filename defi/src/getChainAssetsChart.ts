92eab4d087ea85e3d4e2920d1191598a7edb26d3
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-03-13 15:04:29 +0000

    Update getChainAssetsChart.ts

--- defi/src/getChainAssetsChart.ts
+++ defi/src/getChainAssetsChart.ts
@@ -2,7 +2,7 @@ import { wrap, IResponse, successResponse } from "./utils/shared";
 import { fetchHistoricalFromDB } from "../l2/storeToDb";
 
 const handler = async (event: any): Promise<IResponse> => {
-  const chain = event.pathParameters?.sub?.chain?.toLowerCase();
+  const chain = event.pathParameters?.chain?.toLowerCase();
   const chains = await fetchHistoricalFromDB(chain);
   return successResponse(chains, 10 * 60); // 10 min cache
 };

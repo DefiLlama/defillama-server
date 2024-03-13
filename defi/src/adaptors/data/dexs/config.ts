47248c124d162cfc59efaf1f58b7e4eb8980ad50
dtmkeng <phumaisut@gmail.com>
2024-03-13 01:07:17 +0000

    disable dexs

--- defi/src/adaptors/data/dexs/config.ts
+++ defi/src/adaptors/data/dexs/config.ts
@@ -2550,7 +2550,7 @@ export default {
     },
     "nile-exchange-v1": {
         parentId: "Nile Exchange",
-        "enabled": true,
+        "enabled": false,
         "id": "4285"
     }
 } as AdaptorsConfig

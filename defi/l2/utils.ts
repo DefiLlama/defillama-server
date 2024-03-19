275ecfbebe220156f57499977d57460423767a80
waynebruce0x <waynebruce0x@gmail.com>
2024-03-19 15:12:17 +0000

    asset flows

--- defi/l2/utils.ts
+++ defi/l2/utils.ts
@@ -296,6 +296,26 @@ export async function fetchBridgeTokenList(chain: Chain): Promise<Address[]> {
     throw new Error(`${chain} bridge adapter failed with ${e}`);
   }
 }
+
+const letterToSeconds: { [symbol: string]: number } = {
+  w: 604800,
+  d: 86400,
+  h: 3600,
+  m: 60,
+};
+export function quantisePeriod(period: string): number {
+  let normalizedPeriod: number;
+  const normalized = Object.keys(letterToSeconds)
+    .map((s: string) => {
+      if (!period.includes(s)) return;
+      const numberPeriod = period.replace(new RegExp(`[${s}]`, "i"), "");
+      normalizedPeriod = Number(numberPeriod == "" ? 1 : numberPeriod);
+      return normalizedPeriod * letterToSeconds[s];
+    })
+    .find((t: any) => t != null);
+  if (normalized == null) return Number(period);
+  return normalized;
+}
 export function sortBySize() {
   const coins: { [value: string]: string } = {};
 

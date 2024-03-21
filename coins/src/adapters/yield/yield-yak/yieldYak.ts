0a7f5b51a80c0662f83de26ede85cae7e1fe533f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-21 17:09:35 +0100

    track more yield yak tokens

--- coins/src/adapters/yield/yield-yak/yieldYak.ts
+++ coins/src/adapters/yield/yield-yak/yieldYak.ts
@@ -17,6 +17,10 @@ export const config = {
   },
   arbitrum: {
     tokens: {
+      "WETH": "0x2fc8e171b2688832b41881aaf3da4d180bda1f33",
+      "WBTC": "0x3b85d87104091fac2940c3d5bda44748c5fea946",
+      "USDC": "0x5847eb0ac310845510880c6871e0ce6d8b0f57fc",
+      "USDC_WBTC": "0x056e4f4701c7a5bfa95a36154c7135edb134e0c6",
       "YY_WOMAT-LP-DAI": "0x1817fe376740b53cae73224b7f0a57f23dd4c9b5",
       "YY_WOMAT-LP-USDT": "0x8bc6968b7a9eed1dd0a259efa85dc2325b923dd2",
       "YY GMX sGLP-": "0x28f37fa106aa2159c91c769f7ae415952d28b6ac",
@@ -38,6 +42,5 @@ export default async function getTokenPrices(chain: string, timestamp: number) {
   calls.forEach((vault: any, i: any) => { 
     if (isNaN(totalDeposits[i]) || isNaN(totalSupply[i])) return 
     pricesObject[vault] = { underlying: depositToken[i], price: totalDeposits[i] / totalSupply[i]  } })
-
   return getWrites({ chain, timestamp, writes, pricesObject, projectName: 'yield-yak' })
 }

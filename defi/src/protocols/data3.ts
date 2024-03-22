5c95d55a284174c8f315340f363ab42111d1765c
0xngmi <0xngmi@protonmail.com>
2024-03-22 19:03:02 +0000

    re-enable dydx v4

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -30469,8 +30469,7 @@ const data3: Protocol[] = [
     chains: ["dYdX"],
     oracles: [],  
     forkedFrom: [],
-    // module: "projects/dydx-v4/index.js", // added on the 02/02/2024
-    module: "dummy.js", // added on the 02/02/2024
+    module: "projects/dydx-v4/index.js", // added on the 02/02/2024
     parentProtocol: "parent#dydx",
     twitter: "dYdX",
   },

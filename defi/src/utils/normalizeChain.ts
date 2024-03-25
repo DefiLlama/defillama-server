de9ceb5852fe61ba18a040d7f0aa384bb857a9b1
Define101 <Lovelovenow@protonmail.com>
2024-03-25 14:35:28 +0000

    fix spelling error

--- defi/src/utils/normalizeChain.ts
+++ defi/src/utils/normalizeChain.ts
@@ -3345,7 +3345,7 @@ export function getChainDisplayName(normalizedChain: string, useNewChainNames: b
       return "Kinto"
     case "imx":
       return "Immutable zkEVM"
-    case "zors":
+    case "zora":
       return "Zora"
     default:
       return normalizedChain.slice(0, 1).toUpperCase() + normalizedChain.slice(1) // Capitalize first letter

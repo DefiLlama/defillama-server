baa4e8fe72376670553fb3c491cdaafda86698f9
0xngmi <0xngmi@protonmail.com>
2024-03-27 04:12:11 +0000

    change category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -28276,7 +28276,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Staking Pool",
+    category: "Dexes",
     chains: ["EOS"],
     oracles: [],
     forkedFrom: [],

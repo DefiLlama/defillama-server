a40f3f47172b435812398e14e14990200e1a28f1
Define101 <Lovelovenow@protonmail.com>
2024-03-30 20:40:02 +0000

    Ton Locker Category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -19966,7 +19966,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Chain",
+    category: "Staking Pool",
     chains: ["TON"],
     oracles: [],
     forkedFrom: [],

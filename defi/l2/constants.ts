840f6f80d5dfeed02cc6e37ccacb31467e136001
realdealshaman <realdealshaman11@gmail.com>
2024-04-17 21:55:16 +0100

    add meter

--- defi/l2/constants.ts
+++ defi/l2/constants.ts
@@ -101,6 +101,7 @@ export const ownTokens: { [chain: Chain]: { ticker: string; address: string } } 
   loopring: { ticker: "LRC", address: "0xbbbbca6a901c926f240b89eacb641d8aec7aeafd" },
   immutablex: { ticker: "IMX", address: "0xf57e7e7c23978c3caec3c3548e3d615c346e79ff" },
   metis: { ticker: "METIS", address: "0x0000000000000000000000000000000000001010" },
+  meter: { ticker: "MTRG", address: "0x228ebbee999c6a7ad74a6130e81b12f9fe237ba3" },
   // ethereum: { ticker: "ETH", address: "0x0000000000000000000000000000000000000000" },
   // solana: { ticker: "SOL", address: "" },
   // cronos: { ticker: "CRO", address: "0x0000000000000000000000000000000000000000" },

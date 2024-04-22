a2fc184922292cc54ef221e3f93197befe1e0927
Define101 <Lovelovenow@protonmail.com>
2024-04-22 17:25:38 +0100

    fix

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -40274,7 +40274,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Dexes",
+    category: "Decentralized Stablecoin",
     chains: ["Base"],
     module: "flat-money/index.js",
     twitter: "0xflatmoney",

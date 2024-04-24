f6d4b2236833ae5de96304d444fc5511a77f6d8a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-24 21:51:50 +0200

    coins: seamless fix bug

--- coins/src/adapters/other/seamless.ts
+++ coins/src/adapters/other/seamless.ts
@@ -24,6 +24,7 @@ async function getTokenPrice(chain: string, timestamp: number) {
   const tokenSupplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: rTokens })
   const bals = await api.multiCall({ abi: 'erc20:balanceOf', calls: tokens.map((target: string, idx: number) => ({ target, params: rTokens[idx] })) })
   rTokens.forEach((v: any, i: number) => {
+    if (+tokenSupplies[i] === 0) return;
     pricesObject[v] = { underlying: tokens[i], price: bals[i] / tokenSupplies[i] }
   })
   return getWrites({ chain, timestamp, writes, pricesObject, projectName })

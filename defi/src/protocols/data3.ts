12dff05287ebd044990099f6f31dbe4a72f6d780
realdealshaman <realdealshaman11@gmail.com>
2024-05-01 04:17:41 +0100

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -41303,7 +41303,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquid Staking",
+    category: "Dexes",
     chains: ["Ethereum", "Arbitrum"],
     oracles: [],  
     forkedFrom: [],

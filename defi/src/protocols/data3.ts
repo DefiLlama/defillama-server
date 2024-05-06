6fed424121c3d559d6fb3c38801b4aaf79bb65f8
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2024-05-06 22:12:05 +0100

    Update data3.ts
    use only primary oracle

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -19003,7 +19003,7 @@ const data3: Protocol[] = [
     module: "shoebillFinance-v2/index.js",
     oraclesByChain: {
       wemix: ["Pyth"], // https://github.com/DefiLlama/defillama-server/pull/5434#issuecomment-1889510580
-      manta: ["RedStone", 'Supra'], //https://twitter.com/redstone_defi/status/1763580020751245496 , https://github.com/DefiLlama/defillama-server/pull/6353#discussion_r1550765182
+      manta: ["RedStone"], //https://twitter.com/redstone_defi/status/1763580020751245496 , https://github.com/DefiLlama/defillama-server/pull/6353#discussion_r1550765182
       bsquared: ["RedStone"],
       Klaytn: ["Witnet"],
       metis: ['Chainlink']

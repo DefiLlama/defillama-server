63bc6d7f5196be08daf51d2e5d03d45ae8f9982b
basechad <164132667+basechad@users.noreply.github.com>
2024-05-27 14:33:52 -0700

    Update data.ts
    remove based-chad section, wrong file

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -27272,30 +27272,6 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     listedAt: 1644446314,
     github: ["minimaxdefi", "minimaxfinance"]
   },
-  {
-    id: "1381",
-    name: "Based Chad",
-    address: "0xecaf81eb42cd30014eb44130b89bcd6d4ad98b92",
-    symbol: "CHAD",
-    url: "https://basedchad.io",
-    description:
-      "Wake Up. Chad Harder. Repeat.",
-    chain: "Base",
-    logo: `${baseIconsUrl}/based-chad.png`,
-    audits: "3",
-    audit_links: ["https://github.com/basechad/bc/blob/main/open_zeppelin_2018-10_audit.pdf"],
-    audit_note: null,
-    gecko_id: "based-chad",
-    cmcId: null,
-    category: "Memes",
-    chains: ["Base"],
-    oracles: [],
-    module: "based-chad/index.js",
-    twitter: "BasedChad6969",
-    language: "Solidity",
-    listedAt: 1644446314,
-    github: ["basechad"]
-  },
 ];
 
 const protocols = data.concat(data2, data3)

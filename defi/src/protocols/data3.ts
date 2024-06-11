e608a867c88cb0ded2e456177dd391b1209ea7b9
Kresh <andrey@symbiotic.fi>
2024-06-11 17:40:42 +0400

    fix symbiotic category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -45913,7 +45913,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquid Staking",
+    category: "Restaking",
     chains: ["Ethereum"],
     oracles: [],
     forkedFrom: [],

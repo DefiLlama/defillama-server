a0aa59127be046e7eaa94c411fe0934ec7432eb8
sakulstra <lukasstrassel@googlemail.com>
2024-06-13 13:07:01 +0200

    fix: use raw addresses

--- coins/src/adapters/moneyMarkets/aave/index.ts
+++ coins/src/adapters/moneyMarkets/aave/index.ts
@@ -1,69 +1,47 @@
-import {
-  AaveV2Polygon,
-  AaveV2Ethereum,
-  AaveV2EthereumAMM,
-  AaveV2Avalanche,
-  AaveV3Base,
-  AaveV3Optimism,
-  AaveV3Arbitrum,
-  AaveV3Ethereum,
-  AaveV3Polygon,
-  AaveV3Avalanche,
-  AaveV3Scroll,
-  AaveV3BNB,
-  AaveV3Metis,
-  AaveV3Gnosis,
-} from "@bgd-labs/aave-address-book";
 import getTokenPrices from "./aave";
 
 export function aave(timestamp: number = 0) {
   return Promise.all([
     getTokenPrices(
       "base",
-      AaveV3Base.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Base.STATIC_A_TOKEN_FACTORY,
+      "0x2f6571d3Eb9a4e350C68C36bCD2afe39530078E2",
+      "0x940F9a5d5F9ED264990D0eaee1F3DD60B4Cb9A22",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "optimism",
-      AaveV3Optimism.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Optimism.STATIC_A_TOKEN_FACTORY,
+      "0x770ef9f4fe897e59daCc474EF11238303F9552b6",
+      "0x22D76094730fA377184100EFB8CEfC673B89B372",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "arbitrum",
-      AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Arbitrum.STATIC_A_TOKEN_FACTORY,
+      "0x770ef9f4fe897e59daCc474EF11238303F9552b6",
+      "0xD61BF98649EA8F8D09e184184777b1867F00E5CB",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "ethereum",
-      AaveV2Ethereum.POOL_ADDRESSES_PROVIDER_REGISTRY,
+      "0x52D306e36E3B6B02c153d0266ff0f85d18BCD413",
       null,
       "v2",
       timestamp
     ),
     getTokenPrices(
       "ethereum",
-      AaveV3Ethereum.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Ethereum.STATIC_A_TOKEN_FACTORY,
+      "0xbaA999AC55EAce41CcAE355c77809e68Bb345170",
+      "0x411D79b8cC43384FDE66CaBf9b6a17180c842511",
       "v3",
       timestamp
     ),
-    // this still doesn't work though as in aave.ts just the first provider is taken(which is wrong)
-    // getTokenPrices(
-    //   "ethereum",
-    //   AaveV2EthereumAMM.POOL_ADDRESSES_PROVIDER_REGISTRY,
-    //   null,
-    //   "v2",
-    //   timestamp
-    // ),
+    // AMM market has no registry
+    //getTokenPrices("ethereum", "0x7937d4799803fbbe595ed57278bc4ca21f3bffcb");
     getTokenPrices(
       "polygon",
-      AaveV2Polygon.POOL_ADDRESSES_PROVIDER_REGISTRY,
+      "0x3ac4e9aa29940770aeC38fe853a4bbabb2dA9C19",
       null,
       "v2",
       timestamp
@@ -71,14 +49,14 @@ export function aave(timestamp: number = 0) {
     //polygon V3
     getTokenPrices(
       "polygon",
-      AaveV3Polygon.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Polygon.STATIC_A_TOKEN_FACTORY,
+      "0x770ef9f4fe897e59daCc474EF11238303F9552b6",
+      "0x397202AB0b4E7C954ac0c493c00749C517210953",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "avax",
-      AaveV2Avalanche.POOL_ADDRESSES_PROVIDER_REGISTRY,
+      "0x4235E22d9C3f28DCDA82b58276cb6370B01265C2",
       null,
       "v2",
       timestamp
@@ -86,36 +64,36 @@ export function aave(timestamp: number = 0) {
     //avax V3
     getTokenPrices(
       "avax",
-      AaveV3Avalanche.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Avalanche.STATIC_A_TOKEN_FACTORY,
+      "0x770ef9f4fe897e59daCc474EF11238303F9552b6",
+      "0x691C316b2Eec7e64d17e7E3E01f3dB44c9CcEf19",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "scroll",
-      AaveV3Scroll.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Scroll.STATIC_A_TOKEN_FACTORY,
+      "0xFBedc64AeE24921cb43004312B9eF367a4162b57",
+      "0x970b77b96D94966939a8F867c2BfC2aC0310C0e7",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "metis",
-      AaveV3Metis.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Metis.STATIC_A_TOKEN_FACTORY,
+      "0x9E7B73ffD9D2026F3ff4212c29E209E09C8A91F5",
+      "0x9C62AdC332888F56998542415c38D7CDf3Ff7619",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "bsc",
-      AaveV3BNB.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3BNB.STATIC_A_TOKEN_FACTORY,
+      "0x117684358D990E42Eb1649E7e8C4691951dc1E71",
+      "0x326aB0868bD279382Be2DF5E228Cb8AF38649AB4",
       "v3",
       timestamp
     ),
     getTokenPrices(
       "gnosis",
-      AaveV3Gnosis.POOL_ADDRESSES_PROVIDER_REGISTRY,
-      AaveV3Gnosis.STATIC_A_TOKEN_FACTORY,
+      "0x1236010CECea55998384e795B59815D871f5f94d",
+      "0x02e9b27599C4Bf8f789d34b6E65C51092c3d9FA6",
       "v3",
       timestamp
     ),

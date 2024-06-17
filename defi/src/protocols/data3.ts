0424310fcd11d817d42a93010c594efad45e2979
realdealshaman <realdealshaman11@gmail.com>
2024-06-17 15:44:44 +0100

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -46436,7 +46436,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Derivatives",
+    category: "Options",
     chains: ["zkSync Era"],
     oracles: ["Chainlink"], 
     forkedFrom: [],

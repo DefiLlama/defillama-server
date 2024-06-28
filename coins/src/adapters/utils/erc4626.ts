d119c08113348fb5b8bbfad4e6e8f22db563363a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-28 23:40:02 +0200

    coins: erc4626 bugfix

--- coins/src/adapters/utils/erc4626.ts
+++ coins/src/adapters/utils/erc4626.ts
@@ -15,15 +15,18 @@ export async function calculate4626Prices(
   projectName: string,
 ): Promise<Write[]> {
   const api = await getApi(chain, timestamp)
-  const uTokens = await api.multiCall({  abi: 'address:asset', calls: tokens,  permitFailure: true  } as any)
-  const supply = await api.multiCall({  abi: 'uint256:totalSupply', calls: tokens,  permitFailure: true  } as any)
-  const tokenBals = await api.multiCall({  abi: 'uint256:totalAssets', calls: tokens,  permitFailure: true  } as any)
+  const uTokens = await api.multiCall({ abi: 'address:asset', calls: tokens, permitFailure: true } as any)
+  const supply = await api.multiCall({ abi: 'uint256:totalSupply', calls: tokens, permitFailure: true } as any)
+  const tokenBals = await api.multiCall({ abi: 'uint256:totalAssets', calls: tokens, permitFailure: true } as any)
+  const tokenDecimals = await api.multiCall({ abi: 'uint8:decimals', calls: tokens, permitFailure: true } as any)
+  const uTokenDecimals = (await api.multiCall({ abi: 'uint8:decimals', calls: uTokens.map(i => i ?? '0x0000000000000000000000000000000000000000'), permitFailure: true } as any)).map(i => i ?? 18)
   const pricesObject: any = {}
   tokens.forEach((token, i) => {
-    if (!uTokens[i] || !supply[i] || !tokenBals[i]) return; // Skip if any of the values are null/call failed
-    const price = tokenBals[i] / supply[i] 
+    if (!uTokens[i] || !supply[i] || !tokenBals[i] || !tokenDecimals[i]) return; // Skip if any of the values are null/call failed
+    const decimalDiff = +tokenDecimals[i] - uTokenDecimals[i]
+    const price = (tokenBals[i] / supply[i]) * (10 ** decimalDiff)
     if (isNaN(price)) return; // Skip if price is not a number
-    pricesObject[token] = { underlying: uTokens[i], price}
+    pricesObject[token] = { underlying: uTokens[i], price }
   })
   return getWrites({ chain, timestamp, pricesObject, projectName, })
 }

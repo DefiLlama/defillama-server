0b5c853a709cf66180e674e8c9cd5f615407f3f1
Define101 <Lovelovenow@protonmail.com>
2024-07-03 09:29:05 +0100

    category Xei

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -48432,7 +48432,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Derivatives", 
+    category: "Dexes", 
     chains: ["Sei"],
     oracles: [], 
     forkedFrom: [],

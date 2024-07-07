0d8f9783b16f62dc17372e8a14f24c7998a9f897
Define101 <Lovelovenow@protonmail.com>
2024-07-07 10:06:54 +0100

    fix category binlayer

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -46316,7 +46316,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Restaking",
+    category: "Yield", // no restaking
     chains: ["Binance"],
     module: "binlayer/index.js",
     twitter: "binlayer_xyz",

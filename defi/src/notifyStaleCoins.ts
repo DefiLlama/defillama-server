079c9c4fdc07edc0c67b6d04c5dd286bf8256057
waynebruce0x <waynebruce0x@gmail.com>
2024-07-17 16:32:05 +0100

    notify

--- defi/src/notifyStaleCoins.ts
+++ defi/src/notifyStaleCoins.ts
@@ -3,8 +3,10 @@ import {
   notifyStaleCoins,
   notifyChangedAdapter,
 } from "./storeTvlInterval/staleCoins";
+import setEnvSecrets from "./utils/shared/setEnvSecrets";
 
 const handler = async (_event: any) => {
+  await setEnvSecrets()
   await Promise.all([notifyStaleCoins(), notifyChangedAdapter()]);
 };
 

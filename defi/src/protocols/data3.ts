47fa37d9b217d0969742408a8da52a25c7ef18ae
Define101 <Lovelovenow@protonmail.com>
2024-07-19 11:49:19 +0100

    disable Teahouse Permissionless (api problems)

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -13330,6 +13330,7 @@ const data3: Protocol[] = [
     parentProtocol: "parent#teahouse-finance",
     listedAt: 1690211924,
   },
+  /*
   {
     id: "3286",
     name: "Teahouse Permissionless",
@@ -13356,6 +13357,7 @@ const data3: Protocol[] = [
     parentProtocol: "parent#teahouse-finance",
     listedAt: 1690211928,
   },
+  */
   {
     id: "3287",
     name: "TruStake",

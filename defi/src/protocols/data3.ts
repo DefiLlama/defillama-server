876189b73c01c62c0edaef0b559bdd423baf7549
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-30 16:30:04 +0300

    change hive protocol category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -42191,7 +42191,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Staking Pool",
+    category: "Chain",
     chains: ["BSquared"],
     oracles: [],
     module: "hive-protocol/index.js",

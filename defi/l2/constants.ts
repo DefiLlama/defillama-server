a3b2eb57b848b97265b33ffb54f7238b9823ca02
waynebruce0x <waynebruce0x@gmail.com>
2024-08-01 14:40:59 +0100

    revert for today

--- defi/l2/constants.ts
+++ defi/l2/constants.ts
@@ -74,7 +74,7 @@ export const canonicalBridgeIds: { [id: string]: Chain } = {
   "4796": "sanko",
   "4797": "xai",
   "4124": "merlin",
-  "4937": "reya",
+  // "4937": "reya",
 };
 
 export const protocolBridgeIds: { [chain: string]: Chain } = {

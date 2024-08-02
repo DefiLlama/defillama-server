5b322bda52be43493e421da4e2d783435b9c907d
realdealshaman <realdealshaman11@gmail.com>
2024-08-02 20:09:26 +0100

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -50776,7 +50776,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Lending",
+    category: "CDP",
     chains: ["Ethereum"],
     oracles: [],
     forkedFrom: [],

b6429a323ace54325971b11f88bf843ab404d0be
realdealshaman <realdealshaman11@gmail.com>
2024-08-02 20:14:00 +0100

    add parent

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -50784,6 +50784,7 @@ const data3: Protocol[] = [
     twitter: "mahaxyz_",
     github: ["mahaxyz"],
     treasury: "mahaxyz.js",
+    parentProtocol: "parent#mahadao",
     listedAt: 1722616997
   },
   {

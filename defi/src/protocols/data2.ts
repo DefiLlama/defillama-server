c626e437f8c1707a1c7a9d6bfbfb7a0dce148422
OptimGMI <optimgmi@gmail.com>
2024-08-13 12:59:34 -0700

    Fix category back to previous
    In previous commit, was changed to yield but this was not accurate, so changed back

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -23150,7 +23150,7 @@ const data2: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield",
+    category: "Lending",
     chains: ["Cardano"],
     module: "optim-liquidity-bonds/index.js",
     twitter: "OptimFi",

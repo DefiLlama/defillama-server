4db1fb7b74dc0f0b1d352e2dd0111a098d7077b1
waynebruce0x <waynebruce0x@gmail.com>
2024-08-14 16:45:38 +0100

    cg without coins2

--- coins/src/adapters/utils/dbInterfaces.ts
+++ coins/src/adapters/utils/dbInterfaces.ts
@@ -31,6 +31,13 @@ export interface DbEntry {
   symbol: string;
   confidence: number;
 }
+export interface CgEntry {
+  PK: string;
+  SK: number;
+  price: number;
+  symbol: string;
+  confidence: number;
+}
 export interface Read {
   dbEntry: DbEntry;
   redirect: any;

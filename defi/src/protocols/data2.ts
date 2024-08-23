0d2406d98a6c90005daca9a689139b0f05f911bd
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2024-08-23 01:55:23 +0100

    Update data2.ts
    add reference link

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -18741,7 +18741,7 @@ const data2: Protocol[] = [
       arbitrum: ["Chainlink"],
       polygon_zkevm: ["Chainlink"],
       xlayer: ["Chainlink"],
-      mantle: ["Chronicle"],
+      mantle: ["Chronicle"], // https://github.com/DefiLlama/defillama-server/pull/7767
     },
     audit_links: [
       "https://github.com/dolomite-exchange/dolomite-margin/blob/master/docs/Dolomite%20Margin%20-%20Cyfrin%20-%202023-08-23.pdf",

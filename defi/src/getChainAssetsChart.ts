40f8d2be1935734c1ef72d917787fe8d1f89c128
waynebruce0x <waynebruce0x@gmail.com>
2024-09-03 11:26:18 +0100

    fix tron bridged chart

--- defi/src/getChainAssetsChart.ts
+++ defi/src/getChainAssetsChart.ts
@@ -4,7 +4,7 @@ import setEnvSecrets from "./utils/shared/setEnvSecrets";
 
 const handler = async (event: any): Promise<IResponse> => {
   try {
-    const chain = event.pathParameters?.chain?.toLowerCase();
+    const chain = event.pathParameters?.chain;
     await setEnvSecrets()
     const chains = await fetchHistoricalFromDB(chain);
     return successResponse(chains, 10 * 60); // 10 min cache

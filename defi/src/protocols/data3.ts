01734943dbc04583cc425bda400c12c0856b5cdc
realdealshaman <realdealshaman11@gmail.com>
2024-09-12 15:20:51 +0100

    fix category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -55241,7 +55241,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "DEX Aggregators",
+    category: "DEX Aggregator",
     chains: ["Polygon"],
     oracles: [],
     forkedFrom: [],

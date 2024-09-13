f751f88aa49e62cc7deba0b62c89c603a0443906
pie <greatpie@live.com>
2024-09-14 02:05:35 +0800

    feat: Update the catalogue to Yield

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -34269,7 +34269,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "CDP",
+    category: "Yield",
     chains: ["Base","TON"],
     module: "cygnus-finance/index.js",
     twitter: "CygnusFi",

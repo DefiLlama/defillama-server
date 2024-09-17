d32b3b62d42530fd0207d1193843659bd01351c5
Double <dev@double2win.xyz>
2024-09-16 17:46:09 -0700

    fix: update defi category and project name into correct one for double (#8037)
    * fix: update defi category and project name into correct one
    
    * fix: update category into Yield for double

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -52720,7 +52720,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquidity manager",
+    category: "Yield",
     chains: ["Arbitrum"],
     oracles: [],
     forkedFrom: [],

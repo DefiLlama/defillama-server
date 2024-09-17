e8d653d1d78630722f5ed79f4839cacec4b8a433
realdealshaman <realdealshaman11@gmail.com>
2024-09-17 06:00:57 +0100

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -204,7 +204,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Services",
+    category: "Governance Incentives",
     chains: ["Solana"],
     oracles: [],
     forkedFrom: [],

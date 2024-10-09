2fb2b3c0242c9a5aaa78cbe8f05fc95e677d0683
Define101 <Lovelovenow@protonmail.com>
2024-10-09 13:45:29 +0100

    pstake category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -51474,7 +51474,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Bridge",
+    category: "Restaked BTC",
     chains: ["Bitcoin"],
     oracles: [],
     forkedFrom: [],

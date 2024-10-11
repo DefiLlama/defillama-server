de284ed39c552a6168a729b007a319a44ff9741f
Define101 <Lovelovenow@protonmail.com>
2024-10-11 11:10:30 +0100

    okex name

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -56687,14 +56687,14 @@ const data3: Protocol[] = [
   },
   {
     id: "5201",
-    name: "OKX DEX Aggregator",
+    name: "OKX DEX", // previous OKX DEX Aggregator
     address: null,
     symbol: "-",
     url: "https://www.okx.com/web3/dex-swap",
     description:
       "Freely choose trading routes from all major DEX aggregators, while X Routing continues to find the best quotes across 400+ DEXs and 30+ networks.",
     chain: "Ethereum",
-    logo: `${baseIconsUrl}/okx-dex-aggregator.jpg`,
+    logo: `${baseIconsUrl}/okx-dex.jpg`,
     audits: "0",
     audit_note: null,
     gecko_id: null,

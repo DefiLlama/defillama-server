9c9b0a92eabfa8f0e2e3c597e5cec2ba101e2a4b
Define101 <Lovelovenow@protonmail.com>
2024-10-15 16:05:18 +0100

    name M^0

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -57588,14 +57588,14 @@ const data3: Protocol[] = [
   },
   {
     id: "5241",
-    name: "M^0",
+    name: "M0", // this breaked probablty the ^ , preivous name M^0
     address: "0x5983B89FA184f14917013B9C3062afD9434C5b03",
     symbol: "POWER",
     url: "https://www.m0.org/",
     description:
       "M^0 is decentralized money middleware that allows institutions to issue cryptodollars",
     chain: "Ethereum",
-    logo: `${baseIconsUrl}/m^0.png`,
+    logo: `${baseIconsUrl}/m0.png`,
     audits: "2",
     audit_note: null,
     gecko_id: null,

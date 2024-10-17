f72da35bde29035e3413bb92d6f09185991d627f
realdealshaman <realdealshaman11@gmail.com>
2024-10-17 04:46:04 +0100

    add new listings

--- defi/src/protocols/parentProtocols.ts
+++ defi/src/protocols/parentProtocols.ts
@@ -5596,6 +5596,19 @@ const parentProtocols: IParentProtocol[] = [
     chains: [],
     twitter: "tonstakers",
   },
+  {
+    id: "parent#kelp-dao",
+    name: "Kelp DAO",
+    url: "https://kelpdao.xyz/",
+    description:
+      "The Kelp DAO team is currently building an LRT solution, rsETH, on EigenLayer for Ethereum",
+    logo: `${baseIconsUrl}/kelp-dao.png`,
+    gecko_id: null,
+    cmcId: null,
+    chains: [],
+    twitter: "KelpDAO",
+    github: ["Kelp-DAO"],
+  },
 ];
 
 export default parentProtocols;

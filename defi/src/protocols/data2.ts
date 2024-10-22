c58c03f856804c5bfedf55b468d56d371cb68705
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2024-10-22 18:57:54 +0100

    update oracle list and add ref link
    update oracle list and add ref link

--- defi/src/protocols/data2.ts
+++ defi/src/protocols/data2.ts
@@ -15289,7 +15289,7 @@ const data2: Protocol[] = [
     cmcId: null,
     category: "CDP",
     chains: ["Binance"],
-    oracles: ["RedStone", "Chainlink", "Binance Oracle", "Pyth"],
+    oracles: ["RedStone"], // https://github.com/DefiLlama/defillama-server/pull/8309
     forkedFrom: ["MakerDAO"],
     module: "helio-money/index.js",
     twitter: "LISTA_DAO",

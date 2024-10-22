c589163cf7c30d0ed8281b0232e1558c97e5c004
Mick de Graaf <mick@mickdegraaf.nl>
2024-10-22 03:16:29 +0200

    Add Redstone, Pyth and Chainlink oracles for Euler v2 (#8231)
    * Add Redstone, Pyth and Chainlink oracles for Euler v2
    
    * Fix typo
    
    * Update data3.ts
    
    update reference link
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -53173,7 +53173,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "Lending",
     chains: ["Ethereum"],
-    oracles: ["Chronicle"], // https://github.com/DefiLlama/defillama-server/pull/8110
+    oracles: ["Chronicle", "Redstone", "Pyth", "Chainlink"], // https://oracles.euler.finance/1
     forkedFrom: [],
     module: "euler-v2/index.js",
     twitter: "eulerfinance",

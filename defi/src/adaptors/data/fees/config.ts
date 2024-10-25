4c55dff7d63a6260ff6239cca1d1197a1b1aaf4e
dtmkeng <phumaisut@gmail.com>
2024-10-25 16:03:14 +0000

    update config

--- defi/src/adaptors/data/fees/config.ts
+++ defi/src/adaptors/data/fees/config.ts
@@ -2088,12 +2088,7 @@ export default {
     },
     "sparkdex-v3-1": {
         parentId: "SparkDEX",
-        id: "5223",
-        protocolsData: {
-            "v3": {
-                "id": "5223",
-            }
-        }
+        id: "5223"
     },
     "sparkdex-v2": {
         parentId: "SparkDEX",

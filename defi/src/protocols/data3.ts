fcf16ff44626772ce1e83a5cbcd74f99392fd06c
mgild <mgild@users.noreply.github.com>
2024-10-28 14:30:10 -0400

    Remove adrastrea for now

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -54556,7 +54556,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "Yield",
     chains: ["Solana"],
-    oracles: ["Switchboard"],
+    oracles: [],
     forkedFrom: [],
     module: "adrastea/index.js",
     twitter: "AdrasteaFinance",

fc4e4a17f178c51b8ef0c94e58540a75158f2dab
waynebruce0x <waynebruce0x@gmail.com>
2024-10-28 13:11:22 +0000

    Revert "okx"

--- coins/src/utils/missingCoins.ts
+++ coins/src/utils/missingCoins.ts
@@ -1,25 +1,38 @@
-import { queryPostgresWithRetry } from "../../coins2";
-import { getCoins2Connection } from "../../getDBConnection";
+import mysql from 'mysql2/promise';
+import { getCurrentUnixTimestamp } from './date';
 
-export async function storeMissingCoins(
-  missingCoins: { chain: string; address: string }[],
-) {
-  if (!missingCoins.length) return;
-  const sql = await getCoins2Connection();
+// Tables
+// CREATE TABLE missing (time INT, coin VARCHAR(500), timestampRequested INT, rawCoin VARCHAR(500), chain VARCHAR(100), PRIMARY KEY(time, coin, timestampRequested), INDEX `idx_time` (`time` ASC) VISIBLE);
 
-  const coins: { chain: string; address: string; hasbeenchecked: boolean }[] =
-    missingCoins.map((coin) => ({
-      hasbeenchecked: false,
-      ...coin,
-    }));
+let connection: mysql.Pool;
 
-  await queryPostgresWithRetry(
-    sql`
-    insert into unsupportedkeys
-    ${sql(coins, "chain", "address", "hasbeenchecked")}
-    on conflict (chain, address)
-    do nothing
-  `,
-    sql,
-  );
+function getConnection() {
+  if (!connection)
+    connection = mysql.createPool({
+        host: '65.21.124.238',
+        port: 9000,
+        user: 'clcao5p1q02z5b3p84l6b0ow2',
+        database: 'clcao5p1r02z7b3p83eklevpm',
+        password: process.env.MISSING_COINS_DB_PWD,
+        waitForConnections: true,
+        connectionLimit: 1,
+      });
+  return connection
+}
+
+function executeAndIgnoreErrors(sql: string, values: any){
+    return getConnection().execute(sql, values)
+    .catch((e:any) => console.log("mysql error", e));
+}
+
+export function storeMissingCoins(requestedCoins: string[], response: {
+    [coin:string]:any
+}, timestampRequested:number){ // timestampRequested = 0 -> current data
+   return Promise.all(requestedCoins.map(async coin=>{
+    if(response[coin] === undefined){
+        const lowercaseCoin = coin.toLowerCase()
+        return executeAndIgnoreErrors('INSERT INTO `missing` VALUES (?, ?, ?, ?, ?)', 
+            [getCurrentUnixTimestamp(), lowercaseCoin, timestampRequested, coin, lowercaseCoin.split(":")[0]])
+    }
+   }))
 }

425d1282e30ab394165d3df1ec9dc840757aa281
realdealshaman <realdealshaman11@gmail.com>
2024-11-04 21:49:28 +0000

    fix category syntax

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -59327,7 +59327,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Dexs",
+    category: "Dexes",
     chains: ["Ethereum"],
     oracles: [],
     forkedFrom: [],

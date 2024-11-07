b6e509fe531cbe084f6a6f337d471dec0d8bf06d
waynebruce0x <waynebruce0x@gmail.com>
2024-11-07 22:18:30 +0000

    blacklist pt

--- coins/src/adapters/other/distressed.ts
+++ coins/src/adapters/other/distressed.ts
@@ -118,6 +118,7 @@ export const contracts: { [chain: string]: { [token: string]: string } } = {
     BCCOIN: "0x2940566Eb50F15129238f4Dc599ADC4F742D7d8E",
     PNIC: "0x76d36d44dc4595e8d2eb3ad745f175eda134284f",
     "PENDLE-LPT": "0x0921ccc98956b1599003fd9739d5e66bf319a161",
+    "PT-USD0++-27MAR2025": "0x5bae9a5d67d1ca5b09b14c91935f635cfbf3b685"
   },
   cronos: {
     CRK: "0x065de42e28e42d90c2052a1b49e7f83806af0e1f",

ee9e713a2b381a2889e3d5de1f02af821b591cd9
Define101 <Lovelovenow@protonmail.com>
2024-11-15 15:57:18 +0000

    add m-btc

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -58813,7 +58813,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquid Staking",
+    category: "Liquid Restaking",
     chains: ["Solana"],
     oracles: [], 
     forkedFrom: [],

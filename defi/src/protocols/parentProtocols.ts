b81049c92353412607a584ce47a709d82725af80
rav <85165079+rav-ipor@users.noreply.github.com>
2024-11-25 16:46:23 +0100

    Update chains in IPOR Derivatives and IPOR Fusion (#8635)
    * Update chains in IPOR Derivatives and IPOR Fusion
    
    Other updates in IPOR Derivatives and IPOR Fusion:
    - url
    - gecko_id
    - cmcId
    
    * Remove wrongLiquidity from IPOR Protocol

--- defi/src/protocols/parentProtocols.ts
+++ defi/src/protocols/parentProtocols.ts
@@ -5462,7 +5462,7 @@ const parentProtocols: IParentProtocol[] = [
   {
     id: "parent#ipor",
     name: "IPOR Protocol",
-    url: "https://www.ipor.io",
+    url: "https://ipor.io",
     description: "IPOR refers to a set of protocols, smart contracts, and software that forms a set of DApps for DeFi focused on interest rate derivatives",
     logo: `${baseIconsUrl}/ipor-protocol.svg`,
     gecko_id: "ipor",
@@ -5471,7 +5471,6 @@ const parentProtocols: IParentProtocol[] = [
     twitter: "ipor_io",
     treasury: "ipor.js",
     github: ["IPOR-Labs"],
-    wrongLiquidity: true,
     governanceID: ["snapshot:ipordao.eth"],
   },
   {

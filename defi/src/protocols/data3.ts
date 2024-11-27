cb17695b5ff25ebe32fded0ceea8d5aa378b44eb
Define101 <Lovelovenow@protonmail.com>
2024-11-27 14:37:28 +0000

    euler

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -53181,7 +53181,7 @@ const data3: Protocol[] = [
     symbol: "EUL",
     url: "https://www.euler.finance",
     description:
-      "Permissionless crypto lending markets are almost here. Euler is a non-custodial protocol on Ethereum that allows users to lend and borrow almost any crypto asset",
+      "Euler revolutionizes DeFi by letting any asset become collateral for a lending market. Lenders and borrowers get market leading risk-adjusted rates. Builders can create and manage markets exactly how they want them, with institutional-grade security.",
     chain: "Ethereum",
     logo: `${baseIconsUrl}/euler-v2.png`,
     audits: "2",

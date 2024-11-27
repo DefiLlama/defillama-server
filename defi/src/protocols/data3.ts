3e106adb79a3aa26c079eb5446bfd4c3756f19a7
Define101 <Lovelovenow@protonmail.com>
2024-11-27 10:05:23 +0000

    Catton CDP

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -61428,7 +61428,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Gaming",
+    category: "CDP",
     chains: ["TON"],
     module: "catton/index.js",
     twitter: "Cattontw",

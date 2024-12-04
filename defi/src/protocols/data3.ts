f04401ecc81d9bb3131ceb185b57399cc1622997
realdealshaman <realdealshaman11@gmail.com>
2024-12-04 15:14:30 -0600

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -33883,7 +33883,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Cross Chain",
+    category: "Bridge",
     chains: ["Bitcoin", "Ripple", "Cardano", "Solana", "Polkadot", "Litecoin"],
     oracles: [],
     forkedFrom: [],

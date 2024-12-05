12622534654ac6cfc4bb883a2e5901321324226a
krishnafractol <137801527+krishnafractol@users.noreply.github.com>
2024-12-06 00:04:55 +0530

    Update Upshift Category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -62544,7 +62544,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield Aggregator",
+    category: "Yield",
     chains: ["Ethereum", "Avalanche", "Base"],
     oracles: [], 
     forkedFrom: [],

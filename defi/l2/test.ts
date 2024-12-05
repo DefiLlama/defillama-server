f75dd92edaee2d1772a594b724075ba65c11c002
waynebruce0x <waynebruce0x@gmail.com>
2024-12-05 17:30:01 +0000

    check lowercase

--- defi/l2/test.ts
+++ defi/l2/test.ts
@@ -18,7 +18,11 @@ export async function verifyChanges(chains: FinalData) {
 
     const totalNew = allNew.total.total;
     const totalOld = allOld.total.total;
-    if (chain == "Tron" && totalNew < 20_000_000_000) throw new Error(`USDT not counted for Tron`);
+    if (chain.toLowerCase() == "tron" && totalNew < 20_000_000_000) {
+      chains;
+      allNew;
+      throw new Error(`USDT not counted for Tron`);
+    }
     const forwardChange = totalOld != "0" ? (100 * Math.abs(totalNew - totalOld)) / totalOld : 0;
     const backwardChange = totalNew != 0 ? (100 * Math.abs(totalNew - totalOld)) / totalNew : 0;
     if (forwardChange < 30 || backwardChange < 30) return;

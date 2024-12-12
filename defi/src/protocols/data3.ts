78d820420ebd22453885f1a3b08267233788b767
Define101 <Lovelovenow@protonmail.com>
2024-12-12 11:11:37 +0000

    remove pell

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -45338,7 +45338,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Restaking",
+    category: "Farm",
     chains: ["Merlin"],
     oracles: [],
     forkedFrom: [],

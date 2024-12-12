19f9ef56881217b837ed80471dc58b2f160a6f9c
krishnafractol <137801527+krishnafractol@users.noreply.github.com>
2024-12-13 04:27:15 +0530

    Update Upshift Category (#8746)
    * Update Upshift Category
    
    * use new category 
    
    use new category for projects using a strategist to manage a token vault
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -62545,7 +62545,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield Aggregator",
+    category: "Managed Token Pools",
     chains: ["Ethereum", "Avalanche", "Base"],
     oracles: [], 
     forkedFrom: [],

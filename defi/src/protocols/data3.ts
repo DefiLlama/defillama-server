a0fa4ad23690b27c3cfdf480887ed0a4d9e59704
Define101 <Lovelovenow@protonmail.com>
2024-12-13 10:18:33 +0000

    pell category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -45339,7 +45339,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Farm",
+    category: "Restaking",
     chains: ["Merlin"],
     oracles: [],
     forkedFrom: [],

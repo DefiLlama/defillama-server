1f836c9552c0a029bcbbb11e1780f2dd134d1865
mr.Christof <68239965+0xChristof@users.noreply.github.com>
2024-12-16 11:22:17 +0100

    Update data3.ts

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -32147,7 +32147,7 @@ const data3: Protocol[] = [
     gecko_id: "synonym-finance",
     cmcId: null,
     category: "Lending",
-    chains: ["Arbitrum", "Optimism", "Base", "Scroll"],
+    chains: ["Arbitrum", "Scroll", "Base", "Optimism", "Ethereum"],
     oracles: ["Pyth", "Chainlink"], // https://synonym-finance.gitbook.io/synonym-finance/documentation/risk-management#oracles
     forkedFrom: [],
     module: "synonym-finance/index.js",

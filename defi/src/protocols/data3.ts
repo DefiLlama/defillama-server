61196de999f3a5d9dad67ea99c867ad7bcdb38d7
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-16 10:11:27 +0400

    Update data3.ts

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -63447,7 +63447,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Yield",
+    category: "Launchpad",
     chains: ["XDC"],
     module: "bbbpump/index.js",
     twitter: "bbbpumpdotfun",

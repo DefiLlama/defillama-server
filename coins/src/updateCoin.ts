e8e2b6dc29d816b2171cdad81cb7e746d9b9da4d
waynebruce0x <waynebruce0x@gmail.com>
2024-12-19 13:10:21 +0000

    response formatted

--- coins/src/updateCoin.ts
+++ coins/src/updateCoin.ts
@@ -1,35 +1,34 @@
+import { successResponse, wrap, IResponse } from "./utils/shared";
+import { batchWrite } from "./utils/shared/dynamodb";
 import {
-  successResponse,
-  wrap,
-  IResponse,
-  errorResponse,
-} from "./utils/shared";
-import { batchGet, batchWrite } from "./utils/shared/dynamodb";
-import parseRequestBody from "./utils/shared/parseRequestBody";
-import { coinToPK } from "./utils/processCoin";
-import { CoinsResponse, fetchCgPriceData } from "./utils/getCoinsUtils";
+  CoinsResponse,
+  fetchCgPriceData,
+  getBasicCoins,
+} from "./utils/getCoinsUtils";
 import { getCache, setCache } from "./utils/cache";
 import setEnvSecrets from "./utils/shared/setEnvSecrets";
 import { setTimer } from "./utils/shared/coingeckoLocks";
 
+const margin = 60 * 60 * 2; // 2 hours
 const handler = async (event: any): Promise<IResponse> => {
+  // this should require internal key
   await setEnvSecrets();
+  const start = new Date().getTime();
+  const unixStart = start / 1000;
   setTimer();
-  const { coins } = parseRequestBody(event.body);
-  const coinData = await batchGet(
-    coins.map((coin: string) => ({
-      PK: coinToPK(coin),
-      SK: 0,
-    })),
-  );
+  const requestedCoins = (event.pathParameters?.coins ?? "").split(",");
+  const { PKTransforms, coins } = await getBasicCoins(requestedCoins);
+
   const response = {} as CoinsResponse;
   const cgIds: { [pk: string]: string } = {};
-  coinData.map((d) => {
+  coins.map((d) => {
+    if (d.timestamp && d.timestamp > unixStart - margin) return;
     if (!d.redirect || !d.redirect.startsWith("coingecko#")) return;
     const id = d.redirect.substring(d.redirect.indexOf("#") + 1);
     cgIds[d.PK] = id;
   });
 
+  if (!cgIds.length) return successResponse({});
   // fetch new data and cache
   const [newData, bulk] = await Promise.all([
     fetchCgPriceData(Object.values(cgIds)),
@@ -37,7 +36,7 @@ const handler = async (event: any): Promise<IResponse> => {
   ]);
 
   const writes: any[] = [];
-  coinData.map(async ({ PK, symbol }) => {
+  coins.map(async ({ PK, symbol, decimals }) => {
     if (!(PK in cgIds)) return;
     const confidence = 0.99;
     const id = cgIds[PK];
@@ -46,6 +45,16 @@ const handler = async (event: any): Promise<IResponse> => {
       usd: price,
       last_updated_at: SK,
     } = newData[id];
+
+    response[PKTransforms[PK]] = {
+      decimals,
+      price,
+      symbol,
+      timestamp: SK,
+      confidence,
+    };
+
+    if (PK in bulk && bulk[PK] > unixStart - margin * 2) return;
     writes.push(
       ...[
         {
@@ -69,26 +78,26 @@ const handler = async (event: any): Promise<IResponse> => {
   });
 
   // write to cache and DB
-  //   await Promise.all([
-  //     batchWrite(writes, false),
-  //     setCache("coins-swap", "bulk", bulk),
-  //   ]);
+  await Promise.all([
+    batchWrite(writes, false),
+    setCache("coins-swap", "bulk", bulk),
+  ]);
 
   // return anyway
+  const end = new Date().getTime();
+  const duration = `${end - start}ms`;
   return successResponse({
     coins: response,
+    duration,
   });
 };
 
 export default wrap(handler);
 
 handler({
-  body: JSON.stringify({
-    coins: [
-      "base:0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842",
-      "base:0vdfvdfv",
-      "ethereum:0x57f6bddf2e015feb13e8f86cb7bb5a6a62723ffe",
-    ],
-  }),
+  pathParameters: {
+    coins:
+      "base:0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842,base:0vdfvdfv,ethereum:0x57f6bddf2e015feb13e8f86cb7bb5a6a62723ffe",
+  },
 });
 // ts-node coins/src/updateCoin.ts

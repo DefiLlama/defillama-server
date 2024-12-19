b7f61a19f6818b6de129bc52e51b38010020479c
krishnafractol <137801527+krishnafractol@users.noreply.github.com>
2024-12-19 14:14:52 +0530

    Updated Category to Yield

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -62561,7 +62561,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Managed Token Pools",
+    category: "Yield",
     chains: ["Ethereum", "Avalanche", "Base"],
     oracles: [], 
     forkedFrom: [],

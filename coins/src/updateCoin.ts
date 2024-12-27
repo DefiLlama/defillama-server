81def9967546206fb519c1f8f97392c8279f1703
waynebruce0x <waynebruce0x@gmail.com>
2024-12-27 13:37:42 +0000

    include /current/ on /update/

--- coins/src/updateCoin.ts
+++ coins/src/updateCoin.ts
@@ -3,41 +3,124 @@ import { batchWrite } from "./utils/shared/dynamodb";
 import {
   CoinsResponse,
   fetchCgPriceData,
+  batchGetLatest,
   getBasicCoins,
 } from "./utils/getCoinsUtils";
 import { setTimer } from "./utils/shared/coingeckoLocks";
 import setEnvSecrets from "./utils/shared/setEnvSecrets";
 import { getR2, storeR2JSONString } from "./utils/r2";
-console.log("imports done");
+import { quantisePeriod } from "./utils/timestampUtils";
+
+type Coin = {
+  PK: string;
+  symbol: string;
+  decimals: number;
+  timestamp: number;
+  redirect?: string;
+};
+
 const margin = 5 * 60; // 5 mins
 
+const isFresh = (timestamp: number, searchWidth: number) => {
+  if (!timestamp) return true;
+  const now = Date.now() / 1e3;
+  return now - timestamp < searchWidth;
+};
+
+const currentCoins = async (
+  event: any,
+): Promise<{
+  coins: Coin[];
+  PKTransforms: { [key: string]: string };
+  response: CoinsResponse;
+  allRequestedCoins: string[];
+}> => {
+  const allRequestedCoins = (event.pathParameters?.coins ?? "").split(",");
+  const searchWidth: number = quantisePeriod(
+    event.queryStringParameters?.searchWidth?.toLowerCase() ?? "12h",
+  );
+  const { PKTransforms, coins } = await getBasicCoins(allRequestedCoins);
+  const response = {} as CoinsResponse;
+  const coinsWithRedirect = {} as { [redirect: string]: any[] };
+  coins.forEach((coin) => {
+    if (coin.redirect === undefined) {
+      if (isFresh(coin.timestamp, searchWidth)) {
+        response[PKTransforms[coin.PK]] = {
+          decimals: coin.decimals,
+          price: coin.price,
+          symbol: coin.symbol,
+          timestamp: coin.timestamp,
+          confidence: coin.confidence,
+        };
+      }
+    } else {
+      coinsWithRedirect[coin.redirect] = [
+        ...(coinsWithRedirect[coin.redirect] ?? []),
+        coin,
+      ];
+    }
+  });
+  const redirects = Object.keys(coinsWithRedirect);
+  if (redirects.length > 0) {
+    const resolvedRedirectedCoins = await batchGetLatest(redirects);
+    resolvedRedirectedCoins.forEach((redirectedCoin) => {
+      coinsWithRedirect[redirectedCoin.PK].forEach((ogCoin) => {
+        if (isFresh(redirectedCoin.timestamp, searchWidth)) {
+          response[PKTransforms[ogCoin.PK]] = {
+            decimals: ogCoin.decimals,
+            symbol: ogCoin.symbol,
+            price: redirectedCoin.price,
+            timestamp: redirectedCoin.timestamp,
+            confidence: redirectedCoin.confidence,
+          };
+        }
+      });
+    });
+  }
+
+  return { response, coins, PKTransforms, allRequestedCoins };
+};
+
 const handler = async (event: any): Promise<IResponse> => {
+  // set up env and init promises
   await setEnvSecrets();
   process.env.tableName = "prod-coins-table";
-  console.log("entered handler");
   const start = new Date().getTime();
-  console.log("fetching r2");
-  const bulkPromise = await getR2(`updated-coins`).then((r) =>
+  const currentPromise: any = currentCoins(event);
+  const bulkPromise: Promise<any> = getR2(`updated-coins`).then((r) =>
     JSON.parse(r.body!),
   );
-  console.log("fetched r2");
+
   const unixStart = Math.floor(start / 1000);
   setTimer();
 
-  console.log("timer set");
-  const requestedCoins = (event.pathParameters?.coins ?? "").split(",");
-  console.log("fetching basic coins");
-  const { PKTransforms, coins } = await getBasicCoins(requestedCoins);
-  console.log(`basic coins length: ${coins.length}`);
+  // find coins missing from /current/ res
+  const { allRequestedCoins, response, PKTransforms, coins } =
+    await currentPromise;
+  const requestedCoins = allRequestedCoins.filter(
+    (r: string) =>
+      !(
+        Object.keys(response).includes(r) &&
+        response[r].timestamp > unixStart - margin
+      ),
+  );
 
-  const response = {} as CoinsResponse;
+  if (!requestedCoins.length) {
+    const end = new Date().getTime();
+    const duration = `${end - start}ms`;
+    return successResponse({
+      coins: response,
+      duration,
+    });
+  }
+
+  // find cg coins missing from /current/ res
   const cgIds: { [pk: string]: string } = {};
-  let bulk: { [id: string]: any } = bulkPromise;
-  coins.map((d) => {
+  let bulk: { [id: string]: any } = await bulkPromise;
+  coins.map((d: Coin) => {
+    if (!requestedCoins.includes(PKTransforms[d.PK])) return;
     if (d.PK in bulk && bulk[d.PK] > unixStart - margin) return;
-    if (d.timestamp && d.timestamp > unixStart - margin) return;
     if (!d.redirect || !d.redirect.startsWith("coingecko#")) return;
-
     const id = d.redirect.substring(d.redirect.indexOf("#") + 1);
     if (id in bulk && bulk[id] > unixStart - margin) return;
 
@@ -45,14 +128,20 @@ const handler = async (event: any): Promise<IResponse> => {
     bulk[id] = unixStart;
   });
 
-  console.log(`mapped`);
-  if (!Object.keys(cgIds).length) return successResponse({});
-  console.log(`fetching from cg`);
+  if (!Object.keys(cgIds).length) {
+    const end = new Date().getTime();
+    const duration = `${end - start}ms`;
+    return successResponse({
+      coins: response,
+      duration,
+    });
+  }
+
+  // refetch from cg
   const newData = await fetchCgPriceData(Object.values(cgIds), true);
-  console.log(`new data length: ${newData.length}`);
 
   const writes: any[] = [];
-  coins.map(async ({ PK, symbol, decimals }) => {
+  coins.map(async ({ PK, symbol, decimals }: Coin) => {
     if (!(PK in cgIds)) return;
     const confidence = 0.99;
     const id = cgIds[PK];
@@ -63,7 +152,6 @@ const handler = async (event: any): Promise<IResponse> => {
       last_updated_at: SK,
     } = newData[id];
 
-    bulk[PK] = unixStart;
     response[PKTransforms[PK]] = {
       decimals,
       price,
@@ -72,39 +160,40 @@ const handler = async (event: any): Promise<IResponse> => {
       confidence,
     };
 
-    if (PK in bulk && bulk[PK] > unixStart - margin / 2) return;
-    writes.push(
-      ...[
-        {
-          PK: `coingecko#${id}`,
-          SK: 0,
-          price,
-          mcap,
-          timestamp: SK,
-          symbol,
-          confidence,
-        },
-        {
-          PK: `coingecko#${id}`,
-          SK,
-          price,
-          confidence,
-          adapter: "updateCoin",
-        },
-      ],
-    );
+    if (!(PK in bulk && bulk[PK] > unixStart - margin / 2)) {
+      writes.push(
+        ...[
+          {
+            PK: `coingecko#${id}`,
+            SK: 0,
+            price,
+            mcap,
+            timestamp: SK,
+            symbol,
+            confidence,
+          },
+          {
+            PK: `coingecko#${id}`,
+            SK,
+            price,
+            confidence,
+            adapter: "updateCoin",
+          },
+        ],
+      );
+    }
+    bulk[PK] = unixStart;
   });
 
-  console.log(`writes length: ${writes.length}`);
+  // store fresh data
   await Promise.all([
     batchWrite(writes, false),
     storeR2JSONString("updated-coins", JSON.stringify(bulk)),
   ]);
 
-  console.log(`writes written`);
+  // respond
   const end = new Date().getTime();
   const duration = `${end - start}ms`;
-  console.log(`response`);
   return successResponse({
     coins: response,
     duration,
@@ -115,7 +204,8 @@ export default wrap(handler);
 
 // handler({
 //   pathParameters: {
-//     coins: "ethereum:0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+//     coins:
+//       "ethereum:0x1f9840a85d5af5bf1d1762f925bdaddc4201f984,base:0x5ae84075f0e34946821a8015dab5299a00992721,fantom:0x582423c10c9e83387a96d00a69ba3d11ee47b7b5,base:0x27d2decb4bfc9c76f0309b8e88dec3a601fe25a8",
 //   },
 // });
 // ts-node coins/src/updateCoin.ts

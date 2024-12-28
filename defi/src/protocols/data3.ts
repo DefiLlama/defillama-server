2b2cb233b29b3a4bcfe6bca037bf7d5fce5ba9e3
realdealshaman <realdealshaman11@gmail.com>
2024-12-28 06:02:20 +0000

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -56772,7 +56772,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Services",
+    category: "AI Agents",
     chains: ["Optimism"],
     module: "infinite/index.js",
     twitter: "InfiniteTradePr",

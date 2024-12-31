d450de7db458340fc62dfbfd72dd40ce6fe76694
MadReza <mohammadreza@refractedlabs.com>
2024-12-31 16:13:01 +0330

    feat: adapt Pryzm Protocol with adapters change

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -64691,7 +64691,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "Yield",
     chains: ["Pryzm"],
-    module: "pryzm/index.js",
+    module: "pryzm-protocol/index.js",
     twitter: "Pryzm_Zone",
     forkedFrom: [],
     oracles: [],

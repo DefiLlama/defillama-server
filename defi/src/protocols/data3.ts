138061835c717e62803de5bce25a633fa81fb95c
realdealshaman <realdealshaman11@gmail.com>
2025-01-07 17:37:15 +0000

    add reference link

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -59741,7 +59741,7 @@ const data3: Protocol[] = [
     module: "carrot/index.js",
     twitter: "DeFiCarrot",
     forkedFrom: [],
-    oracles: ["Switchboard", "Pyth"],
+    oracles: ["Switchboard", "Pyth"], // https://github.com/DefiLlama/defillama-server/pull/8978
     audit_links: ["https://deficarrot.com/assets/carrot_final_report.pdf"],
     listedAt: 1731107322
   },

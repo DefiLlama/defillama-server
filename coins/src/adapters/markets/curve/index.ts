9e56b15c97c9ce52da8c5957e3babf5782b443c8
waynebruce0x <waynebruce0x@gmail.com>
2025-01-08 10:28:00 +0000

    morpho tokens

--- coins/src/adapters/markets/curve/index.ts
+++ coins/src/adapters/markets/curve/index.ts
@@ -26,6 +26,7 @@ export function curve(timestamp: number = 0) {
       "0xc7de47b9ca2fc753d6a2f167d8b3e19c6d18b19a",
       "0x744793b5110f6ca9cc7cdfe1ce16677c3eb192ef",
       "0x8b83c4aa949254895507d09365229bc3a8c7f710",
+      "0x2570f1bd5d2735314fc102eb12fc1afe9e6e7193",
     ]),
     getGaugePrices("ethereum", timestamp),
   ]);

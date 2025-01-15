d9e001a039a07fd7ead03eb1efe095043c74bba8
ivicmad <victor.l@biswap.dev>
2025-01-15 14:33:14 +0200

    Update Biswap Token data and description

--- defi/src/protocols/data.ts
+++ defi/src/protocols/data.ts
@@ -5764,7 +5764,7 @@ const data: Protocol[] = [
     gecko_id: "biswap",
     cmcId: "10746",
     category: "Dexes",
-    chains: ["Binance"],
+    chains: ["Binance", "Ethereum", "Arbitrum", "Base"],
     module: "biswap/index.js",
     twitter: "Biswap_Dex",
     audit_links: ["https://www.certik.org/projects/biswap"],

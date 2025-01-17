0897ef729694891e8e893334ae208581d2da753d
dtmkeng <phumaisut@gmail.com>
2025-01-17 13:21:29 +0000

    reset cache v14

--- defi/src/api2/utils/dimensionsUtils.ts
+++ defi/src/api2/utils/dimensionsUtils.ts
@@ -21,7 +21,7 @@ export function getFileCacheKey(adaptorRecordType: AdapterType) {
 
 
 export function getFileCacheKeyV2() {
-  return `dimensions-data-v2-v1.0.13`
+  return `dimensions-data-v2-v1.0.14`
 }
 
 // to ensure that we pull the cache data only once

0dcc5f3a76fdada629e479d3689074cba33dfe78
joe-wachirawit <wachirawit.i@datawow.io>
2025-01-28 15:55:46 +0700

    Update data3.ts

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -56875,7 +56875,7 @@ const data3: Protocol[] = [
     address: null,
     symbol: "-",
     url: "https://portal.yay.space/stake",
-    description: "The SocialFi “Yay!” provides a place where web2 and web3 meet, where users and investors co-exist, and where everyone can connect, create a community and earn rewards. Since January 2020, Yay! offers over 9 million users a place where users find their interests, get connected, create community, casually make group calls, and more. Now, we are building our tokenomics which require the participation of Web3 investors worldwide. Therefore, Yay! Staking Campaign is launched as part of our long-term vision.\nThe Yay! Staking Campaign is designed to strengthen the Yay! ecosystem liquidity by allowing users to earn various rewards through participation. The campaign primarily targets Yay! supporters, encouraging them to stake their assets with protocol partners such as StakeStone, Kelp, and others via Yay! Dashboard. In return, participants can earn not only various points from these partners but also Yay! Gold and additional points along with future planned airdrop by Yay!",
+    description: "Yay! is a SocialFi platform where web2 and web3 meet, offering a unique space where everyone can belong. Yay! is heading toward 10 million users, highlighting its significant growth since its launch. With seamless new web3 features, Yay! aims to accelerate onboarding and drive mass adoption through its innovative tokenomics.",
     chain: "Ethereum",
     logo: `${baseIconsUrl}/yay-staking.png`,
     audits: "2",

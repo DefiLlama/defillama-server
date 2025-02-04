6e3d06f9cf46731d0cf42ccdcc939e68d48eb14e
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2025-02-04 18:54:28 +0000

    Update data3.ts
    revert seismic change

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -41557,7 +41557,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "Lending",
     chains: ["Blast"],
-    oracles: ["RedStone", "API3"], //https://docs.seismic.finance/accepted-collateral-types#oracles-utilized
+    oracles: ["RedStone"], ///https://docs.seismic.finance/accepted-collateral-types#oracles-utilized
     forkedFrom: ["AAVE V2"],
     module: "seismic/index.js",
     twitter: "seismicfinance",

9d303c29f6c039aeee2962bb22fa6b1386433dd3
waynebruce0x <waynebruce0x@gmail.com>
2025-02-07 11:41:15 +0000

    kodiak

--- coins/src/adapters/markets/uniswap/index.ts
+++ coins/src/adapters/markets/uniswap/index.ts
@@ -58,6 +58,7 @@ const config = {
   thruster1: { chain: 'blast', factory: '0x37836821a2c03c171fB1a595767f4a16e2b93Fc4' },
   equalizerSonic: { chain: 'sonic', factory: '0xDDD9845Ba0D8f38d3045f804f67A1a8B9A528FcC' },
   bulbaswap: { chain: "morph", factory: "0x8D2A8b8F7d200d75Bf5F9E84e01F9272f90EFB8b" },
+  kodiakv2: { chain: 'berachain', factory: '0x5e705e184d233ff2a7cb1553793464a9d0c3028f' },
 };
 
 export function extraUniV2Lps(timestamp: number = 0) {

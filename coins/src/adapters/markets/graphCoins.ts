e5cf957c1ce15f6cc666ded3eb1ab7169c49720d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-10 09:01:45 +0100

    track beraswap token prices

--- coins/src/adapters/markets/graphCoins.ts
+++ coins/src/adapters/markets/graphCoins.ts
@@ -1,12 +1,12 @@
 
 import getWrites from "../utils/getWrites";
 import { getApi } from "../utils/sdk";
-import { graph,  } from "@defillama/sdk"
+import { graph, } from "@defillama/sdk"
 
 type BlockQueryArgs = {
   block: number;
   minVolume?: number
-  minTVL: number
+  minTVL?: number
 }
 
 const defaultQuery = ({ block, minVolume = 100, minTVL = 100 }: BlockQueryArgs) => `{
@@ -26,20 +26,20 @@ const defaultQuery = ({ block, minVolume = 100, minTVL = 100 }: BlockQueryArgs) 
 }`
 
 function getGraphCoinsAdapter({ chain, endpoint, minVolume = 1e4, minTVL = 1e5, projectName = 'graph-coins', query = defaultQuery }: { chain: string, endpoint: string, minVolume?: number, minTVL?: number, projectName?: string, query?: (args: BlockQueryArgs) => string }) {
-  return async function adapter(timestamp: number = 0) {
+  async function adapter(timestamp: number = 0) {
     const chainApi = await getApi(chain, timestamp);
     const block = await chainApi.getBlock();
     const queryString = query({ block, minVolume, minTVL });
     const { tokens } = await graph.request(endpoint, queryString);
     const pricesObject: any = {}
     tokens.forEach((token: any) => {
-      if (token.totalValueLockedUSD >  51 * 1e6) {
-        console.log(`coin: ${token.id} has too high TVL (${Number(token.totalValueLockedUSD/1e6).toFixed(2)}M), skipping`)
+      if (token.totalValueLockedUSD > 51 * 1e6) {
+        console.log(`coin: ${token.id} has too high TVL (${Number(token.totalValueLockedUSD / 1e6).toFixed(2)}M), skipping`)
         return;
-      } 
+      }
       let price = token.totalValueLockedUSD / token.totalValueLocked
       let underlying: any = undefined
-      if (!price&& token.derivedETH) {
+      if (!price && token.derivedETH) {
         price = token.derivedETH
         underlying = '0x0000000000000000000000000000000000000000'
       }
@@ -52,6 +52,26 @@ function getGraphCoinsAdapter({ chain, endpoint, minVolume = 1e4, minTVL = 1e5, 
     })
     return getWrites({ chain, timestamp, pricesObject, projectName, });
   }
+
+  async function beraswapAdapter(timestamp: number = 0) {
+    const chainApi = await getApi(chain, timestamp);
+    const block = await chainApi.getBlock();
+    const queryString = beraswapQuery({ block, });
+    const { pools } = await graph.request(endpoint, queryString);
+
+    const pricesObject: any = {}
+    pools.forEach((token: any) => {
+      let price = token.totalLiquidity / token.totalShares
+      pricesObject[token.address] = { price, }
+    })
+    return getWrites({ chain, timestamp, pricesObject, projectName, });
+  }
+
+  switch (projectName) {
+    case 'beraswap': return beraswapAdapter;
+  }
+
+  return adapter;
 }
 
 export const adapters = {} as any;
@@ -70,10 +90,29 @@ const taraswapQuery = ({ block, }: BlockQueryArgs) => `{
   }
 }`
 
+const beraswapQuery = ({ block, }: BlockQueryArgs) => `query SFPMChunks {
+  pools ( 
+    block: {number: ${block - 100}}
+    first:1000 where: {
+    totalLiquidity_gt: 50000
+    totalSwapFee_gt: 100
+  } orderBy: totalLiquidity orderDirection:desc) {
+    id
+    totalSwapFee
+    totalLiquidity
+    swapsCount
+    address
+    name
+    symbol
+    totalShares
+  }
+}`
+
 const items = [
   { chain: 'ace', endpoint: 'https://endurance-subgraph-v2.fusionist.io/subgraphs/name/catalist/exchange-v3-v103', minTVL: 1e4, projectName: 'catalist' },
   { chain: 'vana', endpoint: 'https://api.goldsky.com/api/public/project_clnbo3e3c16lj33xva5r2aqk7/subgraphs/data-dex-vana/prod/gn', minTVL: 1e4, projectName: 'datadex' },
   { chain: 'tara', endpoint: 'https://indexer.lswap.app/subgraphs/name/taraxa/uniswap-v3', minTVL: 1e4, projectName: 'taraswap', },
+  { chain: 'berachain', endpoint: 'https://api.goldsky.com/api/public/project_clq1h5ct0g4a201x18tfte5iv/subgraphs/bex-subgraph/mainnet-v1.0.1/gn', projectName: 'beraswap', },
 ]
 
 items.forEach((config: any) => adapters[config.projectName] = getGraphCoinsAdapter(config));

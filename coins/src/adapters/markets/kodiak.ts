91af7dbd800ed728a62a11093f907f156108d330
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-16 14:07:41 +0100

    coins: fix kodiak

--- coins/src/adapters/markets/kodiak.ts
+++ coins/src/adapters/markets/kodiak.ts
@@ -1,4 +1,3 @@
-import { getCurrentUnixTimestamp } from "../../utils/date";
 import {
   addToDBWritesList,
   getTokenAndRedirectDataMap,
@@ -11,15 +10,9 @@ const target: string = "0x5261c5A5f08818c08Ed0Eb036d9575bA1E02c1d6";
 const chain: string = "berachain";
 
 export async function kodiak(timestamp: number = 0): Promise<Write[]> {
-  const api = await getApi(
-    chain,
-    timestamp == 0 ? getCurrentUnixTimestamp() : timestamp,
-  );
+  const api = await getApi(chain, timestamp,);
 
-  const deployers: string[] = await api.call({
-    target,
-    abi: "address[]:getDeployers",
-  });
+  const deployers: string[] = await api.call({ target, abi: "address[]:getDeployers", });
 
   const islands: string[] = (
     await api.multiCall({
@@ -30,18 +23,9 @@ export async function kodiak(timestamp: number = 0): Promise<Write[]> {
   ).flat();
 
   const [token0s, token1s, balances, infos] = await Promise.all([
-    api.multiCall({
-      abi: "address:token0",
-      calls: islands.map((target: string) => ({ target })),
-    }),
-    api.multiCall({
-      abi: "address:token1",
-      calls: islands.map((target: string) => ({ target })),
-    }),
-    api.multiCall({
-      abi: "uint256:getUnderlyingBalances",
-      calls: islands.map((target: string) => ({ target })),
-    }),
+    api.multiCall({ abi: "address:token0", calls: islands, }),
+    api.multiCall({ abi: "address:token1", calls: islands, }),
+    api.multiCall({ abi: "function getUnderlyingBalances() view returns (uint256, uint256)", calls: islands, }),
     getTokenInfo(chain, islands, undefined, { withSupply: true }),
   ]);
 
@@ -54,9 +38,9 @@ export async function kodiak(timestamp: number = 0): Promise<Write[]> {
   function tokenAum(
     token: string,
     balance: number,
-  ): { aum: number; confidence: number } {
-    const tokenData = underlyingData[token.toLowerCase()];
-    if (!tokenData) return { aum: NaN, confidence: NaN };
+  ): { aum: number; confidence: number } | undefined {
+    const tokenData = underlyingData[token];
+    if (!tokenData) return;
     return {
       aum: (tokenData.price * balance) / 10 ** tokenData.decimals,
       confidence: tokenData.confidence ?? 0.6,
@@ -69,12 +53,11 @@ export async function kodiak(timestamp: number = 0): Promise<Write[]> {
     const supply = infos.supplies[i].output;
     if (supply == "0") return;
 
-    const token0 = tokenAum(token0s[i], balances[i]);
-    const token1 = tokenAum(token1s[i], balances[i]);
-    if ([token0.aum, token1.aum].includes(NaN)) return;
+    const token0 = tokenAum(token0s[i], balances[i][0]);
+    const token1 = tokenAum(token1s[i], balances[i][1]);
+    if (!token0 || !token1) return;
 
-    const price =
-      (token0.aum + token1.aum) / (infos.supplies[i].output / 10 ** decimals);
+    const price = (token0.aum + token1.aum) / (infos.supplies[i].output / 10 ** decimals);
 
     addToDBWritesList(
       writes,

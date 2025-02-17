f80c6c3c3d422c8b5fdbaf8a1d1b1a60b0ee13a7
realdealshaman <realdealshaman11@gmail.com>
2025-02-17 16:59:25 +0000

    set oracles

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -15267,7 +15267,7 @@ const data3: Protocol[] = [
     module: "gmx-v2/index.js",
     twitter: "GMX_IO",
     forkedFrom: [],
-    oracles: ["Chainlink"], // https://x.com/GMX_IO/status/1866794916392874021 https://gov.gmx.io/t/implementation-of-chaos-labs-risk-oracles/3861
+    oracles: ["Chainlink","Edge"], // https://x.com/GMX_IO/status/1866794916392874021 https://gov.gmx.io/t/implementation-of-chaos-labs-risk-oracles/3861
     oraclesBreakdown: [
       {
         name: "Chainlink",

37c7a194a7ffb95620d5b3d4be05fc77c438887e
dangduoc97 <dangvanduoc1997@gmail.com>
2025-02-26 19:06:47 +0700

    Update Info Cgk-Id Svic

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -45113,7 +45113,7 @@ const data3: Protocol[] = [
   {
     id: "4669",
     name: "deFusion",
-    address: null,
+    address: "tomochain:0xcdde1f5d971a369eb952192f9a5c367f33a0a891",
     symbol: "-",
     assetToken: "SVIC",
     url: "https://www.defusion.xyz/",
@@ -45123,7 +45123,7 @@ const data3: Protocol[] = [
     logo: `${baseIconsUrl}/defusion.jpg`,
     audits: "0",
     audit_note: null,
-    gecko_id: null,
+    gecko_id: "defusion-staked-vic",
     cmcId: null,
     category: "Liquid Staking",
     chains: ["TomoChain"],

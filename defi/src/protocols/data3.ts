57a2b43400edc8624612fb55ed9270b3edb4d1b0
realdealshaman <realdealshaman11@gmail.com>
2025-03-07 15:51:26 +0000

    update category

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -64548,7 +64548,7 @@ const data3: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "CDP",
+    category: "Yield Aggregator",
     chains: ["Sonic", "Ethereum"],
     oracles: [],
     forkedFrom: [],

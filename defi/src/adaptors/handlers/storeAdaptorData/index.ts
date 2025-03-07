770503ff7ee936434725e1960e61b7d0303fc870
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-07 13:46:23 +0100

    fix: set default timestamp in handler2

--- defi/src/adaptors/handlers/storeAdaptorData/index.ts
+++ defi/src/adaptors/handlers/storeAdaptorData/index.ts
@@ -59,7 +59,7 @@ const ONE_DAY_IN_SECONDS = 24 * 60 * 60
 
 export const handler2 = async (event: IStoreAdaptorDataHandlerEvent) => {
   const defaultMaxConcurrency = 21
-  let { timestamp, adapterType, protocolNames, maxConcurrency = defaultMaxConcurrency, isDryRun = false, isRunFromRefillScript = false,
+  let { timestamp = timestampAnHourAgo, adapterType, protocolNames, maxConcurrency = defaultMaxConcurrency, isDryRun = false, isRunFromRefillScript = false,
     runType = 'default', yesterdayIdSet = new Set(), todayIdSet = new Set(),
 
   } = event

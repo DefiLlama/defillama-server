4975e6d18da1fac7967a27c94adb74de1a1fa7ee
bats4 <bats4.game@gmail.com>
2025-03-08 22:42:45 +0900

    Update data3.ts

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -32876,7 +32876,7 @@ const data3: Protocol[] = [
     module: "sobax-io/index.js",
     twitter: "SOBAX_DEX",
     forkedFrom: ["GMX V1"],
-    oracles: [],
+    oracles: ["Pyth"], // https://sobax.gitbook.io/sobax
     listedAt: 1708338460,
   },
   {

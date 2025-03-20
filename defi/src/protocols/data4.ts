f3c60d177f0a01ed778752e702430df1d986cd17
Hundert1000 <157396997+Hundert1000@users.noreply.github.com>
2025-03-20 20:55:32 +0100

    Update spelling beraborrow Oracle.ts (#9480)
    * Update spelling beraborrow Oracle.ts
    
    Hey Llamas,
    thanks to our community we just noticed that beraborrow was not assigned to RedStone due to spelling mistake in the code.
    
    https://x.com/Khees_/status/1901980098128986563
    
    * fix spelling
    
    fix spelling
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -3842,10 +3842,10 @@ const data4: Protocol[] = [
     cmcId: null,
     category: "CDP",
     chains: ["Berachain"],
-    oracles: ["Restone"],
+    oracles: ["RedStone"],
     oraclesBreakdown: [
       {
-        name: "Restone",
+        name: "RedStone",
         type: "Primary",
         proof: ["https://app.redstone.finance/app/feeds/?page=1&sortBy=popularity&sortDesc=false&perPage=32&networks=80094", "https://berascan.com//address/0x83c6f7F61A55Fc7A1337AbD45733AD9c1c68076D"]
       },

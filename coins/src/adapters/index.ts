448930151431a57fb3a4f58588bf16fe0a3651d1
zz <zz@asseto.finance>
2025-03-25 00:15:56 +0800

    add asseto token (#9500)

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -131,5 +131,6 @@ export default {
   jtoDerivs: require("./solana/jtoDerivs"),
   pac: require("./rwa/thepac"),
   tonDerivs: require("./other/ton"),
+  asseto: require("./rwa/asseto"),
   radixUnkownToken: require("./markets/radix"),
 };

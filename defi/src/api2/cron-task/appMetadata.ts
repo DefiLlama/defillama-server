327665b57c11e98eed6262f264f5a7370ddf953b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-01 11:41:14 +0200

    update app metadata to handle Ethereum chain denominations

--- defi/src/api2/cron-task/appMetadata.ts
+++ defi/src/api2/cron-task/appMetadata.ts
@@ -335,10 +335,16 @@ async function _storeAppMetadata() {
     // chart denominations
     const chainDenominationsKeys = ['geckoId', 'cmcId', 'symbol']
     Object.entries(chainCoingeckoIds).map(([chain, data]: any) => {
-      const obj: any = {}
+      let obj: any = {}
       chainDenominationsKeys.forEach((key) => {
         obj[key] = data[key] ?? null
       })
+      if (data.parent?.chain === 'Ethereum') 
+        obj = {
+          "geckoId": "ethereum",
+          "cmcId": "1027",
+          "symbol": "ETH"
+        }
       chartDenominationsChainMap[chain] = obj
     })
   }

371dd213fd6ddbd74e94a46905f90906f8bccd1c
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-14 05:22:33 -0700

    tag deprecated protocols in /lite/protocols

--- defi/src/storeGetProtocols.ts
+++ defi/src/storeGetProtocols.ts
@@ -70,6 +70,7 @@ export async function storeGetProtocols({
           defillamaId: protocol.id,
           governanceID: protocol.governanceID,
           geckoId: protocol.gecko_id,
+        ...(protocol.deprecated ? {deprecared: protocol.deprecated} : {})
         };
       })
     )

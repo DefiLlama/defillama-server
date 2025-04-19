df19cf050562a79f8e690635cf1965945f674b0a
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2025-04-19 11:40:21 -0600

    use only the reference link
    use only the reference link

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -60159,7 +60159,7 @@ const data3: Protocol[] = [
     cmcId: null,
     category: "CDP",
     chains: ["Ethereum", "Binance", "Mantle"],
-    oracles: ["Chainlink"], //https://docs.avalonfinance.xyz/avalon-products/cedefi-cdp-usda/risk-management#oracle-solutions Avalon uses the industry-standard decentralized oracle Chainlink as its primary oracle for the BTC-USD market securing our BTC-backed stablecoin, USDa, across all supported networks.
+    oracles: ["Chainlink"], //https://docs.avalonfinance.xyz/avalon-products/cedefi-cdp-usda/risk-management#oracle-solutions 
     forkedFrom: [],
     module: "avalon-finance-usda/index.js",
     twitter: "avalonfinance_",

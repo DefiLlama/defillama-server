b208724c9782bee4361294c8f65aad05b0a80ed0
alexsserban <alex.serban.dev@gmail.com>
2025-04-29 22:46:10 +0300

    fix: update zivoe copy

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -65129,28 +65129,6 @@ const data3: Protocol[] = [
     twitter: "Vader_AI_",
     listedAt: 1734701945
   },
-  /*{
-    id: "5536",
-    name: "Zivoe",
-    address: "0xe412D46a0fBD567332b7689cFFfE1F8A4f19A9d2",
-    symbol: "ZVE",
-    url: "https://www.zivoe.com",
-    description: "Zivoe is a Real-World Asset (RWA) protocol that connects DeFi users to private credit opportunities in the consumer lending market. By pooling on-chain capital and lending to consumers via our network of origination/servicing partners, Zivoe helps underserved borrowers escape high-interest debt while generating sustainable returns for liquidity providers.",
-    chain: "Ethereum",
-    logo: `${baseIconsUrl}/zivoe.png`,
-    audits: "0",
-    audit_note: null,
-    gecko_id: null,
-    cmcId: null,
-    category: "Uncollateralized Lending",
-    chains: ["Ethereum"],
-    oracles: [],
-    forkedFrom: [],
-    module: "zivoe/index.js",
-    twitter: "ZivoeProtocol",
-    github: ["Zivoe"],
-    listedAt: 1734702192
-  },*/
   {
     id: "5537",
     name: "Kernel", //previous KernelDAO
@@ -65478,7 +65456,7 @@ const data3: Protocol[] = [
     address: "0xe412D46a0fBD567332b7689cFFfE1F8A4f19A9d2",
     symbol: "ZVE",
     url: "https://zivoe.com/",
-    description: "Zivoe is a Real-World Asset (RWA) protocol that connects DeFi users to private credit opportunities in the consumer lending market. By pooling on-chain capital and lending to consumers via our network of origination/servicing partners, Zivoe helps underserved borrowers escape high-interest debt while generating sustainable returns for liquidity providers",
+    description: "Zivoe is a real-world asset (RWA) credit protocol that enables qualified participants to gain tokenized exposure to the consumer lending market.",
     chain: "Ethereum",
     logo: `${baseIconsUrl}/zivoe.jpg`,
     audits: "2",
@@ -65490,7 +65468,7 @@ const data3: Protocol[] = [
     oracles: [],
     forkedFrom: [],
     module: "zivoe/index.js",
-    twitter: "landerfinance",
+    twitter: "ZivoeProtocol",
     audit_links: [
       "https://github.com/sherlock-protocol/sherlock-reports/blob/main/audits/2024.04.25%20-%20Final%20-%20Zivoe%20Audit%20Report.pdf",
       "https://github.com/runtimeverification/publications/blob/main/reports/smart-contracts/Zivoe_Core_Contracts.pdf",

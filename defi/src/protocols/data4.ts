8215f50a9e890005fd5b17c92164af215f515907
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-05-06 09:09:17 +0200

    fix justlock category

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -13199,7 +13199,7 @@ const data4: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Staking Pool",
+    category: "Token Locker",
     chains: ["Radix"],
     module: "justlock-io/index.js",
     twitter: "JustLockRadix",

0ac9c83a79875784f534380d55965f06385a3386
realshaman <realdealshaman11@gmail.com>
2025-05-09 13:18:47 -0600

    fix icon

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -62828,7 +62828,7 @@ const data3: Protocol[] = [
     url: "https://bluefin.io",
     description: "Bluefin is a decentralized orderbook-based exchange built for both professional and first-time traders. It focuses on security, transparency and redefining the user experience of using on-chain trading platforms - and is backed by Polychain, SIG, Brevan Howard, and other leading firms.",
     chain: "Sui",
-    logo: `${baseIconsUrl}/bluefin-amm.png`,
+    logo: `${baseIconsUrl}/bluefin-spot.png`,
     audits: "0",
     audit_note: null,
     gecko_id: null,

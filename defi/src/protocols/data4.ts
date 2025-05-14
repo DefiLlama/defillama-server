b36fa0831f9d1ea33fe9ef7de6d455fa9258160a
realshaman <realdealshaman11@gmail.com>
2025-05-14 16:35:46 -0600

    update category

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -11345,7 +11345,7 @@ const data4: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Dexs",
+    category: "Trading App",
     chains: ["Solana"],
     forkedFrom: [],
     oracles: [],

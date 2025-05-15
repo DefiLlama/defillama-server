4dd10f3b76bd736355e316795982a84c20aa4a5b
darvin <todarvinrio@gmail.com>
2025-05-16 00:20:20 +0530

    add other tempest chains

--- coins/src/adapters/yield/tempest/index.ts
+++ coins/src/adapters/yield/tempest/index.ts
@@ -1,7 +1,11 @@
 import getTempestTokenPrices from "./tempest";
 
+const chains = ["ethereum", "swellchain", "scroll"];
+
 export function tempest(timestamp: number = 0) {
-  return Promise.all([
-    getTempestTokenPrices("ethereum", timestamp),
-  ]);
+    return Promise.all(
+        chains.map((chain) => {
+            return getTempestTokenPrices(chain, timestamp);
+        })
+    );
 }

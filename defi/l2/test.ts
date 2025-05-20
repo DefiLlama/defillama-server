2c03bc6744baead8bb823624f8a86e455640cf9e
waynebruce0x <waynebruce0x@gmail.com>
2025-05-20 16:30:24 +0100

    better missing tvl L2 error handling

--- defi/l2/test.ts
+++ defi/l2/test.ts
@@ -2,7 +2,8 @@ import fetch from "node-fetch";
 import { FinalData } from "./types";
 import { getCurrentUnixTimestamp } from "../src/utils/date";
 import { allChainKeys, ownTokens } from "./constants";
-import { sendMessage } from "../src/utils/discord";
+import { fetchFailedDeps } from "./outgoing";
+import { getChainDisplayName } from "../src/utils/normalizeChain";
 
 export async function verifyChanges(chains: FinalData) {
   const res = await fetch(`https://api.llama.fi/chain-assets/chains?apikey=${process.env.COINS_KEY}`).then((r) =>
@@ -30,6 +31,12 @@ export async function verifyChanges(chains: FinalData) {
     message += `\n${chain} has had a ${totalNew > totalOld ? "increase" : "decrease"} of ${forwardChange.toFixed(
       0
     )}% in ${hours}`;
+  });
+
+  const rawFailedDeps = fetchFailedDeps();
+  const failedDeps = rawFailedDeps.map((dep) => getChainDisplayName(dep, true));
+  Object.keys(res).map((chain: string) => {
+    if (!chains[chain] && failedDeps.includes(chain)) chains[chain] = res[chain];
   });
 
   if (message.length) throw new Error(message);

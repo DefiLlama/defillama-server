ed73f23cf58c6cb86e808234c155dd96b9348fb0
realshaman <realdealshaman11@gmail.com>
2025-06-07 14:16:12 -0600

    comment indx

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -15667,8 +15667,8 @@ const data4: Protocol[] = [
     ],
     listedAt: 1749019352,
   },
-  {
-    id: "6270",
+  /*{
+    id: "6270", // TODO: add back once the contract in the adapter is deployed
     name: "INDX",
     address: null,
     symbol: "-",
@@ -15694,7 +15694,7 @@ const data4: Protocol[] = [
     module: "indx/index.js",
     twitter: "indxsapp",
     listedAt: 1749084415,
-  },
+  },*/
   {
     id: "6271",
     name: "Hydration Lending",

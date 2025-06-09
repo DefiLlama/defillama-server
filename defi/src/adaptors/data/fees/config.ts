6d053c52cc4fdcc1ec40e16228c75693710a9542
treeoflife2 <sol.analyzoor@gmail.com>
2025-06-09 14:35:56 +0530

    Update config.ts

--- defi/src/adaptors/data/fees/config.ts
+++ defi/src/adaptors/data/fees/config.ts
@@ -3220,7 +3220,8 @@ export default {
             "genuineSpikes": {
                 "1660176000": false,
                 "1665014400": false,
-                "1684713600": false
+                "1684713600": false,
+                "1749340800": false
             }
         },
         id: "2769"

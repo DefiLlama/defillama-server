626cd5263b11669f7e09ebd52f4577381f569860
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-06-14 00:13:51 +0700

    Merge pull request #9985 from ReynardoEW/fix_oracles
    tvs per chain

--- defi/src/getOracles.ts
+++ defi/src/getOracles.ts
@@ -12,7 +12,13 @@ interface SumDailyTvls {
 }
 
 interface OracleProtocols {
-  [oracle: string]: { [protocol: string]: number };
+  [timestamp: number]: {
+    [oracle: string]: {
+      [protocol: string]: {
+        [chain: string]: number;
+      };
+    };
+  };
 }
 
 interface Item {
@@ -29,21 +35,24 @@ function sum(
   oracle: string,
   time: number,
   item: Item = {},
-  oracleProtocols: OracleProtocols,
+  oracleProtocolsHistory: OracleProtocols,
   protocol: IProtocol,
   chain: string | null
 ) {
-  if (!totalByChain[time]) {
-    totalByChain[time] = {};
-  }
-  if (!total[time]) {
-    total[time] = {};
-  }
+  if (!totalByChain[time]) totalByChain[time] = {};
+  if (!total[time]) total[time] = {};
+  if (!oracleProtocolsHistory[time]) oracleProtocolsHistory[time] = {};
 
   const dataByChain = totalByChain[time][oracle] ?? {};
   const data = total[time][oracle] ?? {};
 
-  let totalTvl = 0;
+  if (!oracleProtocolsHistory[time][oracle]) {
+    oracleProtocolsHistory[time][oracle] = {};
+  }
+  if (!oracleProtocolsHistory[time][oracle][protocol.name]) {
+    oracleProtocolsHistory[time][oracle][protocol.name] = {};
+  }
+
   const isOldTvlRecord = Object.keys(item).filter((item) => !["PK", "SK", "tvl"].includes(item)).length === 0;
   
   for (const section in item) {
@@ -73,27 +82,21 @@ function sum(
       (chain ? sectionSplit[0] === chain : true)
     ) {
       const sectionKey = `${getChainDisplayName(sectionSplit[0], true)}${sectionSplit[1] ? `-${sectionSplit[1]}` : ""}`;
-
       dataByChain[sectionKey] = (dataByChain[sectionKey] ?? 0) + item[section];
 
-      if (!sectionSplit[1]) {
-        if (extraSections.includes(section)) {
-          data[section] = (data[section] ?? 0) + item[section];
-        } else {
-          data.tvl = (data.tvl ?? 0) + item[section];
-          totalTvl += item[section];
-        }
+      if (extraSections.includes(section)) {
+        data[section] = (data[section] ?? 0) + item[section];
+      } else {
+        data.tvl = (data.tvl ?? 0) + item[section];
       }
+
+      oracleProtocolsHistory[time][oracle][protocol.name][sectionKey] =
+          (oracleProtocolsHistory[time][oracle][protocol.name][sectionKey] ?? 0) + item[section];
     }
   }
 
   totalByChain[time][oracle] = dataByChain;
   total[time][oracle] = data;
-
-  if (!oracleProtocols[oracle]) {
-    oracleProtocols[oracle] = {};
-  }
-  oracleProtocols[oracle][protocol.name] = (oracleProtocols[oracle][protocol.name] ?? 0) + totalTvl;
 }
 
 function isActive(timestamp: number, startDateStr?: string, endDateStr?: string): 'active' | 'inactive' | 'not-started' {
@@ -118,6 +121,7 @@ export async function getOraclesInternal({ ...options }: any = {}) {
   const sumDailyTvls = {} as SumDailyTvls;
   const sumDailyTvlsByChain = {} as SumDailyTvls;
   const oracleProtocols = {} as OracleProtocols;
+
 
   await processProtocols(
     async (timestamp: number, item: TvlItem, protocol: IProtocol) => {
@@ -212,6 +216,11 @@ export async function getOraclesInternal({ ...options }: any = {}) {
     { includeBridge: false, ...options }
   );
 
+  const timestamps = Object.keys(oracleProtocols);
+  const latestTimestamp = timestamps[timestamps.length - 1];
+
+  const oraclesTVS = latestTimestamp ? oracleProtocols[parseInt(latestTimestamp)] : {};
+
   const oracleTvlByChain = {} as IChainByOracle;
   const latestTvlByChainByOracle = Object.entries(sumDailyTvlsByChain).slice(-1)[0][1];
   for (const oracle in latestTvlByChainByOracle) {
@@ -240,9 +249,9 @@ export async function getOraclesInternal({ ...options }: any = {}) {
   return {
     chart: sumDailyTvls,
     chainChart: sumDailyTvlsByChain,
-    oraclesTVS: oracleProtocols,
+    oraclesTVS: oraclesTVS,
     oracles: Object.fromEntries(
-      Object.entries(oracleProtocols).map(([oracle, protocols]) => [
+      Object.entries(oraclesTVS).map(([oracle, protocols]) => [
         oracle,
         Object.keys(protocols)
       ])

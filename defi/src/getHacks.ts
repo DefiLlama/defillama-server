1eeebc0e98ad8c228a607a80bb0bc84cfd3472d0
mintdart <96025197+mintdart@users.noreply.github.com>
2025-06-17 22:25:34 +0900

    include parent protocol id in hacks api

--- defi/src/getHacks.ts
+++ defi/src/getHacks.ts
@@ -1,27 +1,31 @@
+import protocols from "./protocols/data";
 import { getAllAirtableRecords } from "./utils/airtable";
 import { successResponse, wrap, IResponse } from "./utils/shared";
 
 export async function getHacksInternal() {
-  let allRecords = await getAllAirtableRecords('appNED1rpGDbQDjEX/Hacks')
+  let allRecords = await getAllAirtableRecords("appNED1rpGDbQDjEX/Hacks");
   return allRecords
-    .filter(
-      (r) =>
-        r.fields["Name"] !== undefined
-    )
-    .map((r) => ({
-      date: new Date(r.fields["Date"]).getTime() / 1000,
-      name: r.fields["Name"],
-      classification: r.fields["Classification"] ?? null,
-      technique: r.fields["Technique"] ?? null,
-      amount: r.fields["Funds Lost"] ?? null,
-      chain: r.fields["Chain"] ?? null,
-      bridgeHack: r.fields["Bridge / Multichain Application"] ?? false,
-      targetType: r.fields["Target Type"] ?? null,
-      source: r.fields["Link"],
-      returnedFunds: r.fields["Refunded funds to users"] ?? null,
-      defillamaId: r.fields["DefiLlama Id"] ?? null,
-      language: r.fields["Language"] ?? null,
-    }));
+    .filter((r) => r.fields["Name"] !== undefined)
+    .map((r) => {
+      const defillamaId = r.fields["DefiLlama Id"] ?? null;
+      const parentProtocolId = defillamaId ? protocols.find((p) => p.id === defillamaId)?.parentProtocol : null;
+      
+      return {
+        date: new Date(r.fields["Date"]).getTime() / 1000,
+        name: r.fields["Name"],
+        classification: r.fields["Classification"] ?? null,
+        technique: r.fields["Technique"] ?? null,
+        amount: r.fields["Funds Lost"] ?? null,
+        chain: r.fields["Chain"] ?? null,
+        bridgeHack: r.fields["Bridge / Multichain Application"] ?? false,
+        targetType: r.fields["Target Type"] ?? null,
+        source: r.fields["Link"],
+        returnedFunds: r.fields["Refunded funds to users"] ?? null,
+        defillamaId: r.fields["DefiLlama Id"] ?? null,
+        ...(parentProtocolId ? { parentProtocolId } : {}),
+        language: r.fields["Language"] ?? null,
+      };
+    });
 }
 
 const handler = async (_event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {

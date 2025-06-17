e0f17fb7eb56f04eac4b7f8ca638a0adae506235
llama <waynebruce0x@gmail.com>
2025-06-17 13:11:39 +0100

    linea bridge adapter

--- coins/src/adapters/bridges/linea.ts
+++ coins/src/adapters/bridges/linea.ts
@@ -1,20 +1,27 @@
 import { fetch, formatExtraTokens } from "../utils";
 
-const chain = 'linea'
+const chain = "linea";
 export default async function bridge() {
   const bridge = (
-    await fetch("https://raw.githubusercontent.com/Consensys/linea-token-list/main/json/linea-mainnet-token-fulllist.json")
+    await fetch(
+      "https://raw.githubusercontent.com/Consensys/linea-token-list/main/json/linea-mainnet-token-shortlist.json",
+    )
   ).tokens as any[];
 
   return bridge
-  .filter((token) => token.chainId === 59144 && token.extension?.rootChainId === 1 && token.tokenType.includes('bridged'))
+    .filter(
+      (token) =>
+        token.chainId === 59144 &&
+        token.extension?.rootChainId === 1 &&
+        token.tokenType.includes("canonical-bridge"),
+    )
     .map((token) => {
       const bridged = token.extension.rootAddress;
       return {
         from: `${chain}:${token.address}`,
         to: `ethereum:${bridged}`,
         symbol: token.symbol,
-        decimals: token.decimals
+        decimals: token.decimals,
       };
     })
     .filter((t) => t.to != "null")

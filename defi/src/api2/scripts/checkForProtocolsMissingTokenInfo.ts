8314506e129f5e1c39cdc2380571e26d6ea55b48
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-06-18 16:50:16 +0200

    script to auto pull missing token info

--- defi/src/api2/scripts/checkForProtocolsMissingTokenInfo.ts
+++ defi/src/api2/scripts/checkForProtocolsMissingTokenInfo.ts
@@ -10,8 +10,9 @@ import { tableToString } from '../utils';
 import { addCGTokenMetadatas, addCMCTokenMetadatas, getAllCGTokenMetadata, getAllCMCTokenMetadata } from '../../../dev-metrics/db';
 import { IProtocol } from '../../types';
 import { chainCoingeckoIds } from '../../utils/normalizeChain';
-import fs from 'fs';
 import sleep from '../../utils/shared/sleep';
+
+// create chain metadata objects from chainCoingeckoIds like protocols from data.ts
 const chains = Object.entries(chainCoingeckoIds).map(([chain, id]: any) => {
   return {
     ...id,
@@ -22,119 +23,52 @@ const chains = Object.entries(chainCoingeckoIds).map(([chain, id]: any) => {
 })
 
 const allMetadata = protocols.concat(parentProtocols as IProtocol[], chains)
-let _cgMetadataMap: any = {}
-let _cmcMetadataMap: any = {}
-
-/* const descriptionMap: any = {}
-const tableData: any = []
-protocols.forEach((protocol: any) => {
-  if (protocol.description) {
-    if (descriptionMap[protocol.description]) {
-      tableData.push([protocol.description.slice(0, 100), protocol.name, descriptionMap[protocol.description]])
-      return;
-    }
-    descriptionMap[protocol.description] = protocol.name
-  }
-})
-
-console.table(tableData)
- */
-
-let processedParentProtocols = new Set<string>()
-
-const sortTableData = (a: any, b: any) => {
-  if (a.category === 'chain-listing') return -1; // Chain listings should come first
-  if (b.category === 'chain-listing') return 1; // Chain listings should come first
-  if (a.domainK < b.domainK) return -1;
-  if (a.domainK > b.domainK) return 1;
-  if (a.twitterK < b.twitterK) return -1;
-  if (a.twitterK > b.twitterK) return 1;
-  return 0;
-}
-
-async function getCMCMetadatas() {
-
-  const axiosInstance = axios.create({
-    baseURL: "https://pro-api.coinmarketcap.com",
-    headers: {
-      'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY
-    }
-  });
-  const metadataMap: any = await getAllCMCTokenMetadata();
-  _cmcMetadataMap = metadataMap;
-  // return metadataMap
-  let { data: { data: coinMap } } = await axiosInstance.get('/v1/cryptocurrency/map')
-  console.log('Fetched coin map from CoinMarketCap', coinMap.length, 'coins');
-  coinMap = coinMap.filter((coin: any) => coin.platform && coin.is_active === 1 && new Date(coin.first_historical_data).getFullYear() >= 2022);
-  coinMap.forEach((coin: any) => coin.id = '' + coin.id); // Ensure IDs are strings for consistency
-
-  console.log('Filtered coin map to', coinMap.length, 'coins with platforms');
-  const coinsMissingMetadata = coinMap.filter((coin: any) => !metadataMap[coin.id]);
-  console.log('Coins missing metadata:', coinsMissingMetadata.length);
-  const idChunks = sliceIntoChunks(coinsMissingMetadata, 99);
-  console.log('Processing', idChunks.length, 'chunks of IDs');
-
-  let i = 0
-  for (const chunk of idChunks) {
-    const coinIdMap: any = {}
-    chunk.forEach((coin: any) => coinIdMap[coin.id] = coin)
-
-    const { data: { data: metadataMap } } = await axiosInstance.get('/v1/cryptocurrency/info', {
-      params: {
-        id: chunk.map((coin: any) => coin.id).join(','),
-        skip_invalid: true
-      }
-    });
-
-    console.log(i++, 'Fetched metadata for chunk', chunk.length, 'coins', Object.keys(metadataMap).length, 'metadata entries found');
-
-    for (const id of Object.keys(metadataMap)) {
-      if (!coinIdMap[id]) {
-        console.log(id, metadataMap[id])
-        console.log('Coin ID not found in chunk:', id);
-      } else coinIdMap[id].metadata = metadataMap[id]
-    }
-
-    chunk.forEach((coin: any) => {
-      if (!coin.metadata) {
-        console.log('No metadata for coin', coin.id, coin.name);
-        coin.metadata = {}
-        coin.missingMetadata = true
-      }
-      metadataMap[coin.id] = coin
-    })
-
-    console.log('Chunk processed, writing cmc metadata to db', chunk.length);
-    await addCMCTokenMetadatas(chunk)
-
-
-  }
-
-  return metadataMap
-}
+let _cgMetadataMap: any
+let _cmcMetadataMap: any
+let cmcSymbolCoinMap: any = {}
+let cgSymbolCoinMap: any = {}
+const parentChildProtocolMap: any = {} // map of parent id and array of child protocols + parent protocol object
 
 const ignoredDomains = [
-  'coinbase.com', 't.me', 'twitter.com', 'binance.com', 'dexscreener.com', 'kraken.com', 'gitbook.io', 'opensea.io', 'pump.fun',
+  'coinbase.com', 't.me', 'twitter.com', 'binance.com', 'dexscreener.com', 'kraken.com', 'gitbook.io', 'opensea.io', 'pump.fun', 'vercel.app',
 ]
 
 const configs: any = {
   cmc: {
     getMetadatas: getCMCMetadatas,
-    hasCoinInfo: (protocol: any) => protocol.cmcId,
+    hasCoinInfo: (i: any) => i.metadatas.cmc_ids.some((m: any) => m),
     ignoredCoinsSet: new Set([
       '24165', '35459', '31370', '32882', '33903', '31369', '33834', '21583', '20641',
       '21882', '35458', '36175', '33312', '30141', '36068', '30907', '32194', '35437',
       '24760', '22318', '32690', '29256', '21540', '35293', '33310', '36067', '33652', '32689', '22880', '36289', '29599', '29242', '24875', '28910', '35670', '28909', '23773'
     ])
   },
-  coingecko: {
+  cg: {
     getMetadatas: getCGMetadatas,
-    hasCoinInfo: (protocol: any) => protocol.gecko_id,
+    hasCoinInfo: (i: any) => i.metadatas.gecko_ids.some((m: any) => m),
     ignoredCoinsSet: new Set([
-      'defrogs', 'compounding-open-dollar', 'opyn-squeeth','bucket-protocol-buck-stablecoin', 'bob',
+      'defrogs', 'compounding-open-dollar', 'opyn-squeeth', 'bucket-protocol-buck-stablecoin', 'bob',
     ])
   },
 }
+
+
+
+async function main() {
+  await init()
+
+
+  const missingCMCData = await getCoinTableData('cmc');
+  const missingCGCData = await getCoinTableData('cg');
+  const missingSymbols = getMissingSymbols();
+  const missingAddresses = getMissingAddresses();
+
+}
+
+
+main().catch(err => {
+  console.error('Error:', err);
+}).then(() => process.exit(0))
 
 
 async function getCoinTableData(coinType: string) {
@@ -143,13 +77,26 @@ async function getCoinTableData(coinType: string) {
     return;
   }
 
-  const { getMetadatas, hasCoinInfo, ignoredCoinsSet = new Set() } = configs[coinType];
+  const { getMetadatas, hasCoinInfo, ignoredCoinsSet = new Set(), symbolCoinMap, } = configs[coinType];
 
   console.log(`-----Fetching missing ${coinType} token link------`)
   const existingCoinLinks = new Set(allMetadata.map(hasCoinInfo).filter((id: any) => id));
   const existingDomainsWithCoinLinks = new Set(allMetadata.filter((protocol: any) => hasCoinInfo(protocol) && protocol.url?.includes(('://'))).map((protocol: any) => getDomain(protocol.url!)))
   ignoredDomains.forEach((domain: string) => existingDomainsWithCoinLinks.add(domain.toLowerCase()));
-  console.log('Existing Coin links count:', existingCoinLinks.size);
+
+  const cgStablecoinIds = allMetadata.map((i: any) => i.stablecoins).filter((i: any) => i).flat()
+  const assetTokenSymbols = allMetadata.map((i: any) => i.assetToken).filter((i: any) => i)
+  cgStablecoinIds.forEach((id: string) => {
+    existingCoinLinks.add(id)
+    if (_cgMetadataMap?.[id])
+      assetTokenSymbols.push(_cgMetadataMap[id].symbol)
+  })
+
+  assetTokenSymbols.forEach((symbol: string) => {
+    let metadata = symbolCoinMap[symbol.toLowerCase()]
+    if (metadata)
+      existingCoinLinks.add(metadata.id)
+  })
 
   const metadataMap = await getMetadatas();
   const tagStats: any = {}
@@ -221,21 +168,8 @@ async function getCoinTableData(coinType: string) {
   // console.table(Object.entries(tagStats).filter(([_, count]: any) => count > 10).sort((a: any, b: any) => b[1] - a[1]))
 
 
-  let filteredProtocols = protocols.concat(chains).filter((protocol: any) => {
-    if (hasCoinInfo(protocol)) return false
-    if (processedParentProtocols.has(protocol.id)) return false
-    const parentId = protocol.parentProtocol
-    if (!parentId) return true
-    if (processedParentProtocols.has(parentId)) return false
-    const parentProtocol = parentProtocolsById[parentId]
-    if (!parentProtocol) {
-      console.warn('No parent protocol found for', protocol.name, 'with ID', parentId);
-      return true; // Keep protocol if no parent found
-    }
-    protocol.parentProtocol = parentProtocol
-    return !hasCoinInfo(parentProtocol)
-  })
-  console.log('Filtered protocols:', filteredProtocols.length, 'from', protocols.length, 'total protocols');
+  let filteredProtocols = protocols.concat(chains).filter((protocol: any) => !hasCoinInfo(protocol))
+  console.log('protocols missing coin info:', filteredProtocols.length, 'from', protocols.concat(chains).length, 'total protocols (& chains)');
 
   const protocolsFound: any = {}
   filteredProtocols.forEach((protocol: any) => {
@@ -301,7 +235,7 @@ async function getCoinTableData(coinType: string) {
   })
 
 
-  console.log('Protocols found:', Object.keys(protocolsFound).length);
+  console.log('Protocols found:', Object.keys(protocolsFound).length, `[${coinType}]`);
   const tableData = Object.values(protocolsFound).map((entry: any, idx: number) => {
     let coinSet = new Set<string>()
     let coins = entry.coins.filter((coin: any) => {
@@ -310,7 +244,7 @@ async function getCoinTableData(coinType: string) {
       return true
     })
     return {
-      idx: idx + 1,
+      // idx: idx + 1,
       // id: entry.protocol.id,
       name: entry.protocol.name,
       // url: entry.protocol.url || entry.protocol.parentProtocol?.url,
@@ -319,9 +253,9 @@ async function getCoinTableData(coinType: string) {
       domainK: entry.domainKey ?? '',
       twitterK: entry.twitterHandle ?? '',
       // githubK: entry.githubHandle ?? '',
-      symboks: coins.map((coin: any) => coin.symbol).join(', '),
-      coinIds: coins.map((coin: any) => coin.id).join(', '),
-      coins: coins.map((coin: any) => coin.name).join(', ').slice(0, 45),
+      symbols: coins.map((coin: any) => coin.symbol).join(', ').slice(0, 16),
+      coinIds: coins.map((coin: any) => coin.id).join(', ').slice(0, 25),
+      coins: coins.map((coin: any) => coin.name).join(', ').slice(0, 35),
       category: entry.protocol.category
     }
   })
@@ -346,10 +280,75 @@ async function getCoinTableData(coinType: string) {
   // }
 }
 
-async function getCGMetadatas() {
+async function getCMCMetadatas() {
+  if (_cmcMetadataMap)
+    return _cmcMetadataMap;
 
   const axiosInstance = axios.create({
-    // baseURL: "https://pro-api.coingecko.com/api",
+    baseURL: "https://pro-api.coinmarketcap.com",
+    headers: {
+      'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY
+    }
+  });
+  const metadataMap: any = await getAllCMCTokenMetadata();
+  _cmcMetadataMap = metadataMap;
+  // return metadataMap
+  let { data: { data: coinMap } } = await axiosInstance.get('/v1/cryptocurrency/map')
+  console.log('Fetched coin map from CoinMarketCap', coinMap.length, 'coins');
+  coinMap = coinMap.filter((coin: any) => coin.platform && coin.is_active === 1 && new Date(coin.first_historical_data).getFullYear() >= 2022);
+  coinMap.forEach((coin: any) => coin.id = '' + coin.id); // Ensure IDs are strings for consistency
+
+  console.log('Filtered coin map to', coinMap.length, 'coins with platforms');
+  const coinsMissingMetadata = coinMap.filter((coin: any) => !metadataMap[coin.id]);
+  console.log('Coins missing metadata:', coinsMissingMetadata.length);
+  const idChunks = sliceIntoChunks(coinsMissingMetadata, 99);
+  console.log('Processing', idChunks.length, 'chunks of IDs');
+
+  let i = 0
+  for (const chunk of idChunks) {
+    const coinIdMap: any = {}
+    chunk.forEach((coin: any) => coinIdMap[coin.id] = coin)
+
+    const { data: { data: metadataMap } } = await axiosInstance.get('/v1/cryptocurrency/info', {
+      params: {
+        id: chunk.map((coin: any) => coin.id).join(','),
+        skip_invalid: true
+      }
+    });
+
+    console.log(i++, 'Fetched metadata for chunk', chunk.length, 'coins', Object.keys(metadataMap).length, 'metadata entries found');
+
+    for (const id of Object.keys(metadataMap)) {
+      if (!coinIdMap[id]) {
+        console.log(id, metadataMap[id])
+        console.log('Coin ID not found in chunk:', id);
+      } else coinIdMap[id].metadata = metadataMap[id]
+    }
+
+    chunk.forEach((coin: any) => {
+      if (!coin.metadata) {
+        console.log('No metadata for coin', coin.id, coin.name);
+        coin.metadata = {}
+        coin.missingMetadata = true
+      }
+      metadataMap[coin.id] = coin
+    })
+
+    console.log('Chunk processed, writing cmc metadata to db', chunk.length);
+    await addCMCTokenMetadatas(chunk)
+
+
+  }
+
+  return metadataMap
+}
+
+
+async function getCGMetadatas() {
+  if (_cgMetadataMap)
+    return _cgMetadataMap;
+
+  const axiosInstance = axios.create({
     baseURL: "https://pro-api.coingecko.com/api",
     headers: {
       'x-cg-pro-api-key': process.env.CG_KEY
@@ -359,10 +358,7 @@ async function getCGMetadatas() {
   _cgMetadataMap = metadataMap;
   // return transformMetadatas(metadataMap)
   let { data: coinMap } = await axiosInstance.get('/v3/coins/list', { params: { include_platform: true } });
-  // const coinMapString = fs.readFileSync('cg-coins.json.log', 'utf-8')
-  // let coinMap = JSON.parse(coinMapString);
-  console.log('Fetched coin map from coingecko', coinMap.length, 'coins', 'already have metadata for', Object.keys(metadataMap).length, 'coins');
-  // coinMap = coinMap.filter((coin: any) => coin.platforms && Object.keys(coin.platforms).length > 0 && ['wrapped ', 'coinbase', 'kraken', ' usd'].some((i: any) => coin.name.toLowerCase().includes(i)) && !existingCGIDMaps.has(coin.id));
+  console.log('Fetched coin map from cg', coinMap.length, 'coins', 'already have metadata for', Object.keys(metadataMap).length, 'coins');
   coinMap = coinMap.filter((coin: any) => !metadataMap[coin.id]);
   console.log('Filtered coin map to', coinMap.length, 'coins with platforms');
 
@@ -409,18 +405,147 @@ async function getCGMetadatas() {
   }
 }
 
+function getMissingSymbols() {
+  const missingSymbols: any = []
+  protocols.concat(parentProtocols as any).forEach((protocol: any) => {
+    if (protocol.symbol && protocol.symbol !== '-') return;
+    if (protocol.gecko_id) {
+      if (_cgMetadataMap?.[protocol.gecko_id]) {
+        missingSymbols.push({
+          protocolId: protocol.id,
+          symbol: _cgMetadataMap[protocol.gecko_id].metadata.symbol,
+          source: 'cg',
+        });
+        return;
+      } else {
+        console.warn(`No CG metadata found for protocol ${protocol.name} (${protocol.id}) with gecko_id ${protocol.gecko_id}`);
+      }
+    } else if (protocol.cmcId) {
+      if (_cmcMetadataMap?.[protocol.cmcId]) {
+        missingSymbols.push({
+          protocolId: protocol.id,
+          symbol: _cmcMetadataMap[protocol.cmcId].metadata.symbol,
+          source: 'cmc',
+        });
+        return;
+      } else {
+        console.warn(`No CMC metadata found for protocol ${protocol.name} (${protocol.id}) with cmcId ${protocol.cmcId}`);
+      }
+    }
+  })
+
+  if (missingSymbols.length) {
+    console.log('Protocol listings missing symbol:', missingSymbols.length);
+    console.table(missingSymbols)
+  }
+  return missingSymbols
+}
+
+function getMissingAddresses() {
+  const missingAddresses: any = []
+  protocols.concat(parentProtocols as any).forEach((protocol: any) => {
+    if (protocol.address && protocol.address !== '-') return;
+    if (protocol.gecko_id) {
+      if (_cgMetadataMap?.[protocol.gecko_id]) {
+        const { contract_address, asset_platform_id } = _cgMetadataMap[protocol.gecko_id].metadata
+        if (!contract_address || !asset_platform_id) return;
+        missingAddresses.push({
+          protocolId: protocol.id,
+          address: `${asset_platform_id}:${contract_address}`,
+          source: 'cg',
+        });
+        return;
+      } else {
+        console.warn(`No CG metadata found for protocol ${protocol.name} (${protocol.id}) with gecko_id ${protocol.gecko_id}`);
+      }
+    } else if (protocol.cmcId) {
+      if (_cmcMetadataMap?.[protocol.cmcId]) {
+        const { platform: { name, token_address } = {} } = _cmcMetadataMap[protocol.cmcId].metadata;
+        if (!name || !token_address) return;
+        missingAddresses.push({
+          protocolId: protocol.id,
+          address: `${name}:${token_address}`,
+          source: 'cmc',
+        });
+        return;
+      } else {
+        console.warn(`No CMC metadata found for protocol ${protocol.name} (${protocol.id}) with cmcId ${protocol.cmcId}`);
+      }
+    }
+  })
+
+  if (missingAddresses.length) {
+    console.log('Protocol listings missing address:', missingAddresses.length);
+    console.table(missingAddresses)
+  }
+
+  return missingAddresses
+}
+
+const sortTableData = (a: any, b: any) => {
+  if (a.category === 'chain-listing') return -1; // Chain listings should come first
+  if (b.category === 'chain-listing') return 1; // Chain listings should come first
+  if (a.domainK < b.domainK) return -1;
+  if (a.domainK > b.domainK) return 1;
+  if (a.twitterK < b.twitterK) return -1;
+  if (a.twitterK > b.twitterK) return 1;
+  return 0;
+}
 
 const getDomain = (url: string) => {
   // console.log('getDomain', url)
   return url.split('://')[1].split('/')[0].split('.').slice(-2).join('.').toLowerCase()
 }
 
-async function main() {
-  // const missingCMCData = await getCoinTableData('cmc');
-  const missingCGCData = await getCoinTableData('coingecko');
+
+async function init() {
+
+  // we want to ignore protocols if either they/their parent protocol or their siblings have cg/cmc id
+  protocols.forEach((protocol: any) => {
+    if (protocol.parentProtocol) {
+      if (!parentChildProtocolMap[protocol.parentProtocol]) {
+        parentChildProtocolMap[protocol.parentProtocol] = [];
+        let parentProtocol = parentProtocolsById[protocol.parentProtocol]
+        if (!parentProtocol) {
+          console.warn('No parent protocol found for', protocol.name, 'with ID', protocol.parentProtocol);
+        }
+        parentChildProtocolMap[protocol.parentProtocol].push(parentProtocol);
+      }
+      parentChildProtocolMap[protocol.parentProtocol].push(protocol);
+    }
+  })
+
+  allMetadata.forEach((protocol: any) => {
+    let metadataItems = [protocol]
+    if (protocol.parentProtocol && parentChildProtocolMap[protocol.parentProtocol])
+      metadataItems = parentChildProtocolMap[protocol.parentProtocol]
+    protocol.metadatas = {
+      gecko_ids: metadataItems.map((p: any) => p.gecko_id).filter((id: any) => id),
+      cmc_ids: metadataItems.map((p: any) => p.cmcId).filter((id: any) => id),
+      twitter: metadataItems.map((p: any) => p.twitter).filter((id: any) => id),
+      github: metadataItems.map((p: any) => p.github).filter((id: any) => id),
+      assetTokens: metadataItems.map((p: any) => p.assetToken).filter((id: any) => id),
+      stablecoins: metadataItems.map((p: any) => p.stablecoins).filter((id: any) => id).flat(),
+      addresses: metadataItems.map((p: any) => p.address).filter((id: any) => id),
+    }
+  })
+
+  // load coin metadatas from db
+  const [cmcMetadataMap, cgMetadataMap] = await Promise.all([getCMCMetadatas(), getCGMetadatas()]);
+
+  Object.values(cgMetadataMap).forEach((metadata: any) => {
+    if (metadata.symbol) {
+      const symbol = metadata.symbol.toLowerCase();
+      cgSymbolCoinMap[symbol] = metadata;
+    }
+  })
+
+  Object.values(cmcMetadataMap).forEach((metadata: any) => {
+    if (metadata.symbol) {
+      const symbol = metadata.symbol.toLowerCase();
+      cmcSymbolCoinMap[symbol] = metadata;
+    }
+  })
+  configs.cmc.symbolCoinMap = cmcSymbolCoinMap;
+  configs.cg.symbolCoinMap = cgSymbolCoinMap;
 }
-
-
-main().catch(err => {
-  console.error('Error:', err);
-}).then(() => process.exit(0))

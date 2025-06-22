c4e53fab4492d8deb7f6ad7a0f462c168987bb65
Eden <noat.eth@gmail.com>
2025-06-22 15:56:51 +0700

    increase cache version to 46

--- defi/src/api2/utils/dimensionsUtils.ts
+++ defi/src/api2/utils/dimensionsUtils.ts
@@ -21,7 +21,7 @@ export function getFileCacheKey(adaptorRecordType: AdapterType) {
 
 
 export function getFileCacheKeyV2() {
-  return `dimensions-data-v2-v1.0.45`
+  return `dimensions-data-v2-v1.0.46`
 }
 
 // to ensure that we pull the cache data only once

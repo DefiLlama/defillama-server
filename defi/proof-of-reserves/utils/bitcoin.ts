920e16cdc8873291fec1b06720d18c885da96ce0
Eden <noat.eth@gmail.com>
2025-07-09 02:27:02 +0700

    fix M-BTC minted

--- defi/proof-of-reserves/utils/bitcoin.ts
+++ defi/proof-of-reserves/utils/bitcoin.ts
@@ -2,11 +2,11 @@ import { GetPoROptions, IPoRAdapter, TokenConfig } from "../types";
 import { getTotalMinted } from "./getReserves";
 import { getBTCPriceUSD, getLlamaTvl } from "./llamaApis";
 
-export function getBitcoinReservesAdapter(protocolId: string, mintedTokens: Array<TokenConfig>): IPoRAdapter {
+export function getBitcoinReservesAdapter(protocolId: string, mintedTokens: Array<TokenConfig>, excludeWallets: Array<string> | undefined = undefined): IPoRAdapter {
   return {
     protocolId: protocolId,
     minted: async function(_: GetPoROptions): Promise<number> {
-      const totalMinted = await getTotalMinted(mintedTokens);
+      const totalMinted = await getTotalMinted(mintedTokens, false, excludeWallets);
       return totalMinted * (await getBTCPriceUSD());
     },
     reserves: async function(): Promise<number> {

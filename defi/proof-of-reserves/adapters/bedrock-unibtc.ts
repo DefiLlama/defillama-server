787f6dc2b83dcdb3d80641a13927f8e17b586808
Eden <noat.eth@gmail.com>
2025-07-14 13:27:37 +0700

    add check por bedrock btc

---
+++ defi/proof-of-reserves/adapters/bedrock-unibtc.ts
@@ -0,0 +1,72 @@
+import { getBitcoinReservesAdapter } from '../utils/bitcoin';
+
+const protocolId = 'bedrock-unibtc';
+
+const mintedTokens = [
+  {
+    chain: 'ethereum',
+    address: '0x004E9C3EF86bc1ca1f0bB5C7662861Ee93350568',
+  },
+  {
+    chain: 'bsc',
+    address: '0x6B2a01A5f79dEb4c2f3c0eDa7b01DF456FbD726a',
+  },
+  {
+    chain: 'zeta',
+    address: '0x6B2a01A5f79dEb4c2f3c0eDa7b01DF456FbD726a',
+  },
+  {
+    chain: 'iotex',
+    address: '0x93919784c523f39cacaa98ee0a9d96c3f32b593e',
+  },
+  {
+    chain: 'taiko',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'arbitrum',
+    address: '0x6B2a01A5f79dEb4c2f3c0eDa7b01DF456FbD726a',
+  },
+  {
+    chain: 'optimism',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'bob',
+    address: '0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894',
+  },
+  {
+    chain: 'mantle',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'mode',
+    address: '0x6B2a01A5f79dEb4c2f3c0eDa7b01DF456FbD726a',
+  },
+  {
+    chain: 'merlin',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'btr',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'bsquared',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'corn',
+    address: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+  },
+  {
+    chain: 'berachain',
+    address: '0xC3827A4BC8224ee2D116637023b124CED6db6e90',
+  },
+  {
+    chain: 'sonic',
+    address: '0xC3827A4BC8224ee2D116637023b124CED6db6e90',
+  },
+]
+
+export default getBitcoinReservesAdapter(protocolId, mintedTokens);

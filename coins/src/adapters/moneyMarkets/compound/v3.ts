66006a7dfe6e0e28eaf0531ab8530e95e1588ba8
waynebruce0x <waynebruce0x@gmail.com>
2025-07-23 09:59:28 +0100

    comp v3 base tokens (#10247)
    * xstrk
    
    * zeta mappings
    
    * botanix usdc
    
    * comp v3
    
    * base tokens
    
    * harvest vault coins
    
    * slisbnbsy
    
    * tac cg
    
    * tac assets
    
    * comp v3 base tokens

--- coins/src/adapters/moneyMarkets/compound/v3.ts
+++ coins/src/adapters/moneyMarkets/compound/v3.ts
@@ -1,4 +1,7 @@
-import { addToDBWritesList } from "../../utils/database";
+import {
+  addToDBWritesList,
+  getTokenAndRedirectData,
+} from "../../utils/database";
 import { getTokenInfo } from "../../utils/erc20";
 import { Write } from "../../utils/dbInterfaces";
 import * as sdk from "@defillama/sdk";
@@ -10,10 +13,20 @@ const assets: { [chain: string]: string } = {
 async function getTokenPrices(chain: string, timestamp: number) {
   const api = new sdk.ChainApi({ chain, timestamp });
 
-  const numAssets = await api.call({
-    target: assets[chain],
-    abi: "uint8:numAssets",
-  });
+  const [numAssets, baseAsset, baseScale] = await Promise.all([
+    api.call({
+      target: assets[chain],
+      abi: "uint8:numAssets",
+    }),
+    api.call({
+      target: assets[chain],
+      abi: "address:baseToken",
+    }),
+    api.call({
+      target: assets[chain],
+      abi: "uint256:baseScale",
+    }),
+  ]);
 
   const assetInfos = await api.multiCall({
     calls: Array.from({ length: numAssets }, (_, i) => ({
@@ -23,7 +36,7 @@ async function getTokenPrices(chain: string, timestamp: number) {
     abi: "function getAssetInfo(uint8 i) view returns (tuple(uint8 offset, address asset, address priceFeed, uint64 scale, uint64 borrowCollateralFactor, uint64 liquidateCollateralFactor, uint64 liquidationFactor, uint128 supplyCap))",
   });
 
-  const [prices, tokenInfo] = await Promise.all([
+  const [prices, tokenInfo, [baseTokenInfo]] = await Promise.all([
     api.multiCall({
       calls: assetInfos.map((info: any) => ({
         target: assets[chain],
@@ -33,9 +46,10 @@ async function getTokenPrices(chain: string, timestamp: number) {
     }),
     getTokenInfo(
       chain,
-      assetInfos.map((info: any) => info.asset),
+      [baseAsset, ...assetInfos.map((info: any) => info.asset)],
       undefined,
     ),
+    getTokenAndRedirectData([baseAsset], chain, timestamp),
   ]);
 
   let writes: Write[] = [];
@@ -51,6 +65,24 @@ async function getTokenPrices(chain: string, timestamp: number) {
       "compound-v3",
       1,
     ),
+  );
+
+  if (!baseTokenInfo) return writes;
+
+  const basePrice =
+    (baseTokenInfo.price * baseScale) / 10 ** baseTokenInfo.decimals;
+  if (!basePrice) return writes;
+
+  addToDBWritesList(
+    writes,
+    chain,
+    assets[chain],
+    basePrice,
+    baseTokenInfo.decimals,
+    baseTokenInfo.symbol,
+    timestamp,
+    "compound-v3",
+    1,
   );
 
   return writes;

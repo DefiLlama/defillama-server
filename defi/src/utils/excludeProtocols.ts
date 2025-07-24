ddeaa9605cd80556ab42a11eb9fa95821aafa074
mintdart <96025197+mintdart@users.noreply.github.com>
2025-07-24 23:05:00 +0900

    update protocols list (#10262)
    * update protocols list
    
    * refactor
    
    * Update excludeProtocols.ts

--- defi/src/utils/excludeProtocols.ts
+++ defi/src/utils/excludeProtocols.ts
@@ -10,7 +10,10 @@ export function excludeProtocolInCharts(category: string, includeBridge?: boolea
   return exclude;
 }
 
-const excludedCategoriesSet = new Set(['Chain', 'CEX', 'Infrastructure', 'Staking Pool', 'RWA', 'Basis Trading', 'CeDeFi',])  // protocols with these categories are excluded from overall/chain tvl charts
+const excludedCategoriesSet = new Set(['Chain', 'CEX', 'Infrastructure', 'Staking Pool', 'RWA', 'Basis Trading', 'CeDeFi'])  // protocols with these categories are excluded from overall/chain tvl charts
 
 export const tvlExcludedBridgeCategoriesSet = new Set(['Bridge', 'Canonical Bridge'])  // list of bridge categories that are excluded from TVL charts by default
 export const bridgeCategoriesSet = new Set(['Bridge', 'Cross Chain Bridge', 'Canonical Bridge'])  // this is used in appMetadata.ts for setting bridge flag, used no where else
+
+export const hiddenCategoriesFromUISet = new Set(["Chain", "CEX"]) // hide these categories on defillama.com/categories page
+export const hiddenCategoriesFromProtocolsListSet = new Set(["Chain", "CEX", 'Canonical Bridge']) // hide protocols with these categories on defillama.com home page , api.llama.fi/lite/protocols2 endpoint

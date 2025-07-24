8dbc24c96bb90c890cc69b5357cb96dc7f5442bf
mintdart <96025197+mintdart@users.noreply.github.com>
2025-07-24 23:00:23 +0900

    Update excludeProtocols.ts

--- defi/src/utils/excludeProtocols.ts
+++ defi/src/utils/excludeProtocols.ts
@@ -15,5 +15,5 @@ const excludedCategoriesSet = new Set(['Chain', 'CEX', 'Infrastructure', 'Stakin
 export const tvlExcludedBridgeCategoriesSet = new Set(['Bridge', 'Canonical Bridge'])  // list of bridge categories that are excluded from TVL charts by default
 export const bridgeCategoriesSet = new Set(['Bridge', 'Cross Chain Bridge', 'Canonical Bridge'])  // this is used in appMetadata.ts for setting bridge flag, used no where else
 
-export const hiddenCategoriesFromUISet = new Set(["CEX", "Chain"]) // hide these categories on defillama.com/categories page
+export const hiddenCategoriesFromUISet = new Set(["Chain", "CEX"]) // hide these categories on defillama.com/categories page
 export const hiddenCategoriesFromProtocolsListSet = new Set(["Chain", "CEX", 'Canonical Bridge']) // hide protocols with these categories on defillama.com home page , api.llama.fi/lite/protocols2 endpoint

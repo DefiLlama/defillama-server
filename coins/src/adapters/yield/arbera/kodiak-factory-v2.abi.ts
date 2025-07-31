bd32a8ddb58f9343a08b4ace9f0b345ec02d6519
mmesser-rr <michaelmesser0795@gmail.com>
2025-08-01 01:04:10 +0800

    Add arbera protocol for token prices

---
+++ coins/src/adapters/yield/arbera/kodiak-factory-v2.abi.ts
@@ -0,0 +1,12 @@
+export default {
+  getPair: {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "getPair",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+} as const;

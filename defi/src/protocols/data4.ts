38cf06d23cf5e1afe2a9ebe52510a81b1d47fdaa
clmikestone <mike.stone@chaoslabs.xyz>
2025-08-14 16:13:17 -0400

    Add Edge Oracle to EtherFi Cash

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -18234,6 +18234,13 @@ const data4: Protocol[] = [
     forkedFrom: [],
     parentProtocol: "parent#etherfi-cash",
     listedAt: 1751313285,
+    oraclesBreakdown: [
+      {
+        name: "Edge",
+        type: "Primary",
+        proof: ["https://etherfi.gitbook.io/etherfi/contracts-and-integrations/integrations"]
+      }
+    ]
   },
   {
     id: "6374",

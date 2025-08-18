b7fd04a0a78ede5b96e386246a6de39a0caa39e6
Define101 <Lovelovenow@protonmail.com>
2025-08-18 14:08:10 +0100

    fix gmx ref code

--- defi/src/protocols/parentProtocols.ts
+++ defi/src/protocols/parentProtocols.ts
@@ -2544,7 +2544,8 @@ const parentProtocols: IParentProtocol[] = [
   {
     id: "parent#gmx",
     name: "GMX",
-    url: "https://gmx.io",
+    url: "https://gmx.io/#/?ref=defillama",
+    referralUrl: "https://gmx.io/#/?ref=defillama",
     description:
       "GMX is a leading onchain exchange for perpetual and spot trading, offering deep liquidity, low fees, multi-chain support, and seamless one-click trading. Powered by isolated and multi-asset pools, the permissionless DEX enables anybody to provide liquidity and integrates with 80+ DeFi projects across Arbitrum, Avalanche, and Solana.",
     logo: `${baseIconsUrl}/gmx.png`,

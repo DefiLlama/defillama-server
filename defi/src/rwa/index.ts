cedbdfd31179ce5cbf44b7eade6de2dc6a5ad8b3
waynebruce0x <waynebruce0x@gmail.com>
2025-08-20 14:59:58 +0100

    RWA debug endpoint  (#10449)
    * api2 rwa
    
    * vbill prices
    
    * more rwa
    
    * BUIDL sol
    
    * rwa try no api key
    
    * pulse lps
    
    * pulsex new factory
    
    * trying no api key again
    
    * pulse lps
    
    * debug rwa endpoint

--- defi/src/rwa/index.ts
+++ defi/src/rwa/index.ts
@@ -110,8 +110,9 @@ async function fetchSymbols() {
 async function fetchStats(symbols: { [id: string]: string[] }) {
   // if (!process.env.INTERNAL_API_KEY) throw new Error("INTERNAL_API_KEY is not set");
   // `https://pro-api.llama.fi/${process.env.INTERNAL_API_KEY}/yields/pools`
-  const { data } = await fetch(`https://pro-api.llama.fi//yields/pools`).then((r) => r.json());
-  const lps = data.filter((item: any) => item.exposure == "multi");
+  const data = await fetch(`https://pro-api.llama.fi//yields/pools`).then((r) => r.json());
+  if (!data.data) throw new Error(`No data: ${JSON.stringify(data)}`);
+  const lps = data.data.filter((item: any) => item.exposure == "multi");
 
   const res: { [id: string]: Stats } = {};
   Object.keys(symbols).map((id: string) => {

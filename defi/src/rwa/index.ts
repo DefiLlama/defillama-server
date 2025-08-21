8d4edd27cf0c0f80ff1e205d0185c218234f6a95
llama <waynebruce0x@gmail.com>
2025-08-21 13:47:45 +0100

    rwa tags 1

--- defi/src/rwa/index.ts
+++ defi/src/rwa/index.ts
@@ -19,11 +19,21 @@ type Characteristics = {
   selfCustody: boolean;
 };
 
-const faultyIds: { [id: string]: string } = { "415": "NAOS", "1459": "ELYFI", "1667": "Solv Vesting" };
+const faultyIds: { [id: string]: string } = {
+  "415": "NAOS",
+  "1459": "ELYFI",
+  "1667": "Solv Vesting",
+  "2181": "Frigg.eco",
+  "2231": "Tangible RWA",
+  "2315": "Invar Finance",
+  "3267": "Solid World",
+  "3466": "Bloom",
+  "3573": "Solv RWA",
+  "3598": "Mountain Protocol",
+};
 
 // KYC is true where required for mint / redeem.
 // transferable is true where transfer doesnt require KYC.
-// WIP: 2181
 const metadata: { [id: string]: Characteristics } = {
   "753": {
     symbols: ["REALTOKEN"], // need to match on inclusion
@@ -75,6 +85,136 @@ const metadata: { [id: string]: Characteristics } = {
     transferable: true,
     selfCustody: true,
   },
+  "2482": {
+    symbols: ["18SML711", "20SML025"],
+    matchExact: true,
+    redeemable: false,
+    attestations: false,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "2542": {
+    symbols: ["OUSG", "USDY", "USDYc", "rUSDY", "rOUSG"],
+    matchExact: true,
+    redeemable: true,
+    attestations: true,
+    cexListed: true, // bybit
+    kyc: true,
+    transferable: true,
+    selfCustody: true,
+  },
+  "2782": {
+    symbols: [], // random tickers for each listing
+    matchExact: true,
+    redeemable: false,
+    attestations: false,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "2807": {
+    symbols: ["STBT"],
+    matchExact: true,
+    redeemable: true,
+    attestations: true,
+    cexListed: false,
+    kyc: true,
+    transferable: false,
+    selfCustody: true,
+  },
+  "2923": {
+    symbols: ["TBT", "wTBT"],
+    matchExact: true,
+    redeemable: true,
+    attestations: false,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "2965": {
+    symbols: ["GOLD$", "SILVER$"],
+    matchExact: true,
+    redeemable: true,
+    attestations: false,
+    cexListed: false,
+    kyc: true,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3057": {
+    symbols: ["TBILL"],
+    matchExact: true,
+    redeemable: true,
+    attestations: true,
+    cexListed: false,
+    kyc: true,
+    transferable: false,
+    selfCustody: true,
+  },
+  "3193": {
+    symbols: ["stUSDT"],
+    matchExact: true,
+    redeemable: true,
+    attestations: false,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3467": {
+    symbols: ["bERNX", "bCSPX", "bCOIN", "bC3M", "bIB01", "bIBTA", "bHIGH", "bNVDA"],
+    matchExact: false,
+    redeemable: true,
+    attestations: true,
+    cexListed: false,
+    kyc: true,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3560": {
+    symbols: ["USDP", "rUSDP"],
+    matchExact: true,
+    redeemable: true,
+    attestations: false,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3580": {
+    symbols: ["uMINT", "pEAK0825", "iSNR", "pEAK2", "pEAK", "HYDB1025", "DMMF01", "CMBMINT"],
+    matchExact: true,
+    redeemable: true,
+    attestations: false,
+    cexListed: false,
+    kyc: true,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3665": {
+    symbols: ["sUSDS", "sDAI"],
+    matchExact: true,
+    redeemable: true,
+    attestations: true,
+    cexListed: false,
+    kyc: false,
+    transferable: true,
+    selfCustody: true,
+  },
+  "3698": {
+    symbols: ["USYC"],
+    matchExact: true,
+    redeemable: true,
+    attestations: true,
+    cexListed: true, // Deribit
+    kyc: true,
+    transferable: true,
+    selfCustody: true,
+  },
   "5506": {
     symbols: ["USDtb"],
     matchExact: true,
@@ -100,7 +240,7 @@ const metadata: { [id: string]: Characteristics } = {
 function fetchSymbols() {
   const rwaProtocols = protocols
     .filter((p) => p.tags?.some((t) => CategoryTagMap.RWA.includes(t)))
-    .filter((p) => Object.keys(metadata).includes(p.id)); // THIS IS FOR TESTING ONLY // !Object.keys(faultyIds).includes(p.id)
+    .filter((p) => Object.keys(metadata).includes(p.id) && !Object.keys(faultyIds).includes(p.id)); // ! FOR TESTING ONLY
   const res: { [id: string]: string[] } = {};
   rwaProtocols.map((p) => (res[p.id] = metadata[p.id].symbols));
 
@@ -110,7 +250,9 @@ function fetchSymbols() {
 export async function fetchRWAStats() {
   const symbols = fetchSymbols();
   if (!process.env.INTERNAL_API_KEY) throw new Error("INTERNAL_API_KEY is not set");
-  const data = await fetch(`https://pro-api.llama.fi/${process.env.INTERNAL_API_KEY}/yields/pools`).then((r) => r.json());
+  const data = await fetch(`https://pro-api.llama.fi/${process.env.INTERNAL_API_KEY}/yields/pools`).then((r) =>
+    r.json()
+  );
   if (!data.data) throw new Error(`No data: ${JSON.stringify(data)}`);
   const lps = data.data.filter((item: any) => item.exposure == "multi");
 
@@ -126,8 +268,8 @@ export async function fetchRWAStats() {
     symbols[id].map((symbol: string) => {
       const rwa = lps.filter((item: any) =>
         item.matchExact
-          ? item.symbol.toLowerCase().startsWith(`${symbol.toLowerCase()}-`) ||
-            item.symbol.toLowerCase().endsWith(`-${symbol.toLowerCase()}`)
+          ? item.symbol.toLowerCase().startsWith(`${symbol.toLowerCase()}`) ||
+            item.symbol.toLowerCase().endsWith(`${symbol.toLowerCase()}`)
           : item.symbol.toLowerCase().includes(symbol.toLowerCase())
       );
 
@@ -144,3 +286,5 @@ export async function fetchRWAStats() {
 
   return res;
 }
+
+// fetchRWAStats(); // ts-node defi/src/rwa/index.ts

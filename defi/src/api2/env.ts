de26ea3e3ec277bdacb928aefe946cca55b7e54f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-23 06:51:07 +0200

    Add logging for loading DEV_METRICS_DB_CONFIG from JSON config

--- defi/src/api2/env.ts
+++ defi/src/api2/env.ts
@@ -27,6 +27,7 @@ export default function getTvlCacheEnv() {
   const isCoolifyTask = ENV.IS_COOLIFY_TASK === 'true'
 
   if (ENV.DEV_METRICS_DB_CONFIG) {
+    console.log("Loading DEV_METRICS_DB_CONFIG from JSON config", ENV.DEV_METRICS_DB_CONFIG);
     const metricsConfig = JSON.parse(ENV.DEV_METRICS_DB_CONFIG);
     ENV.DEV_METRICS_DB_NAME = metricsConfig.db_name;
     ENV.DEV_METRICS_DB_HOST = metricsConfig.host;

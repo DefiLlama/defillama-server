7a582e35954e156ad5bcdff6add769d7f53c9cd8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-23 06:46:08 +0200

    bugfix

--- defi/ui-tool/src/utils/config.ts
+++ defi/ui-tool/src/utils/config.ts
@@ -11,7 +11,7 @@ export async function setConfig() {
     const response = await axios.get(process.env.CONFIG_REST_SERVER_URL);
     if (response.status === 200) {
       const _config = response.data;
-      console.log('Configuration loaded successfully:', _config);
+      console.log('Configuration loaded successfully:', Object.keys(_config).length, 'keys');
       
       // You can set the configuration in a global state or export it as needed
       Object.entries(_config).forEach(([key, value]) => {

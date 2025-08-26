8a80b1922ebf477e81a5354909cd5631c3f302dc
mike stone <166530234+clmikestone@users.noreply.github.com>
2025-08-26 08:23:11 -0400

    Update data4.ts

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -18212,6 +18212,13 @@ const data4: Protocol[] = [
       Scroll: ["LIQUIDETH", "LIQUIDUSD", "LIQUIDBTC", "EBTC"],
     },
     listedAt: 1751313277,
+    oraclesBreakdown: [
+      {
+        name: "Edge",
+        type: "Primary",
+        proof: ["https://etherfi.gitbook.io/etherfi/contracts-and-integrations/integrations"]
+      }
+    ]
   },
   {
     id: "6373",

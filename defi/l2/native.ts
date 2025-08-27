9df5900bd1fd94ca35eef0c7dc16ce559db46fa1
llama <waynebruce0x@gmail.com>
2025-08-27 15:03:31 +0100

    debug chain assets

--- defi/l2/native.ts
+++ defi/l2/native.ts
@@ -8,6 +8,7 @@ import { geckoSymbols, ownTokens, zero } from "./constants";
 import { getMcaps, getPrices, fetchBridgeTokenList, fetchSupplies } from "./utils";
 import { fetchAdaTokens } from "./adapters/ada";
 import { nativeWhitelist } from "./adapters/manual";
+import { withTimeout } from "../src/utils/shared/withTimeout";
 
 export async function fetchMinted(params: {
   chains: TokenTvlData;
@@ -20,82 +21,89 @@ export async function fetchMinted(params: {
 
   await Promise.all(
     Object.keys(params.chains).map(async (chain: Chain) => {
-      try {
-        const incomingTokens: Address[] = await fetchBridgeTokenList(chain);
+      await withTimeout(1000 * 60 * 15, minted(chain)).catch(() => {
+        console.error(`fetchMinted() timed out for ${chain}`);
+      });
 
-        let storedTokens = await fetchAllTokens(chain);
+      async function minted(chain: Chain) {
+        try {
+          const incomingTokens: Address[] = await fetchBridgeTokenList(chain);
 
-        // filter any tokens that arent natively minted
-        incomingTokens.map((t: Address) => {
-          const i = storedTokens.indexOf(t);
-          if (i == -1) return;
-          storedTokens.splice(i, 1);
-        });
+          let storedTokens = await fetchAllTokens(chain);
 
-        if (chain == "cardano") storedTokens = await fetchAdaTokens();
-
-        const ownTokenCgid: string | undefined = ownTokens[chain]?.address.startsWith("coingecko:")
-          ? ownTokens[chain].address
-          : undefined;
-        if (ownTokenCgid) storedTokens.push(ownTokenCgid);
-
-        // do these in order to lighten rpc, rest load
-        const prices = await getPrices(
-          storedTokens.map((t: string) => (t.startsWith("coingecko:") ? t : `${chain}:${t}`)),
-          timestamp
-        );
-        Object.keys(prices).map((p: string) => {
-          if (p.startsWith("coingecko:")) prices[p].decimals = 0;
-        });
-        const mcaps = await getMcaps(Object.keys(prices), timestamp);
-
-        const supplies = await fetchSupplies(
-          chain,
-          Object.keys(prices).map((t: string) => t.substring(t.indexOf(":") + 1)),
-          params.timestamp
-        );
-
-        if ("tron:TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t" in supplies) console.log("tron USDT has a supply");
-
-        if (ownTokenCgid && ownTokenCgid in mcaps)
-          supplies[ownTokenCgid] = mcaps[ownTokenCgid].mcap / prices[ownTokenCgid].price;
-
-        function findDollarValues() {
-          Object.keys(mcaps).map((t: string) => {
-            const priceInfo = prices[t];
-            const mcapInfo = mcaps[t];
-            const supply = supplies[t];
-            if (!priceInfo || !supply || !mcapInfo) return;
-
-            const symbol = geckoSymbols[priceInfo.symbol.replace("coingecko:", "")] ?? priceInfo.symbol.toUpperCase();
-            const canonicalSymbols = Object.keys(params.chains[chain]);
-            if (
-              canonicalSymbols.includes(symbol) &&
-              !(chain in nativeWhitelist && nativeWhitelist[chain].includes(t.substring(t.indexOf(":") + 1)))
-            )
-              return;
-
-            if (!(symbol in dollarValues)) dollarValues[symbol] = zero;
-            const decimalShift: BigNumber = BigNumber(10).pow(BigNumber(priceInfo.decimals));
-            const usdValue: BigNumber = BigNumber(priceInfo.price).times(BigNumber(supply)).div(decimalShift);
-            if (t != "coingecko:bitcoin" && usdValue.isGreaterThan(BigNumber(1e12))) {
-              console.log(`token ${t} on ${chain} has over a trillion usdValue LOL`);
-              return;
-            }
-            mcapData[chain][symbol] = { native: usdValue, total: BigNumber(mcapInfo.mcap) };
-            if (symbol in mcapData.total) mcapData.total[symbol].native = mcapData.total[symbol].native.plus(usdValue);
-            else mcapData.total[symbol] = { native: usdValue, total: BigNumber(mcapInfo.mcap) };
-            dollarValues[symbol] = BigNumber(usdValue).plus(dollarValues[symbol]);
+          // filter any tokens that arent natively minted
+          incomingTokens.map((t: Address) => {
+            const i = storedTokens.indexOf(t);
+            if (i == -1) return;
+            storedTokens.splice(i, 1);
           });
+
+          if (chain == "cardano") storedTokens = await fetchAdaTokens();
+
+          const ownTokenCgid: string | undefined = ownTokens[chain]?.address.startsWith("coingecko:")
+            ? ownTokens[chain].address
+            : undefined;
+          if (ownTokenCgid) storedTokens.push(ownTokenCgid);
+
+          // do these in order to lighten rpc, rest load
+          const prices = await getPrices(
+            storedTokens.map((t: string) => (t.startsWith("coingecko:") ? t : `${chain}:${t}`)),
+            timestamp
+          );
+          Object.keys(prices).map((p: string) => {
+            if (p.startsWith("coingecko:")) prices[p].decimals = 0;
+          });
+          const mcaps = await getMcaps(Object.keys(prices), timestamp);
+
+          const supplies = await fetchSupplies(
+            chain,
+            Object.keys(prices).map((t: string) => t.substring(t.indexOf(":") + 1)),
+            params.timestamp
+          );
+
+          if ("tron:TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t" in supplies) console.log("tron USDT has a supply");
+
+          if (ownTokenCgid && ownTokenCgid in mcaps)
+            supplies[ownTokenCgid] = mcaps[ownTokenCgid].mcap / prices[ownTokenCgid].price;
+
+          function findDollarValues() {
+            Object.keys(mcaps).map((t: string) => {
+              const priceInfo = prices[t];
+              const mcapInfo = mcaps[t];
+              const supply = supplies[t];
+              if (!priceInfo || !supply || !mcapInfo) return;
+
+              const symbol = geckoSymbols[priceInfo.symbol.replace("coingecko:", "")] ?? priceInfo.symbol.toUpperCase();
+              const canonicalSymbols = Object.keys(params.chains[chain]);
+              if (
+                canonicalSymbols.includes(symbol) &&
+                !(chain in nativeWhitelist && nativeWhitelist[chain].includes(t.substring(t.indexOf(":") + 1)))
+              )
+                return;
+
+              if (!(symbol in dollarValues)) dollarValues[symbol] = zero;
+              const decimalShift: BigNumber = BigNumber(10).pow(BigNumber(priceInfo.decimals));
+              const usdValue: BigNumber = BigNumber(priceInfo.price).times(BigNumber(supply)).div(decimalShift);
+              if (t != "coingecko:bitcoin" && usdValue.isGreaterThan(BigNumber(1e12))) {
+                console.log(`token ${t} on ${chain} has over a trillion usdValue LOL`);
+                return;
+              }
+              mcapData[chain][symbol] = { native: usdValue, total: BigNumber(mcapInfo.mcap) };
+              if (symbol in mcapData.total)
+                mcapData.total[symbol].native = mcapData.total[symbol].native.plus(usdValue);
+              else mcapData.total[symbol] = { native: usdValue, total: BigNumber(mcapInfo.mcap) };
+              dollarValues[symbol] = BigNumber(usdValue).plus(dollarValues[symbol]);
+            });
+          }
+
+          const dollarValues: DollarValues = {};
+          mcapData[chain] = {};
+          findDollarValues();
+
+          tvlData[chain] = dollarValues;
+        } catch (e) {
+          console.error(`fetchMinted() failed for ${chain} with ${e}`);
         }
-
-        const dollarValues: DollarValues = {};
-        mcapData[chain] = {};
-        findDollarValues();
-
-        tvlData[chain] = dollarValues;
-      } catch (e) {
-        console.error(`fetchMinted() failed for ${chain} with ${e}`);
       }
     })
   );

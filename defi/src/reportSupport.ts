583eb60641b28aadde37bb230fc971a541b18f37
0xngmi <0xngmi@protonmail.com>
2025-09-04 21:44:27 +0100

    add tracing

--- defi/src/reportSupport.ts
+++ defi/src/reportSupport.ts
@@ -1,4 +1,5 @@
 import { wrap, IResponse, successResponse, errorResponse } from "./utils/shared";
+import fetch from "node-fetch";
 
 export async function reportError(body: any, contentType: string) {
   const frontResponse = await fetch(`https://webhook.frontapp.com/forms/0f7e04ca1380d461a597/uky4NCniM0Y8XgxBu5OonIta1t4x7FK9rHRUohY5m2-Ae170F0Wxjj-AwWzBoPEJCcOiCTGqMG-vQ1C1iEYyg6vcqMsz9hrfUQE1qfFGh01dk0A1hG_tf3RWez5gvKY`, {
@@ -10,6 +11,8 @@ export async function reportError(body: any, contentType: string) {
       },
       body: body
   })
+
+  console.log(frontResponse, contentType)
   
   if(frontResponse.url !== "https://defillama.com/support?code=ok") {
     throw new Error(`Failed to send a front message`)
@@ -18,6 +21,7 @@ export async function reportError(body: any, contentType: string) {
 
 const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
   try {
+    console.log("req", event.body, event.headers)
     await reportError(event.body, event.headers['content-type']!);
     return successResponse({ message: "success" });
   } catch (e) {

ba2b7e185e3fcdba880cf596574c0021a79567eb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-10 12:56:59 +0200

    add enzoBTC mapping

--- defi/proof-of-reserves/adapters/lorenzo-enzobtc.ts
+++ defi/proof-of-reserves/adapters/lorenzo-enzobtc.ts
@@ -3,14 +3,13 @@ import { getBitcoinReservesAdapter } from '../utils/bitcoin';
 const protocolId = 'lorenzo-enzobtc';
 
 const mintedTokens = [
-  {
-    chain: 'ethereum',
-    address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a',
-  },
-  {
-    chain: 'bsc',
-    address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a',
-  },
+  { chain: 'ethereum', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'bsc', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'goat', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'sei', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'morph', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'hemi', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'plume_mainnet', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
 ]
 
 export default getBitcoinReservesAdapter(protocolId, mintedTokens);

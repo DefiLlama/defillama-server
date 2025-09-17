a59ab12ba94df810865157ab1f9b8cfc4883d095
realshaman <realdealshaman11@gmail.com>
2025-09-17 17:07:39 -0600

    fix category

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -26771,7 +26771,7 @@ const data4: Protocol[] = [
     audit_note: null,
     gecko_id: null,
     cmcId: null,
-    category: "Liquidity Manager",
+    category: "Liquidity manager",
     chains: ["Sui"],
     module: "ember-protocol/index.js",
     forkedFromIds: [],

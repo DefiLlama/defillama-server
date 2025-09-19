6cb6d0409982f7824132b37af1b0fa13506bcb20
realshaman <realdealshaman11@gmail.com>
2025-09-19 09:12:37 -0600

    fix description

--- defi/src/protocols/types.ts
+++ defi/src/protocols/types.ts
@@ -56,7 +56,7 @@ export interface Protocol {
       | "Fallback" // Oracle that isn't actively used and is just there in case the primary or secondary oracles fail
       | "RNG" // Oracle just used to provide random values (eg for games), it doesn't secure any TVL
       | "Aggregator" // Oracle used in conjuction with other oracles (eg by taking the median of multiple oracles), and thus a failure of it doesn't imply direct losses
-      | "Reference" // Used as a reference price for comparison or validation purposes. Not directly used for critical protocol operations that would result in TVL loss if the oracle fails.
+      | "Reference" // Used for price display or off-chain quoting. Not directly used for critical protocol operations that would result in TVL loss if the oracle fails.
       // pls add more as needed
     proof: Array<string>,
     startDate?: string, // YYYY-MM-DD

edb45f2346ebbd110dc8978ed5134ac0d7e06df6
Define101 <Lovelovenow@protonmail.com>
2025-09-23 11:47:37 +0100

    fix

--- defi/src/utils/normalizeChain.ts
+++ defi/src/utils/normalizeChain.ts
@@ -5073,7 +5073,7 @@ const chainLabelMap = {
   "ailayer": "AILayer",
   "ox_chain": "OXFUN",
   "etlk": "Etherlink",
-  "reya": "ReyaChain",
+  "reya": " Reya Network",
   "cronos_zkevm": "Cronos zkEVM",
   "band": "BandChain",
   "immutablex": "Immutable X",

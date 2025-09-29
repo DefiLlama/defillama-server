b6e637b5d1bb0cd3a721dc0fa6bdacae0316c71d
wayne <waynebruce0x@gmail.com>
2025-09-29 15:05:48 +0100

    reimplement cache

--- coins/src/adapters/utils/erc20.ts
+++ coins/src/adapters/utils/erc20.ts
@@ -173,7 +173,7 @@ async function _getCachedData(params: {
   if (!cacheObject[cacheKey])
     cacheObject[cacheKey] = await getCache(key, chain);
 
-  const cache: any = {} // cacheObject[cacheKey];
+  const cache = cacheObject[cacheKey];
   targets = targets.map((i) => i.toLowerCase());
   const missing = targets.filter((i) => !cache[i]);
   const decimals = await api.multiCall({

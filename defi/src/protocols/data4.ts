caf1208b42a6f769fe4ca7e164b9399a7bdd92f0
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2025-10-01 17:17:38 -0700

    update TakaraLend primary oracle (#10731)

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -5392,11 +5392,16 @@ const data4: Protocol[] = [
     chains: ["Sei"],
     oraclesBreakdown: [
       {
-        name: "RedStone",
+        name: "Api3",
         type: "Primary",
         proof: [
           "https://takara.gitbook.io/takara-lend/protocol-information/security#takara-security-measures",
           "https://app.takaralend.com/market/WSEI",
+          "https://app.takaralend.com/market/spSEI",
+          "https://app.takaralend.com/market/iSEI",
+          "https://app.takaralend.com/market/uBTC",
+          "https://app.takaralend.com/market/MBTC",
+          "https://app.takaralend.com/market/SBTC",
         ],
       },
       {
@@ -5408,7 +5413,7 @@ const data4: Protocol[] = [
         ],
       },
       {
-        name: "Api3",
+        name: "RedStone",
         type: "Fallback",
         proof: [
           "https://takara.gitbook.io/takara-lend/protocol-information/security#takara-security-measures",

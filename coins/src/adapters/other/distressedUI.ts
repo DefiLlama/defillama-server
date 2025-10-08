541131fece34adedbfd0c5cade7988792164a228
wayne <waynebruce0x@gmail.com>
2025-10-08 14:37:54 +0100

    auto check for distressed assets

--- coins/src/adapters/other/distressedUI.ts
+++ coins/src/adapters/other/distressedUI.ts
@@ -3,40 +3,10 @@ import { getTokenInfo } from "../utils/erc20";
 import { Write } from "../utils/dbInterfaces";
 import { addToDBWritesList } from "../utils/database";
 import { getCurrentUnixTimestamp } from "../../utils/date";
-import { contracts } from "./distressed";
-import { storeR2JSONString, getR2JSONString } from "../../utils/r2";
-import { chainsThatShouldNotBeLowerCased } from "../../utils/shared/constants";
+import { getR2JSONString } from "../../utils/r2";
 import PromisePool from "@supercharge/promise-pool";
 
 const r2Key = "distressedAssetsList.json";
-
-export async function isDistressed(key: string) {
-  const chain = key.split(":")[0];
-  const address = key.substring(chain.length + 1);
-  const normalizedAddress = chainsThatShouldNotBeLowerCased.includes(chain)
-    ? address
-    : address.toLowerCase();
-  const data = await getR2JSONString(r2Key);
-
-  if (!data[chain]) return false;
-  if (data[chain][normalizedAddress]) return true;
-
-  return false;
-}
-
-export async function addToDistressedList(key: string) {
-  const chain = key.split(":")[0];
-  const address = key.substring(chain.length + 1);
-  const normalizedAddress = chainsThatShouldNotBeLowerCased.includes(chain)
-    ? address
-    : address.toLowerCase();
-  const data = await getR2JSONString(r2Key);
-
-  if (!data[chain]) data[chain] = {};
-  data[chain].push(normalizedAddress);
-
-  await storeR2JSONString(r2Key, JSON.stringify(data));
-}
 
 export default async function getTokenPrices(timestamp: number) {
   const writes: Write[] = [];

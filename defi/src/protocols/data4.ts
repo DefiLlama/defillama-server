aa4e7ef4aacd250130555a67604909bf44a580cb
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2025-10-10 15:51:49 -0600

    add second proof link

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -23823,7 +23823,7 @@ const data4: Protocol[] = [
       {
         name: "Edge",
         type: "Primary",
-        proof: ["https://arbiscan.io/address/0x7b09d87945102d25069c173ee45db397c6e055b3#readContract#F4"],
+        proof: ["https://arbiscan.io/address/0x7b09d87945102d25069c173ee45db397c6e055b3#readContract#F4","https://github.com/DefiLlama/defillama-server/pull/10782],
       },
     ],
   },

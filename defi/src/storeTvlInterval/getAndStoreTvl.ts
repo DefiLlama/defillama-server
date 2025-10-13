512433cc0a52d92d2d323cadc2e39de20b553f96
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-10-13 14:17:04 +0200

    Fix deadChains set by replacing hyphen with underscore

--- defi/src/storeTvlInterval/getAndStoreTvl.ts
+++ defi/src/storeTvlInterval/getAndStoreTvl.ts
@@ -177,7 +177,7 @@ type StoreTvlOptions = {
   isRunFromUITool?: boolean
 }
 
-const deadChains = new Set(['heco', 'astrzk', 'real', 'milkomeda', 'milkomeda_a1', 'eos-evm'])
+const deadChains = new Set(['heco', 'astrzk', 'real', 'milkomeda', 'milkomeda_a1', 'eos_evm'])
 
 export type storeTvl2Options = StoreTvlOptions & {
   unixTimestamp: number,

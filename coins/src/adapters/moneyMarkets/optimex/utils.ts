292310f4563c75c251a510173ea5f191c39e95a3
phuccaliber <phuc@caliber.build>
2025-11-03 14:09:56 +0700

    feat(optimex): Using decimal constant

--- coins/src/adapters/moneyMarkets/optimex/utils.ts
+++ coins/src/adapters/moneyMarkets/optimex/utils.ts
@@ -3,10 +3,12 @@ import { AGGREGATOR_ABI } from "./abi";
 
 // BTC/USD Chainlink Oracle on Ethereum
 const BTC_TO_USD_ORACLE_ETH = "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c";
+// Rounding decimals for the above chainlink oracle
+const ROUNDING_DECIMALS = 1e8; 
 
 export async function getBtcToUsdPrice(timestamp: number): Promise<number> {
     const api = await getApi("ethereum", timestamp);
     const response = await api.call({ abi: AGGREGATOR_ABI, target: BTC_TO_USD_ORACLE_ETH });
-    // Scale down to 8 decimals
-    return response.answer / 1e8;
+    // Scale down using decimals
+    return response.answer / ROUNDING_DECIMALS;
   }

82d5544d388914b2d2e498463183c00c0dc222ab
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-03 15:40:02 +0100

    track enzoBTC on base

--- defi/proof-of-reserves/adapters/lorenzo-enzobtc.ts
+++ defi/proof-of-reserves/adapters/lorenzo-enzobtc.ts
@@ -10,6 +10,7 @@ const mintedTokens = [
   // { chain: 'morph', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
   { chain: 'hemi', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
   { chain: 'plume_mainnet', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
+  { chain: 'base', address: '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a' },
 ]
 
 export default getBitcoinReservesAdapter(protocolId, mintedTokens);

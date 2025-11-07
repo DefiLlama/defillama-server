e5a3a197c75d919840cf87d033f3506031a47011
waynebruce0x <waynebruce0x@gmail.com>
2025-11-07 09:52:30 +0000

    V2 Chain Assets (#10820)
    * pending ES allTokens, SDK price+mcap fetchers, error handling, remove WIP work
    
    * error logging, ES alltokens pending
    
    * L2 allToken ES
    
    * cached fetches
    
    * coins sdk integration
    
    * patch own tokens, tron, sol, totals
    
    * v2 in job

--- defi/l2/utils.ts
+++ defi/l2/utils.ts
@@ -1,5 +1,5 @@
 import BigNumber from "bignumber.js";
-import { AllProtocols, CoinsApiData, McapsApiData, TokenTvlData } from "./types";
+import { AllProtocols, TokenTvlData } from "./types";
 import { canonicalBridgeIds, excludedTvlKeys, geckoSymbols, protocolBridgeIds, zero } from "./constants";
 import fetch from "node-fetch";
 import { bridgedTvlMixedCaseChains } from "../src/utils/shared/constants";
@@ -91,113 +91,6 @@ async function restCallWrapper(request: () => Promise<any>, retries: number = 8,
     }
   }
   throw new Error(`couldnt work ${name} call after retries!`);
-}
-export async function getPrices(
-  readKeys: string[],
-  timestamp: number | "now"
-): Promise<{ [address: string]: CoinsApiData }> {
-  if (!readKeys.length) return {};
-  const bodies: string[] = [];
-  for (let i = 0; i < readKeys.length; i += 100) {
-    const body = {
-      coins: readKeys.slice(i, i + 100),
-    } as any;
-    if (timestamp !== "now") {
-      body.timestamp = timestamp;
-    }
-    bodies.push(JSON.stringify(body));
-  }
-
-  const tokenData: any[] = [];
-  await PromisePool.withConcurrency(10)
-    .for(bodies)
-    .process(async (body) => {
-      const res = await restCallWrapper(
-        () =>
-          fetch(
-            `https://coins.llama.fi/prices?source=internal${
-              process.env.COINS_KEY ? `?apikey=${process.env.COINS_KEY}` : ""
-            }`,
-            {
-              method: "POST",
-              body,
-              headers: { "Content-Type": "application/json" },
-            }
-          )
-            .then((r) => r.json())
-            .then((r) =>
-              Object.entries(r.coins).map(([PK, value]) => ({
-                ...(value as any),
-                PK,
-              }))
-            ),
-        undefined,
-        "coin prices"
-      );
-      tokenData.push(res);
-    })
-    .catch((e) => {
-      throw new Error(`coin prices call failed with ${e}`);
-    });
-
-  const aggregatedRes: { [address: string]: CoinsApiData } = {};
-  const normalizedReadKeys = readKeys.map((k: string) => k.toLowerCase());
-  tokenData.map((batch: CoinsApiData[]) => {
-    batch.map((a: CoinsApiData) => {
-      if (!a.PK) return;
-      const i = normalizedReadKeys.indexOf(a.PK.toLowerCase());
-      aggregatedRes[readKeys[i]] = a;
-    });
-  });
-
-  return aggregatedRes;
-}
-export async function getMcaps(
-  readKeys: string[],
-  timestamp: number | "now"
-): Promise<{ [address: string]: McapsApiData }> {
-  if (!readKeys.length) return {};
-  const bodies: string[] = [];
-  for (let i = 0; i < readKeys.length; i += 100) {
-    const body = {
-      coins: readKeys.slice(i, i + 100),
-    } as any;
-    if (timestamp !== "now") {
-      body.timestamp = timestamp;
-    }
-    bodies.push(JSON.stringify(body));
-  }
-
-  const tokenData: any[] = [];
-  await PromisePool.withConcurrency(10)
-    .for(bodies)
-    .process(async (body) => {
-      const res = await restCallWrapper(
-        () =>
-          fetch(`https://coins.llama.fi/mcaps${process.env.COINS_KEY ? `?apikey=${process.env.COINS_KEY}` : ""}`, {
-            method: "POST",
-            body,
-            headers: { "Content-Type": "application/json" },
-          }).then((r) => r.json()),
-        undefined,
-        "mcaps"
-      );
-      tokenData.push(res);
-    })
-    .catch((e) => {
-      throw new Error(`coin mcaps call failed with ${e}`);
-    });
-
-  const aggregatedRes: { [address: string]: any } = {};
-  const normalizedReadKeys = readKeys.map((k: string) => k.toLowerCase());
-  tokenData.map((batch: { [address: string]: McapsApiData }[]) => {
-    Object.keys(batch).map((a: any) => {
-      if (!batch[a].mcap) return;
-      const i = normalizedReadKeys.indexOf(a.toLowerCase());
-      aggregatedRes[readKeys[i]] = batch[a];
-    });
-  });
-  return aggregatedRes;
 }
 async function getOsmosisSupplies(tokens: string[], timestamp?: number): Promise<{ [token: string]: number }> {
   if (timestamp) throw new Error(`timestamp incompatible with Osmosis adapter!`);
@@ -364,15 +257,15 @@ async function getEVMSupplies(
         })),
         abi: "erc20:totalSupply",
         permitFailure: true,
-        block: block.block,
+        block: block?.block,
       });
       contracts.slice(i, i + step).map((c: Address, i: number) => {
         if (res[i]) supplies[`${chain}:${bridgedTvlMixedCaseChains.includes(chain) ? c : c.toLowerCase()}`] = res[i];
       });
-    } catch {
+    } catch (e) {
       try {
         process.env.TRON_RPC = process.env.TRON_RPC?.substring(process.env.TRON_RPC.indexOf(",") + 1);
-        await PromisePool.withConcurrency(2)
+        await PromisePool.withConcurrency(5)
           .for(contracts.slice(i, i + step))
           .process(async (target) => {
             const res = await call({
@@ -381,7 +274,7 @@ async function getEVMSupplies(
               abi: "erc20:totalSupply",
               block,
             }).catch(async (e) => {
-              await sleep(2000);
+              await sleep(1000);
               if (chain == "tron") console.log(`${target}:: \t ${e.message}`);
             });
             if (res)
@@ -412,9 +305,8 @@ export async function fetchSupplies(
   }
 }
 export async function fetchBridgeTokenList(chain: Chain): Promise<Address[]> {
-  const j = Object.keys(incomingAssets).indexOf(chain);
   try {
-    const tokens: Address[] = j == -1 ? [] : await Object.values(incomingAssets)[j]();
+    const tokens: Address[] = incomingAssets[chain as keyof typeof incomingAssets] ? await incomingAssets[chain as keyof typeof incomingAssets]() : []
     tokens.push(...((await fetchThirdPartyTokenList())[chain] ?? []));
     let filteredTokens: Address[] =
       chain in excluded ? tokens.filter((t: string) => !excluded[chain].includes(t)) : tokens;

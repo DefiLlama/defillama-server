37ca77328e2c7df7c13dedd38ab037bb1935787d
cll-jay <jeyan.thangaraj@smartcontract.com>
2025-11-12 01:16:45 -0500

    Update Hyperdrive HL primary oracle to Chainlink (#10968)
    * Change Hyperdrive primary oracle to Chainlink
    
    * keep RedStone oracle data
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -18204,6 +18204,13 @@ const data4: Protocol[] = [
         name: "RedStone",
         type: "Primary",
         proof: ["https://hyperdrive-2.gitbook.io/hyperdrive/borrowing/oracles"],
+        endDate: "2025-11-08"
+      },
+      {
+        name: "Chainlink",
+        type: "Primary",
+        proof: ["https://hyperdrive-2.gitbook.io/hyperdrive/borrowing/oracles"],
+        startDate: "2025-11-08"
       },
     ],
     audit_links: ["https://github.com/ambitlabsxyz/hyperdrive-audits"],

538d58af8a2fe0315f8c1828982fc468f75780d0
bats4 <bats4.game@gmail.com>
2025-12-01 00:05:24 +0900

    Pyth and Stork as Aggregator
    Added Pyth as Aggregate Oracle, and changed Stork to Agrregator as well

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -21081,7 +21081,12 @@ const data4: Protocol[] = [
     oraclesBreakdown: [
       {
         name: "Stork",
-        type: "Primary",
+        type: "Aggregator",
+        proof: ["https://docs.rysk.finance/resources/oracle"],
+      },
+      {
+        name: "Stork",
+        type: "Aggregator",
         proof: ["https://docs.rysk.finance/resources/oracle"],
       },
     ],

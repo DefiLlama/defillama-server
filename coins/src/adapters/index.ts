5dc081d6dd6eae76b7d4dc370a48ca66678231e4
wayne <waynebruce0x@gmail.com>
2025-12-02 16:06:34 +0000

    wisdomtree

--- coins/src/adapters/index.ts
+++ coins/src/adapters/index.ts
@@ -180,4 +180,5 @@ export default {
   suirewards: require("./markets/suirewards"),
   axlp: require("./liquidStaking/axlp"),
   altai: require("./rwa/altai"),
+  wisdomtree: require("./rwa/wisdomtree"),
 };

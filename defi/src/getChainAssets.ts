d2e8065fb7948819570c4cc610086bc2425462f5
waynebruce0x <waynebruce0x@gmail.com>
2025-12-03 15:46:04 +0000

    Revert "Migrate Chain Assets  (#11015)"
    This reverts commit 64734a54ae620f35d4ef3bf9ab6d855a8000c625.

--- defi/src/getChainAssets.ts
+++ defi/src/getChainAssets.ts
@@ -1,14 +1,9 @@
-import { getChainDisplayName } from "./utils/normalizeChain";
 import { getR2 } from "./utils/r2";
 import { wrap, IResponse, successResponse, errorResponse } from "./utils/shared";
 
 const handler = async (_event: any): Promise<IResponse> => {
   try {
-    const chainsRaw = await getR2(`chainAssets2`).then((res) => JSON.parse(res.body!));
-    const chains: { [key: string]: any } = { timestamp: chainsRaw.timestamp,  };
-    Object.keys(chainsRaw.value).map((chain) => {
-      chains[getChainDisplayName(chain, true)] = chainsRaw.value[chain];
-    });
+    const chains = await getR2(`chainAssets`).then((res) => JSON.parse(res.body!));
     return successResponse(chains, 10 * 60); // 10 min cache
   } catch (e: any) {
     return errorResponse({ message: e.message });
@@ -16,3 +11,4 @@ const handler = async (_event: any): Promise<IResponse> => {
 };
 
 export default wrap(handler);
+// handler({}); // ts-node defi/src/getChainAssets.ts

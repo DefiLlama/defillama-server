5de075ecd89ad509f9f23d5e3d17aeefe90dd083
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-12-03 16:29:27 +0100

    minor fix

--- defi/src/api2/env.ts
+++ defi/src/api2/env.ts
@@ -66,8 +66,12 @@ export default function getTvlCacheEnv() {
   }
 }
 
+const WHITLISTED_ENV_VARS = [
+  'LLAMA_PRO_API_KEY',
+]
+
 const booleanEnvVarsSet = new Set(['DIM_CRON_NOTIFY_ON_DISCORD'])
-const validEnvVarsSet = new Set([ ...requiredEnvVars, ...booleanEnvVarsSet, ])
+const validEnvVarsSet = new Set([ ...requiredEnvVars, ...booleanEnvVarsSet, ...WHITLISTED_ENV_VARS ])
 
 export function getEnv(env: string): string | boolean | undefined {
   if (!validEnvVarsSet.has(env)) {

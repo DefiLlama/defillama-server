83754391f0df15051b59b184360c94c7f785a833
realshaman <realdealshaman11@gmail.com>
2025-12-15 14:53:35 -0600

    add new listings

--- defi/src/protocols/data.test.ts
+++ defi/src/protocols/data.test.ts
@@ -318,7 +318,8 @@ test("no surprise category", async () => {
     "Luck Games",
     "ve-Incentive Automator",
     "Cross Chain Bridge",
-    "Decentralized AI"
+    "Decentralized AI",
+    "Identity & Reputation"
   ]
   for (const protocol of protocols) {
     expect(whitelistedCategories).toContain(protocol.category);

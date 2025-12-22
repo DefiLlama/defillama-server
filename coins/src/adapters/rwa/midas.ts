b2c238857e8ed3ab4e9743024bfb5eec4e9b71fc
wayne <waynebruce0x@gmail.com>
2025-12-22 13:28:30 +0000

    mhyperbtc

--- coins/src/adapters/rwa/midas.ts
+++ coins/src/adapters/rwa/midas.ts
@@ -131,12 +131,12 @@ const contracts: Record<string, TokenConfig[]> = {
       oracle: "0xbD560c1E87752717C34912D128168BfE26021EA2",
       denomination: "ETH",
     },
-  //   {
-  //     name: "mHyperBTC",
-  //     token: "0xC8495EAFf71D3A563b906295fCF2f685b1783085",
-  //     oracle: "0xb75B82b2012138815d1A2c4aB5B8b987da043157",
-  //     denomination: "BTC",
-  //   },
+    {
+      name: "mHyperBTC",
+      token: "0xC8495EAFf71D3A563b906295fCF2f685b1783085",
+      oracle: "0xb75B82b2012138815d1A2c4aB5B8b987da043157",
+      denomination: "BTC",
+    },
   ],
   // base: [
   //   {

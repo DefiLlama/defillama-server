98002ce9010ac5d7c81a42b631a07a8258ff98d8
realshaman <realdealshaman11@gmail.com>
2026-01-13 07:59:32 -0600

    add new oracle type

--- defi/src/protocols/types.ts
+++ defi/src/protocols/types.ts
@@ -57,6 +57,7 @@ export interface Protocol {
       | "RNG" // Oracle just used to provide random values (eg for games), it doesn't secure any TVL
       | "Aggregator" // Oracle used in conjuction with other oracles (eg by taking the median of multiple oracles), and thus a failure of it doesn't imply direct losses
       | "Reference" // Used for price display or off-chain quoting. Not directly used for critical protocol operations that would result in TVL loss if the oracle fails.
+      | "PoR" // Proof of Reserve oracle, used to prove the protocol has the reserves to back the TVL
       // pls add more as needed
     proof: Array<string>,
     startDate?: string, // YYYY-MM-DD

c5587b406de3455c493cf1d21b0a1370f01ce214
Felix Bruguera <felixbruguera0@gmail.com>
2026-01-20 10:07:23 -0400

    add volume and change comparison logic

--- defi/src/api2/scripts/getStaleAdapters.ts
+++ defi/src/api2/scripts/getStaleAdapters.ts
@@ -1,17 +1,42 @@
+function getCondition(protocol: any, min: number, threshold: number) {
+    return Number(protocol.total24h >= min) &&
+            Number(protocol.change_1d) >= threshold * -1 &&
+            Number(protocol.change_7d) >= threshold * -1 &&
+            Number(protocol.change_1d) <= threshold &&
+            Number(protocol.change_7d) <= threshold 
+}
+
 async function _getStaleProtocols() {
-    const response = await fetch("https://api.llama.fi/overview/fees?excludeTotalDataChartBreakdown=true")
-    const data = await response.json()
-    const protocols = data.protocols
+    const [fees,volume] = await Promise.all([
+        fetch("https://api.llama.fi/overview/fees?excludeTotalDataChartBreakdown=true&dataType=dailyFees").then(response => response.json()).then(data => data.protocols),
+        fetch("https://api.llama.fi/overview/dexs?excludeTotalDataChartBreakdown=true").then(response => response.json()).then(data => data.protocols),
+    ])
     const responseTable : any = []
-    protocols.forEach((protocol: Record<string, any>) => {
-        if (Number(protocol.total24h === Number(protocol.total7DaysAgo)) && Number(protocol.total24h === Number(protocol.total30DaysAgo))) {
+    fees.forEach((protocol: Record<string, any>) => {
+        const threshold = protocol.category === "Lending" ? 1 : 3
+        if (getCondition(protocol, 100, threshold))  {
             responseTable.push({
+                metric: "Fees",
                 protocol: protocol.name,
                 total24h: protocol.total24h,
+                change_1d: protocol.change_1d,
+                change_7d: protocol.change_7d,
                 total7DaysAgo: protocol.total7DaysAgo,
                 total30DaysAgo: protocol.total30DaysAgo,
-                totalAllTime: protocol.totalAllTime,
-                average1y: protocol.average1y,
+                methodologyURL: protocol.methodologyURL
+            })
+        }
+    })
+    volume.forEach((protocol: Record<string, any>) => {
+        if (getCondition(protocol, 1000, 3))  {
+            responseTable.push({
+                metric: "Volume",
+                protocol: protocol.name,
+                total24h: protocol.total24h,
+                change_1d: protocol.change_1d,
+                change_7d: protocol.change_7d,
+                total7DaysAgo: protocol.total7DaysAgo,
+                total30DaysAgo: protocol.total30DaysAgo,
                 methodologyURL: protocol.methodologyURL
             })
         }

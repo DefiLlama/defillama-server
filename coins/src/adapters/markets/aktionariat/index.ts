7eaf5b558f90a63c8f229712dc37cc502dcb54fa
Luzius Meisser <luzius@aktionariat.com>
2026-01-28 17:43:16 +0100

    Lowercase for hardcoded addresses
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- coins/src/adapters/markets/aktionariat/index.ts
+++ coins/src/adapters/markets/aktionariat/index.ts
@@ -7,7 +7,7 @@ export function aktionariat(timestamp: number = 0) {
       "ethereum",
       sdk.graph.modifyEndpoint('2ZoJCp4S7YP7gbYN2ndsYNjPeZBV1PMti7BBoPRRscNq'),
       timestamp,
-      ["0xb4272071ecadd69d933adcd19ca99fe80664fc08", "0xB58E61C3098d85632Df34EecfB899A1Ed80921cB"] // ignore xchf and zchf
+      ["0xb4272071ecadd69d933adcd19ca99fe80664fc08", "0xb58e61c3098d85632df34eecfb899a1ed80921cb"] // ignore xchf and zchf
     ),
   ]);
 }

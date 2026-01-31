a6b6dbe23814f05b49673db19c2654b4afad1870
mintdart <96025197+mintdart@users.noreply.github.com>
2026-01-31 01:53:12 -0800

    bump cache version

--- defi/src/rwa/file-cache.ts
+++ defi/src/rwa/file-cache.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 // Bump this version to reset the cache
-const CACHE_VERSION = 'v1.3';
+const CACHE_VERSION = 'v1.4';
 
 const CACHE_DIR = process.env.RWA_CACHE_DIR || path.join(__dirname, '.rwa-cache');
 const VERSIONED_CACHE_DIR = path.join(CACHE_DIR, CACHE_VERSION);

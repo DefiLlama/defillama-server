bdcd4d1f8173e9ba679e079c9087b8a8f7602c69
bheluga <anonymous_cat_llama@protonmail.com>
2026-02-02 12:02:44 +0530

    sanctum dex genuine spike (#11301)

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -15181,7 +15181,7 @@ const data3_0: Protocol[] = [
     dimensions: {
       fees: "sanctum",
       dexs: {
-        genuineSpikes: ["1704240000", "1760054400"],
+        genuineSpikes: ["1704240000", "1760054400", "1769817600"],
         adapter: "sanctum"
       }
     }

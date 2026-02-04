dd4080f5f145c1cd48d37cefd10b38ad727c1c05
wayne <waynebruce0x@gmail.com>
2026-02-04 11:50:29 +0000

    aave debt

--- coins/src/adapters/moneyMarkets/aave-debt/aaveDebt.ts
+++ coins/src/adapters/moneyMarkets/aave-debt/aaveDebt.ts
@@ -69,7 +69,14 @@ export default async function getTokenPrices(chain: string, timestamp: number) {
     projectName: "aave-debt",
   });
 
-  return writes.filter((w: Write) => w.symbol?.toLowerCase().includes("debt"));
+  const pksToInclude: string[] = []
+  writes.forEach((w: Write) => {
+    if (w.symbol?.toLowerCase().includes("debt")) {
+      pksToInclude.push(w.PK);
+    }
+  });
+
+  return writes.filter((w: Write) => pksToInclude.includes(w.PK));
 }
 
 const abi: any = {

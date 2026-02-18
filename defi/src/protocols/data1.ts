53da4e7b510414d54cfcee98f5d25519ad8a1af6
cll-jay <jeyan.thangaraj@smartcontract.com>
2026-02-18 16:55:30 -0500

    Added Chainlink to Venus Core Pool

--- defi/src/protocols/data1.ts
+++ defi/src/protocols/data1.ts
@@ -2352,6 +2352,12 @@ const data: Protocol[] = [
         type: "Primary",
         proof: ["https://docs-v4.venus.io/risk/resilient-price-oracle#bnb-chain", "https://docs-v4.venus.io/risk/resilient-price-oracle", "https://docs-v4.venus.io/risk/resilient-price-oracle#unichain-mainnet:~:text=%2D-,Unichain%20Mainnet,-Pool"],
         chains: [{ chain: "Binance" }, { chain: "Unichain" }]
+      },
+      {
+        name: "Chainlink",
+        type: "Primary",
+        proof: ["https://docs-v4.venus.io/risk/resilient-price-oracle"],
+        chains: [{ chain: "Binance" }, { chain: "Ethereum" }, { chain: "Arbitrum"}, { chain: "zkSync"}, { chain: "Optimism"}, { chain: "Base"}]
       }
     ],
     parentProtocol: "parent#venus-finance",

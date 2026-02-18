b0006b251aeaca30ed8b059836454bc8348b4df9
realshaman <realdealshaman11@gmail.com>
2026-02-18 11:40:19 -0600

    add hallmark

--- defi/src/protocols/data3.ts
+++ defi/src/protocols/data3.ts
@@ -30626,6 +30626,9 @@ const data3_1: Protocol[] = [
     audit_links: ["https://github.com/catalogfi/swapper/blob/main/audits/audit-01-ottersec.pdf"],
     //parentProtocol: "parent#garden",
     listedAt: 1706793085,
+    hallmarks: [
+      ['2025-10-30', 'solver exploit']
+    ],
     dimensions: {
       fees: "garden",
       dexs: "garden",

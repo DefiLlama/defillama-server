f6a67001eea8c4fde0020fd19b44daba84c58997
cll-jay <jeyan.thangaraj@smartcontract.com>
2026-02-25 10:26:14 -0500

    Fixed year on startDate

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -16814,7 +16814,7 @@ const data4: Protocol[] = [
         name: "Chainlink",
         type: "Primary",
         proof: ["https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/wsteth-eth-25x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/susds-usdt-25x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/siusd-usdc-11x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/rlp-usdc-6.75x"],
-        startDate: "2024-02-25"
+        startDate: "2026-02-25"
       },
     ],
     dimensions: {

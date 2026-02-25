6c2b2117805b97425389e3789601fd844db474e2
cll-jay <jeyan.thangaraj@smartcontract.com>
2026-02-25 10:21:15 -0500

    Added dates to update

--- defi/src/protocols/data4.ts
+++ defi/src/protocols/data4.ts
@@ -16805,9 +16805,16 @@ const data4: Protocol[] = [
     listedAt: 1749485042,
     oraclesBreakdown: [
       {
+        name: "RedStone",
+        type: "Primary",
+        proof: ["https://docs.seamlessprotocol.com/technical/oracles"],
+        endDate: "2026-02-24"
+      },
+      {
         name: "Chainlink",
         type: "Primary",
         proof: ["https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/wsteth-eth-25x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/susds-usdt-25x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/siusd-usdc-11x", "https://docs.seamlessprotocol.com/overview/leverage-tokens-lts/ethereum-mainnet-lts/rlp-usdc-6.75x"],
+        startDate: "2024-02-25"
       },
     ],
     dimensions: {

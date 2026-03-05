dd2106d7b8843153a7c621c47c13286d79ff9366
saito <bluewhale.saito@gmail.com>
2026-03-05 17:38:09 +0900

    delete console.

--- coins/src/adapters/markets/pumpspace.ts
+++ coins/src/adapters/markets/pumpspace.ts
@@ -305,7 +305,6 @@ async function getPrices(readTimestamp: number, writeTimestamp: number): Promise
     const liquidityUsd = busdtAmt * 2;
     const confidence = confidenceFromLiquidityUsd(liquidityUsd);
 
-    // console.log(symbol + ' : ' + price);
     addToDBWritesList(
       writes,
       CHAIN,

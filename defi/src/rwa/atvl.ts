0af72e66c44cfa0ed3dac15aeac72a8fe87714f7
wayne <waynebruce0x@gmail.com>
2026-03-12 16:01:41 +0000

    entry

--- defi/src/rwa/atvl.ts
+++ defi/src/rwa/atvl.ts
@@ -485,10 +485,8 @@ export default async function main(ts: number = 0, ids: string[] = []) {
   return finalData;
 }
 
-if (!process.env.RWA_REFILL) {
-  main().catch(async (error) => {
-    console.error('Error running the script: ', error);
-      await sendMessage(`Error running the script: ${error}`, process.env.RWA_WEBHOOK!, false);
-      process.exit(1);
-    }).then(() => process.exit(0)); // ts-node defi/src/rwa/atvl.ts
-}
+main().catch(async (error) => {
+  console.error('Error running the script: ', error);
+    await sendMessage(`Error running the script: ${error}`, process.env.RWA_WEBHOOK!, false);
+    process.exit(1);
+  }).then(() => process.exit(0)); // ts-node defi/src/rwa/atvl.ts

e12355c3d86662af64fe058a28e967f619d3d97f
realshaman <realdealshaman11@gmail.com>
2026-04-13 12:30:19 -0600

    add new listings

--- defi/src/protocols/data5.ts
+++ defi/src/protocols/data5.ts
@@ -16595,5 +16595,46 @@ const data5: Protocol[] = [
       aggregators: "lilswap",
     }
   },
+  {
+    id: "7659",
+    name: "Auki",
+    address: null,
+    symbol: "-",
+    url: "https://www.auki.com/",
+    description: "Auki is a decentralized spatial computing network that lets devices, robots, and AI systems privately share spatial data and compute power through its posemesh protocol to build a shared understanding of the physical world.",
+    chain: "Base",
+    logo: `${baseIconsUrl}/auki.jpg`,
+    audits: "0",
+    gecko_id: null,
+    cmcId: null,
+    category: "DePIN",
+    chains: ["Base"],
+    module: "dummy.js",
+    twitter: "Auki",
+    github: ["aukilabs"],
+    dimensions: {
+      fees: "auki",
+    }
+  },
+  {
+    id: "7660",
+    name: "dexVAULTS V2",
+    address: "arbitrum:0x92a212d9f5eef0b262ac7d84aea64a0d0758b94f",
+    symbol: "GDEX",
+    url: "https://dexfi.com/",
+    description:
+      "dexVAULTS is the modern way to manage yield farm investments. It empowers investors by incorporating advanced trading strategies employed by experienced investors",
+    chain: "Arbitrum",
+    logo: `${baseIconsUrl}/dexvaults-v2.jpg`,
+    audits: "0",
+    gecko_id: null,
+    cmcId: null,
+    category: "Yield",
+    chains: ["Binance", "Sonic", "Base", "Ethereum", "Arbitrum", "Avalanche"],
+    module: "dexfinance-vault-v2/index.js",
+    twitter: "DexFinance",
+    parentProtocol: "parent#dexfinance",
+    listedAt: 1776104927,
+  },
 ];
 export default data5;

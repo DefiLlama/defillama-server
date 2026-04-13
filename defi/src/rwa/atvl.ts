aff61aa5e6cf396751fd888e4bc48cd1b5bc4afe
wayne <waynebruce0x@gmail.com>
2026-04-13 18:58:45 +0100

    remove if entry

--- defi/src/rwa/atvl.ts
+++ defi/src/rwa/atvl.ts
@@ -515,10 +515,8 @@ export default async function main(ts: number = 0) {
   return finalData;
 }
 
-if (require.main === module) {
-  main().catch(async (error) => {
-    console.error('Error running the script: ', error);
-    await sendMessage(`Error running the script: ${error}`, process.env.RWA_WEBHOOK!, false);
-    process.exit(1);
-  }).then(() => process.exit(0)); // ts-node defi/src/rwa/atvl.ts
-}
+main().catch(async (error) => {
+  console.error('Error running the script: ', error);
+  await sendMessage(`Error running the script: ${error}`, process.env.RWA_WEBHOOK!, false);
+  process.exit(1);
+}).then(() => process.exit(0)); // ts-node defi/src/rwa/atvl.ts

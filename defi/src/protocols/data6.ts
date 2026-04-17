3004ed27d07451382223aafe2ceeac887a8b41cf
realshaman <realdealshaman11@gmail.com>
2026-04-17 16:44:45 -0600

    add new listings

--- defi/src/protocols/data6.ts
+++ defi/src/protocols/data6.ts
@@ -40,5 +40,45 @@ import type { Protocol } from "./types";
 `chains` is not used by the current code, but good to fill it out because it is used in our test to detect errors
 */
 const data6: Protocol[] = [
+  {
+    id: "7709",
+    name: "MOKEE",
+    address: null,
+    symbol: "-",
+    url: " ", // pending to add url https://www.mokee.app/
+    description:
+      "MOKEE Protocol is the first non-custodial crypto fail-safe protocol. Users deposit native tokens into personal vaults, designate heirs, and set an inactivity threshold",
+    chain: "Binance",
+    logo: `${baseIconsUrl}/monkee.jpg`,
+    audits: "0",
+    gecko_id: null,
+    cmcId: null,
+    category: "Token Locker",
+    chains: ["Binance", "Base","Ethereum", "Polygon", "Arbitrum", "Optimism"],
+    module: "monkee/index.js",
+    twitter: "MokeeApp",
+    audit_links: [],
+    listedAt: 1776463251
+  },
+  {
+    id: "7710",
+    name: "Trinity Protocol",
+    address: null,
+    symbol: "-",
+    url: " ", // pending to add url https://graceful-begonia-2b7634.netlify.app/.
+    description:
+      "Gold-collateralized USDC lending market on Morpho Blue (Arbitrum One)",
+    chain: "Arbitrum",
+    logo: `${baseIconsUrl}/trinity-protocol.jpg`,
+    audits: "0",
+    gecko_id: null,
+    cmcId: null,
+    category: "Risk Curator",
+    chains: ["Arbitrum"],
+    module: "trinity-protocol/index.js",
+    twitter: "cameron_arc_ai",
+    audit_links: [],
+    listedAt: 1776465699
+  },
 ];
 export default data6;

b3b5bfec3df473f6b2b8b75317d1f80058786620
alxn787 <alenabraham787@gmail.com>
2026-04-29 13:36:27 +0530

    Add THORWallet DEX dimension mapping.

--- defi/src/protocols/data5.ts
+++ defi/src/protocols/data5.ts
@@ -16565,7 +16565,8 @@ const data5: Protocol[] = [
     twitter: "thorwallet",
     audit_links: ["https://code4rena.com/reports/2025-02-thorwallet"],
     dimensions: {
-      fees: "thorwallet"
+      fees: "thorwallet",
+      dexs: "thorwallet",
     }
   },
   {
